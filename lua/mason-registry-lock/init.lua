255b59531200634dced2b15c24d0c85b6a0549fa
GitHub Action <action@github.com>
2025-05-22 18:54:34 +0000

    Update mason-registry lock to 2025-05-21-rear-eon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-21-rubber-jitter"
+M.registry_release = "github:mason-org/mason-registry@2025-05-21-rear-eon"
 
 return M
