ecec62db3bb2940ecc66d6c18c12a688c6b3ace7
GitHub Action <action@github.com>
2025-05-22 18:54:34 +0000

    Update mason-registry lock to 2025-05-22-aqua-pump

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-22-amused-angle"
+M.registry_release = "github:mason-org/mason-registry@2025-05-22-aqua-pump"
 
 return M
