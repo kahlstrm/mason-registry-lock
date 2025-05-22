196eddd9ddddb34e901708e82c0a8deaaa5dc35a
GitHub Action <action@github.com>
2025-05-22 18:54:34 +0000

    Update mason-registry lock to 2025-05-21-wilted-client

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-21-okay-art"
+M.registry_release = "github:mason-org/mason-registry@2025-05-21-wilted-client"
 
 return M
