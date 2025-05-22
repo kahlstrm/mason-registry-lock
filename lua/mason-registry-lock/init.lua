f0b61e4e7cd0a6705a7045fe783d84377f700379
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-05-22 21:20:36 +0300

    Update mason-registry lock to 2025-05-21-only-poison

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-21-wavy-sink"
+M.registry_release = "github:mason-org/mason-registry@2025-05-21-only-poison"
 
 return M
