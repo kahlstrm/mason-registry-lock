6d58af67b55d8749dd04fd35c0dd18ccc05f78d7
GitHub Action <action@github.com>
2025-05-22 18:54:34 +0000

    Update mason-registry lock to 2025-05-21-live-damage

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-21-only-poison"
+M.registry_release = "github:mason-org/mason-registry@2025-05-21-live-damage"
 
 return M
