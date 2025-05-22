2db2bad42e75c316a68cbf50dedce0e43713a5a2
GitHub Action <action@github.com>
2025-05-22 18:54:34 +0000

    Update mason-registry lock to 2025-05-22-flaky-story

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-22-aqua-pump"
+M.registry_release = "github:mason-org/mason-registry@2025-05-22-flaky-story"
 
 return M
