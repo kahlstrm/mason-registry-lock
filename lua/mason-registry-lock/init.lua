1531a7b7718e97a9f80a36c77ea9d31dfdc1b627
GitHub Action <action@github.com>
2025-05-23 00:41:19 +0000

    Update mason-registry lock to 2025-05-22-hearty-tapir

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-22-boring-lute"
+M.registry_release = "github:mason-org/mason-registry@2025-05-22-hearty-tapir"
 
 return M
