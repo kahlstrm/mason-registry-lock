f25a50d1a60f782e3d7fbfffbae40684c233a1b9
GitHub Action <action@github.com>
2025-05-24 00:39:20 +0000

    Update mason-registry lock to 2025-05-23-sleepy-crate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-23-fearless-driver"
+M.registry_release = "github:mason-org/mason-registry@2025-05-23-sleepy-crate"
 
 return M
