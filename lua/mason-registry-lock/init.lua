cced29c6afdda5c440a7e96cc39758a8a52b2bd6
GitHub Action <action@github.com>
2025-05-24 00:39:20 +0000

    Update mason-registry lock to 2025-05-23-total-jelly

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-22-hearty-tapir"
+M.registry_release = "github:mason-org/mason-registry@2025-05-23-total-jelly"
 
 return M
