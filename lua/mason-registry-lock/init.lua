67af8db1a68d893c6310beec2158548292e45705
GitHub Action <action@github.com>
2025-05-24 00:39:20 +0000

    Update mason-registry lock to 2025-05-23-biting-lamp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-23-fluffy-option"
+M.registry_release = "github:mason-org/mason-registry@2025-05-23-biting-lamp"
 
 return M
