ac584259c74c09303037cd134e3001353debefb8
GitHub Action <action@github.com>
2025-05-25 00:45:46 +0000

    Update mason-registry lock to 2025-05-24-east-panty

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-23-empty-hope"
+M.registry_release = "github:mason-org/mason-registry@2025-05-24-east-panty"
 
 return M
