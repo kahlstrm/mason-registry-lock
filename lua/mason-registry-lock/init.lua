f8d6203f30a3251463e7b44325ddf044ca1f371f
GitHub Action <action@github.com>
2025-05-26 00:42:55 +0000

    Update mason-registry lock to 2025-05-25-quaint-pine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-25-baked-oboe"
+M.registry_release = "github:mason-org/mason-registry@2025-05-25-quaint-pine"
 
 return M
