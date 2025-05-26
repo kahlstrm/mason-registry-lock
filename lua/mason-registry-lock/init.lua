116f3c0f3edc59cd1446fb26254c79277092546a
GitHub Action <action@github.com>
2025-05-26 00:42:55 +0000

    Update mason-registry lock to 2025-05-25-ornate-forest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-25-frayed-cesium"
+M.registry_release = "github:mason-org/mason-registry@2025-05-25-ornate-forest"
 
 return M
