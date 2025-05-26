ff115f230c416d23d514fb887760a52f95d54e60
GitHub Action <action@github.com>
2025-05-26 00:42:55 +0000

    Update mason-registry lock to 2025-05-25-lime-object

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-24-pushy-glass"
+M.registry_release = "github:mason-org/mason-registry@2025-05-25-lime-object"
 
 return M
