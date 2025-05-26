6ae9eea76795e7363b2f123f12ac7cc52684b316
GitHub Action <action@github.com>
2025-05-26 00:42:55 +0000

    Update mason-registry lock to 2025-05-25-frayed-cesium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-25-quaint-pine"
+M.registry_release = "github:mason-org/mason-registry@2025-05-25-frayed-cesium"
 
 return M
