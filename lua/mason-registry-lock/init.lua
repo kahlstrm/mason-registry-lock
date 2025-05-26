708a73c467673c66a3375d71df0540b347485390
GitHub Action <action@github.com>
2025-05-26 00:42:55 +0000

    Update mason-registry lock to 2025-05-25-key-tuba

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-25-ornate-forest"
+M.registry_release = "github:mason-org/mason-registry@2025-05-25-key-tuba"
 
 return M
