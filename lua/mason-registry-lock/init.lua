79bf46105683341946dde564d6a9e72276d474b8
GitHub Action <action@github.com>
2025-05-27 00:40:30 +0000

    Update mason-registry lock to 2025-05-26-toxic-newt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-26-feisty-nancy"
+M.registry_release = "github:mason-org/mason-registry@2025-05-26-toxic-newt"
 
 return M
