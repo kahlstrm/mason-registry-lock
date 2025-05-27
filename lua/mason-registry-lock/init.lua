9f143644d91512793f3a6a81f652d719905c5ac8
GitHub Action <action@github.com>
2025-05-27 00:40:30 +0000

    Update mason-registry lock to 2025-05-26-damp-diving

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-26-ancient-adult"
+M.registry_release = "github:mason-org/mason-registry@2025-05-26-damp-diving"
 
 return M
