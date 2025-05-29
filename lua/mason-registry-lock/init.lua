0f36101eb0fc6384f6961a0e0f52ce9206229ee6
GitHub Action <action@github.com>
2025-05-29 00:41:30 +0000

    Update mason-registry lock to 2025-05-28-adept-herd

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-28-dopey-love"
+M.registry_release = "github:mason-org/mason-registry@2025-05-28-adept-herd"
 
 return M
