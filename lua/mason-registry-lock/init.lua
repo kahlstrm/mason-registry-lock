ee7b0ee601fc76a8a7073250f6e6d56cdda00c94
GitHub Action <action@github.com>
2025-05-29 00:41:30 +0000

    Update mason-registry lock to 2025-05-28-steep-range

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-28-plucky-owner"
+M.registry_release = "github:mason-org/mason-registry@2025-05-28-steep-range"
 
 return M
