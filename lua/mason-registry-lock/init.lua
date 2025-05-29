4637d8e8c06e7ef550478190804090cf38690e4b
GitHub Action <action@github.com>
2025-05-29 00:41:30 +0000

    Update mason-registry lock to 2025-05-28-minute-stream

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-28-strict-skunk"
+M.registry_release = "github:mason-org/mason-registry@2025-05-28-minute-stream"
 
 return M
