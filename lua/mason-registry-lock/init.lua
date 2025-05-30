94e414069645b32fa727a9002bf8bf3d025024aa
GitHub Action <action@github.com>
2025-05-30 00:41:11 +0000

    Update mason-registry lock to 2025-05-29-inner-math

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-29-total-washer"
+M.registry_release = "github:mason-org/mason-registry@2025-05-29-inner-math"
 
 return M
