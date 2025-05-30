89b4a35771e0171e640e7cb5bd2570d9a7edfbb3
GitHub Action <action@github.com>
2025-05-30 00:41:11 +0000

    Update mason-registry lock to 2025-05-29-total-washer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-28-shrill-codlet"
+M.registry_release = "github:mason-org/mason-registry@2025-05-29-total-washer"
 
 return M
