7d39603affd20ad2faacdbfc2b3717d787374c83
GitHub Action <action@github.com>
2025-05-30 00:41:11 +0000

    Update mason-registry lock to 2025-05-29-easy-ling

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-29-native-jaguar"
+M.registry_release = "github:mason-org/mason-registry@2025-05-29-easy-ling"
 
 return M
