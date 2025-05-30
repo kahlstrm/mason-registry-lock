eed900eaf7622e2724b091b436bb6db5f09876d4
GitHub Action <action@github.com>
2025-05-30 00:41:11 +0000

    Update mason-registry lock to 2025-05-29-bloody-feast

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-29-absurd-era"
+M.registry_release = "github:mason-org/mason-registry@2025-05-29-bloody-feast"
 
 return M
