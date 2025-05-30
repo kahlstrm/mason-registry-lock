fa0c6306122388ece88ea15671b5e3e731c39310
GitHub Action <action@github.com>
2025-05-30 00:41:11 +0000

    Update mason-registry lock to 2025-05-29-shady-free

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-29-merry-debtor"
+M.registry_release = "github:mason-org/mason-registry@2025-05-29-shady-free"
 
 return M
