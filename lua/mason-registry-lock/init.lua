b4405f73b4462a0f799d8bd4c44ae76cf36ec2a7
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-worst-moon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-allied-hemp"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-worst-moon"
 
 return M
