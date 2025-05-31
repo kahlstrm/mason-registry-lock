bb4051870ddee98eda92d8f23a56f93daec4fc61
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-slow-siren

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-woozy-jackal"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-slow-siren"
 
 return M
