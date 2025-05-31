ce8ced0bcff4d25814563d192198a2b1dcc36150
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-bottled-search

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-livid-space"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-bottled-search"
 
 return M
