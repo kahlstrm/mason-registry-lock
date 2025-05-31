0ed11016504ff65436b568ce193881ccd90dfc95
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-tinted-skates

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-bottled-search"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-tinted-skates"
 
 return M
