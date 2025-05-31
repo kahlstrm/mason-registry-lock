966bb2ec4528abf58f47a0e7c95d3063dd51d929
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-free-mouse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-first-brow"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-free-mouse"
 
 return M
