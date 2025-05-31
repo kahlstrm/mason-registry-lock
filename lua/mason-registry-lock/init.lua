643c488c1e69613085a20fca3f927c39ec7b9097
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-moody-chip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-slow-siren"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-moody-chip"
 
 return M
