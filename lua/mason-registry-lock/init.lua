ebf9cddc2ab22b9e408cac6ace37e95936d721e0
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-eight-palm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-worst-moon"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-eight-palm"
 
 return M
