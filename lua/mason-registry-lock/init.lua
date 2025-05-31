065d05104e2c862a0b835cdf30d96ffc83c83627
GitHub Action <action@github.com>
2025-05-31 00:40:10 +0000

    Update mason-registry lock to 2025-05-30-perky-orange

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-tinted-skates"
+M.registry_release = "github:mason-org/mason-registry@2025-05-30-perky-orange"
 
 return M
