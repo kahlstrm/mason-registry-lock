32fc0d3b2644c4a3739c39d89ba7acd1ba60290b
GitHub Action <action@github.com>
2025-06-01 00:53:00 +0000

    Update mason-registry lock to 2025-05-31-pale-coyote

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-31-curly-trust"
+M.registry_release = "github:mason-org/mason-registry@2025-05-31-pale-coyote"
 
 return M
