c5452e3060fcdabde936ab7a9dd120db2bc19c26
GitHub Action <action@github.com>
2025-06-01 00:53:00 +0000

    Update mason-registry lock to 2025-05-31-kosher-sleet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-30-unfair-delete"
+M.registry_release = "github:mason-org/mason-registry@2025-05-31-kosher-sleet"
 
 return M
