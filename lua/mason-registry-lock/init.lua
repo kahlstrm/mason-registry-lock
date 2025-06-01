73c328e656f6973e451bc688dff5b8f2b9c8b46f
GitHub Action <action@github.com>
2025-06-01 00:53:00 +0000

    Update mason-registry lock to 2025-05-31-curly-trust

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-05-31-happy-cuban"
+M.registry_release = "github:mason-org/mason-registry@2025-05-31-curly-trust"
 
 return M
