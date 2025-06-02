72903533b9e71b35d1956f21f5e102839d1d6f8d
GitHub Action <action@github.com>
2025-06-02 00:44:05 +0000

    Update mason-registry lock to 2025-06-01-brisk-mrigal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-01-wrong-shears"
+M.registry_release = "github:mason-org/mason-registry@2025-06-01-brisk-mrigal"
 
 return M
