ccce29a6df302a3581e2392f4831961f391cb961
GitHub Action <action@github.com>
2025-06-02 00:44:05 +0000

    Update mason-registry lock to 2025-06-01-woeful-drain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-01-glad-adult"
+M.registry_release = "github:mason-org/mason-registry@2025-06-01-woeful-drain"
 
 return M
