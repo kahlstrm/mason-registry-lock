c30b6f0cee66d4c63ff1ce9624fb03878081859e
GitHub Action <action@github.com>
2025-06-03 00:42:29 +0000

    Update mason-registry lock to 2025-06-02-earthy-twine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-02-unable-test"
+M.registry_release = "github:mason-org/mason-registry@2025-06-02-earthy-twine"
 
 return M
