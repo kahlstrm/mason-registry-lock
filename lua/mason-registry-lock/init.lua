4577d9ffbcc9e33bff16802a49f606cc6201c9ea
GitHub Action <action@github.com>
2025-06-03 00:42:29 +0000

    Update mason-registry lock to 2025-06-02-plain-plate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-02-tepid-mall"
+M.registry_release = "github:mason-org/mason-registry@2025-06-02-plain-plate"
 
 return M
