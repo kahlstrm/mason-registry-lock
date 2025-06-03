c53430933c9e3fbb1c38c3bc2cf7ab26edde41ed
GitHub Action <action@github.com>
2025-06-03 00:42:29 +0000

    Update mason-registry lock to 2025-06-02-allied-smoke

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-01-grey-fork"
+M.registry_release = "github:mason-org/mason-registry@2025-06-02-allied-smoke"
 
 return M
