0617f1da5939ea7cdb7a60398c666feec46119d1
GitHub Action <action@github.com>
2025-06-04 00:42:23 +0000

    Update mason-registry lock to 2025-06-03-future-punch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-02-curved-airbus"
+M.registry_release = "github:mason-org/mason-registry@2025-06-03-future-punch"
 
 return M
