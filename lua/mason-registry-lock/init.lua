a096bb5fea429317291d951ca5bd4bb43b020d4f
GitHub Action <action@github.com>
2025-06-04 00:42:23 +0000

    Update mason-registry lock to 2025-06-03-heady-three

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-03-inborn-error"
+M.registry_release = "github:mason-org/mason-registry@2025-06-03-heady-three"
 
 return M
