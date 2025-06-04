df463792e89e4464680773eb0b0f699ed1e0bab3
GitHub Action <action@github.com>
2025-06-04 00:42:23 +0000

    Update mason-registry lock to 2025-06-03-white-thumb

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-03-ancient-kiss"
+M.registry_release = "github:mason-org/mason-registry@2025-06-03-white-thumb"
 
 return M
