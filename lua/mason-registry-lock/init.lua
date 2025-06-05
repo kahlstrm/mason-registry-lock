4d0cbe90086e9a4d4634b80e80c798afeace381c
GitHub Action <action@github.com>
2025-06-05 00:41:59 +0000

    Update mason-registry lock to 2025-06-04-lawful-system

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-04-amuck-umbrella"
+M.registry_release = "github:mason-org/mason-registry@2025-06-04-lawful-system"
 
 return M
