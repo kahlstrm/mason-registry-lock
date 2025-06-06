1d93beaf4edad7984e34a6d3f18b9de950e877ea
GitHub Action <action@github.com>
2025-06-06 00:41:19 +0000

    Update mason-registry lock to 2025-06-05-dim-order

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-05-invisible-toe"
+M.registry_release = "github:mason-org/mason-registry@2025-06-05-dim-order"
 
 return M
