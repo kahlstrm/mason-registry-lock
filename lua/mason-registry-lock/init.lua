74b9fd2d2c9d84c7f2197a81dc711854ede29cd4
GitHub Action <action@github.com>
2025-06-06 00:41:19 +0000

    Update mason-registry lock to 2025-06-05-invisible-toe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-05-common-metal"
+M.registry_release = "github:mason-org/mason-registry@2025-06-05-invisible-toe"
 
 return M
