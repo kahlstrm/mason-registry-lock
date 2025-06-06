47f9ea02559151c3022171e2b1c29d891b91cb58
GitHub Action <action@github.com>
2025-06-06 00:41:19 +0000

    Update mason-registry lock to 2025-06-05-static-air

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-05-charming-quilt"
+M.registry_release = "github:mason-org/mason-registry@2025-06-05-static-air"
 
 return M
