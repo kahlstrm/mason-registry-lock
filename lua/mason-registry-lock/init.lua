d5e010aff5d9f273d52960c46abc3adcfd045ff8
GitHub Action <action@github.com>
2025-06-07 00:41:21 +0000

    Update mason-registry lock to 2025-06-06-stable-peace

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-05-blonde-wave"
+M.registry_release = "github:mason-org/mason-registry@2025-06-06-stable-peace"
 
 return M
