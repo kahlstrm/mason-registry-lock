8a57945974eaf414b9bc9efe88c8588204f597a3
GitHub Action <action@github.com>
2025-06-07 00:41:21 +0000

    Update mason-registry lock to 2025-06-06-absent-saturn

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-06-huge-fridge"
+M.registry_release = "github:mason-org/mason-registry@2025-06-06-absent-saturn"
 
 return M
