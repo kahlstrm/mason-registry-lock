d5347ce03d01e78a8a62c1cc976f43e77ccb6066
GitHub Action <action@github.com>
2025-06-07 00:41:21 +0000

    Update mason-registry lock to 2025-06-06-unused-doll

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-06-faded-satin"
+M.registry_release = "github:mason-org/mason-registry@2025-06-06-unused-doll"
 
 return M
