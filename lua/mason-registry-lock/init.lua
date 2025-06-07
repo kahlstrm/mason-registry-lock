86e8aa2b1dd00c8c7555d93ca10d54b2e484f9e8
GitHub Action <action@github.com>
2025-06-07 00:41:21 +0000

    Update mason-registry lock to 2025-06-06-able-jack

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-06-rigid-tulip"
+M.registry_release = "github:mason-org/mason-registry@2025-06-06-able-jack"
 
 return M
