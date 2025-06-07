5d457f511137076c74ab587bb9b34af9f773aa32
GitHub Action <action@github.com>
2025-06-07 00:41:21 +0000

    Update mason-registry lock to 2025-06-06-jolly-tube

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-06-absent-saturn"
+M.registry_release = "github:mason-org/mason-registry@2025-06-06-jolly-tube"
 
 return M
