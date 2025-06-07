fe873eecc9d44c3a611d8db26c2f0da9433e93d3
GitHub Action <action@github.com>
2025-06-07 00:41:21 +0000

    Update mason-registry lock to 2025-06-06-future-dingo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-06-jolly-tube"
+M.registry_release = "github:mason-org/mason-registry@2025-06-06-future-dingo"
 
 return M
