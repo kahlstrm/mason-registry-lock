4f9f1db8001ac528b3364fba88b3f45b2308acd7
GitHub Action <action@github.com>
2025-06-07 00:41:21 +0000

    Update mason-registry lock to 2025-06-06-abrupt-blast

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-06-like-barium"
+M.registry_release = "github:mason-org/mason-registry@2025-06-06-abrupt-blast"
 
 return M
