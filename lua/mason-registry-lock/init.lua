fae1ce268b686aa0fe241833d6d73c8a671a9b9f
GitHub Action <action@github.com>
2025-06-08 00:46:45 +0000

    Update mason-registry lock to 2025-06-07-green-peony

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-07-brave-system"
+M.registry_release = "github:mason-org/mason-registry@2025-06-07-green-peony"
 
 return M
