40f26b6defabaada5752bf4f900286a6f2c219c6
GitHub Action <action@github.com>
2025-06-08 00:46:45 +0000

    Update mason-registry lock to 2025-06-07-fragile-value

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-07-scary-ruffe"
+M.registry_release = "github:mason-org/mason-registry@2025-06-07-fragile-value"
 
 return M
