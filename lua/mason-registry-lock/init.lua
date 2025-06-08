d9c9f23ae95969f5d0dc6ff755b8f17692b09d1f
GitHub Action <action@github.com>
2025-06-08 00:46:45 +0000

    Update mason-registry lock to 2025-06-07-brave-system

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-06-winged-tray"
+M.registry_release = "github:mason-org/mason-registry@2025-06-07-brave-system"
 
 return M
