af7d45289df46887bee1e2bb58db0237bf975c64
GitHub Action <action@github.com>
2025-06-09 00:44:59 +0000

    Update mason-registry lock to 2025-06-08-aware-berry

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-08-live-slum"
+M.registry_release = "github:mason-org/mason-registry@2025-06-08-aware-berry"
 
 return M
