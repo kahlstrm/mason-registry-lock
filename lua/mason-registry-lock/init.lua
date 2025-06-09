e25771e2e6d840d2f4a824780a21651c2b803f4b
GitHub Action <action@github.com>
2025-06-09 00:44:59 +0000

    Update mason-registry lock to 2025-06-08-bent-closet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-07-glad-spruce"
+M.registry_release = "github:mason-org/mason-registry@2025-06-08-bent-closet"
 
 return M
