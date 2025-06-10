63893073f13380ec5940302f157213c4fe655efc
GitHub Action <action@github.com>
2025-06-10 00:42:07 +0000

    Update mason-registry lock to 2025-06-09-aqua-panda

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-09-annual-fender"
+M.registry_release = "github:mason-org/mason-registry@2025-06-09-aqua-panda"
 
 return M
