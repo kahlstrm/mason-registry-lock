3be0bfb9b4574b2a99e3472cc6779d677a8b9932
GitHub Action <action@github.com>
2025-06-11 00:42:12 +0000

    Update mason-registry lock to 2025-06-10-hidden-aoudad

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-10-all-debt"
+M.registry_release = "github:mason-org/mason-registry@2025-06-10-hidden-aoudad"
 
 return M
