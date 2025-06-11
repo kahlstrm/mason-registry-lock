a82da0842f9afd3ed36d0042af05ae0fb41fe2f9
GitHub Action <action@github.com>
2025-06-11 00:42:12 +0000

    Update mason-registry lock to 2025-06-10-known-cymbal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-10-dutch-goose"
+M.registry_release = "github:mason-org/mason-registry@2025-06-10-known-cymbal"
 
 return M
