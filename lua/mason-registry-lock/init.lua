7a53491c5f1455871702d36d25dc200b5cdc73d4
GitHub Action <action@github.com>
2025-06-11 00:42:12 +0000

    Update mason-registry lock to 2025-06-10-able-paint

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-10-tinted-argon"
+M.registry_release = "github:mason-org/mason-registry@2025-06-10-able-paint"
 
 return M
