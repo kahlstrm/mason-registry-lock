d13c0e8d2a9509dcff027bcd078632eef747c097
GitHub Action <action@github.com>
2025-06-11 00:42:12 +0000

    Update mason-registry lock to 2025-06-10-dutch-goose

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-10-deaf-owner"
+M.registry_release = "github:mason-org/mason-registry@2025-06-10-dutch-goose"
 
 return M
