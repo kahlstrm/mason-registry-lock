4ace588d0892527e5ac646a11f3a9bf985fdb840
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-annual-mood

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-lawful-tile"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-annual-mood"
 
 return M
