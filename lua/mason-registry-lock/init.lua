50b8feae9a7bbdd0b435fccc8f2c1de7966872c0
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-little-bay

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-fancy-hammer"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-little-bay"
 
 return M
