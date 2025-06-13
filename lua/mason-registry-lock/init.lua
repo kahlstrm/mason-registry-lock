a34de1059e57795a4fba7f4c11d0729b92ca91ec
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-overt-world

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-modest-mango"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-overt-world"
 
 return M
