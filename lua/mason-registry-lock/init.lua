4c36aeedd6f4faaf4b4c6f1a9acb87e4144f890e
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-north-sushi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-sinful-disk"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-north-sushi"
 
 return M
