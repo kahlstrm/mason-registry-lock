c9dc9f5b373252067b079d0c4c2791f4740d8fb6
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-sinful-disk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-annual-mood"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-sinful-disk"
 
 return M
