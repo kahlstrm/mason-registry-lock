ec0ccb4ba24746dd140da42fb5a1cb96ffe8076c
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-net-pepper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-north-sushi"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-net-pepper"
 
 return M
