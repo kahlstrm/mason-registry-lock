4e78a53fe8a019336917f98b65acd3fb016337f3
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-sudden-cannon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-female-dress"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-sudden-cannon"
 
 return M
