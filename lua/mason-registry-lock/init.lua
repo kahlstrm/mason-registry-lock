712811f74d365de68bb622e039839542a8789b0c
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-cuddly-sound

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-visual-debt"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-cuddly-sound"
 
 return M
