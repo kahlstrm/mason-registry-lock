c35f2cff037036012b9adc59877ffa599279aeb9
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-wobbly-stork

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-10-hidden-aoudad"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-wobbly-stork"
 
 return M
