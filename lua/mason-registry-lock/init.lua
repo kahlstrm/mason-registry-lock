913a6254dd743e27b183ef058b7637ace279f2f5
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-slender-trial

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-sane-zinc"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-slender-trial"
 
 return M
