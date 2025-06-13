d6bb6bc7c6fc02788c4a1f21a65ab5681f68225f
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-sane-zinc

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-pastel-zombie"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-sane-zinc"
 
 return M
