4ed4aca371728b69da2dab9d0d6a19d179e7afb0
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-pastel-zombie

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-wobbly-stork"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-pastel-zombie"
 
 return M
