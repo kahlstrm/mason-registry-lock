a367554848953eb1770afb1dac1c910cad1bcb9e
GitHub Action <action@github.com>
2025-06-13 00:42:15 +0000

    Update mason-registry lock to 2025-06-12-visual-debt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-12-fragile-help"
+M.registry_release = "github:mason-org/mason-registry@2025-06-12-visual-debt"
 
 return M
