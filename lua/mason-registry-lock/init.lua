11c99df6aa8e1e17fe5cbcb2f1018963e8850e49
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-last-target

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-ample-sneeze"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-last-target"
 
 return M
