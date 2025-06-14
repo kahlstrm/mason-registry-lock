0989e30b6185fc15369a8d000ec13fa76e806b91
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-putrid-iguana

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-last-target"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-putrid-iguana"
 
 return M
