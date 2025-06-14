887d7d93c5a38877c0f8801b821c75d0bf6e45a3
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-aboard-chair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-alone-brown"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-aboard-chair"
 
 return M
