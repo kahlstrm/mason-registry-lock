b7cbb762cdeb934353f9a842c08b1a62612b5f83
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-shut-dragon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-tawdry-vision"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-shut-dragon"
 
 return M
