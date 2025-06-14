95193f86db424601c0a5db3f65f2097e8e1ca212
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-alone-brown

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-recent-rotate"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-alone-brown"
 
 return M
