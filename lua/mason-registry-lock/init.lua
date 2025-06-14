6d3c852b085bbbe7afe27ee7f25f1c42814cced3
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-dusty-push

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-putrid-iguana"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-dusty-push"
 
 return M
