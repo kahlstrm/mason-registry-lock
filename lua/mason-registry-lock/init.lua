0bfad294b1ead43ba60fbb3d46b839206780f6ed
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-tawdry-vision

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-noisy-able"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-tawdry-vision"
 
 return M
