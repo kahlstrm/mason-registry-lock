7307b42bcde42d9fe9fee99433fdd2c7ba3d439f
GitHub Action <action@github.com>
2025-06-14 00:40:39 +0000

    Update mason-registry lock to 2025-06-13-novel-kangaroo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-13-dusty-push"
+M.registry_release = "github:mason-org/mason-registry@2025-06-13-novel-kangaroo"
 
 return M
