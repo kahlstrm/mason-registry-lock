9ae8fb8a2d250f2634713d724c79c1de39af96b0
GitHub Action <action@github.com>
2025-06-15 00:47:37 +0000

    Update mason-registry lock to 2025-06-14-normal-bengal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-14-zonked-harp"
+M.registry_release = "github:mason-org/mason-registry@2025-06-14-normal-bengal"
 
 return M
