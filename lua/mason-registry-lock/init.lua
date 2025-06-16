bdd5018f70523b0db3225cfd5d731f1cf1fa6f48
GitHub Action <action@github.com>
2025-06-16 00:44:20 +0000

    Update mason-registry lock to 2025-06-15-tricky-cony

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-14-warped-waste"
+M.registry_release = "github:mason-org/mason-registry@2025-06-15-tricky-cony"
 
 return M
