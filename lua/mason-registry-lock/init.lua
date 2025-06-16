5ca3fa1135e56574247631aefef00ab0b84624bb
GitHub Action <action@github.com>
2025-06-16 00:44:20 +0000

    Update mason-registry lock to 2025-06-15-front-witch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-15-quick-cafe"
+M.registry_release = "github:mason-org/mason-registry@2025-06-15-front-witch"
 
 return M
