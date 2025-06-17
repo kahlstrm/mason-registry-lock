ef05f532607dc392420c594bd3db2de4379e46da
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-ordinary-parcel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-mellow-coach"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-ordinary-parcel"
 
 return M
