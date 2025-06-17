1e8b3bd7916c17c7fa955c2ababf654054075e39
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-noisy-sugar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-closed-adult"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-noisy-sugar"
 
 return M
