3d94d52eac2e081ce9b78b599c32a4d7fca41064
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-arid-low

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-stingy-aster"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-arid-low"
 
 return M
