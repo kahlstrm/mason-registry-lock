a97fdd6d690630716556d65ae7828331779f0fdd
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-barren-cast

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-civic-fax"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-barren-cast"
 
 return M
