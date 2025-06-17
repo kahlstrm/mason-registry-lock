30284547aa2eb342549b01972de8c2fe09c51c52
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-senior-drain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-arid-low"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-senior-drain"
 
 return M
