142840564e7998974f9b9cece69505f6da0cb7cc
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-closed-adult

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-solid-piece"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-closed-adult"
 
 return M
