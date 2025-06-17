0be215ac3caaabe67ff8ef338729783bc12f8ad4
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-crispy-sandra

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-senior-drain"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-crispy-sandra"
 
 return M
