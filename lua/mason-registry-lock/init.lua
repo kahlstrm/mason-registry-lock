bd392c517bcfc0bba3c8dbcf7ac2b32bedb0b545
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-mellow-coach

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-tired-art"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-mellow-coach"
 
 return M
