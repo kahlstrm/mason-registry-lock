eab77e43f10ebb613d09d0b43759f537a9ea2123
GitHub Action <action@github.com>
2025-06-17 00:42:21 +0000

    Update mason-registry lock to 2025-06-16-solid-piece

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-16-lavish-edge"
+M.registry_release = "github:mason-org/mason-registry@2025-06-16-solid-piece"
 
 return M
