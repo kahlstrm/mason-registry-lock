59b5950ea5f84a6e200d42967d83cab1ac60dfac
GitHub Action <action@github.com>
2025-06-18 00:42:31 +0000

    Update mason-registry lock to 2025-06-17-juicy-tanker

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-17-woeful-melody"
+M.registry_release = "github:mason-org/mason-registry@2025-06-17-juicy-tanker"
 
 return M
