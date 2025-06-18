241e4f91479454db98bb20d6f756489f597d935b
GitHub Action <action@github.com>
2025-06-18 00:42:31 +0000

    Update mason-registry lock to 2025-06-17-elated-number

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-17-overt-love"
+M.registry_release = "github:mason-org/mason-registry@2025-06-17-elated-number"
 
 return M
