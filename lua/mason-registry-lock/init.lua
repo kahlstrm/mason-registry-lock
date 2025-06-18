bd2f4f557d603b63a2eaa347ef0051c2af75eb08
GitHub Action <action@github.com>
2025-06-18 00:42:31 +0000

    Update mason-registry lock to 2025-06-17-lumpy-dream

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-17-curved-forest"
+M.registry_release = "github:mason-org/mason-registry@2025-06-17-lumpy-dream"
 
 return M
