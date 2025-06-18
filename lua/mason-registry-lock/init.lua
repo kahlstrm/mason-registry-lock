da30a8c699a5b5b9da0f8f76fb01f98f0e790027
GitHub Action <action@github.com>
2025-06-18 00:42:31 +0000

    Update mason-registry lock to 2025-06-17-final-powder

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-17-elated-number"
+M.registry_release = "github:mason-org/mason-registry@2025-06-17-final-powder"
 
 return M
