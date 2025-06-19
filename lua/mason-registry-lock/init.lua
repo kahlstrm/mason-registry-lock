defbbdf9eb4dd31d9c8893d69ffa0fcd4de30592
GitHub Action <action@github.com>
2025-06-19 00:42:32 +0000

    Update mason-registry lock to 2025-06-18-sticky-grunt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-18-unfair-guilt"
+M.registry_release = "github:mason-org/mason-registry@2025-06-18-sticky-grunt"
 
 return M
