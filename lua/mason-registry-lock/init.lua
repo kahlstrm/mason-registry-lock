23aa55e013e5635cbfe593b4ba6ad3f74043c4e6
GitHub Action <action@github.com>
2025-06-19 00:42:32 +0000

    Update mason-registry lock to 2025-06-18-free-nic

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-18-rotten-fridge"
+M.registry_release = "github:mason-org/mason-registry@2025-06-18-free-nic"
 
 return M
