554f5017efdb79beb7f328f7c2730711f425d802
GitHub Action <action@github.com>
2025-06-19 00:42:32 +0000

    Update mason-registry lock to 2025-06-18-useful-mood

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-18-breezy-breath"
+M.registry_release = "github:mason-org/mason-registry@2025-06-18-useful-mood"
 
 return M
