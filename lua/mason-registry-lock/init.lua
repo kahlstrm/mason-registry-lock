5a36b5535326e06fc43e2261ee42f74d0a1b43e7
GitHub Action <action@github.com>
2025-06-19 00:42:32 +0000

    Update mason-registry lock to 2025-06-18-beefy-ankle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-18-dense-vacuum"
+M.registry_release = "github:mason-org/mason-registry@2025-06-18-beefy-ankle"
 
 return M
