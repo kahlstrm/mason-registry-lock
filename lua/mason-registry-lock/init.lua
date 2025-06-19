c9c1157595e64d99b420095fde45245bd7ab2cb2
GitHub Action <action@github.com>
2025-06-19 00:42:32 +0000

    Update mason-registry lock to 2025-06-18-expert-cast

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-17-superb-curve"
+M.registry_release = "github:mason-org/mason-registry@2025-06-18-expert-cast"
 
 return M
