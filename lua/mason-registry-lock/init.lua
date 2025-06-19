ccf81a9de4590a9926d072f637bc3ee3951c2de5
GitHub Action <action@github.com>
2025-06-19 00:42:32 +0000

    Update mason-registry lock to 2025-06-18-rotten-fridge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-18-sticky-grunt"
+M.registry_release = "github:mason-org/mason-registry@2025-06-18-rotten-fridge"
 
 return M
