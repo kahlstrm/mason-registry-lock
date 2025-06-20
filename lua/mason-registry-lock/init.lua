68b318ce29585c7a2b70871c73b712d34c34af08
GitHub Action <action@github.com>
2025-06-20 00:42:21 +0000

    Update mason-registry lock to 2025-06-19-floppy-carp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-19-third-motion"
+M.registry_release = "github:mason-org/mason-registry@2025-06-19-floppy-carp"
 
 return M
