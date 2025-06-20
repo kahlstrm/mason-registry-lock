2037c61cdaac7a92e4323243fab2cf83a6ac42e3
GitHub Action <action@github.com>
2025-06-20 00:42:21 +0000

    Update mason-registry lock to 2025-06-19-three-editor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-18-drunk-sneeze"
+M.registry_release = "github:mason-org/mason-registry@2025-06-19-three-editor"
 
 return M
