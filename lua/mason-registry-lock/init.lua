720097763f462fba3dbdf966793c3d4ed2bb6b07
GitHub Action <action@github.com>
2025-06-20 00:42:21 +0000

    Update mason-registry lock to 2025-06-19-weak-video

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-19-late-route"
+M.registry_release = "github:mason-org/mason-registry@2025-06-19-weak-video"
 
 return M
