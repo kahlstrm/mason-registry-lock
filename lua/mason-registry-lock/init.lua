665a49025da0f66daa6304dc812e651877022b8d
GitHub Action <action@github.com>
2025-06-20 00:42:21 +0000

    Update mason-registry lock to 2025-06-19-idle-bar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-19-earthy-fired"
+M.registry_release = "github:mason-org/mason-registry@2025-06-19-idle-bar"
 
 return M
