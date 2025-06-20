3ff575a298303105004360d4b868c9aebdd15f09
GitHub Action <action@github.com>
2025-06-20 00:42:21 +0000

    Update mason-registry lock to 2025-06-19-third-motion

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-19-weak-profit"
+M.registry_release = "github:mason-org/mason-registry@2025-06-19-third-motion"
 
 return M
