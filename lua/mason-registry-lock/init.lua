115edff003011181fdc37828fab157851e9bf577
GitHub Action <action@github.com>
2025-06-21 00:41:33 +0000

    Update mason-registry lock to 2025-06-20-quie-ferret

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-20-antiquet-violin"
+M.registry_release = "github:mason-org/mason-registry@2025-06-20-quie-ferret"
 
 return M
