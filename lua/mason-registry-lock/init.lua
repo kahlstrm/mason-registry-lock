9367b95f46e15b988a5ad9d1301eb7d98ead5c19
GitHub Action <action@github.com>
2025-06-21 00:41:33 +0000

    Update mason-registry lock to 2025-06-20-super-rugby

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-19-real-budget"
+M.registry_release = "github:mason-org/mason-registry@2025-06-20-super-rugby"
 
 return M
