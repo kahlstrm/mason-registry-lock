32b44f41f6378ab76f9b12d453e118c43546e50c
GitHub Action <action@github.com>
2025-06-21 00:41:33 +0000

    Update mason-registry lock to 2025-06-20-bored-supper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-20-arab-nase"
+M.registry_release = "github:mason-org/mason-registry@2025-06-20-bored-supper"
 
 return M
