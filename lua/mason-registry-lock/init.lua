fc41097ea9ea0db08fba75ddc6f6ff6f37c3fa6f
GitHub Action <action@github.com>
2025-06-21 00:41:33 +0000

    Update mason-registry lock to 2025-06-20-arab-nase

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-20-zany-german"
+M.registry_release = "github:mason-org/mason-registry@2025-06-20-arab-nase"
 
 return M
