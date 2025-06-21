79826842d992e2e72714c556930659c627fe3bc8
GitHub Action <action@github.com>
2025-06-21 00:41:33 +0000

    Update mason-registry lock to 2025-06-20-agile-flax

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-20-bored-supper"
+M.registry_release = "github:mason-org/mason-registry@2025-06-20-agile-flax"
 
 return M
