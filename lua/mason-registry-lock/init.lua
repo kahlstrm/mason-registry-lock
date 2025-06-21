b85bac7ed8a93b860c6af55f5758845a33115074
GitHub Action <action@github.com>
2025-06-21 00:41:33 +0000

    Update mason-registry lock to 2025-06-20-antiquet-violin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-20-rigid-koodoo"
+M.registry_release = "github:mason-org/mason-registry@2025-06-20-antiquet-violin"
 
 return M
