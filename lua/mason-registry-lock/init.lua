be14de9536362ab22e206bebcc341e30bf0b1c2e
GitHub Action <action@github.com>
2025-06-22 00:47:25 +0000

    Update mason-registry lock to 2025-06-21-wiry-trash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-20-evil-peach"
+M.registry_release = "github:mason-org/mason-registry@2025-06-21-wiry-trash"
 
 return M
