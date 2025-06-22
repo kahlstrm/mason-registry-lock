3ed21e3aefdc7d91259cbfca59929ce6b3a55e4a
GitHub Action <action@github.com>
2025-06-22 00:47:25 +0000

    Update mason-registry lock to 2025-06-21-kind-twist

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-21-silky-vermin"
+M.registry_release = "github:mason-org/mason-registry@2025-06-21-kind-twist"
 
 return M
