891a101b6b65a2495d4eb9a75b99e5433ad105cb
GitHub Action <action@github.com>
2025-06-23 00:45:31 +0000

    Update mason-registry lock to 2025-06-22-level-fruit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-21-kind-twist"
+M.registry_release = "github:mason-org/mason-registry@2025-06-22-level-fruit"
 
 return M
