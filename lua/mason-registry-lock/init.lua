830ce6c0931d397ecd8134a949162051d88e575e
GitHub Action <action@github.com>
2025-06-24 00:42:51 +0000

    Update mason-registry lock to 2025-06-23-flat-right

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-23-merry-pear"
+M.registry_release = "github:mason-org/mason-registry@2025-06-23-flat-right"
 
 return M
