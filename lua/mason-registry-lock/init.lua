6dde416cdb0033f3792cd4d219073844ae712469
GitHub Action <action@github.com>
2025-06-24 00:42:51 +0000

    Update mason-registry lock to 2025-06-23-nifty-wrist

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-23-chosen-fuscia"
+M.registry_release = "github:mason-org/mason-registry@2025-06-23-nifty-wrist"
 
 return M
