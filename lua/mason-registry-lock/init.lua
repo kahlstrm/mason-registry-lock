fe332280d308ed76f2d75eb5d8b32e7c36b71661
GitHub Action <action@github.com>
2025-06-24 00:42:51 +0000

    Update mason-registry lock to 2025-06-23-yearly-quilt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-22-loving-unicorn"
+M.registry_release = "github:mason-org/mason-registry@2025-06-23-yearly-quilt"
 
 return M
