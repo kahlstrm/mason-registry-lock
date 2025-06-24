548e95b5684dab36669209c17d182f6980cf43e1
GitHub Action <action@github.com>
2025-06-24 00:42:51 +0000

    Update mason-registry lock to 2025-06-23-ornery-epoxy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-23-bold-eagle"
+M.registry_release = "github:mason-org/mason-registry@2025-06-23-ornery-epoxy"
 
 return M
