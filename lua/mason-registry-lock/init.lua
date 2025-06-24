f64eba2c2038dba1f0c5c7d46fb5ca6e513b9071
GitHub Action <action@github.com>
2025-06-24 00:42:51 +0000

    Update mason-registry lock to 2025-06-23-rigid-course

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-23-yearly-quilt"
+M.registry_release = "github:mason-org/mason-registry@2025-06-23-rigid-course"
 
 return M
