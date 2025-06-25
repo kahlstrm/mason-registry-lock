7667b8446c99aa0a9df2a070e1d69722e65744a2
GitHub Action <action@github.com>
2025-06-25 00:43:15 +0000

    Update mason-registry lock to 2025-06-24-cuddly-atm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-24-scarce-shelf"
+M.registry_release = "github:mason-org/mason-registry@2025-06-24-cuddly-atm"
 
 return M
