93064a8764b6c813111d20d8eecb145bcf5fbad1
GitHub Action <action@github.com>
2025-06-25 00:43:15 +0000

    Update mason-registry lock to 2025-06-24-first-soccer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-24-pricey-bottom"
+M.registry_release = "github:mason-org/mason-registry@2025-06-24-first-soccer"
 
 return M
