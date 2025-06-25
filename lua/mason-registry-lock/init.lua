c7b090182b7a6a39f33bf5b232434b574e2fb369
GitHub Action <action@github.com>
2025-06-25 00:43:15 +0000

    Update mason-registry lock to 2025-06-24-added-stamp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-24-emerald-kitten"
+M.registry_release = "github:mason-org/mason-registry@2025-06-24-added-stamp"
 
 return M
