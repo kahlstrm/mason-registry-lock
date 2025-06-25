c140dc2614f1bac2895793b233112f4c5972cb74
GitHub Action <action@github.com>
2025-06-25 00:43:15 +0000

    Update mason-registry lock to 2025-06-24-ablaze-frog

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-24-pastel-summer"
+M.registry_release = "github:mason-org/mason-registry@2025-06-24-ablaze-frog"
 
 return M
