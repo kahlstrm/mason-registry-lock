3026cfa2faa4102f1c30439b936d5ab9b8e956fb
GitHub Action <action@github.com>
2025-06-25 00:43:15 +0000

    Update mason-registry lock to 2025-06-24-unique-store

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-24-intent-push"
+M.registry_release = "github:mason-org/mason-registry@2025-06-24-unique-store"
 
 return M
