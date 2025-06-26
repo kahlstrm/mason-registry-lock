580d844f4e3b00133f710c9a97f7714338d8a1b3
GitHub Action <action@github.com>
2025-06-26 00:42:47 +0000

    Update mason-registry lock to 2025-06-26-six-cicada

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-26-happy-thomas"
+M.registry_release = "github:mason-org/mason-registry@2025-06-26-six-cicada"
 
 return M
