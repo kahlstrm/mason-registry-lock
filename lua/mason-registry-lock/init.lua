86f4045a5709cd5c859c2ed5ed5ffe9e6ed0768b
GitHub Action <action@github.com>
2025-06-26 00:42:47 +0000

    Update mason-registry lock to 2025-06-25-left-safe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-25-lowly-trail"
+M.registry_release = "github:mason-org/mason-registry@2025-06-25-left-safe"
 
 return M
