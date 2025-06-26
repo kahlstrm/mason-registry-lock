4ccd0b86c0a046722aef03d49b00084b1d16204d
GitHub Action <action@github.com>
2025-06-26 00:42:47 +0000

    Update mason-registry lock to 2025-06-25-chief-blow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-25-placid-nepal"
+M.registry_release = "github:mason-org/mason-registry@2025-06-25-chief-blow"
 
 return M
