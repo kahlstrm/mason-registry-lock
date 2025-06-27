a9b1890eaaed91dfb0b4edca2a18d43923ec955d
GitHub Action <action@github.com>
2025-06-27 00:43:03 +0000

    Update mason-registry lock to 2025-06-26-high-fifth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-26-forked-devil"
+M.registry_release = "github:mason-org/mason-registry@2025-06-26-high-fifth"
 
 return M
