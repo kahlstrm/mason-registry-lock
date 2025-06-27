318dad2a59510c4c6150b8840c05b98c524414d2
GitHub Action <action@github.com>
2025-06-27 00:43:03 +0000

    Update mason-registry lock to 2025-06-26-irate-index

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-26-giddy-island"
+M.registry_release = "github:mason-org/mason-registry@2025-06-26-irate-index"
 
 return M
