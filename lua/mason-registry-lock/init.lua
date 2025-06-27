de561c55fdcb9ea7ffac978cec5b233404cb1053
GitHub Action <action@github.com>
2025-06-27 00:43:03 +0000

    Update mason-registry lock to 2025-06-26-forked-devil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-26-both-zinc"
+M.registry_release = "github:mason-org/mason-registry@2025-06-26-forked-devil"
 
 return M
