b174628e0da153ce07625a999a4284f1566ca9e0
GitHub Action <action@github.com>
2025-06-28 00:41:04 +0000

    Update mason-registry lock to 2025-06-27-curly-flat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-26-sinful-guilt"
+M.registry_release = "github:mason-org/mason-registry@2025-06-27-curly-flat"
 
 return M
