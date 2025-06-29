38b29558d7625475652e66c5a592fbc6724e8d26
GitHub Action <action@github.com>
2025-06-29 00:49:13 +0000

    Update mason-registry lock to 2025-06-28-zigzag-rabbi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-28-pesky-trowel"
+M.registry_release = "github:mason-org/mason-registry@2025-06-28-zigzag-rabbi"
 
 return M
