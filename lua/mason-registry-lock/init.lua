20f7e06f4cbc45f70dedb325fa55440d310ff74c
GitHub Action <action@github.com>
2025-06-29 00:49:13 +0000

    Update mason-registry lock to 2025-06-28-pesky-trowel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-28-giddy-dime"
+M.registry_release = "github:mason-org/mason-registry@2025-06-28-pesky-trowel"
 
 return M
