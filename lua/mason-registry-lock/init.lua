943158bd9e0d725c87a310ac81b6173d1be29c18
GitHub Action <action@github.com>
2025-06-30 00:46:06 +0000

    Update mason-registry lock to 2025-06-29-shrill-oven

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-29-ahead-value"
+M.registry_release = "github:mason-org/mason-registry@2025-06-29-shrill-oven"
 
 return M
