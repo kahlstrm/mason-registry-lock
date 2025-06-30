a84837bbf096c5b0e2c40c6c4470b114fca92da0
GitHub Action <action@github.com>
2025-06-30 00:46:06 +0000

    Update mason-registry lock to 2025-06-29-dreary-stamp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-29-shrill-oven"
+M.registry_release = "github:mason-org/mason-registry@2025-06-29-dreary-stamp"
 
 return M
