df0b29b71382feef8d5e79a51ca6486ce3fbd222
GitHub Action <action@github.com>
2025-07-01 00:49:55 +0000

    Update mason-registry lock to 2025-06-30-filthy-talon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-30-actual-bus"
+M.registry_release = "github:mason-org/mason-registry@2025-06-30-filthy-talon"
 
 return M
