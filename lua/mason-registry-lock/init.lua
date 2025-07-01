3f62135c0cb8b99dc808e77898e17405e3730eb5
GitHub Action <action@github.com>
2025-07-01 00:49:55 +0000

    Update mason-registry lock to 2025-06-30-murky-flamingo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-30-active-spike"
+M.registry_release = "github:mason-org/mason-registry@2025-06-30-murky-flamingo"
 
 return M
