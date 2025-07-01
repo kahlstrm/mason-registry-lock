3b55f84116ac4ad2444b0d9d6d1d831afe01c512
GitHub Action <action@github.com>
2025-07-01 00:49:55 +0000

    Update mason-registry lock to 2025-06-30-vast-motion

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-30-rough-parent"
+M.registry_release = "github:mason-org/mason-registry@2025-06-30-vast-motion"
 
 return M
