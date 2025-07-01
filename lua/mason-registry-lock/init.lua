858837a1907bf3f58a56f202b109e1a349143d35
GitHub Action <action@github.com>
2025-07-01 00:49:55 +0000

    Update mason-registry lock to 2025-06-30-posh-parent

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-30-proud-hell"
+M.registry_release = "github:mason-org/mason-registry@2025-06-30-posh-parent"
 
 return M
