c669df1b4e677584e39ed5998b7524e7f2cca0d3
GitHub Action <action@github.com>
2025-07-02 00:42:31 +0000

    Update mason-registry lock to 2025-07-02-winged-angora

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-01-wet-meal"
+M.registry_release = "github:mason-org/mason-registry@2025-07-02-winged-angora"
 
 return M
