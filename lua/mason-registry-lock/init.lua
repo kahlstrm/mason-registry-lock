5e649b0c0226aaf9590542fef2ec302376ba6b47
GitHub Action <action@github.com>
2025-07-02 00:42:31 +0000

    Update mason-registry lock to 2025-07-01-spry-mimosa

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-01-gross-bream"
+M.registry_release = "github:mason-org/mason-registry@2025-07-01-spry-mimosa"
 
 return M
