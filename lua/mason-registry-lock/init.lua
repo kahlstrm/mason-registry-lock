7e94b54ae1c312d4fb2b70d29d7cb647ecf8de51
GitHub Action <action@github.com>
2025-07-02 00:42:31 +0000

    Update mason-registry lock to 2025-07-01-wet-meal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-01-trashy-peony"
+M.registry_release = "github:mason-org/mason-registry@2025-07-01-wet-meal"
 
 return M
