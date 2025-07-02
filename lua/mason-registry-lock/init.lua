ed92f87edc6392371a92c6d737a44299872d0551
GitHub Action <action@github.com>
2025-07-02 00:42:31 +0000

    Update mason-registry lock to 2025-07-01-given-zone

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-01-absurd-noodle"
+M.registry_release = "github:mason-org/mason-registry@2025-07-01-given-zone"
 
 return M
