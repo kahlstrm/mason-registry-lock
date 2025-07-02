131689ce7acaf2a5308a46ed42b47b37d09117c1
GitHub Action <action@github.com>
2025-07-02 00:42:31 +0000

    Update mason-registry lock to 2025-06-30-level-hedge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-30-barren-march"
+M.registry_release = "github:mason-org/mason-registry@2025-06-30-level-hedge"
 
 return M
