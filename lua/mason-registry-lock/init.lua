c61f7d4b21e25271b3749daafbed934b656c4385
GitHub Action <action@github.com>
2025-07-02 00:42:31 +0000

    Update mason-registry lock to 2025-06-30-heavy-ton

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-06-30-spotty-turret"
+M.registry_release = "github:mason-org/mason-registry@2025-06-30-heavy-ton"
 
 return M
