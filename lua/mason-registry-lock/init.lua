1639b12b4ba1d77027912d8b50cdfcc36369a614
GitHub Action <action@github.com>
2025-07-03 00:42:38 +0000

    Update mason-registry lock to 2025-07-02-worse-ide

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-02-linear-horse"
+M.registry_release = "github:mason-org/mason-registry@2025-07-02-worse-ide"
 
 return M
