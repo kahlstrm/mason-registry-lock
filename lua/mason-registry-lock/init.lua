10c437b7e4efbe8ac5a6b3441240e4cac43888da
GitHub Action <action@github.com>
2025-07-03 00:42:38 +0000

    Update mason-registry lock to 2025-07-02-frigid-share

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-02-winged-angora"
+M.registry_release = "github:mason-org/mason-registry@2025-07-02-frigid-share"
 
 return M
