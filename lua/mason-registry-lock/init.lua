fe9473591b6753d3c46ee410ab1853f80db902db
GitHub Action <action@github.com>
2025-07-03 00:42:38 +0000

    Update mason-registry lock to 2025-07-02-linear-horse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-02-rapid-ton"
+M.registry_release = "github:mason-org/mason-registry@2025-07-02-linear-horse"
 
 return M
