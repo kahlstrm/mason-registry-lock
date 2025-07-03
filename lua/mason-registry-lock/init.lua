cb67fe3431526bde243d8533fc1304bf36178515
GitHub Action <action@github.com>
2025-07-03 00:42:38 +0000

    Update mason-registry lock to 2025-07-02-our-skate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-02-tough-route"
+M.registry_release = "github:mason-org/mason-registry@2025-07-02-our-skate"
 
 return M
