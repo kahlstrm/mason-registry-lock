6ac8c6882458616c4d8acdde1b756e54a0fb0033
GitHub Action <action@github.com>
2025-07-03 00:42:38 +0000

    Update mason-registry lock to 2025-07-02-jumpy-alloy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-02-adept-way"
+M.registry_release = "github:mason-org/mason-registry@2025-07-02-jumpy-alloy"
 
 return M
