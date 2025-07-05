ce846921a34ea52d75eaed88203d8d5e562b16ac
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-shaky-canvas

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-lavish-domain"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-shaky-canvas"
 
 return M
