470b58afacfe32ec8e3b9a972581df017dc1e6ba
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-fiscal-grouse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-sour-save"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-fiscal-grouse"
 
 return M
