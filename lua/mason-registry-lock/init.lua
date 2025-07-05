311bf5079e79ca1f62aefff9ec49368c3fb017f6
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-serene-zipper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-quain-sucker"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-serene-zipper"
 
 return M
