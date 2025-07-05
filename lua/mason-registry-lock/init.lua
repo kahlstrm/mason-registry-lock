3374ee9542cb004f055e2681da3912510378a902
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-04-level-river

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-rear-finch"
+M.registry_release = "github:mason-org/mason-registry@2025-07-04-level-river"
 
 return M
