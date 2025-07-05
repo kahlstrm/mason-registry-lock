e85bfcf27775b09fd6218a42aebbe0fa41dad00f
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-burly-buffet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-fiscal-grouse"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-burly-buffet"
 
 return M
