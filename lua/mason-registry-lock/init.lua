ded345284b06ed359b1713e31c9d78a6d61bb305
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-bold-stool

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-shaky-canvas"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-bold-stool"
 
 return M
