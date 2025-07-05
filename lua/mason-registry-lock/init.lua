72fddb8a370295551f45ed0a0b6741ff530ab579
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-moist-rosebud

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-02-sore-cattle"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-moist-rosebud"
 
 return M
