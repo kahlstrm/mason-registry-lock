40fe369b3cb1824ca6b5b869028673f1289967de
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-latin-screw

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-moist-rosebud"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-latin-screw"
 
 return M
