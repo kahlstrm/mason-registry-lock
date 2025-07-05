049d507f6bdc8f80e224ad9a99d66713a7422043
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-04-bouncy-fawn

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-04-needy-smash"
+M.registry_release = "github:mason-org/mason-registry@2025-07-04-bouncy-fawn"
 
 return M
