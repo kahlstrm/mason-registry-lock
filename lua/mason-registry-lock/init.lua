1bfa8172041b992817340ecd4033d0f1b40d8dfd
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-04-corny-creek

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-04-wobbly-vault"
+M.registry_release = "github:mason-org/mason-registry@2025-07-04-corny-creek"
 
 return M
