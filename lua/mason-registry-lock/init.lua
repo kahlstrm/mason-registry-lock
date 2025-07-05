d290e9c2bf341fcbff6ba449cbd24a4d6965dd79
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-rear-finch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-bold-stool"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-rear-finch"
 
 return M
