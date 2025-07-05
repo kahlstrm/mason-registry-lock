d23b8916b0392f2213c5d0c99b7db0723ab1433c
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-04-wobbly-vault

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-04-plush-kick"
+M.registry_release = "github:mason-org/mason-registry@2025-07-04-wobbly-vault"
 
 return M
