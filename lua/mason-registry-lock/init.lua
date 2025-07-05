8d695fb1ad99a9916261994fc7f5d8e0f30c7982
GitHub Action <action@github.com>
2025-07-05 00:40:25 +0000

    Update mason-registry lock to 2025-07-03-quain-sucker

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-03-burly-buffet"
+M.registry_release = "github:mason-org/mason-registry@2025-07-03-quain-sucker"
 
 return M
