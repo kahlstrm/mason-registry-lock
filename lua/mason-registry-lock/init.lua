caeec07a886f7b99ea9938a45bc04b4281d03f02
GitHub Action <action@github.com>
2025-07-06 00:47:55 +0000

    Update mason-registry lock to 2025-07-05-third-umbrella

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-05-deaf-noodle"
+M.registry_release = "github:mason-org/mason-registry@2025-07-05-third-umbrella"
 
 return M
