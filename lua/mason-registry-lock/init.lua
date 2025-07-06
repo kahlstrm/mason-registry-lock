d75545cbae3c2daec46001ef50666532744aa3e0
GitHub Action <action@github.com>
2025-07-06 00:47:55 +0000

    Update mason-registry lock to 2025-07-05-sparse-joke

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-05-third-umbrella"
+M.registry_release = "github:mason-org/mason-registry@2025-07-05-sparse-joke"
 
 return M
