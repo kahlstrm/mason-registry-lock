d2231151e183ad3495aa7ac623e294576ea87182
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-rigid-sack

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-eight-riddle"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-rigid-sack"
 
 return M
