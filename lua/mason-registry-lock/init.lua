f39ad5386382eebdba1c51804a553bdf943280c4
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-eight-riddle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-heady-ermine"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-eight-riddle"
 
 return M
