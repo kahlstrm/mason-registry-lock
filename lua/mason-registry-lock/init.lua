f084ea1e5bf3f9919062de52b90139e1aece82c1
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-dirty-twine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-nearby-voodoo"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-dirty-twine"
 
 return M
