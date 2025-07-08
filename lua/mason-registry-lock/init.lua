38a21dde4c609b9a1e8c28404fc05e93d7251cd6
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-vivid-rat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-snappy-gluten"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-vivid-rat"
 
 return M
