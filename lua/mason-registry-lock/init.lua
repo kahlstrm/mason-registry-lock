b5d3c00bdc4c2128c27dc016c936cba50f752441
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-drafty-icon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-06-bawdy-epoxy"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-drafty-icon"
 
 return M
