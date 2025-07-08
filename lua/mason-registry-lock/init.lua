c2d8e6e59d7a989f9b775dd5b9e1a6b6277cd286
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-bony-year

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-unruly-rayon"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-bony-year"
 
 return M
