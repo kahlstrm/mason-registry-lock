288ff15113085056a9cb777889b4dd8979d754cd
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-united-oven

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-sudden-four"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-united-oven"
 
 return M
