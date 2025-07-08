d9bfae84f1ca90e4988dc421f08a6c2cc58d3577
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-06-elite-mrigal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-06-ritzy-gator"
+M.registry_release = "github:mason-org/mason-registry@2025-07-06-elite-mrigal"
 
 return M
