7967fc694944d7d8da237abf0e2ba232a17ba699
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-nearby-voodoo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-peaceful-scene"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-nearby-voodoo"
 
 return M
