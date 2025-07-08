cd6f6e0cd20a437d0d82ed20ea96f42005876c8d
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-usual-doctor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-scarce-santa"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-usual-doctor"
 
 return M
