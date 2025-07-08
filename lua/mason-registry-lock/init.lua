f8dc8cbdf2bf0cd9b67ef3a787545744ae36f919
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-06-bawdy-epoxy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-06-elite-mrigal"
+M.registry_release = "github:mason-org/mason-registry@2025-07-06-bawdy-epoxy"
 
 return M
