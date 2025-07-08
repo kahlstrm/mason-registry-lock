5351a1f75a470ba6553e775f1a292060c53aedb3
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-unruly-rayon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-dirty-twine"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-unruly-rayon"
 
 return M
