b03305ad9bf5567c949c650e7abe7dd58bf84553
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-06-rough-hell

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-06-overt-state"
+M.registry_release = "github:mason-org/mason-registry@2025-07-06-rough-hell"
 
 return M
