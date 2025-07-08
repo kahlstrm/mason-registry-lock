8d4a5a63c36cb6ad1d7e41d6e7066166c1d4b82b
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-whole-sailor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-drafty-icon"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-whole-sailor"
 
 return M
