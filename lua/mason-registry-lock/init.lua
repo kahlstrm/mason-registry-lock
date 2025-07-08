8a2a648072fa9864c0be2aefdcf520dc76d6a71a
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-snappy-gluten

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-rigid-sack"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-snappy-gluten"
 
 return M
