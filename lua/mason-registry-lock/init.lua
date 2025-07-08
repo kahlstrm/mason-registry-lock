64694241aa1d2c1124a9fcab19abc73916c7e4ac
GitHub Action <action@github.com>
2025-07-08 00:42:51 +0000

    Update mason-registry lock to 2025-07-07-heady-ermine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-minute-spot"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-heady-ermine"
 
 return M
