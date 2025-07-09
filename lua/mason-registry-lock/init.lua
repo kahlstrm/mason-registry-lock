cfd5c7e1407be7f3611c7605988a123038953ed3
GitHub Action <action@github.com>
2025-07-09 00:43:51 +0000

    Update mason-registry lock to 2025-07-07-worse-litter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-07-vivid-rat"
+M.registry_release = "github:mason-org/mason-registry@2025-07-07-worse-litter"
 
 return M
