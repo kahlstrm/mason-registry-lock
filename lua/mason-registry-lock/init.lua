9da518d6af89180e53b136ce051e645d058b88ba
GitHub Action <action@github.com>
2025-07-09 00:43:52 +0000

    Update mason-registry lock to 2025-07-08-crisp-woolen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-08-square-virgo"
+M.registry_release = "github:mason-org/mason-registry@2025-07-08-crisp-woolen"
 
 return M
