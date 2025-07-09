0e4cb678f04c0c3352c856d628740806a31e384b
GitHub Action <action@github.com>
2025-07-09 00:43:52 +0000

    Update mason-registry lock to 2025-07-08-strange-canna

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-08-crisp-woolen"
+M.registry_release = "github:mason-org/mason-registry@2025-07-08-strange-canna"
 
 return M
