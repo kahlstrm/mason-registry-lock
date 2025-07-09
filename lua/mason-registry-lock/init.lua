396d502b467a1d9ff68e088903982873fc7b4af3
GitHub Action <action@github.com>
2025-07-09 00:43:52 +0000

    Update mason-registry lock to 2025-07-08-golden-brake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-08-zonked-hail"
+M.registry_release = "github:mason-org/mason-registry@2025-07-08-golden-brake"
 
 return M
