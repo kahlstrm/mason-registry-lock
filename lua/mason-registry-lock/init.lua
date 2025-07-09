f5730bca110729578587868f3ca869eeb0905669
GitHub Action <action@github.com>
2025-07-09 00:43:52 +0000

    Update mason-registry lock to 2025-07-08-peach-purple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-08-rainy-goby"
+M.registry_release = "github:mason-org/mason-registry@2025-07-08-peach-purple"
 
 return M
