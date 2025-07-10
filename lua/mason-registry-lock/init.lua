0bd55aa1c978122b915648a90b306c3721263018
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-manual-safe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-solid-throne"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-manual-safe"
 
 return M
