db505bc1034ceb0adfbad34ba13a4039d3c56d79
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-tested-medaka

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-crisp-canary"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-tested-medaka"
 
 return M
