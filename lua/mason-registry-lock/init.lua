041f2b9ccc4a9dfb85a146625db27180bdd62b74
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-soggy-fence

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-tested-medaka"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-soggy-fence"
 
 return M
