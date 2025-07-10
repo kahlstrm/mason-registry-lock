2cb1d49c8c23fdd0602c6b0fa9aaf2a3c18a5131
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-crisp-canary

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-right-repair"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-crisp-canary"
 
 return M
