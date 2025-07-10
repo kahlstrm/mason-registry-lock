7e7d655db3d943b9b3a05b51efe595c3d6a28b56
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-wobbly-riddle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-painless-pasta"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-wobbly-riddle"
 
 return M
