ff4d6fd1eef2a3a1c6932c987fa6f5b72b4bcdf7
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-like-hotel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-weekly-berry"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-like-hotel"
 
 return M
