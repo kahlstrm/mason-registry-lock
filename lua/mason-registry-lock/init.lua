e765fcb2c770982dcffbd1b3c4a8cab8b018a89e
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-painless-pasta

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-soggy-fence"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-painless-pasta"
 
 return M
