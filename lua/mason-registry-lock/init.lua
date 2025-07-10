9cf76511204ab69b8fe6b1d12244c5d30747ceaf
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-right-repair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-like-hotel"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-right-repair"
 
 return M
