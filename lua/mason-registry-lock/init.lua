de9664bab5d3d69169b3f2556f42ef817f08d811
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-weekly-berry

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-knotty-iniom"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-weekly-berry"
 
 return M
