ebc9f845e53bfce454026268fda948097c9e2205
GitHub Action <action@github.com>
2025-07-10 00:43:10 +0000

    Update mason-registry lock to 2025-07-09-knotty-iniom

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-09-lost-float"
+M.registry_release = "github:mason-org/mason-registry@2025-07-09-knotty-iniom"
 
 return M
