c758c4fdda7adf006577d69c40348f6c751a17d4
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-10-paltry-powder

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-10-orange-flax"
+M.registry_release = "github:mason-org/mason-registry@2025-07-10-paltry-powder"
 
 return M
