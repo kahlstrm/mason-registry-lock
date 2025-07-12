534b3f67a50c264fb1e22f99e5dc1c49818831ea
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-11-slimy-leaf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-11-wiry-bream"
+M.registry_release = "github:mason-org/mason-registry@2025-07-11-slimy-leaf"
 
 return M
