6227681db8abd7396062273e9d3e5cb94cbe15ce
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-11-fancy-cafe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-11-onyx-octopus"
+M.registry_release = "github:mason-org/mason-registry@2025-07-11-fancy-cafe"
 
 return M
