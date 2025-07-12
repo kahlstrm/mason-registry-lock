bc8d13482a351e3532579e2dfd6d39f37cee4d2c
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-10-bored-offer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-10-cloudy-order"
+M.registry_release = "github:mason-org/mason-registry@2025-07-10-bored-offer"
 
 return M
