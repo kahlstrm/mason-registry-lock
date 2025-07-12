e3d94eeebf9ef2c39cf247a5a19c86d0abdfe7c4
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-10-abrupt-drake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-10-sore-bamboo"
+M.registry_release = "github:mason-org/mason-registry@2025-07-10-abrupt-drake"
 
 return M
