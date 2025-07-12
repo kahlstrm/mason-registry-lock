753225a53cb334630bce10a0902daa5998b1a0d9
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-10-rainy-modem

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-10-past-badger"
+M.registry_release = "github:mason-org/mason-registry@2025-07-10-rainy-modem"
 
 return M
