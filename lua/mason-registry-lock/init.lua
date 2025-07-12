0a31d0480d3d607312a0202c8e601cc90637f953
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-10-intact-planet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-10-single-fine"
+M.registry_release = "github:mason-org/mason-registry@2025-07-10-intact-planet"
 
 return M
