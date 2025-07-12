938c435f5fe18877106e39db60dac57ba65418ab
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-10-single-fine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-10-bored-offer"
+M.registry_release = "github:mason-org/mason-registry@2025-07-10-single-fine"
 
 return M
