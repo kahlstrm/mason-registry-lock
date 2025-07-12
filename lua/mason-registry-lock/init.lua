886cddd7d8b67552e81e0bef941132a677275fb4
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-11-onyx-octopus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-11-oval-layer"
+M.registry_release = "github:mason-org/mason-registry@2025-07-11-onyx-octopus"
 
 return M
