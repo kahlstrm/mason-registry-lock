e1172241e4fbf48b7002628e01c7c9b7bd8844ab
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-11-solar-butane

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-11-boring-smell"
+M.registry_release = "github:mason-org/mason-registry@2025-07-11-solar-butane"
 
 return M
