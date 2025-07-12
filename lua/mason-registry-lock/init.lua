0316f6ae67dd9478c40fc97714234d8b68f76880
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-11-thorny-wrasse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-11-solar-butane"
+M.registry_release = "github:mason-org/mason-registry@2025-07-11-thorny-wrasse"
 
 return M
