06d01a7e8db138006fd83957b1d26e1eb6b1a0c9
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-10-cloudy-order

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-10-paltry-powder"
+M.registry_release = "github:mason-org/mason-registry@2025-07-10-cloudy-order"
 
 return M
