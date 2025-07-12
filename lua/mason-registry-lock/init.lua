2e9867b137412ab5855724112f7feaca85a4db42
GitHub Action <action@github.com>
2025-07-12 00:44:27 +0000

    Update mason-registry lock to 2025-07-11-oval-layer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-11-slimy-leaf"
+M.registry_release = "github:mason-org/mason-registry@2025-07-11-oval-layer"
 
 return M
