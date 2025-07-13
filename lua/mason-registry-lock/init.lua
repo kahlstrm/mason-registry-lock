75fc30513724b1123fcd615dec9674c3c4deef58
GitHub Action <action@github.com>
2025-07-13 00:50:10 +0000

    Update mason-registry lock to 2025-07-12-brawny-alarm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-12-melted-news"
+M.registry_release = "github:mason-org/mason-registry@2025-07-12-brawny-alarm"
 
 return M
