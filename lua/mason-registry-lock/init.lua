3e3a5566df78663c274346c4effe3d913400119b
GitHub Action <action@github.com>
2025-07-13 00:50:11 +0000

    Update mason-registry lock to 2025-07-12-cheeky-secret

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-12-light-july"
+M.registry_release = "github:mason-org/mason-registry@2025-07-12-cheeky-secret"
 
 return M
