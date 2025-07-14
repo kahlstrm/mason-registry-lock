4ed4609521cded10aa918574b8375b1a4ae854a6
GitHub Action <action@github.com>
2025-07-14 00:47:22 +0000

    Update mason-registry lock to 2025-07-13-thirsty-start

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-13-wobbly-ice"
+M.registry_release = "github:mason-org/mason-registry@2025-07-13-thirsty-start"
 
 return M
