a7655b912246ba9bf62e3e43eebc2a291eee7d48
GitHub Action <action@github.com>
2025-07-14 00:47:22 +0000

    Update mason-registry lock to 2025-07-13-mealy-bead

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-13-silver-egg"
+M.registry_release = "github:mason-org/mason-registry@2025-07-13-mealy-bead"
 
 return M
