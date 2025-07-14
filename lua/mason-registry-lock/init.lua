7652c32be9bb06e77cbf7d2a17c904e1939e705e
GitHub Action <action@github.com>
2025-07-14 00:47:22 +0000

    Update mason-registry lock to 2025-07-13-wobbly-ice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-13-three-automobile"
+M.registry_release = "github:mason-org/mason-registry@2025-07-13-wobbly-ice"
 
 return M
