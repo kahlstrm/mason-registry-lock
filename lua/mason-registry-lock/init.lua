4c4ed9e2082cffb8e81ecc48fe552ee8effee024
GitHub Action <action@github.com>
2025-07-15 00:45:18 +0000

    Update mason-registry lock to 2025-07-14-only-archer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-14-varied-mantra"
+M.registry_release = "github:mason-org/mason-registry@2025-07-14-only-archer"
 
 return M
