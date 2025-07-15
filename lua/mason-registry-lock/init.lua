d9a57953e30e9387044c149b4da384d493956d3c
GitHub Action <action@github.com>
2025-07-15 00:45:18 +0000

    Update mason-registry lock to 2025-07-14-intact-wolf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-14-some-quince"
+M.registry_release = "github:mason-org/mason-registry@2025-07-14-intact-wolf"
 
 return M
