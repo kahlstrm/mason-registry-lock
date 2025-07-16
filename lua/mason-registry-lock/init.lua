c4a8ef2b6843b768d1132be0c8659f268616ace7
GitHub Action <action@github.com>
2025-07-16 00:44:19 +0000

    Update mason-registry lock to 2025-07-15-spry-world

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-15-even-dugong"
+M.registry_release = "github:mason-org/mason-registry@2025-07-15-spry-world"
 
 return M
