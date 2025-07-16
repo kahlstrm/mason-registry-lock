fafb898634b6feaa0cf42372351e10ce7b680711
GitHub Action <action@github.com>
2025-07-16 00:44:19 +0000

    Update mason-registry lock to 2025-07-15-overt-dew

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-15-charming-detail"
+M.registry_release = "github:mason-org/mason-registry@2025-07-15-overt-dew"
 
 return M
