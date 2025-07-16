5510b5a5aacdaa654cc980dfa0593fa99ce997ad
GitHub Action <action@github.com>
2025-07-16 00:44:19 +0000

    Update mason-registry lock to 2025-07-15-proper-pimple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-15-tidy-cheetah"
+M.registry_release = "github:mason-org/mason-registry@2025-07-15-proper-pimple"
 
 return M
