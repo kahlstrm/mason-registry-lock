1664ecdb0560ad6a85dd2294ae70e3c0d1111458
GitHub Action <action@github.com>
2025-07-16 00:44:19 +0000

    Update mason-registry lock to 2025-07-16-glass-skunk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-15-wet-tempo"
+M.registry_release = "github:mason-org/mason-registry@2025-07-16-glass-skunk"
 
 return M
