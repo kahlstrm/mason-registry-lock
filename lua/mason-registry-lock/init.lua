0df1ae3aef9eeee24aee2d872653e10e5ca53bf8
GitHub Action <action@github.com>
2025-07-16 00:44:19 +0000

    Update mason-registry lock to 2025-07-15-manual-galaxy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-15-proper-pimple"
+M.registry_release = "github:mason-org/mason-registry@2025-07-15-manual-galaxy"
 
 return M
