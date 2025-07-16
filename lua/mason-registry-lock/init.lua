d459fa0eb237bbbbb8c78cfb501c92de73afdd65
GitHub Action <action@github.com>
2025-07-16 00:44:19 +0000

    Update mason-registry lock to 2025-07-15-showy-sandra

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-15-fair-battle"
+M.registry_release = "github:mason-org/mason-registry@2025-07-15-showy-sandra"
 
 return M
