e3776ab282ca1278e8583c5dd627c63c5dc913e0
GitHub Action <action@github.com>
2025-07-17 00:44:46 +0000

    Update mason-registry lock to 2025-07-16-due-plain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-16-glass-skunk"
+M.registry_release = "github:mason-org/mason-registry@2025-07-16-due-plain"
 
 return M
