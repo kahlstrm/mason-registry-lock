0170a704436479fb561bec4ff0a3efd8f5a7de1f
GitHub Action <action@github.com>
2025-07-17 00:44:46 +0000

    Update mason-registry lock to 2025-07-16-petty-true

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-16-earthy-angle"
+M.registry_release = "github:mason-org/mason-registry@2025-07-16-petty-true"
 
 return M
