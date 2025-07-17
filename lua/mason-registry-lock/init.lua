37e23771108a93dba5d1891ca94a5bef3ea063c0
GitHub Action <action@github.com>
2025-07-17 00:44:46 +0000

    Update mason-registry lock to 2025-07-16-worthy-italy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-16-innate-key"
+M.registry_release = "github:mason-org/mason-registry@2025-07-16-worthy-italy"
 
 return M
