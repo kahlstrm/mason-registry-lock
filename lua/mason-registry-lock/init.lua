fcff903e030fea02e6de0d35aa40cedd7e7e4deb
GitHub Action <action@github.com>
2025-07-17 00:44:46 +0000

    Update mason-registry lock to 2025-07-16-second-peace

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-16-soviet-design"
+M.registry_release = "github:mason-org/mason-registry@2025-07-16-second-peace"
 
 return M
