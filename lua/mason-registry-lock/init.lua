1b6ef4b539a59370bf460ba49b5ae009df1816c2
GitHub Action <action@github.com>
2025-07-17 00:44:46 +0000

    Update mason-registry lock to 2025-07-16-earthy-angle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-16-bloody-bee"
+M.registry_release = "github:mason-org/mason-registry@2025-07-16-earthy-angle"
 
 return M
