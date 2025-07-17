439e86af5949cb1572ac5f4503ef9b13d5bdff01
GitHub Action <action@github.com>
2025-07-17 00:44:46 +0000

    Update mason-registry lock to 2025-07-16-innate-key

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-16-painless-income"
+M.registry_release = "github:mason-org/mason-registry@2025-07-16-innate-key"
 
 return M
