9feebe079689c295a6e61d76315c960c12461c27
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-18-hard-radio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-18-numb-desert"
+M.registry_release = "github:mason-org/mason-registry@2025-07-18-hard-radio"
 
 return M
