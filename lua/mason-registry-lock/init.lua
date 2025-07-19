a22c067018362e0b85639edead07621b816b3ce8
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-17-nippy-pillow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-secure-soda"
+M.registry_release = "github:mason-org/mason-registry@2025-07-17-nippy-pillow"
 
 return M
