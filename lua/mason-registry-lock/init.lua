4217be5f880beacabbfebc2f9a50fce84eebc021
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-17-nearby-guru

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-full-base"
+M.registry_release = "github:mason-org/mason-registry@2025-07-17-nearby-guru"
 
 return M
