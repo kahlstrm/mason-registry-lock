db5dfd7d572232d6811b34de88bbb8b385e45f9e
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-17-bulky-boar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-blonde-arm"
+M.registry_release = "github:mason-org/mason-registry@2025-07-17-bulky-boar"
 
 return M
