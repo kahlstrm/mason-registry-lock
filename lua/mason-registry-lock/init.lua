7cd63e504507b3644f8fe4f8f622801eec737e3d
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-17-fair-toilet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-common-vault"
+M.registry_release = "github:mason-org/mason-registry@2025-07-17-fair-toilet"
 
 return M
