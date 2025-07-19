741e358e4f71c7a55934fa91378acfda44fbc8e4
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-17-tepid-volume

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-nippy-pillow"
+M.registry_release = "github:mason-org/mason-registry@2025-07-17-tepid-volume"
 
 return M
