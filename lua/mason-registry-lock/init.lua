2f5cb771534af29e394c5cce6cece1f59f027c88
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-18-mutual-tapir

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-18-normal-nylon"
+M.registry_release = "github:mason-org/mason-registry@2025-07-18-mutual-tapir"
 
 return M
