e7f24288ef0980db845bcdc678e5a83df0eaf3a2
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-18-numb-desert

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-late-bead"
+M.registry_release = "github:mason-org/mason-registry@2025-07-18-numb-desert"
 
 return M
