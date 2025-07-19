c96810899193469e6ac52640643bc4da4b653c4c
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-17-late-bead

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-tepid-volume"
+M.registry_release = "github:mason-org/mason-registry@2025-07-17-late-bead"
 
 return M
