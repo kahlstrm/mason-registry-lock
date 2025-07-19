7795a6657c7bdb1e7f5105b29e61998c02fefe64
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-18-latin-girdle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-18-aboard-bobcat"
+M.registry_release = "github:mason-org/mason-registry@2025-07-18-latin-girdle"
 
 return M
