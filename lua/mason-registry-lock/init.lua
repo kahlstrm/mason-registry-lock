2190cbae439305488397ae1d8e2b1951a2b7442d
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-18-gloomy-close

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-18-uneven-dace"
+M.registry_release = "github:mason-org/mason-registry@2025-07-18-gloomy-close"
 
 return M
