2f0f23bcf7e2af0ab8ac87529fd8f35c0c2b555b
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-18-oblong-agency

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-18-deeply-men"
+M.registry_release = "github:mason-org/mason-registry@2025-07-18-oblong-agency"
 
 return M
