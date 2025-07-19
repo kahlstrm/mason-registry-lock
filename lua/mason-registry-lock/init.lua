1e88ff2e1759ef13a16f6add3646d91c6fc286e8
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-17-common-vault

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-17-zesty-cave"
+M.registry_release = "github:mason-org/mason-registry@2025-07-17-common-vault"
 
 return M
