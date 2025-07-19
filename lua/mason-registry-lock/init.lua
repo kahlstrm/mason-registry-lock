3259218f85049b2101c54462c00a0f19e11e5cbc
GitHub Action <action@github.com>
2025-07-19 00:43:37 +0000

    Update mason-registry lock to 2025-07-18-aboard-bobcat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-18-mutual-tapir"
+M.registry_release = "github:mason-org/mason-registry@2025-07-18-aboard-bobcat"
 
 return M
