354eb5c88928e527e4209d43c16b68c637ebd59b
GitHub Action <action@github.com>
2025-07-20 00:51:20 +0000

    Update mason-registry lock to 2025-07-19-purple-drill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-19-alive-tin"
+M.registry_release = "github:mason-org/mason-registry@2025-07-19-purple-drill"
 
 return M
