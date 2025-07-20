6392e0b6cf58272551bd4b8223ef06c5a0a1e4bc
GitHub Action <action@github.com>
2025-07-20 00:51:20 +0000

    Update mason-registry lock to 2025-07-19-humble-film

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-19-six-felony"
+M.registry_release = "github:mason-org/mason-registry@2025-07-19-humble-film"
 
 return M
