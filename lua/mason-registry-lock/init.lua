10e078f697f1bc1ef6ba4256d2a94cc5a768616d
GitHub Action <action@github.com>
2025-07-20 00:51:20 +0000

    Update mason-registry lock to 2025-07-19-fine-author

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-19-purple-drill"
+M.registry_release = "github:mason-org/mason-registry@2025-07-19-fine-author"
 
 return M
