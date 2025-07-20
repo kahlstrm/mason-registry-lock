8fd098f9009f6e2db8a8a48147340a5092812f35
GitHub Action <action@github.com>
2025-07-20 00:51:20 +0000

    Update mason-registry lock to 2025-07-19-alive-tin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-19-humble-film"
+M.registry_release = "github:mason-org/mason-registry@2025-07-19-alive-tin"
 
 return M
