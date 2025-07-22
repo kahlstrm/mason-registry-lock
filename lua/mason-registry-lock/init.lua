20dcf919d3e4653fc17c352a5ae20736aecb4bcc
GitHub Action <action@github.com>
2025-07-22 00:44:59 +0000

    Update mason-registry lock to 2025-07-20-showy-worm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-20-liquid-coach"
+M.registry_release = "github:mason-org/mason-registry@2025-07-20-showy-worm"
 
 return M
