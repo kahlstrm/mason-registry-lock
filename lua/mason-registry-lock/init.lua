4317da7c84ee2cdb65092f9112c38e25c96ab4c6
GitHub Action <action@github.com>
2025-07-22 00:44:59 +0000

    Update mason-registry lock to 2025-07-20-solid-icon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-20-showy-worm"
+M.registry_release = "github:mason-org/mason-registry@2025-07-20-solid-icon"
 
 return M
