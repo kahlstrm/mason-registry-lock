72860bb4960cb54b4a3b96ea826fc17c534319a2
GitHub Action <action@github.com>
2025-07-22 00:44:59 +0000

    Update mason-registry lock to 2025-07-21-total-arm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-21-nifty-piece"
+M.registry_release = "github:mason-org/mason-registry@2025-07-21-total-arm"
 
 return M
