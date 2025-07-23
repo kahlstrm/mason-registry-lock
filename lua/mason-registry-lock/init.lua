5405b6bdb187d92a5baa06bfb3ce766158b5a95b
GitHub Action <action@github.com>
2025-07-23 00:47:44 +0000

    Update mason-registry lock to 2025-07-22-arid-pear

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-22-subtle-crown"
+M.registry_release = "github:mason-org/mason-registry@2025-07-22-arid-pear"
 
 return M
