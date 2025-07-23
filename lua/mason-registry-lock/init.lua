d43e3738e0d507d4cab3be420887cd436e04f046
GitHub Action <action@github.com>
2025-07-23 00:47:44 +0000

    Update mason-registry lock to 2025-07-22-rare-cod

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-22-uppity-minute"
+M.registry_release = "github:mason-org/mason-registry@2025-07-22-rare-cod"
 
 return M
