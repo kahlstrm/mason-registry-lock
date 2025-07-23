66bfa0d649001e68f9ab4beb191f3fff87988476
GitHub Action <action@github.com>
2025-07-23 00:47:44 +0000

    Update mason-registry lock to 2025-07-22-quaint-moon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-22-aqua-skill"
+M.registry_release = "github:mason-org/mason-registry@2025-07-22-quaint-moon"
 
 return M
