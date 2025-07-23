a9fefd4ab0ff1694717c97f84b40c3589334c351
GitHub Action <action@github.com>
2025-07-23 00:47:44 +0000

    Update mason-registry lock to 2025-07-22-stuck-lunge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-22-neat-jason"
+M.registry_release = "github:mason-org/mason-registry@2025-07-22-stuck-lunge"
 
 return M
