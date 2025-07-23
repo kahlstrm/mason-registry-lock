1bf5a96be1d15e668fd224965a23078363a6c4a9
GitHub Action <action@github.com>
2025-07-23 00:47:44 +0000

    Update mason-registry lock to 2025-07-22-sleepy-nature

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-22-stuck-lunge"
+M.registry_release = "github:mason-org/mason-registry@2025-07-22-sleepy-nature"
 
 return M
