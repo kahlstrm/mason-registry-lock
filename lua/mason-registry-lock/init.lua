47c70f1080af178ca9815ce5ddd71c5075da053f
GitHub Action <action@github.com>
2025-07-24 00:46:15 +0000

    Update mason-registry lock to 2025-07-23-phobic-pleco

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-23-glad-secret"
+M.registry_release = "github:mason-org/mason-registry@2025-07-23-phobic-pleco"
 
 return M
