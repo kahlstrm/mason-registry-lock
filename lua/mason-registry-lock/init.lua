833cee391fb6911b57406dd32a66f2378ac9b77b
GitHub Action <action@github.com>
2025-07-24 00:46:15 +0000

    Update mason-registry lock to 2025-07-24-bony-reward

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-24-ample-bay"
+M.registry_release = "github:mason-org/mason-registry@2025-07-24-bony-reward"
 
 return M
