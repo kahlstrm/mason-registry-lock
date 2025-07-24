8579b4711fde1cf834d2cd536f2b37da784446dc
GitHub Action <action@github.com>
2025-07-24 00:46:15 +0000

    Update mason-registry lock to 2025-07-23-real-search

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-23-chubby-steven"
+M.registry_release = "github:mason-org/mason-registry@2025-07-23-real-search"
 
 return M
