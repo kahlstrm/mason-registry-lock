5200acdb494412e0b4058e32229164736038fe1e
GitHub Action <action@github.com>
2025-07-25 00:44:57 +0000

    Update mason-registry lock to 2025-07-24-unsung-client

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-24-weekly-pie"
+M.registry_release = "github:mason-org/mason-registry@2025-07-24-unsung-client"
 
 return M
