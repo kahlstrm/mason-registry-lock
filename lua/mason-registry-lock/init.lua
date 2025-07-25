1667a25c5e487dbc8c8aac2000f1851c26e5d50b
GitHub Action <action@github.com>
2025-07-25 00:44:57 +0000

    Update mason-registry lock to 2025-07-24-baked-scent

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-24-unfit-opah"
+M.registry_release = "github:mason-org/mason-registry@2025-07-24-baked-scent"
 
 return M
