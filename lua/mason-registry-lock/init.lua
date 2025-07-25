34efc7d536f05310efed3a7df2076dc32559a4b7
GitHub Action <action@github.com>
2025-07-25 00:44:57 +0000

    Update mason-registry lock to 2025-07-24-painless-lagena

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-24-numb-dollar"
+M.registry_release = "github:mason-org/mason-registry@2025-07-24-painless-lagena"
 
 return M
