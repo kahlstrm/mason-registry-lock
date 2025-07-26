a1b8eb80043df7eed99097eb9a8f98aa3f2bff4c
GitHub Action <action@github.com>
2025-07-26 00:44:20 +0000

    Update mason-registry lock to 2025-07-25-chosen-coin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-25-lost-fuel"
+M.registry_release = "github:mason-org/mason-registry@2025-07-25-chosen-coin"
 
 return M
