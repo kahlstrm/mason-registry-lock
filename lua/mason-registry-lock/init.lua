a548673a2268ba73dc1731986065e768cdef0751
GitHub Action <action@github.com>
2025-07-28 00:50:21 +0000

    Update mason-registry lock to 2025-07-27-tough-wall

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-26-noble-wahoo"
+M.registry_release = "github:mason-org/mason-registry@2025-07-27-tough-wall"
 
 return M
