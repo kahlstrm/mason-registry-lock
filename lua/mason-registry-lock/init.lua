c492bf6f53d95a3a43af8ceabd1f752993f5b47b
GitHub Action <action@github.com>
2025-07-28 00:50:21 +0000

    Update mason-registry lock to 2025-07-27-pretty-expert

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-27-tough-wall"
+M.registry_release = "github:mason-org/mason-registry@2025-07-27-pretty-expert"
 
 return M
