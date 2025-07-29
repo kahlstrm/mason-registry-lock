f45f2ca081a04cb0097338fa7c49b9528f84487d
GitHub Action <action@github.com>
2025-07-29 00:52:10 +0000

    Update mason-registry lock to 2025-07-28-thorny-smelt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-28-low-save"
+M.registry_release = "github:mason-org/mason-registry@2025-07-28-thorny-smelt"
 
 return M
