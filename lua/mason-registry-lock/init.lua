ffa8633c51c650762cdf053b8e7da6aaf812c495
GitHub Action <action@github.com>
2025-07-29 00:52:10 +0000

    Update mason-registry lock to 2025-07-28-rusty-rayon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-28-civic-raid"
+M.registry_release = "github:mason-org/mason-registry@2025-07-28-rusty-rayon"
 
 return M
