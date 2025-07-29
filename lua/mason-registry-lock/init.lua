dc1580e650c4779d69ee1da819e203290f48c8cc
GitHub Action <action@github.com>
2025-07-29 00:52:10 +0000

    Update mason-registry lock to 2025-07-28-nimble-flood

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-27-handy-tree"
+M.registry_release = "github:mason-org/mason-registry@2025-07-28-nimble-flood"
 
 return M
