37c9ea2b925e062152d2e0f6658453efcc44fe6a
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-grey-gaze

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-causal-plane"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-grey-gaze"
 
 return M
