7633dcab3acb05aa928c1c4c85517411398271dc
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-white-format

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-godly-yew"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-white-format"
 
 return M
