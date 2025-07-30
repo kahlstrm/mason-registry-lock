2e3e5ea681add973807d594a8170dbfdefc214c2
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-wacky-team

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-white-format"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-wacky-team"
 
 return M
