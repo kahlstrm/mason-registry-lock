d3d353866e5d01855f1eb1ddbb2bd2bc5689c307
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-causal-plane

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-musty-risk"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-causal-plane"
 
 return M
