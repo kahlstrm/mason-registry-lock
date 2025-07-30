969e70731c3c4fd5901aee0dfcd8ffef2841fe6e
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-musty-risk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-green-blue"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-musty-risk"
 
 return M
