22f4d1dcf288bfbdef28ab4984a04782c4f3019c
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-craven-stop

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-quick-garage"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-craven-stop"
 
 return M
