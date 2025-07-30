3f0a6bcbc6e79cfd8287a6fb0b23f5bc28dc561a
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-quick-garage

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-wacky-team"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-quick-garage"
 
 return M
