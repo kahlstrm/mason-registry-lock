9c465547d3e9090dd98749e82f2975afdb18592f
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-acid-quartz

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-29-putrid-wave"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-acid-quartz"
 
 return M
