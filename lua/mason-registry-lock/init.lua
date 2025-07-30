23717ca0c73a749c61acac8a12cb852484c4442b
GitHub Action <action@github.com>
2025-07-30 00:45:32 +0000

    Update mason-registry lock to 2025-07-29-godly-yew

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-28-rusty-action"
+M.registry_release = "github:mason-org/mason-registry@2025-07-29-godly-yew"
 
 return M
