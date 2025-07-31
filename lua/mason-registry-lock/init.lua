15faf39dc057912c815c350111f49580b763a718
GitHub Action <action@github.com>
2025-07-31 00:45:49 +0000

    Update mason-registry lock to 2025-07-30-female-net

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-30-jovial-fork"
+M.registry_release = "github:mason-org/mason-registry@2025-07-30-female-net"
 
 return M
