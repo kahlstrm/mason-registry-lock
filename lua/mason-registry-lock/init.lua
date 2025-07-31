db124cb7f975e0db203c9c3ea883f057219137ff
GitHub Action <action@github.com>
2025-07-31 00:45:49 +0000

    Update mason-registry lock to 2025-07-30-jovial-fork

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-30-secret-jenny"
+M.registry_release = "github:mason-org/mason-registry@2025-07-30-jovial-fork"
 
 return M
