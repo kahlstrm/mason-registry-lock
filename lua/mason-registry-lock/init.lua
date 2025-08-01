25e5de1750bcb085f736fa33b59eabb03de397ee
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-31-cloudy-pillow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-31-scared-flock"
+M.registry_release = "github:mason-org/mason-registry@2025-07-31-cloudy-pillow"
 
 return M
