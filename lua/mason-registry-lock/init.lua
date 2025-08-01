4d965b3b8043cfeb2b48a9d71b23dba2dba78e89
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-31-inland-belief

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-31-brash-sushi"
+M.registry_release = "github:mason-org/mason-registry@2025-07-31-inland-belief"
 
 return M
