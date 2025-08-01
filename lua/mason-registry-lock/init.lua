a6f69da47eb5b6c5c97d50b24f675b9b5f528d5e
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-31-awake-driver

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-31-chilly-unit"
+M.registry_release = "github:mason-org/mason-registry@2025-07-31-awake-driver"
 
 return M
