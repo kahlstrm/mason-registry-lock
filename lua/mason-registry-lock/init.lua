248d6164654588427880f2c1228fe14ad1d40101
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-31-rainy-show

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-31-tinted-advice"
+M.registry_release = "github:mason-org/mason-registry@2025-07-31-rainy-show"
 
 return M
