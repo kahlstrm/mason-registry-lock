793c599389d0f9a8b59ce78885fc95a5b75c43a9
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-31-scared-flock

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-31-outer-colony"
+M.registry_release = "github:mason-org/mason-registry@2025-07-31-scared-flock"
 
 return M
