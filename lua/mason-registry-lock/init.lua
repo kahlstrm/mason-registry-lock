1f8a330330dab12976336a79be388be14d6cd2bf
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-30-stale-kalium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-30-floppy-glass"
+M.registry_release = "github:mason-org/mason-registry@2025-07-30-stale-kalium"
 
 return M
