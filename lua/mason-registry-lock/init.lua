c8fe0187a47da83617a331bbbd7e641604ec16fb
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-31-untrue-peanut

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-31-inland-belief"
+M.registry_release = "github:mason-org/mason-registry@2025-07-31-untrue-peanut"
 
 return M
