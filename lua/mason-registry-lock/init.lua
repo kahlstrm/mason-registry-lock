53f4356228a3103c5a73932df77a7535d83e0175
GitHub Action <action@github.com>
2025-08-01 00:53:11 +0000

    Update mason-registry lock to 2025-07-31-early-cost

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-07-31-rainy-show"
+M.registry_release = "github:mason-org/mason-registry@2025-07-31-early-cost"
 
 return M
