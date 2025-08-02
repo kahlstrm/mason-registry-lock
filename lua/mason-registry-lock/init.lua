b8b5548f81868d07e70b9c5074cbf56af7d635b2
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-chubby-alibi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-fluid-asia"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-chubby-alibi"
 
 return M
