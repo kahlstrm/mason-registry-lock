cc4dfeca955ed637e74133a9348a9e783d70f672
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-abrupt-run

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-inc-cement"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-abrupt-run"
 
 return M
