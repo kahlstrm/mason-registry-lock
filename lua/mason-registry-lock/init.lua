479803131e11108529e44de06019152a619f92a1
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-basic-cycle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-mere-bowfin"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-basic-cycle"
 
 return M
