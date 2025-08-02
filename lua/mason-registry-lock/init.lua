e986fbfc308d857cd8606fb95a2c254576757d97
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-roman-nest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-aquatic-shrimp"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-roman-nest"
 
 return M
