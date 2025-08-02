abb3d5c1da482a63426432cef1a5d38e162dd1e1
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-zigzag-faith

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-just-drama"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-zigzag-faith"
 
 return M
