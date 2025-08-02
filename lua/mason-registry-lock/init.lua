508401a2404d0675543e12bbe6fecd141d9c20f3
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-many-water

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-korean-tooth"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-many-water"
 
 return M
