903ce8b246b8ce0d6ecd71ba2d29b40cc5aa31a3
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-native-jumper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-tame-nova"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-native-jumper"
 
 return M
