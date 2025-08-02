0849914ea62377fde706f3d152f420715e50c6b3
GitHub Action <action@github.com>
2025-08-02 00:43:26 +0000

    Update mason-registry lock to 2025-08-01-jazzy-cream

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-01-weak-look"
+M.registry_release = "github:mason-org/mason-registry@2025-08-01-jazzy-cream"
 
 return M
