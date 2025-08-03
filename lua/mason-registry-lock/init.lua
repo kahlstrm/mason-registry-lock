e460b20cdf1a5a24314273f3dadb82f80515d689
GitHub Action <action@github.com>
2025-08-03 00:52:18 +0000

    Update mason-registry lock to 2025-08-02-frail-pond

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-02-fair-guilt"
+M.registry_release = "github:mason-org/mason-registry@2025-08-02-frail-pond"
 
 return M
