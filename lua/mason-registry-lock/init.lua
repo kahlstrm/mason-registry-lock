ee91c5a94ae137b58bb66e39ab4db2a4ea256b70
GitHub Action <action@github.com>
2025-08-03 00:52:18 +0000

    Update mason-registry lock to 2025-08-02-fair-guilt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-02-close-syrup"
+M.registry_release = "github:mason-org/mason-registry@2025-08-02-fair-guilt"
 
 return M
