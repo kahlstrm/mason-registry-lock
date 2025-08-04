3e2130c96f92b3b9461c60db94f804469438d442
GitHub Action <action@github.com>
2025-08-04 00:51:40 +0000

    Update mason-registry lock to 2025-08-03-purple-notify

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-03-armed-group"
+M.registry_release = "github:mason-org/mason-registry@2025-08-03-purple-notify"
 
 return M
