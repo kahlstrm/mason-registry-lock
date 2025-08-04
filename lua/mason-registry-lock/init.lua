dbfc01b5c0b7dc2271281feebd7cd307c93026a9
GitHub Action <action@github.com>
2025-08-04 00:51:40 +0000

    Update mason-registry lock to 2025-08-03-urban-trance

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-03-tough-flight"
+M.registry_release = "github:mason-org/mason-registry@2025-08-03-urban-trance"
 
 return M
