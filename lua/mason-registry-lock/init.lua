03e7aab5d85da74ec44b3089b7b437b480408105
GitHub Action <action@github.com>
2025-08-04 00:51:40 +0000

    Update mason-registry lock to 2025-08-03-formal-lock

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-03-rowdy-damage"
+M.registry_release = "github:mason-org/mason-registry@2025-08-03-formal-lock"
 
 return M
