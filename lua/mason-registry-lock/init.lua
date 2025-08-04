35dd3d6e688a2f91c824473e246b32dc2f4fcd19
GitHub Action <action@github.com>
2025-08-04 00:51:40 +0000

    Update mason-registry lock to 2025-08-03-faint-flat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-03-aware-poppy"
+M.registry_release = "github:mason-org/mason-registry@2025-08-03-faint-flat"
 
 return M
