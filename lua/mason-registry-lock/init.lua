c244f53472f560492a1ff4bfdd4bbc9bae98bc00
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-pushy-manx

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-04-actual-ball"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-pushy-manx"
 
 return M
