bb15595e7135d892c4e943887f0900a64c77b25c
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-varied-size

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-03-formal-lock"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-varied-size"
 
 return M
