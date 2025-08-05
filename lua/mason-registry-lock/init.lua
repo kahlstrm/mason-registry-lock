c2b71fef257bf1b91ea1500d9589d5e668d028da
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-thin-pony

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-04-normal-flock"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-thin-pony"
 
 return M
