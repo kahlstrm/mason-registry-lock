66e8df580331b27cc448924ff680bed77425f5a5
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-minor-cereal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-04-static-fence"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-minor-cereal"
 
 return M
