632b1f8e494fd86c11b02b4f6e599a2a43811554
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-static-fence

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-04-stiff-hash"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-static-fence"
 
 return M
