6b887941ebfa3484f0af8e49dfdc651f7fd6a7d9
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-atomic-delete

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-04-tawdry-dance"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-atomic-delete"
 
 return M
