56a736c1962c51e4112dbcfa524518adacbb9c0b
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-stiff-hash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-04-light-legal"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-stiff-hash"
 
 return M
