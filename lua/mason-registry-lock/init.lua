883c6083af2168a2edbb246530482eaf4381ca9b
GitHub Action <action@github.com>
2025-08-05 00:47:18 +0000

    Update mason-registry lock to 2025-08-04-tawdry-dance

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-04-pushy-manx"
+M.registry_release = "github:mason-org/mason-registry@2025-08-04-tawdry-dance"
 
 return M
