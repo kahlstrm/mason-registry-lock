1ce5630d0a350ab4b0c8b81c0cfeac3e18091e32
GitHub Action <action@github.com>
2025-08-06 00:46:12 +0000

    Update mason-registry lock to 2025-08-05-alike-refund

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-05-rustic-grouse"
+M.registry_release = "github:mason-org/mason-registry@2025-08-05-alike-refund"
 
 return M
