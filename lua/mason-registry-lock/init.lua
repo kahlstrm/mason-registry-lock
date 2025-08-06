143dd826ba798db7a428249e1761fb046161bf6c
GitHub Action <action@github.com>
2025-08-06 00:46:12 +0000

    Update mason-registry lock to 2025-08-05-wise-soil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-05-painless-mantle"
+M.registry_release = "github:mason-org/mason-registry@2025-08-05-wise-soil"
 
 return M
