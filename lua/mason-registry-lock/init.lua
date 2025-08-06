40a6827124afaab2e3d77d4bb2ca2981a37f4954
GitHub Action <action@github.com>
2025-08-06 00:46:12 +0000

    Update mason-registry lock to 2025-08-05-tinted-nature

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-05-plucky-stage"
+M.registry_release = "github:mason-org/mason-registry@2025-08-05-tinted-nature"
 
 return M
