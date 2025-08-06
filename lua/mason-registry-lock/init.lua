556a5a44f16524be5e877388dada35b9112a5637
GitHub Action <action@github.com>
2025-08-06 00:46:12 +0000

    Update mason-registry lock to 2025-08-05-rustic-grouse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-05-wise-soil"
+M.registry_release = "github:mason-org/mason-registry@2025-08-05-rustic-grouse"
 
 return M
