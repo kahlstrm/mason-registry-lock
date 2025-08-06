237eda39eb7534fe64871bdaacce7e491374f06d
GitHub Action <action@github.com>
2025-08-06 00:46:12 +0000

    Update mason-registry lock to 2025-08-05-moral-flamingo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-05-tinted-nature"
+M.registry_release = "github:mason-org/mason-registry@2025-08-05-moral-flamingo"
 
 return M
