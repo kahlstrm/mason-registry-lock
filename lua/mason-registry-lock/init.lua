a59b147da9345809d9a7a3d2720702b118f36f65
GitHub Action <action@github.com>
2025-08-07 00:47:05 +0000

    Update mason-registry lock to 2025-08-06-bland-oil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-06-frank-weight"
+M.registry_release = "github:mason-org/mason-registry@2025-08-06-bland-oil"
 
 return M
