14f733efaf711ff87919f8b88af56380169ba7ee
GitHub Action <action@github.com>
2025-08-07 00:47:05 +0000

    Update mason-registry lock to 2025-08-06-half-snook

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-06-coarse-clock"
+M.registry_release = "github:mason-org/mason-registry@2025-08-06-half-snook"
 
 return M
