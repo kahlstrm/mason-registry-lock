188592168552dcebc16c9f34594510a39ae76812
GitHub Action <action@github.com>
2025-08-07 00:47:05 +0000

    Update mason-registry lock to 2025-08-06-blind-coyote

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-06-joint-warm"
+M.registry_release = "github:mason-org/mason-registry@2025-08-06-blind-coyote"
 
 return M
