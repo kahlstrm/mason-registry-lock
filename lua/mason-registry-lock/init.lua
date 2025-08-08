1795ac582a9bc95249955344a82eaf1aac1d6a7a
GitHub Action <action@github.com>
2025-08-08 00:45:46 +0000

    Update mason-registry lock to 2025-08-07-furry-spam

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-07-bony-shake"
+M.registry_release = "github:mason-org/mason-registry@2025-08-07-furry-spam"
 
 return M
