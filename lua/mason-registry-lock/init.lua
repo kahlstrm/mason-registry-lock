159efd81e1b965c74a810fa3cbb2cade2670cfd1
GitHub Action <action@github.com>
2025-08-09 00:41:36 +0000

    Update mason-registry lock to 2025-08-08-crispy-height

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-08-thirsty-automobile"
+M.registry_release = "github:mason-org/mason-registry@2025-08-08-crispy-height"
 
 return M
