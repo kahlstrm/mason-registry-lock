c3f03fd5d893f849849bba73d68cdb52d6c334cc
GitHub Action <action@github.com>
2025-08-09 00:41:36 +0000

    Update mason-registry lock to 2025-08-08-cuddly-ocean

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-08-red-maple"
+M.registry_release = "github:mason-org/mason-registry@2025-08-08-cuddly-ocean"
 
 return M
