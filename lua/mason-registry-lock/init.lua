5ab950ae8c1a200e43bdf14127ca1a856110423f
GitHub Action <action@github.com>
2025-08-09 00:41:36 +0000

    Update mason-registry lock to 2025-08-08-ultra-melody

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-07-abrupt-nova"
+M.registry_release = "github:mason-org/mason-registry@2025-08-08-ultra-melody"
 
 return M
