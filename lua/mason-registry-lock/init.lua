83f64998754a3135ef00552b51ff8dd6ec233b52
GitHub Action <action@github.com>
2025-08-09 00:41:36 +0000

    Update mason-registry lock to 2025-08-08-red-maple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-08-eldest-cause"
+M.registry_release = "github:mason-org/mason-registry@2025-08-08-red-maple"
 
 return M
