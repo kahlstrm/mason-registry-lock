5095a34508a52ebe6471eda1f45a2a39542a8f35
GitHub Action <action@github.com>
2025-08-09 00:41:36 +0000

    Update mason-registry lock to 2025-08-08-random-elephant

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-08-magical-cocoa"
+M.registry_release = "github:mason-org/mason-registry@2025-08-08-random-elephant"
 
 return M
