db588b1fac9f76b6af75ff5a65ebf8197a118c7f
GitHub Action <action@github.com>
2025-08-09 00:41:36 +0000

    Update mason-registry lock to 2025-08-08-magical-cocoa

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-08-few-barber"
+M.registry_release = "github:mason-org/mason-registry@2025-08-08-magical-cocoa"
 
 return M
