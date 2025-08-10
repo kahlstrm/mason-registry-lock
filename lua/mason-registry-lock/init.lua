3cf4204d4969063af98b24c027a6785583a99f61
GitHub Action <action@github.com>
2025-08-10 00:50:42 +0000

    Update mason-registry lock to 2025-08-09-head-sink

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-09-placid-range"
+M.registry_release = "github:mason-org/mason-registry@2025-08-09-head-sink"
 
 return M
