4f26d64cbfe8bffa4702f5a1654d4dda62e20979
GitHub Action <action@github.com>
2025-08-10 00:50:41 +0000

    Update mason-registry lock to 2025-08-09-lazy-chef

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-09-nimble-quill"
+M.registry_release = "github:mason-org/mason-registry@2025-08-09-lazy-chef"
 
 return M
