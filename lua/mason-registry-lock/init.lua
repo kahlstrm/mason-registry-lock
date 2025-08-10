e16698c9e3f335801df24abc85280d13f8b25e62
GitHub Action <action@github.com>
2025-08-10 00:50:42 +0000

    Update mason-registry lock to 2025-08-09-slender-okapi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-09-shabby-tights"
+M.registry_release = "github:mason-org/mason-registry@2025-08-09-slender-okapi"
 
 return M
