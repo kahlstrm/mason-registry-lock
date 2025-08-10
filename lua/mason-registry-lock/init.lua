7ba839233d2a049f7692bf853081645c8a7d5491
GitHub Action <action@github.com>
2025-08-10 00:50:41 +0000

    Update mason-registry lock to 2025-08-09-nimble-quill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-09-hearty-teapot"
+M.registry_release = "github:mason-org/mason-registry@2025-08-09-nimble-quill"
 
 return M
