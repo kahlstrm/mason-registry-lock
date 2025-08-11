6e363a98725fd803d7874d5ab845ec9132398119
GitHub Action <action@github.com>
2025-08-11 00:48:11 +0000

    Update mason-registry lock to 2025-08-10-spare-cook

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-10-pricey-ship"
+M.registry_release = "github:mason-org/mason-registry@2025-08-10-spare-cook"
 
 return M
