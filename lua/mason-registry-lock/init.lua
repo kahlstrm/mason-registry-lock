14d8beff393348575e0e8561d0a845269b2c31b9
GitHub Action <action@github.com>
2025-08-12 00:42:27 +0000

    Update mason-registry lock to 2025-08-11-flashy-pile

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-11-nearby-bobcat"
+M.registry_release = "github:mason-org/mason-registry@2025-08-11-flashy-pile"
 
 return M
