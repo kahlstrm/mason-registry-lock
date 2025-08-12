2e89000df92b8bf18720e3c4db7e472e14ee8518
GitHub Action <action@github.com>
2025-08-12 00:42:27 +0000

    Update mason-registry lock to 2025-08-11-spry-whorl

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-11-thoughtful-hell"
+M.registry_release = "github:mason-org/mason-registry@2025-08-11-spry-whorl"
 
 return M
