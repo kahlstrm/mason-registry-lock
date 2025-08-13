36281105b499ba431192971eed088675dab27810
GitHub Action <action@github.com>
2025-08-13 00:43:25 +0000

    Update mason-registry lock to 2025-08-12-right-soda

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-12-abject-bream"
+M.registry_release = "github:mason-org/mason-registry@2025-08-12-right-soda"
 
 return M
