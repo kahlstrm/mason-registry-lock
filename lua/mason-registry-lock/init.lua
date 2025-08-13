f28d235d85401ec59eaca97368dd90af5d78e337
GitHub Action <action@github.com>
2025-08-13 00:43:25 +0000

    Update mason-registry lock to 2025-08-12-yellow-hot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-11-flashy-pile"
+M.registry_release = "github:mason-org/mason-registry@2025-08-12-yellow-hot"
 
 return M
