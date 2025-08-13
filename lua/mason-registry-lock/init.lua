b77e0fc508f48178ec422563bf0bb07424b56bbd
GitHub Action <action@github.com>
2025-08-13 00:43:25 +0000

    Update mason-registry lock to 2025-08-12-cooing-giant

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-12-cyan-key"
+M.registry_release = "github:mason-org/mason-registry@2025-08-12-cooing-giant"
 
 return M
