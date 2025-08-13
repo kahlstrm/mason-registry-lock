572672dcc162296485403f49008d4758b3eef9e1
GitHub Action <action@github.com>
2025-08-13 00:43:25 +0000

    Update mason-registry lock to 2025-08-12-help-degree

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-12-frank-paper"
+M.registry_release = "github:mason-org/mason-registry@2025-08-12-help-degree"
 
 return M
