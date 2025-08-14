5deb0b5ed781a903e0159b8a0563d46f119791f6
GitHub Action <action@github.com>
2025-08-14 00:42:50 +0000

    Update mason-registry lock to 2025-08-13-somber-herd

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-12-right-soda"
+M.registry_release = "github:mason-org/mason-registry@2025-08-13-somber-herd"
 
 return M
