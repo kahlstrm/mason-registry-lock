0d6bda626a2c6e0cdc800b092fec6b9343c0c061
GitHub Action <action@github.com>
2025-08-14 00:42:50 +0000

    Update mason-registry lock to 2025-08-13-rapid-barium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-13-bossy-china"
+M.registry_release = "github:mason-org/mason-registry@2025-08-13-rapid-barium"
 
 return M
