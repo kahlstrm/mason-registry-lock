e128f10492bdcdfaf1ec55587ea2ca08b10831c1
GitHub Action <action@github.com>
2025-08-15 00:43:45 +0000

    Update mason-registry lock to 2025-08-14-graceful-apple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-14-aural-mob"
+M.registry_release = "github:mason-org/mason-registry@2025-08-14-graceful-apple"
 
 return M
