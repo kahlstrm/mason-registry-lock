4cc16e6d6380f492f8812a97c9925226bee691d4
GitHub Action <action@github.com>
2025-08-15 00:43:45 +0000

    Update mason-registry lock to 2025-08-14-clean-judge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-14-uneven-otter"
+M.registry_release = "github:mason-org/mason-registry@2025-08-14-clean-judge"
 
 return M
