58a15c826e853ea7fb44a99c97610254e4efa4af
GitHub Action <action@github.com>
2025-08-15 00:43:45 +0000

    Update mason-registry lock to 2025-08-14-uppity-beech

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-14-bony-rate"
+M.registry_release = "github:mason-org/mason-registry@2025-08-14-uppity-beech"
 
 return M
