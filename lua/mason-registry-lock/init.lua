586578afae899b0b18a80dc0b8f8f4b4ef7db52f
GitHub Action <action@github.com>
2025-08-15 00:43:45 +0000

    Update mason-registry lock to 2025-08-14-near-emerald

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-14-unusual-frown"
+M.registry_release = "github:mason-org/mason-registry@2025-08-14-near-emerald"
 
 return M
