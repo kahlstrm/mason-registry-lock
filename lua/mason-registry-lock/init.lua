6572a8124c4b04269d49f3912b398329596d2c3d
GitHub Action <action@github.com>
2025-08-16 00:40:39 +0000

    Update mason-registry lock to 2025-08-15-steady-juice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-15-alive-tan"
+M.registry_release = "github:mason-org/mason-registry@2025-08-15-steady-juice"
 
 return M
