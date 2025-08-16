85e967671f9832e9b710e67795cdcce02327a118
GitHub Action <action@github.com>
2025-08-16 00:40:39 +0000

    Update mason-registry lock to 2025-08-15-any-lamp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-15-living-column"
+M.registry_release = "github:mason-org/mason-registry@2025-08-15-any-lamp"
 
 return M
