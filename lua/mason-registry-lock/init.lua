85589b491c71e193d5e3baa2ba04279643b96067
GitHub Action <action@github.com>
2025-08-16 00:40:39 +0000

    Update mason-registry lock to 2025-08-15-earthy-worm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-15-silent-opera"
+M.registry_release = "github:mason-org/mason-registry@2025-08-15-earthy-worm"
 
 return M
