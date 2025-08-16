f71046a2a7f00bc811956b482db4805333d06f7f
GitHub Action <action@github.com>
2025-08-16 00:40:39 +0000

    Update mason-registry lock to 2025-08-15-liquid-dust

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-15-earthy-worm"
+M.registry_release = "github:mason-org/mason-registry@2025-08-15-liquid-dust"
 
 return M
