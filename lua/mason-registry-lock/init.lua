0bed66b0ce3105248e503725ea511efc038dec15
GitHub Action <action@github.com>
2025-08-16 00:40:39 +0000

    Update mason-registry lock to 2025-08-14-other-silk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-14-dental-monday"
+M.registry_release = "github:mason-org/mason-registry@2025-08-14-other-silk"
 
 return M
