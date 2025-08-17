2408b580c30d8999d70601d7224a9e80b239862d
GitHub Action <action@github.com>
2025-08-17 00:46:58 +0000

    Update mason-registry lock to 2025-08-16-old-cloud

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-16-wiggly-sink"
+M.registry_release = "github:mason-org/mason-registry@2025-08-16-old-cloud"
 
 return M
