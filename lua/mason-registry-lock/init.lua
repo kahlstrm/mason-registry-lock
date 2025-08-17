8d0fc97e057c2ea4edf6b7b068d385fa9337257f
GitHub Action <action@github.com>
2025-08-17 00:46:58 +0000

    Update mason-registry lock to 2025-08-16-pink-gaze

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-16-fine-attack"
+M.registry_release = "github:mason-org/mason-registry@2025-08-16-pink-gaze"
 
 return M
