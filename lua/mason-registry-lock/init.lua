4cef58dbebcf25b1094a3bc5eab32b12ff1149da
GitHub Action <action@github.com>
2025-08-18 00:47:02 +0000

    Update mason-registry lock to 2025-08-17-lucky-shirt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-17-sparse-rule"
+M.registry_release = "github:mason-org/mason-registry@2025-08-17-lucky-shirt"
 
 return M
