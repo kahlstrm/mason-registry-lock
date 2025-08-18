aad27affc021143cefa848670302f105c219a07b
GitHub Action <action@github.com>
2025-08-18 00:47:02 +0000

    Update mason-registry lock to 2025-08-17-sparse-rule

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-17-many-weasel"
+M.registry_release = "github:mason-org/mason-registry@2025-08-17-sparse-rule"
 
 return M
