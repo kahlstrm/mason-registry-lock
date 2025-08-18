4226c343b61ceb8fc8e39988b543f7d8228fc8b1
GitHub Action <action@github.com>
2025-08-18 00:47:02 +0000

    Update mason-registry lock to 2025-08-17-blonde-jewel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-17-faulty-circle"
+M.registry_release = "github:mason-org/mason-registry@2025-08-17-blonde-jewel"
 
 return M
