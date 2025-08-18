74ea6883032d4424111510ddc9ce46a44d4e1b9a
GitHub Action <action@github.com>
2025-08-18 00:47:02 +0000

    Update mason-registry lock to 2025-08-17-faulty-circle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-17-delightful-hammer"
+M.registry_release = "github:mason-org/mason-registry@2025-08-17-faulty-circle"
 
 return M
