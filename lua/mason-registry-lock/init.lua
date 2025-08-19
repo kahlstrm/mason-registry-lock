affc34cb1f503946b72413ac54889f7642889d90
GitHub Action <action@github.com>
2025-08-19 00:41:39 +0000

    Update mason-registry lock to 2025-08-18-dirty-shovel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-18-rotund-butane"
+M.registry_release = "github:mason-org/mason-registry@2025-08-18-dirty-shovel"
 
 return M
