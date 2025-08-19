eaec2c05464fb593ac3c6887c181b119a893ba0d
GitHub Action <action@github.com>
2025-08-19 00:41:39 +0000

    Update mason-registry lock to 2025-08-18-biting-game

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-18-dirty-shovel"
+M.registry_release = "github:mason-org/mason-registry@2025-08-18-biting-game"
 
 return M
