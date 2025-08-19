f01c86ad694a8b8f48d83134bbe79f43104fc225
GitHub Action <action@github.com>
2025-08-19 00:41:39 +0000

    Update mason-registry lock to 2025-08-18-cyan-tail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-18-elegant-fly"
+M.registry_release = "github:mason-org/mason-registry@2025-08-18-cyan-tail"
 
 return M
