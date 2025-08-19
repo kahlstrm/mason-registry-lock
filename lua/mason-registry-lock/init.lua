360245e0276daf997cb9c5f3fc732179493ca3ac
GitHub Action <action@github.com>
2025-08-19 00:41:39 +0000

    Update mason-registry lock to 2025-08-18-elegant-fly

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-18-grumpy-true"
+M.registry_release = "github:mason-org/mason-registry@2025-08-18-elegant-fly"
 
 return M
