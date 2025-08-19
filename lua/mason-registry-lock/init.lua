ab855e681ff1f3e5e848f22aa7a3efb4f40c1cd5
GitHub Action <action@github.com>
2025-08-19 00:41:39 +0000

    Update mason-registry lock to 2025-08-18-free-view

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-18-biting-game"
+M.registry_release = "github:mason-org/mason-registry@2025-08-18-free-view"
 
 return M
