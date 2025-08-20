b74e4b363d7aed717b4151874b62476e4ca3d18d
GitHub Action <action@github.com>
2025-08-20 00:39:58 +0000

    Update mason-registry lock to 2025-08-19-painless-peen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-19-south-mrigal"
+M.registry_release = "github:mason-org/mason-registry@2025-08-19-painless-peen"
 
 return M
