7088e78858939d0b7a8558a62ecd7f7d9b41c4c5
GitHub Action <action@github.com>
2025-08-21 00:38:55 +0000

    Update mason-registry lock to 2025-08-20-soviet-iris

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-20-muted-cymbal"
+M.registry_release = "github:mason-org/mason-registry@2025-08-20-soviet-iris"
 
 return M
