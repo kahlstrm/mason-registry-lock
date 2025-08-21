f8193c2065b8a328cdf93413c0d7492b75404e36
GitHub Action <action@github.com>
2025-08-21 00:38:55 +0000

    Update mason-registry lock to 2025-08-20-raw-balloon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-19-any-insect"
+M.registry_release = "github:mason-org/mason-registry@2025-08-20-raw-balloon"
 
 return M
