d7b5127dc787fd5c2a57ccf1a8198b7fe59ddd24
GitHub Action <action@github.com>
2025-08-21 00:38:55 +0000

    Update mason-registry lock to 2025-08-20-muted-cymbal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-20-ringed-emerald"
+M.registry_release = "github:mason-org/mason-registry@2025-08-20-muted-cymbal"
 
 return M
