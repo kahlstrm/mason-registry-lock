ff7c6878f86a42c29eef733b18acac6077c6dd4e
GitHub Action <action@github.com>
2025-08-21 00:38:55 +0000

    Update mason-registry lock to 2025-08-20-hoarse-icon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-20-grim-gunnel"
+M.registry_release = "github:mason-org/mason-registry@2025-08-20-hoarse-icon"
 
 return M
