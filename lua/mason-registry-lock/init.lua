d06238838174065c3cab5b8a2f695829814add83
GitHub Action <action@github.com>
2025-08-21 00:38:55 +0000

    Update mason-registry lock to 2025-08-20-flawed-ape

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-20-soviet-iris"
+M.registry_release = "github:mason-org/mason-registry@2025-08-20-flawed-ape"
 
 return M
