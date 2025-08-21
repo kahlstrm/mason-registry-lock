48c57418d5bec892f14caf2e0e890bc008785c29
GitHub Action <action@github.com>
2025-08-21 00:38:55 +0000

    Update mason-registry lock to 2025-08-20-alert-quartz

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-20-soviet-bulb"
+M.registry_release = "github:mason-org/mason-registry@2025-08-20-alert-quartz"
 
 return M
