e9e1749ef3628e539f4d2635888e972335cbc9da
GitHub Action <action@github.com>
2025-08-22 00:40:08 +0000

    Update mason-registry lock to 2025-08-21-young-kendo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-21-static-second"
+M.registry_release = "github:mason-org/mason-registry@2025-08-21-young-kendo"
 
 return M
