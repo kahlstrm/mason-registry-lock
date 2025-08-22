48dcaa410bf2aa757801815fcb666b1d4253fc7f
GitHub Action <action@github.com>
2025-08-22 00:40:08 +0000

    Update mason-registry lock to 2025-08-21-tacit-burst

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-21-thirsty-cent"
+M.registry_release = "github:mason-org/mason-registry@2025-08-21-tacit-burst"
 
 return M
