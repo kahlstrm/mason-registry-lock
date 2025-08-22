8a578684ee17023040990010f168dec84e43eae7
GitHub Action <action@github.com>
2025-08-22 00:40:08 +0000

    Update mason-registry lock to 2025-08-21-allied-pail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-21-charming-growth"
+M.registry_release = "github:mason-org/mason-registry@2025-08-21-allied-pail"
 
 return M
