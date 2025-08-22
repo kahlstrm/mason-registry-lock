5ef06e090c1652eaf112d363220e0e842cd11153
GitHub Action <action@github.com>
2025-08-22 00:40:08 +0000

    Update mason-registry lock to 2025-08-21-feline-jump

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-21-young-kendo"
+M.registry_release = "github:mason-org/mason-registry@2025-08-21-feline-jump"
 
 return M
