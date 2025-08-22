0c14502cae697adf4e56193153a9d8e095d03e38
GitHub Action <action@github.com>
2025-08-22 00:40:08 +0000

    Update mason-registry lock to 2025-08-21-simple-breath

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-21-drab-rate"
+M.registry_release = "github:mason-org/mason-registry@2025-08-21-simple-breath"
 
 return M
