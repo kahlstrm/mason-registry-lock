b97d5b713556efe6a6ada85e09c106dca3ffb14d
GitHub Action <action@github.com>
2025-08-23 00:38:40 +0000

    Update mason-registry lock to 2025-08-22-dutch-patch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-22-orange-pest"
+M.registry_release = "github:mason-org/mason-registry@2025-08-22-dutch-patch"
 
 return M
