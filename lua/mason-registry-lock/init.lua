162b127dd52ecc2ba20c5225b0207753c9a123b7
GitHub Action <action@github.com>
2025-08-23 00:38:40 +0000

    Update mason-registry lock to 2025-08-22-sound-middle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-22-wrong-beat"
+M.registry_release = "github:mason-org/mason-registry@2025-08-22-sound-middle"
 
 return M
