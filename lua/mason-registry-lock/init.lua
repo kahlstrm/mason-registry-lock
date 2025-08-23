2e9142ff9b5378a7af47923b66e31c8a5914398d
GitHub Action <action@github.com>
2025-08-23 00:38:40 +0000

    Update mason-registry lock to 2025-08-22-nearby-latex

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-22-poor-jitter"
+M.registry_release = "github:mason-org/mason-registry@2025-08-22-nearby-latex"
 
 return M
