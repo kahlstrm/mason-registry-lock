493d47d5a2196d10a3c1066be9a0890de1df7231
GitHub Action <action@github.com>
2025-08-23 00:38:40 +0000

    Update mason-registry lock to 2025-08-22-amused-hand

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-22-friendly-option"
+M.registry_release = "github:mason-org/mason-registry@2025-08-22-amused-hand"
 
 return M
