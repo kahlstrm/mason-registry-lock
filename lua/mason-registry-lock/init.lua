979d0a9dd73fb985533958a2a33c76204d8cb92a
GitHub Action <action@github.com>
2025-08-23 00:38:40 +0000

    Update mason-registry lock to 2025-08-22-friendly-option

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-22-sulky-path"
+M.registry_release = "github:mason-org/mason-registry@2025-08-22-friendly-option"
 
 return M
