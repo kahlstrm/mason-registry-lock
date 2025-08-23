e14c9c5fe1eaa2895bfa85966420f9618c23f919
GitHub Action <action@github.com>
2025-08-23 00:38:40 +0000

    Update mason-registry lock to 2025-08-22-jade-teeth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-21-tacit-burst"
+M.registry_release = "github:mason-org/mason-registry@2025-08-22-jade-teeth"
 
 return M
