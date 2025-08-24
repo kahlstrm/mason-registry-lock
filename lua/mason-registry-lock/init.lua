8d229d37c3ab6a480c6d738f5c1c8ec725818a99
GitHub Action <action@github.com>
2025-08-24 00:44:56 +0000

    Update mason-registry lock to 2025-08-23-latin-deity

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-23-purple-fall"
+M.registry_release = "github:mason-org/mason-registry@2025-08-23-latin-deity"
 
 return M
