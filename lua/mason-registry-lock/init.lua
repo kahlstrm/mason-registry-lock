8658c68e1b0b3c3e06b9111af4e519de8b58f6c2
GitHub Action <action@github.com>
2025-08-24 00:44:56 +0000

    Update mason-registry lock to 2025-08-23-purple-fall

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-22-flaky-sky"
+M.registry_release = "github:mason-org/mason-registry@2025-08-23-purple-fall"
 
 return M
