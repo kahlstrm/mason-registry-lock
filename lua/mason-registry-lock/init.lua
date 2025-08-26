5867a6fa8cf246b8a71be7c93b4c02bd7b548aa6
GitHub Action <action@github.com>
2025-08-26 00:40:27 +0000

    Update mason-registry lock to 2025-08-25-imaginary-zander

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-25-vivid-nickel"
+M.registry_release = "github:mason-org/mason-registry@2025-08-25-imaginary-zander"
 
 return M
