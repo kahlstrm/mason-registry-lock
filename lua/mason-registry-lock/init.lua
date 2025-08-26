440ca25d9029d475bf9805e6774edea7221bd15b
GitHub Action <action@github.com>
2025-08-26 00:40:27 +0000

    Update mason-registry lock to 2025-08-25-vivid-nickel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-24-placid-lounge"
+M.registry_release = "github:mason-org/mason-registry@2025-08-25-vivid-nickel"
 
 return M
