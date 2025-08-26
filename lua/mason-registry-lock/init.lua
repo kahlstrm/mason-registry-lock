a9f1a4ba6265c387ed2bbc8eacf8989382382578
GitHub Action <action@github.com>
2025-08-26 00:40:27 +0000

    Update mason-registry lock to 2025-08-25-joyful-beef

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-25-blank-pasta"
+M.registry_release = "github:mason-org/mason-registry@2025-08-25-joyful-beef"
 
 return M
