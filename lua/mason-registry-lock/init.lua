d5557e6843e16f6ba1d9c623bbcbd8aeed49ea01
GitHub Action <action@github.com>
2025-08-27 00:39:56 +0000

    Update mason-registry lock to 2025-08-26-stout-kiss

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-26-spotty-driver"
+M.registry_release = "github:mason-org/mason-registry@2025-08-26-stout-kiss"
 
 return M
