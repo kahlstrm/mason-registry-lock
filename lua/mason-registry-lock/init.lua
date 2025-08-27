89b8f755daa447fbb1a9b16f1aff0690508b8650
GitHub Action <action@github.com>
2025-08-27 00:39:57 +0000

    Update mason-registry lock to 2025-08-26-creamy-nine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-26-sugary-book"
+M.registry_release = "github:mason-org/mason-registry@2025-08-26-creamy-nine"
 
 return M
