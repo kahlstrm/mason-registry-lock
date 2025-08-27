46bb62a9b37d2dfb2db966eee0576ecc7eb926e3
GitHub Action <action@github.com>
2025-08-27 00:39:56 +0000

    Update mason-registry lock to 2025-08-26-drafty-sound

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-26-tidy-cedar"
+M.registry_release = "github:mason-org/mason-registry@2025-08-26-drafty-sound"
 
 return M
