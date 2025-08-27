75549def562a3c9ac978cb7e9e6b7c397bfa506a
GitHub Action <action@github.com>
2025-08-27 00:39:56 +0000

    Update mason-registry lock to 2025-08-26-tidy-cedar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-25-frail-condor"
+M.registry_release = "github:mason-org/mason-registry@2025-08-26-tidy-cedar"
 
 return M
