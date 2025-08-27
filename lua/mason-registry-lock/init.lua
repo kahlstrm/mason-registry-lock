31f6a2db97ac96050b0b8ef3cf1cfc9bb5d870b8
GitHub Action <action@github.com>
2025-08-27 00:39:56 +0000

    Update mason-registry lock to 2025-08-26-wise-tanker

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-26-stout-kiss"
+M.registry_release = "github:mason-org/mason-registry@2025-08-26-wise-tanker"
 
 return M
