d19671585e5beb7817484793a8e66fd05c6cda63
GitHub Action <action@github.com>
2025-08-28 00:39:07 +0000

    Update mason-registry lock to 2025-08-27-ancient-snake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-27-abject-folder"
+M.registry_release = "github:mason-org/mason-registry@2025-08-27-ancient-snake"
 
 return M
