46c724e8031a741bf3ce23dee052367a5a13b250
GitHub Action <action@github.com>
2025-08-28 00:39:07 +0000

    Update mason-registry lock to 2025-08-27-varied-object

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-27-tacky-cherry"
+M.registry_release = "github:mason-org/mason-registry@2025-08-27-varied-object"
 
 return M
