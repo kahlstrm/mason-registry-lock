c94c61330ca7dd4e0612834b310616227b0b112e
GitHub Action <action@github.com>
2025-08-30 00:37:08 +0000

    Update mason-registry lock to 2025-08-29-bubbly-botany

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-sturdy-marten"
+M.registry_release = "github:mason-org/mason-registry@2025-08-29-bubbly-botany"
 
 return M
