5ef012b0a333dae570c0507946c272474e6771e7
GitHub Action <action@github.com>
2025-08-30 00:37:08 +0000

    Update mason-registry lock to 2025-08-29-stiff-size

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-knotty-forest"
+M.registry_release = "github:mason-org/mason-registry@2025-08-29-stiff-size"
 
 return M
