7dcc5aa062fd4441e60994706a7ff11f329a6ed8
GitHub Action <action@github.com>
2025-08-30 00:37:08 +0000

    Update mason-registry lock to 2025-08-29-vacant-bangu

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-bubbly-botany"
+M.registry_release = "github:mason-org/mason-registry@2025-08-29-vacant-bangu"
 
 return M
