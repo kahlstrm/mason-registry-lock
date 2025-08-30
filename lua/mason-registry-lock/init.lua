df76884c617c454ef46bbed12bcfb2aa056511b8
GitHub Action <action@github.com>
2025-08-30 00:37:08 +0000

    Update mason-registry lock to 2025-08-29-bright-crib

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-medium-vase"
+M.registry_release = "github:mason-org/mason-registry@2025-08-29-bright-crib"
 
 return M
