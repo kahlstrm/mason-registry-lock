bceba03c5bfe827ea796f740dc9a07d2772ff524
GitHub Action <action@github.com>
2025-08-30 00:37:08 +0000

    Update mason-registry lock to 2025-08-29-knotty-forest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-smart-possum"
+M.registry_release = "github:mason-org/mason-registry@2025-08-29-knotty-forest"
 
 return M
