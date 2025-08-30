ef6af96805854a17990f6d72d5840032b0cbd580
GitHub Action <action@github.com>
2025-08-30 00:37:08 +0000

    Update mason-registry lock to 2025-08-29-stark-monkey

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-webbed-unicorn"
+M.registry_release = "github:mason-org/mason-registry@2025-08-29-stark-monkey"
 
 return M
