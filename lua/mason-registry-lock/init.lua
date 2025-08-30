7366373eff0625d3761359dec1cef0a88f331da8
GitHub Action <action@github.com>
2025-08-30 00:37:08 +0000

    Update mason-registry lock to 2025-08-29-recent-grass

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-daily-turkey"
+M.registry_release = "github:mason-org/mason-registry@2025-08-29-recent-grass"
 
 return M
