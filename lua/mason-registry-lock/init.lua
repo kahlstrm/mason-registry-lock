e9a0a370e8e1aa7f84b23908513da4ab07d16adb
GitHub Action <action@github.com>
2025-08-31 00:42:54 +0000

    Update mason-registry lock to 2025-08-30-rival-calf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-30-sane-radar"
+M.registry_release = "github:mason-org/mason-registry@2025-08-30-rival-calf"
 
 return M
