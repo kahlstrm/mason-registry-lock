f24cb44df0138ab29eca7a61ffba30328810bc9a
GitHub Action <action@github.com>
2025-08-31 00:42:53 +0000

    Update mason-registry lock to 2025-08-30-perfect-guilty

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-29-expert-lynx"
+M.registry_release = "github:mason-org/mason-registry@2025-08-30-perfect-guilty"
 
 return M
