02045dfb564f9839de22ada870bc21ee26b1caa6
GitHub Action <action@github.com>
2025-08-31 00:42:54 +0000

    Update mason-registry lock to 2025-08-30-sane-radar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-30-grubby-newt"
+M.registry_release = "github:mason-org/mason-registry@2025-08-30-sane-radar"
 
 return M
