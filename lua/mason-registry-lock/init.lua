70037843225a1b3f16f086352b1d9da0e006a33a
GitHub Action <action@github.com>
2025-09-01 00:49:44 +0000

    Update mason-registry lock to 2025-08-31-livid-butter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-31-narrow-danio"
+M.registry_release = "github:mason-org/mason-registry@2025-08-31-livid-butter"
 
 return M
