c83e413644259c2a79f0e314b520c54afd2720d2
GitHub Action <action@github.com>
2025-09-01 00:49:44 +0000

    Update mason-registry lock to 2025-08-31-narrow-danio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-08-30-irish-peru"
+M.registry_release = "github:mason-org/mason-registry@2025-08-31-narrow-danio"
 
 return M
