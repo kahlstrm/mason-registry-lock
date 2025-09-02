1a781a6bdf4cb7201fd974a4b2476f6471a11050
GitHub Action <action@github.com>
2025-09-02 00:39:41 +0000

    Update mason-registry lock to 2025-09-01-grateful-rock

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-01-clever-beetle"
+M.registry_release = "github:mason-org/mason-registry@2025-09-01-grateful-rock"
 
 return M
