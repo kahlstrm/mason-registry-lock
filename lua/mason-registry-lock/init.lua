042ca8f7ea7ca12d77ed1e92538b6fbb3ae9896d
GitHub Action <action@github.com>
2025-09-03 00:37:07 +0000

    Update mason-registry lock to 2025-09-02-paltry-roughy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-02-spooky-swan"
+M.registry_release = "github:mason-org/mason-registry@2025-09-02-paltry-roughy"
 
 return M
