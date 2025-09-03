3f470fadeb2e9291a3afd25cd136434466478f45
GitHub Action <action@github.com>
2025-09-03 00:37:07 +0000

    Update mason-registry lock to 2025-09-02-limp-shrine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-02-young-dance"
+M.registry_release = "github:mason-org/mason-registry@2025-09-02-limp-shrine"
 
 return M
