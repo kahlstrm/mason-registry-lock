881a3ef1a4583f1d5eec37130b426f078eef89b5
GitHub Action <action@github.com>
2025-09-03 00:37:07 +0000

    Update mason-registry lock to 2025-09-02-spooky-swan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-02-jovial-mine"
+M.registry_release = "github:mason-org/mason-registry@2025-09-02-spooky-swan"
 
 return M
