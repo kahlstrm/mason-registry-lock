1da1a0724e504415869fa14ab7413e8a70cce55f
GitHub Action <action@github.com>
2025-09-03 00:37:07 +0000

    Update mason-registry lock to 2025-09-02-gritty-email

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-02-paltry-roughy"
+M.registry_release = "github:mason-org/mason-registry@2025-09-02-gritty-email"
 
 return M
