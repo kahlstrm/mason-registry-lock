f708a6f4dea5690630a1eb2612bf5aaf43126d25
GitHub Action <action@github.com>
2025-09-04 00:37:39 +0000

    Update mason-registry lock to 2025-09-03-caring-ankle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-03-aware-dace"
+M.registry_release = "github:mason-org/mason-registry@2025-09-03-caring-ankle"
 
 return M
