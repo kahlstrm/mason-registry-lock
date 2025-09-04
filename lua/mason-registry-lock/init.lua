0ba88d9c75238187639d058883d9e6df2b1b667b
GitHub Action <action@github.com>
2025-09-04 00:37:39 +0000

    Update mason-registry lock to 2025-09-03-barren-grain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-03-left-ground"
+M.registry_release = "github:mason-org/mason-registry@2025-09-03-barren-grain"
 
 return M
