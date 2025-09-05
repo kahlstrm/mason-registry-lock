55fb404b3522a64239cdca768228ea7029530cdd
GitHub Action <action@github.com>
2025-09-05 00:38:39 +0000

    Update mason-registry lock to 2025-09-04-dopey-dahlia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-04-vexed-jute"
+M.registry_release = "github:mason-org/mason-registry@2025-09-04-dopey-dahlia"
 
 return M
