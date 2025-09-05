da0ff5c94311c9194a1dec5c89fa7051801a0924
GitHub Action <action@github.com>
2025-09-05 00:38:39 +0000

    Update mason-registry lock to 2025-09-04-rare-switch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-04-thin-rhino"
+M.registry_release = "github:mason-org/mason-registry@2025-09-04-rare-switch"
 
 return M
