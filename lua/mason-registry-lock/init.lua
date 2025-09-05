a08fe62dd3ddbb9ff0637f23dc76f6f69efc9d15
GitHub Action <action@github.com>
2025-09-05 00:38:39 +0000

    Update mason-registry lock to 2025-09-04-cheeky-beauty

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-04-frilly-gender"
+M.registry_release = "github:mason-org/mason-registry@2025-09-04-cheeky-beauty"
 
 return M
