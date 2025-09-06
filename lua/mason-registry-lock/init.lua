04aa7fb53883c40bbe6e96f5cfad96267dfb6507
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-rebel-lynx

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-severe-veil"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-rebel-lynx"
 
 return M
