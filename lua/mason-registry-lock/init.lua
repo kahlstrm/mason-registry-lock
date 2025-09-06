a23ce1964352a489c21a04668d1a9ee35bf798be
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-solar-tin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-thick-engine"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-solar-tin"
 
 return M
