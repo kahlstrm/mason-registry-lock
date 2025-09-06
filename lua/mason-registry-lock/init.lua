3d2e7e6fdcb564a10dac4b942711dbbca44a2305
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-silver-yard

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-thorny-edge"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-silver-yard"
 
 return M
