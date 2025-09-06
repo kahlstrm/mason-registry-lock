a7c3291b88323238040954e32ef278d867ab934f
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-prior-pilot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-strong-bubble"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-prior-pilot"
 
 return M
