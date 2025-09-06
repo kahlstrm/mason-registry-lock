564c9fc6d22092c19cb97a8b438f584374184ccf
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-untrue-sushi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-frosty-parcel"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-untrue-sushi"
 
 return M
