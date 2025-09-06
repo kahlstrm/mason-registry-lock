c3316a7c947c3158fd91867c9d1f1854eedd1c15
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-elated-coach

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-silver-yard"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-elated-coach"
 
 return M
