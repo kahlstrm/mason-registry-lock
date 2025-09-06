00ff2100161353cd6dc8f4ebea687001a38c2b2a
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-severe-veil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-elated-coach"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-severe-veil"
 
 return M
