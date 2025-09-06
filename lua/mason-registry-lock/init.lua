80f601f9ea5a530cc7b3fc37a50449d39df509f9
GitHub Action <action@github.com>
2025-09-06 00:36:55 +0000

    Update mason-registry lock to 2025-09-05-frosty-parcel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-rear-route"
+M.registry_release = "github:mason-org/mason-registry@2025-09-05-frosty-parcel"
 
 return M
