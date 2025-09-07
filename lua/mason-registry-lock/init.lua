04ad0c71830c2f81400424356859f3f079d55397
GitHub Action <action@github.com>
2025-09-07 00:42:27 +0000

    Update mason-registry lock to 2025-09-06-tired-nail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-05-many-nepal"
+M.registry_release = "github:mason-org/mason-registry@2025-09-06-tired-nail"
 
 return M
