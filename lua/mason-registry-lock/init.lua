a1b312b525be2d5702b86ff7791c4fbdcb7c605b
GitHub Action <action@github.com>
2025-09-08 00:41:16 +0000

    Update mason-registry lock to 2025-09-07-steel-click

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-07-swiss-oil"
+M.registry_release = "github:mason-org/mason-registry@2025-09-07-steel-click"
 
 return M
