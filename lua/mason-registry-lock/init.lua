ba191ae569e017d70e623a8b07ad40dd58d51766
GitHub Action <action@github.com>
2025-09-10 00:38:16 +0000

    Update mason-registry lock to 2025-09-09-musty-beech

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-09-near-nepal"
+M.registry_release = "github:mason-org/mason-registry@2025-09-09-musty-beech"
 
 return M
