e52fac4443b9d9575498f968df9e688fa4d8e806
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-arctic-white

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-wooden-cough"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-arctic-white"
 
 return M
