45c1fb8b9736cdc1e454f3e44723b9bf9d0e540e
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-creamy-floor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-jazzy-tail"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-creamy-floor"
 
 return M
