bf2932c1ba2fcf4f89c1b674300b9920c3bf2a9e
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-native-xenon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-scared-crush"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-native-xenon"
 
 return M
