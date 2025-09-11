b4e8bafe6e05608220380a1db74a50ccc1dd1c1d
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-brainy-stool

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-thin-yogurt"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-brainy-stool"
 
 return M
