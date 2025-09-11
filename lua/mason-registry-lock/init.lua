63bb7da9decf4fbe52a79130a100da93d2de733b
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-breezy-sight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-plucky-medium"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-breezy-sight"
 
 return M
