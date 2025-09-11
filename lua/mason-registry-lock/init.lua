02c2c0b567da4b41d1d3d866172b29ab17b103b5
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-rustic-worm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-spry-snoek"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-rustic-worm"
 
 return M
