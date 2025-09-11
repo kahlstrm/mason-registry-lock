75e5af0c5ebbc040a2b631636212454200f6920d
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-junior-hakea

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-tasty-stone"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-junior-hakea"
 
 return M
