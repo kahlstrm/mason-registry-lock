490285ee7ac96f7a51c1af8e11c492cc4f51262a
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-noted-pen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-modest-fowl"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-noted-pen"
 
 return M
