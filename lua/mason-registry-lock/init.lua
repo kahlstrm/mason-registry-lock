6f37bf4e8e5b039fa16a02e0df5d21536bd00f67
GitHub Action <action@github.com>
2025-09-11 00:38:49 +0000

    Update mason-registry lock to 2025-09-10-jazzy-tail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-10-brainy-stool"
+M.registry_release = "github:mason-org/mason-registry@2025-09-10-jazzy-tail"
 
 return M
