b35de8374e39e28b1151aa1615f2cdc9e83fb4a9
GitHub Action <action@github.com>
2025-09-12 00:37:43 +0000

    Update mason-registry lock to 2025-09-11-smug-pocket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-11-smelly-race"
+M.registry_release = "github:mason-org/mason-registry@2025-09-11-smug-pocket"
 
 return M
