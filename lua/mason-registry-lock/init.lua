53dd8b1e73311e301c9bbc7ea729eb4506c50f10
GitHub Action <action@github.com>
2025-09-12 00:37:43 +0000

    Update mason-registry lock to 2025-09-11-prime-game

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-11-brash-random"
+M.registry_release = "github:mason-org/mason-registry@2025-09-11-prime-game"
 
 return M
