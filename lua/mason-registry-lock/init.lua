bb3a6013a65c3debd0431be42cb1a8500b88f530
GitHub Action <action@github.com>
2025-09-12 00:37:43 +0000

    Update mason-registry lock to 2025-09-11-medium-vein

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-11-acidic-loach"
+M.registry_release = "github:mason-org/mason-registry@2025-09-11-medium-vein"
 
 return M
