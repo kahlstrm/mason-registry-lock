dbcb9d5690bdf9ce4c210c588ae5f84b625aaf69
GitHub Action <action@github.com>
2025-09-12 00:37:43 +0000

    Update mason-registry lock to 2025-09-11-likely-mother

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-11-chilly-dust"
+M.registry_release = "github:mason-org/mason-registry@2025-09-11-likely-mother"
 
 return M
