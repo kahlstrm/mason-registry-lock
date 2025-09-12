b160865e44b463c4cd9539faeedba48901396501
GitHub Action <action@github.com>
2025-09-12 00:37:43 +0000

    Update mason-registry lock to 2025-09-11-feline-drop

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-11-drunk-accord"
+M.registry_release = "github:mason-org/mason-registry@2025-09-11-feline-drop"
 
 return M
