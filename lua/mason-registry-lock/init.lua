bafc54e1be8872549f35dcd11283390e36e05794
GitHub Action <action@github.com>
2025-09-14 00:41:25 +0000

    Update mason-registry lock to 2025-09-13-square-sight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-13-antiquet-tip"
+M.registry_release = "github:mason-org/mason-registry@2025-09-13-square-sight"
 
 return M
