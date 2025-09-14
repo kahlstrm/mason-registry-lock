bf16622ccd5c5675cef0c09b1d6f696175a645f8
GitHub Action <action@github.com>
2025-09-14 00:41:25 +0000

    Update mason-registry lock to 2025-09-13-causal-city

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-13-nice-engine"
+M.registry_release = "github:mason-org/mason-registry@2025-09-13-causal-city"
 
 return M
