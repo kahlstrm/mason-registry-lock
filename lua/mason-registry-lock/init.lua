688542ed847e10d5c3a55d2983b15316ec88f9c6
GitHub Action <action@github.com>
2025-09-14 00:41:25 +0000

    Update mason-registry lock to 2025-09-13-low-kick

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-13-tragic-recess"
+M.registry_release = "github:mason-org/mason-registry@2025-09-13-low-kick"
 
 return M
