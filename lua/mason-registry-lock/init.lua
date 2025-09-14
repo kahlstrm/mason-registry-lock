05a8c8f8047503d6442edba585c2362cc41c1870
GitHub Action <action@github.com>
2025-09-14 00:41:25 +0000

    Update mason-registry lock to 2025-09-13-vague-bubble

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-13-causal-city"
+M.registry_release = "github:mason-org/mason-registry@2025-09-13-vague-bubble"
 
 return M
