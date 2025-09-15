6a0e10b3cd0a911d0029bb22b9b3f807cc1c1ace
GitHub Action <action@github.com>
2025-09-15 00:41:54 +0000

    Update mason-registry lock to 2025-09-14-tiny-tyvek

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-14-swift-cafe"
+M.registry_release = "github:mason-org/mason-registry@2025-09-14-tiny-tyvek"
 
 return M
