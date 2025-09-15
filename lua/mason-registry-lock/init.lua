8eaa64a9f225d82b288aadbe6bf80e0d5091e1d7
GitHub Action <action@github.com>
2025-09-15 00:41:54 +0000

    Update mason-registry lock to 2025-09-14-jumbo-agency

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-14-female-select"
+M.registry_release = "github:mason-org/mason-registry@2025-09-14-jumbo-agency"
 
 return M
