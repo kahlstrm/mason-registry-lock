40027801b4022c958f3e4089eb11251a19f39ca7
GitHub Action <action@github.com>
2025-09-16 00:37:41 +0000

    Update mason-registry lock to 2025-09-15-blank-lotus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-15-solid-pleco"
+M.registry_release = "github:mason-org/mason-registry@2025-09-15-blank-lotus"
 
 return M
