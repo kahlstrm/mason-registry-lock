e719316d2c31355023684503025e8d9442cf0d39
GitHub Action <action@github.com>
2025-09-17 00:37:57 +0000

    Update mason-registry lock to 2025-09-16-dynamic-apple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-16-cruel-burst"
+M.registry_release = "github:mason-org/mason-registry@2025-09-16-dynamic-apple"
 
 return M
