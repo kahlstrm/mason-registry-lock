96788fb783397aec3e3bfefdac86af15736d56fa
GitHub Action <action@github.com>
2025-09-17 00:37:57 +0000

    Update mason-registry lock to 2025-09-16-cruel-burst

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-16-swift-record"
+M.registry_release = "github:mason-org/mason-registry@2025-09-16-cruel-burst"
 
 return M
