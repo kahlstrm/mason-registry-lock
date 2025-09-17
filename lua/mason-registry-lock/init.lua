cd27e9df883ce117f284c23aa839f0b9c0d0a4cb
GitHub Action <action@github.com>
2025-09-17 00:37:57 +0000

    Update mason-registry lock to 2025-09-16-vain-twist

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-16-bent-angel"
+M.registry_release = "github:mason-org/mason-registry@2025-09-16-vain-twist"
 
 return M
