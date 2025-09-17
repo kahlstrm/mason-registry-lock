e72b3447656d2e81761d86ea4f59b7b90246c789
GitHub Action <action@github.com>
2025-09-17 00:37:57 +0000

    Update mason-registry lock to 2025-09-16-wrong-conga

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-16-unfair-harp"
+M.registry_release = "github:mason-org/mason-registry@2025-09-16-wrong-conga"
 
 return M
