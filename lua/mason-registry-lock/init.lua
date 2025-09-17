27f29fa71d8f9a9d9ad8c1640bc1d2276e1cd19d
GitHub Action <action@github.com>
2025-09-17 00:37:57 +0000

    Update mason-registry lock to 2025-09-16-rare-glider

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-16-adored-mask"
+M.registry_release = "github:mason-org/mason-registry@2025-09-16-rare-glider"
 
 return M
