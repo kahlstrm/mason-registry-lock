83ddce1f000a9494e025f8c23b85aac3db6fbc24
GitHub Action <action@github.com>
2025-09-18 00:37:17 +0000

    Update mason-registry lock to 2025-09-17-spiffy-data

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-17-head-nature"
+M.registry_release = "github:mason-org/mason-registry@2025-09-17-spiffy-data"
 
 return M
