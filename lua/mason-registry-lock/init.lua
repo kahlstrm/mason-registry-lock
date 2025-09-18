cdd9dff9cfa6a48ef100949f0287883a18d2cd40
GitHub Action <action@github.com>
2025-09-18 00:37:17 +0000

    Update mason-registry lock to 2025-09-17-frigid-straw

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-17-roman-badger"
+M.registry_release = "github:mason-org/mason-registry@2025-09-17-frigid-straw"
 
 return M
