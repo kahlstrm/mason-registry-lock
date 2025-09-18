9e8375cfe77f658eb031b00454f3c38639f5dd57
GitHub Action <action@github.com>
2025-09-18 00:37:17 +0000

    Update mason-registry lock to 2025-09-17-shared-rap

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-17-frigid-straw"
+M.registry_release = "github:mason-org/mason-registry@2025-09-17-shared-rap"
 
 return M
