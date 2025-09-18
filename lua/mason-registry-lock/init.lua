072bde95508222e3b5bcb71c464c9faa0a276f7c
GitHub Action <action@github.com>
2025-09-18 00:37:17 +0000

    Update mason-registry lock to 2025-09-17-lewd-coat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-17-spiffy-data"
+M.registry_release = "github:mason-org/mason-registry@2025-09-17-lewd-coat"
 
 return M
