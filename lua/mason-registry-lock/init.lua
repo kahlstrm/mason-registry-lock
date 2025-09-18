d281e585c8f7ee00e93a951d84fbbcf3208f221f
GitHub Action <action@github.com>
2025-09-18 00:37:16 +0000

    Update mason-registry lock to 2025-09-17-head-nature

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-17-dim-drink"
+M.registry_release = "github:mason-org/mason-registry@2025-09-17-head-nature"
 
 return M
