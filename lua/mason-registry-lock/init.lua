53ee4f16b277826ad72cfd10888168b39c76f423
GitHub Action <action@github.com>
2025-09-19 00:39:05 +0000

    Update mason-registry lock to 2025-09-18-hollow-growth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-18-vivid-hash"
+M.registry_release = "github:mason-org/mason-registry@2025-09-18-hollow-growth"
 
 return M
