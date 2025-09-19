9a7d344436dcfc0d68ce64589d76f70fa8eb906c
GitHub Action <action@github.com>
2025-09-19 00:39:05 +0000

    Update mason-registry lock to 2025-09-18-welsh-flamingo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-18-fluffy-mint"
+M.registry_release = "github:mason-org/mason-registry@2025-09-18-welsh-flamingo"
 
 return M
