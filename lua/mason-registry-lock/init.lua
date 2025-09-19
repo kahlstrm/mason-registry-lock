503226218a955aa400e5a19be636c2dd82466938
GitHub Action <action@github.com>
2025-09-19 00:39:05 +0000

    Update mason-registry lock to 2025-09-18-closed-pacus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-18-hollow-growth"
+M.registry_release = "github:mason-org/mason-registry@2025-09-18-closed-pacus"
 
 return M
