8a17880ba5e6df85921294dc65e0a3f85f585db3
GitHub Action <action@github.com>
2025-09-19 00:39:05 +0000

    Update mason-registry lock to 2025-09-18-smooth-start

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-18-verbal-neck"
+M.registry_release = "github:mason-org/mason-registry@2025-09-18-smooth-start"
 
 return M
