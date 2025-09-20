fd9ff9c52eaaf1d1187f7b6e9f914dfdc1a1a89d
GitHub Action <action@github.com>
2025-09-20 00:36:49 +0000

    Update mason-registry lock to 2025-09-19-greasy-care

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-19-lumpy-timer"
+M.registry_release = "github:mason-org/mason-registry@2025-09-19-greasy-care"
 
 return M
