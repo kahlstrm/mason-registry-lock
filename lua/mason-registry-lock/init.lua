7629d1b34f50787a8c2057786db6ecf47b46616f
GitHub Action <action@github.com>
2025-09-20 00:36:49 +0000

    Update mason-registry lock to 2025-09-19-lumpy-timer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-19-wobbly-reason"
+M.registry_release = "github:mason-org/mason-registry@2025-09-19-lumpy-timer"
 
 return M
