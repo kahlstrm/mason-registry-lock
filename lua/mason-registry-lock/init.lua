b78fa4c2f46e46545e889cd673cc55f69932bc31
GitHub Action <action@github.com>
2025-09-20 00:36:49 +0000

    Update mason-registry lock to 2025-09-19-retail-elder

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-19-greasy-care"
+M.registry_release = "github:mason-org/mason-registry@2025-09-19-retail-elder"
 
 return M
