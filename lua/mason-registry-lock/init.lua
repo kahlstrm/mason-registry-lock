de447eff0646692a43a3ff6f722e979bb2d0ff0c
GitHub Action <action@github.com>
2025-09-20 00:36:50 +0000

    Update mason-registry lock to 2025-09-19-untrue-day

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-19-thorny-spade"
+M.registry_release = "github:mason-org/mason-registry@2025-09-19-untrue-day"
 
 return M
