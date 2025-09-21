fe34c76b2200c918797a68129142d9b7ab437973
GitHub Action <action@github.com>
2025-09-21 00:42:43 +0000

    Update mason-registry lock to 2025-09-20-bronze-darter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-20-steel-option"
+M.registry_release = "github:mason-org/mason-registry@2025-09-20-bronze-darter"
 
 return M
