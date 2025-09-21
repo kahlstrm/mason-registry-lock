5178f7a8e5c0e8f45ad7bed00d519e84d9458841
GitHub Action <action@github.com>
2025-09-21 00:42:43 +0000

    Update mason-registry lock to 2025-09-20-feeble-alpaca

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-20-fellow-spoon"
+M.registry_release = "github:mason-org/mason-registry@2025-09-20-feeble-alpaca"
 
 return M
