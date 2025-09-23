18e7663143943bd9a97788f20c5e3058a3b53220
GitHub Action <action@github.com>
2025-09-23 00:38:21 +0000

    Update mason-registry lock to 2025-09-22-random-news

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-22-stout-litter"
+M.registry_release = "github:mason-org/mason-registry@2025-09-22-random-news"
 
 return M
