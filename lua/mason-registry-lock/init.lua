4dd521028d9f2f6b2213c8fa05983d14d4156a9b
GitHub Action <action@github.com>
2025-09-24 00:39:10 +0000

    Update mason-registry lock to 2025-09-23-abject-week

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-23-needy-marble"
+M.registry_release = "github:mason-org/mason-registry@2025-09-23-abject-week"
 
 return M
