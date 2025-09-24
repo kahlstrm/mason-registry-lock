6f63dabd33fad4b0223a2fcb7552eed02e5118b0
GitHub Action <action@github.com>
2025-09-24 00:39:10 +0000

    Update mason-registry lock to 2025-09-23-greasy-taste

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-23-alike-window"
+M.registry_release = "github:mason-org/mason-registry@2025-09-23-greasy-taste"
 
 return M
