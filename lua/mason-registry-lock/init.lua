9e6c5e1e1d95df6e39579d16b18906c2f0939249
GitHub Action <action@github.com>
2025-09-24 00:39:10 +0000

    Update mason-registry lock to 2025-09-23-unused-geese

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-23-level-shorts"
+M.registry_release = "github:mason-org/mason-registry@2025-09-23-unused-geese"
 
 return M
