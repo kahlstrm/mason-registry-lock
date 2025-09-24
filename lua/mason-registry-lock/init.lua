1116bc3f671e15ab11b1dda7201807bce0413ef4
GitHub Action <action@github.com>
2025-09-24 00:39:10 +0000

    Update mason-registry lock to 2025-09-23-level-shorts

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-23-gamy-coast"
+M.registry_release = "github:mason-org/mason-registry@2025-09-23-level-shorts"
 
 return M
