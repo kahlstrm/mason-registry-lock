e61bc7ca58009a7ec9e71b0e64de535b8012aa95
GitHub Action <action@github.com>
2025-09-24 00:39:10 +0000

    Update mason-registry lock to 2025-09-23-alike-window

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-23-abject-week"
+M.registry_release = "github:mason-org/mason-registry@2025-09-23-alike-window"
 
 return M
