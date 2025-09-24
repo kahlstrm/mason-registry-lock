049c918c0bfc7505b8ba3116fc647edce25ed66c
GitHub Action <action@github.com>
2025-09-24 00:39:10 +0000

    Update mason-registry lock to 2025-09-23-same-value

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-23-abaft-oboe"
+M.registry_release = "github:mason-org/mason-registry@2025-09-23-same-value"
 
 return M
