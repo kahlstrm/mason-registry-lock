41a08e52e8f50a488c9f9cba152c7115e128344d
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-24-slender-sudan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-24-kind-quilt"
+M.registry_release = "github:mason-org/mason-registry@2025-09-24-slender-sudan"
 
 return M
