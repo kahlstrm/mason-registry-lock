acfac2bddddeb809eb5125dc44e050c335b516a7
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-24-total-crab

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-24-tense-chalk"
+M.registry_release = "github:mason-org/mason-registry@2025-09-24-total-crab"
 
 return M
