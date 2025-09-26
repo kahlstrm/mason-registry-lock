351538a499bcd9aae03d81be95f82ac1f3de348a
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-24-canine-grade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-23-same-value"
+M.registry_release = "github:mason-org/mason-registry@2025-09-24-canine-grade"
 
 return M
