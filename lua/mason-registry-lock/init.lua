53085fb20664e2363cc770016c7b6248eefe259e
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-25-imaginary-deer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-25-sparse-rust"
+M.registry_release = "github:mason-org/mason-registry@2025-09-25-imaginary-deer"
 
 return M
