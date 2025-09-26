cf621337e2b9f7da4a4e666d0ed6d6dfe1cbb371
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-25-sparse-rust

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-25-above-act"
+M.registry_release = "github:mason-org/mason-registry@2025-09-25-sparse-rust"
 
 return M
