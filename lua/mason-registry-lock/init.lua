2795a5a6eb982cf27374c327b77b8cb8429847a9
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-24-scarce-lier

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-24-glad-laptop"
+M.registry_release = "github:mason-org/mason-registry@2025-09-24-scarce-lier"
 
 return M
