2700ef8dd9d37ba05a1f861cce083b8bfcef8371
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-24-yearly-pocket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-24-worst-maraca"
+M.registry_release = "github:mason-org/mason-registry@2025-09-24-yearly-pocket"
 
 return M
