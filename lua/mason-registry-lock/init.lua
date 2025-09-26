13892a50f08e92f2c459b88183bd8d21be4ed641
GitHub Action <action@github.com>
2025-09-26 00:38:24 +0000

    Update mason-registry lock to 2025-09-25-queasy-chain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-25-tricky-automobile"
+M.registry_release = "github:mason-org/mason-registry@2025-09-25-queasy-chain"
 
 return M
