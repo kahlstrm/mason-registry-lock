701f22d396e8411b896b7f08cefd3342802b1b8f
GitHub Action <action@github.com>
2025-09-27 00:37:13 +0000

    Update mason-registry lock to 2025-09-26-abrupt-nickel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-26-six-male"
+M.registry_release = "github:mason-org/mason-registry@2025-09-26-abrupt-nickel"
 
 return M
