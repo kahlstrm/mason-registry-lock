c4c2a7dd411dd5a1bccb87f4b23915045d7ac6d1
GitHub Action <action@github.com>
2025-09-27 00:37:13 +0000

    Update mason-registry lock to 2025-09-26-upset-ear

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-26-cleaver-energy"
+M.registry_release = "github:mason-org/mason-registry@2025-09-26-upset-ear"
 
 return M
