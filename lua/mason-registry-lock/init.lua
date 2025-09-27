20b202e5645fe710bf533ca1ba5902080ceefb39
GitHub Action <action@github.com>
2025-09-27 00:37:12 +0000

    Update mason-registry lock to 2025-09-26-mass-spread

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-25-queasy-chain"
+M.registry_release = "github:mason-org/mason-registry@2025-09-26-mass-spread"
 
 return M
