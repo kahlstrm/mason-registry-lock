f2b371f63accf58c122bab9e3e8a2e7069799421
GitHub Action <action@github.com>
2025-09-27 00:37:13 +0000

    Update mason-registry lock to 2025-09-26-tangy-prairie

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-26-bare-arena"
+M.registry_release = "github:mason-org/mason-registry@2025-09-26-tangy-prairie"
 
 return M
