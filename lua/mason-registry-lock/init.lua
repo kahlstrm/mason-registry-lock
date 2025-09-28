878771cab36e8b340c3830b63f19d3a2378421a2
GitHub Action <action@github.com>
2025-09-28 00:43:19 +0000

    Update mason-registry lock to 2025-09-27-pink-suede

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-27-tawdry-card"
+M.registry_release = "github:mason-org/mason-registry@2025-09-27-pink-suede"
 
 return M
