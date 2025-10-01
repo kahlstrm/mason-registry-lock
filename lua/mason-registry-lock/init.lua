dccd7240280a5720313478a39e04200c3654495b
GitHub Action <action@github.com>
2025-10-01 00:44:28 +0000

    Update mason-registry lock to 2025-09-30-hard-stone

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-30-nasty-gong"
+M.registry_release = "github:mason-org/mason-registry@2025-09-30-hard-stone"
 
 return M
