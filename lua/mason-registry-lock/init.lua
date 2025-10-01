23ff0bec892b48d6ab6d2d344cc9869b2cf6088d
GitHub Action <action@github.com>
2025-10-01 00:44:28 +0000

    Update mason-registry lock to 2025-09-30-wordy-moon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-30-peaceful-comet"
+M.registry_release = "github:mason-org/mason-registry@2025-09-30-wordy-moon"
 
 return M
