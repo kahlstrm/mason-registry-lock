ab726a9ca8cbe9020b423449db6f56911faf2f96
GitHub Action <action@github.com>
2025-10-01 00:44:28 +0000

    Update mason-registry lock to 2025-09-30-quiet-epoxy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-30-big-cover"
+M.registry_release = "github:mason-org/mason-registry@2025-09-30-quiet-epoxy"
 
 return M
