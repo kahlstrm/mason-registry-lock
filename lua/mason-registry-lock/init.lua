75f1dc0d4c7d0c591f0610b3fcaff95b6f483623
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-stout-congo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-obese-llama"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-stout-congo"
 
 return M
