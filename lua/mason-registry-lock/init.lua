ee77e0424fad6917d58e6866003d5b208b6f18fd
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-faded-darter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-fragile-bull"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-faded-darter"
 
 return M
