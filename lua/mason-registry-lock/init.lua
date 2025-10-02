2787369b8e346ea6efbfd2a351e8e0fbab1b1609
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-baggy-cornet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-stout-congo"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-baggy-cornet"
 
 return M
