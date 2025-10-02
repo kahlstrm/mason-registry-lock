1f3f4fe740353c9e53ca86e2e514a752c61bc35e
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-slender-ruby

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-09-30-polish-robin"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-slender-ruby"
 
 return M
