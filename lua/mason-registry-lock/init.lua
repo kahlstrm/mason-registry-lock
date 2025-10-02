641e204142ce74daecffc84276664c1a915066b1
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-fit-sleep

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-slender-ruby"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-fit-sleep"
 
 return M
