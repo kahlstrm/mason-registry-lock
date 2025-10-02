e4d43caf15455450363dc7b74c5d98f0bbd183c8
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-alike-orange

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-fit-sleep"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-alike-orange"
 
 return M
