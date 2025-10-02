6b54870e179613606dbc39e3298219ff3c76330d
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-steady-needle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-smart-ash"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-steady-needle"
 
 return M
