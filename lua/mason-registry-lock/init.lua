f0f024746ba86c18936a969a6c8756b8a1b7d554
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-fragile-bull

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-milky-toilet"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-fragile-bull"
 
 return M
