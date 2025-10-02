890deac337ca651e7f1b9bf70500e165ed803fc9
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-trashy-trade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-baggy-cornet"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-trashy-trade"
 
 return M
