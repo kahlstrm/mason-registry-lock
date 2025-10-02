30d02818fe6f6eac524f476ac1f5215d73e09976
GitHub Action <action@github.com>
2025-10-02 00:37:31 +0000

    Update mason-registry lock to 2025-10-01-used-head

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-faded-darter"
+M.registry_release = "github:mason-org/mason-registry@2025-10-01-used-head"
 
 return M
