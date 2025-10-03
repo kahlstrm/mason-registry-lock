306a637480360687eeb6f6e3e4c219832a75af3e
GitHub Action <action@github.com>
2025-10-03 00:37:50 +0000

    Update mason-registry lock to 2025-10-02-rural-tie

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-02-dental-bill"
+M.registry_release = "github:mason-org/mason-registry@2025-10-02-rural-tie"
 
 return M
