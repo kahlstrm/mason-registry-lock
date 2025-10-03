a8c917b6999ad6aafa5630ca42c819c148e3e7eb
GitHub Action <action@github.com>
2025-10-03 00:37:50 +0000

    Update mason-registry lock to 2025-10-02-flawed-bite

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-02-rural-tie"
+M.registry_release = "github:mason-org/mason-registry@2025-10-02-flawed-bite"
 
 return M
