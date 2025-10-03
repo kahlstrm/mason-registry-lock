534dbf5b2ab954f46ae1fd6bbb83cb08c8dceda4
GitHub Action <action@github.com>
2025-10-03 00:37:49 +0000

    Update mason-registry lock to 2025-10-02-rare-memory

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-01-trashy-trade"
+M.registry_release = "github:mason-org/mason-registry@2025-10-02-rare-memory"
 
 return M
