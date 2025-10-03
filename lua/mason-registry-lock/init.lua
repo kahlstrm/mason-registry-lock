f134187de18a3217e0bf5c2bef76ffebd855f98e
GitHub Action <action@github.com>
2025-10-03 00:37:50 +0000

    Update mason-registry lock to 2025-10-02-needy-danio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-02-worn-purple"
+M.registry_release = "github:mason-org/mason-registry@2025-10-02-needy-danio"
 
 return M
