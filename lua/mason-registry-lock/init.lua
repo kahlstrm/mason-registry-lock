e8891adc97c75356f1ab7093e04930835536bf91
GitHub Action <action@github.com>
2025-10-03 00:37:50 +0000

    Update mason-registry lock to 2025-10-02-alike-fold

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-02-senior-beauty"
+M.registry_release = "github:mason-org/mason-registry@2025-10-02-alike-fold"
 
 return M
