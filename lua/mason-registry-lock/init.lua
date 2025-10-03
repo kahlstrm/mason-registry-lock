ea206326ddc2f60eb57448c84b6f93934edc200e
GitHub Action <action@github.com>
2025-10-03 00:37:50 +0000

    Update mason-registry lock to 2025-10-02-bitter-magic

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-02-pushy-trumpet"
+M.registry_release = "github:mason-org/mason-registry@2025-10-02-bitter-magic"
 
 return M
