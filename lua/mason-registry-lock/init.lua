8c4f00501e13a7f4d4cdd68f6e64be619a7e0e49
GitHub Action <action@github.com>
2025-10-03 00:37:50 +0000

    Update mason-registry lock to 2025-10-02-dental-bill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-02-alike-fold"
+M.registry_release = "github:mason-org/mason-registry@2025-10-02-dental-bill"
 
 return M
