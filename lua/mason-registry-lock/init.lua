5e78236214f950fe4499e0abce961054f309cf7d
GitHub Action <action@github.com>
2025-10-05 00:42:29 +0000

    Update mason-registry lock to 2025-10-04-beefy-horror

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-04-last-vault"
+M.registry_release = "github:mason-org/mason-registry@2025-10-04-beefy-horror"
 
 return M
