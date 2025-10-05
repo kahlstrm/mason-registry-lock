16720a52ecaca215dbeac501757aaeed9fa902cf
GitHub Action <action@github.com>
2025-10-05 00:42:29 +0000

    Update mason-registry lock to 2025-10-04-spiky-toe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-04-beefy-horror"
+M.registry_release = "github:mason-org/mason-registry@2025-10-04-spiky-toe"
 
 return M
