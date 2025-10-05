af266cd21028a06c81b55619c7dead978d4c0203
GitHub Action <action@github.com>
2025-10-05 00:42:29 +0000

    Update mason-registry lock to 2025-10-04-last-vault

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-04-awake-list"
+M.registry_release = "github:mason-org/mason-registry@2025-10-04-last-vault"
 
 return M
