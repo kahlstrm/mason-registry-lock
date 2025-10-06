45b9ed1db3b581d78dd076b966b18644046f4aa8
GitHub Action <action@github.com>
2025-10-06 00:39:40 +0000

    Update mason-registry lock to 2025-10-05-fine-baker

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-05-elated-result"
+M.registry_release = "github:mason-org/mason-registry@2025-10-05-fine-baker"
 
 return M
