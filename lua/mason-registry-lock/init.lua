500860b785fc7525195637d4d9bea772b1bb9938
GitHub Action <action@github.com>
2025-10-06 00:39:40 +0000

    Update mason-registry lock to 2025-10-05-naval-velvet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-04-casual-frost"
+M.registry_release = "github:mason-org/mason-registry@2025-10-05-naval-velvet"
 
 return M
