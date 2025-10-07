173ddb9bab170667774c1b5331ac69a1f57341ea
GitHub Action <action@github.com>
2025-10-07 00:38:28 +0000

    Update mason-registry lock to 2025-10-06-humble-yogurt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-06-nimble-rabbi"
+M.registry_release = "github:mason-org/mason-registry@2025-10-06-humble-yogurt"
 
 return M
