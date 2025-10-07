ed3f927eee9f1b35939c037e337d6737afaa878b
GitHub Action <action@github.com>
2025-10-07 00:38:28 +0000

    Update mason-registry lock to 2025-10-06-wrong-racing

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-06-giant-marble"
+M.registry_release = "github:mason-org/mason-registry@2025-10-06-wrong-racing"
 
 return M
