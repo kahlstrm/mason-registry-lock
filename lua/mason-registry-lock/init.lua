20cca819ae637ff0555bef99c2907faefb5fc539
GitHub Action <action@github.com>
2025-10-07 00:38:28 +0000

    Update mason-registry lock to 2025-10-06-giving-court

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-06-sound-lyre"
+M.registry_release = "github:mason-org/mason-registry@2025-10-06-giving-court"
 
 return M
