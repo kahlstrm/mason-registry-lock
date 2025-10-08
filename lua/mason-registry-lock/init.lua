553e7b03a4525b5bbbae137e05e4c25ec7b83503
GitHub Action <action@github.com>
2025-10-08 00:38:06 +0000

    Update mason-registry lock to 2025-10-07-social-mine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-07-meek-meal"
+M.registry_release = "github:mason-org/mason-registry@2025-10-07-social-mine"
 
 return M
