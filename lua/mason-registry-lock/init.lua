7ee16fcaf8a6631399b1e5a201d121d57e32103b
GitHub Action <action@github.com>
2025-10-08 00:38:06 +0000

    Update mason-registry lock to 2025-10-07-flaky-washer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-07-chunky-yoke"
+M.registry_release = "github:mason-org/mason-registry@2025-10-07-flaky-washer"
 
 return M
