bede2c753b1782e87c8396eff66f441f09848299
GitHub Action <action@github.com>
2025-10-08 00:38:06 +0000

    Update mason-registry lock to 2025-10-07-clever-dab

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-07-vanilla-wound"
+M.registry_release = "github:mason-org/mason-registry@2025-10-07-clever-dab"
 
 return M
