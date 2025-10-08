a5e516abe6ca49723d3a96ec273cc5a13170df2f
GitHub Action <action@github.com>
2025-10-08 00:38:06 +0000

    Update mason-registry lock to 2025-10-07-united-juice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-07-lesser-menu"
+M.registry_release = "github:mason-org/mason-registry@2025-10-07-united-juice"
 
 return M
