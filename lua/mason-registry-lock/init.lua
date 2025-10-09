7b74c80eb2591ba8553889e9ecc75f0f79176681
GitHub Action <action@github.com>
2025-10-09 00:38:46 +0000

    Update mason-registry lock to 2025-10-08-torn-coach

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-08-curved-icicle"
+M.registry_release = "github:mason-org/mason-registry@2025-10-08-torn-coach"
 
 return M
