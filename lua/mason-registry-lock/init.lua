a8dba108b3b2e25dc0b135aacf74860beaf74198
GitHub Action <action@github.com>
2025-10-09 00:38:46 +0000

    Update mason-registry lock to 2025-10-08-smoggy-anime

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-08-breeze-bucket"
+M.registry_release = "github:mason-org/mason-registry@2025-10-08-smoggy-anime"
 
 return M
