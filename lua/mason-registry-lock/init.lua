18028b21ba062da88d4fbaf0f4a5c85179e39bf6
GitHub Action <action@github.com>
2025-10-09 00:38:46 +0000

    Update mason-registry lock to 2025-10-08-unfair-node

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-08-torn-coach"
+M.registry_release = "github:mason-org/mason-registry@2025-10-08-unfair-node"
 
 return M
