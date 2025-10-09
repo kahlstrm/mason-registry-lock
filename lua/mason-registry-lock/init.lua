d868eebe18b8ccd68f7e4842b169deaf387d34a6
GitHub Action <action@github.com>
2025-10-09 00:38:46 +0000

    Update mason-registry lock to 2025-10-08-curved-icicle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-08-wobbly-neon"
+M.registry_release = "github:mason-org/mason-registry@2025-10-08-curved-icicle"
 
 return M
