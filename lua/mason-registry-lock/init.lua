edd591638f9e0ea8a5a19aee4ffd043601869664
GitHub Action <action@github.com>
2025-10-10 00:38:48 +0000

    Update mason-registry lock to 2025-10-09-impure-voice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-09-alike-gray"
+M.registry_release = "github:mason-org/mason-registry@2025-10-09-impure-voice"
 
 return M
