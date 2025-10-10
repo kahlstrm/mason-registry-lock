f0ee7fea53840854db6b5393ea1214723c01bb32
GitHub Action <action@github.com>
2025-10-10 00:38:48 +0000

    Update mason-registry lock to 2025-10-09-wide-sole

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-09-close-puppy"
+M.registry_release = "github:mason-org/mason-registry@2025-10-09-wide-sole"
 
 return M
