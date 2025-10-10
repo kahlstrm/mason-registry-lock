1431fe24d3d1568b8fc38544dd846cf0d5c7f8c2
GitHub Action <action@github.com>
2025-10-10 00:38:48 +0000

    Update mason-registry lock to 2025-10-09-visual-exit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-09-lined-guru"
+M.registry_release = "github:mason-org/mason-registry@2025-10-09-visual-exit"
 
 return M
