ed92984217b6b8e91300998ef4189731bad35e2e
GitHub Action <action@github.com>
2025-10-10 00:38:48 +0000

    Update mason-registry lock to 2025-10-09-delightful-trunk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-09-smarmy-kayak"
+M.registry_release = "github:mason-org/mason-registry@2025-10-09-delightful-trunk"
 
 return M
