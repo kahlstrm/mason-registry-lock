49ee9603b0667fc065fa9e6c1f32227980271e3e
GitHub Action <action@github.com>
2025-10-11 00:36:54 +0000

    Update mason-registry lock to 2025-10-10-dotted-kayak

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-10-other-pastor"
+M.registry_release = "github:mason-org/mason-registry@2025-10-10-dotted-kayak"
 
 return M
