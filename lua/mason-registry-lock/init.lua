ca5747babcf16661de0895e0b6269c7d36cddd15
GitHub Action <action@github.com>
2025-10-11 00:36:54 +0000

    Update mason-registry lock to 2025-10-10-green-film

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-10-brave-dorab"
+M.registry_release = "github:mason-org/mason-registry@2025-10-10-green-film"
 
 return M
