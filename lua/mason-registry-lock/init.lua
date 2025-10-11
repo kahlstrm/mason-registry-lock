2838d54af16753d011544a740c3160132efaff5e
GitHub Action <action@github.com>
2025-10-11 00:36:54 +0000

    Update mason-registry lock to 2025-10-10-bent-gate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-10-clumsy-copy"
+M.registry_release = "github:mason-org/mason-registry@2025-10-10-bent-gate"
 
 return M
