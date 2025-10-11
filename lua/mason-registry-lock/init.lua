6d44aac6637848ce49c9c7824d33577733325d7d
GitHub Action <action@github.com>
2025-10-11 00:36:54 +0000

    Update mason-registry lock to 2025-10-10-other-pastor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-10-abaft-folder"
+M.registry_release = "github:mason-org/mason-registry@2025-10-10-other-pastor"
 
 return M
