1cd6b4bed42b619a2334408809fa11a50ab1d538
GitHub Action <action@github.com>
2025-10-11 00:36:54 +0000

    Update mason-registry lock to 2025-10-10-wicked-boston

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-10-irate-flesh"
+M.registry_release = "github:mason-org/mason-registry@2025-10-10-wicked-boston"
 
 return M
