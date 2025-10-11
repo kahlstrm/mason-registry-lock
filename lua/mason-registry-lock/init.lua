9f0a5058463ab9089be539c3dbedff77613e36ef
GitHub Action <action@github.com>
2025-10-11 00:36:54 +0000

    Update mason-registry lock to 2025-10-10-tepid-toe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-10-native-grunt"
+M.registry_release = "github:mason-org/mason-registry@2025-10-10-tepid-toe"
 
 return M
