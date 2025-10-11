1fba3930b54fa82d8a5ee5d48893a9cbdc014a15
GitHub Action <action@github.com>
2025-10-11 00:36:54 +0000

    Update mason-registry lock to 2025-10-10-gentle-digger

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-09-vital-den"
+M.registry_release = "github:mason-org/mason-registry@2025-10-10-gentle-digger"
 
 return M
