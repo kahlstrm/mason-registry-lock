826ceb06cdd0af5bf7591e92395c694180c8aba8
GitHub Action <action@github.com>
2025-10-12 00:40:48 +0000

    Update mason-registry lock to 2025-10-11-random-quill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-10-tepid-toe"
+M.registry_release = "github:mason-org/mason-registry@2025-10-11-random-quill"
 
 return M
