586ffa2d5ecfb02913c625d58eb37ae5ea065d5c
GitHub Action <action@github.com>
2025-10-12 00:40:48 +0000

    Update mason-registry lock to 2025-10-11-mid-vimba

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-11-global-debtor"
+M.registry_release = "github:mason-org/mason-registry@2025-10-11-mid-vimba"
 
 return M
