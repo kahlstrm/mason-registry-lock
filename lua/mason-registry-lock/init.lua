9794cc06804c330b4e9af7fb9e630daffe72bef2
GitHub Action <action@github.com>
2025-10-12 00:40:48 +0000

    Update mason-registry lock to 2025-10-11-senior-random

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-11-mass-wallet"
+M.registry_release = "github:mason-org/mason-registry@2025-10-11-senior-random"
 
 return M
