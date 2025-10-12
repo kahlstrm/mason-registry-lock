0feb82644aec58b12e67960e99abe71622bcf8d5
GitHub Action <action@github.com>
2025-10-12 00:40:48 +0000

    Update mason-registry lock to 2025-10-11-cleaver-char

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-11-random-quill"
+M.registry_release = "github:mason-org/mason-registry@2025-10-11-cleaver-char"
 
 return M
