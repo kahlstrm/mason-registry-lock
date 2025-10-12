061068b7eb392e756f00bfb3b4f039a40fac01a8
GitHub Action <action@github.com>
2025-10-12 00:40:48 +0000

    Update mason-registry lock to 2025-10-11-global-debtor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-11-senior-random"
+M.registry_release = "github:mason-org/mason-registry@2025-10-11-global-debtor"
 
 return M
