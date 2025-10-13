4a0ffb773a168b0e384091b99f792725011d0efd
GitHub Action <action@github.com>
2025-10-13 00:42:27 +0000

    Update mason-registry lock to 2025-10-13-rapid-chive

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-13-deeply-turkey"
+M.registry_release = "github:mason-org/mason-registry@2025-10-13-rapid-chive"
 
 return M
