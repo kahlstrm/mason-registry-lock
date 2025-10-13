72c03acebdbf0fef0f7173ce68145f3c4300b187
GitHub Action <action@github.com>
2025-10-13 00:42:27 +0000

    Update mason-registry lock to 2025-10-12-crabby-camp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-12-calm-pacus"
+M.registry_release = "github:mason-org/mason-registry@2025-10-12-crabby-camp"
 
 return M
