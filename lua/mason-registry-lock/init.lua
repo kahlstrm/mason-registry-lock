4eb2ce7df07b70356f4f77ee7831e32c7ec46d9a
GitHub Action <action@github.com>
2025-10-14 00:38:10 +0000

    Update mason-registry lock to 2025-10-13-grey-tip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-13-pretty-shore"
+M.registry_release = "github:mason-org/mason-registry@2025-10-13-grey-tip"
 
 return M
