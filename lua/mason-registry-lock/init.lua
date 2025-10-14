292c7c8625b603722a004c3cd658c026eca6dda5
GitHub Action <action@github.com>
2025-10-14 00:38:10 +0000

    Update mason-registry lock to 2025-10-13-quiet-bronze

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-13-wary-sneeze"
+M.registry_release = "github:mason-org/mason-registry@2025-10-13-quiet-bronze"
 
 return M
