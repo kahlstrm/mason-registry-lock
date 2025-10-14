99cb468b1ccf65b8efc1deb9e9b0276785a06b06
GitHub Action <action@github.com>
2025-10-14 00:38:10 +0000

    Update mason-registry lock to 2025-10-13-wary-sneeze

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-13-rapid-chive"
+M.registry_release = "github:mason-org/mason-registry@2025-10-13-wary-sneeze"
 
 return M
