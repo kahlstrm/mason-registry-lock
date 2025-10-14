7cf34f9a20ab29ca25116213bff7cc6b26f337aa
GitHub Action <action@github.com>
2025-10-14 00:38:10 +0000

    Update mason-registry lock to 2025-10-13-chilly-flat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-13-gross-font"
+M.registry_release = "github:mason-org/mason-registry@2025-10-13-chilly-flat"
 
 return M
