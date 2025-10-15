5cc1faf7e483d32e9ea05150c1d0ff53ccf2538a
GitHub Action <action@github.com>
2025-10-15 00:40:05 +0000

    Update mason-registry lock to 2025-10-14-timely-chip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-14-unsung-peak"
+M.registry_release = "github:mason-org/mason-registry@2025-10-14-timely-chip"
 
 return M
