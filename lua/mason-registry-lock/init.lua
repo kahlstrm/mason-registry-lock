6080406f83e3906391b0a7a1a1b2f0d091bb97f8
GitHub Action <action@github.com>
2025-10-15 00:40:05 +0000

    Update mason-registry lock to 2025-10-14-soupy-shelf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-13-chilly-flat"
+M.registry_release = "github:mason-org/mason-registry@2025-10-14-soupy-shelf"
 
 return M
