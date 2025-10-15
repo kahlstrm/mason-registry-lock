f119fb6ec8fb9bf6e816315b8dbdd0d288370b2f
GitHub Action <action@github.com>
2025-10-15 00:40:05 +0000

    Update mason-registry lock to 2025-10-14-gloomy-veil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-14-outer-libra"
+M.registry_release = "github:mason-org/mason-registry@2025-10-14-gloomy-veil"
 
 return M
