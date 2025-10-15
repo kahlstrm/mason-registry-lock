c4ca1db26ad9584c00622bf397686a5003ef9bc9
GitHub Action <action@github.com>
2025-10-15 00:40:05 +0000

    Update mason-registry lock to 2025-10-14-rugged-chive

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-14-jaunty-stork"
+M.registry_release = "github:mason-org/mason-registry@2025-10-14-rugged-chive"
 
 return M
