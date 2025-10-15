d134b6eb891df8de0d4b8979245d788d3498e1f4
GitHub Action <action@github.com>
2025-10-15 00:40:05 +0000

    Update mason-registry lock to 2025-10-14-unsung-peak

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-14-rugged-chive"
+M.registry_release = "github:mason-org/mason-registry@2025-10-14-unsung-peak"
 
 return M
