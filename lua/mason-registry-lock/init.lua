18dfed0c064380cf7f223e536b486eae57d63b1e
GitHub Action <action@github.com>
2025-10-16 00:40:10 +0000

    Update mason-registry lock to 2025-10-15-abrupt-ankle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-15-worst-zebu"
+M.registry_release = "github:mason-org/mason-registry@2025-10-15-abrupt-ankle"
 
 return M
