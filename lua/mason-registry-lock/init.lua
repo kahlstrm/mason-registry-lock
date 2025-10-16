4ad40a1624ab0052740fb9d1e552e20a14087c03
GitHub Action <action@github.com>
2025-10-16 00:40:10 +0000

    Update mason-registry lock to 2025-10-15-unripe-spade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-15-indian-basket"
+M.registry_release = "github:mason-org/mason-registry@2025-10-15-unripe-spade"
 
 return M
