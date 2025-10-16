3fb50215b8ff088ce93923d7b3a0f629939a5378
GitHub Action <action@github.com>
2025-10-16 00:40:10 +0000

    Update mason-registry lock to 2025-10-15-indian-basket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-14-gloomy-veil"
+M.registry_release = "github:mason-org/mason-registry@2025-10-15-indian-basket"
 
 return M
