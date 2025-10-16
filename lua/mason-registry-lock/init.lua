db19e9e2be3d889ee05f035d83c012260aaf3e7b
GitHub Action <action@github.com>
2025-10-16 00:40:10 +0000

    Update mason-registry lock to 2025-10-15-brainy-march

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-15-abrupt-ankle"
+M.registry_release = "github:mason-org/mason-registry@2025-10-15-brainy-march"
 
 return M
