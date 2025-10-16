0f1b0b17d723f11d287d8ffaf6bf36419aca9753
GitHub Action <action@github.com>
2025-10-16 00:40:10 +0000

    Update mason-registry lock to 2025-10-15-phony-korean

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-15-one-elder"
+M.registry_release = "github:mason-org/mason-registry@2025-10-15-phony-korean"
 
 return M
