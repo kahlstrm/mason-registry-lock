3cef1aad2be28c5f0af08d061aac907db9f787ee
GitHub Action <action@github.com>
2025-10-17 00:39:24 +0000

    Update mason-registry lock to 2025-10-16-silent-cerium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-16-faulty-pvc"
+M.registry_release = "github:mason-org/mason-registry@2025-10-16-silent-cerium"
 
 return M
