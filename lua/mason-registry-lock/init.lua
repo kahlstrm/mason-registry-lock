2db7f16d7f531b6ba68e801afccc3f6f60f7a807
GitHub Action <action@github.com>
2025-10-17 00:39:24 +0000

    Update mason-registry lock to 2025-10-16-brainy-editor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-16-plain-vest"
+M.registry_release = "github:mason-org/mason-registry@2025-10-16-brainy-editor"
 
 return M
