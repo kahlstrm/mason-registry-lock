05eacfdff1955792fb9a513aa07b23a9ee52b26d
GitHub Action <action@github.com>
2025-10-17 00:39:24 +0000

    Update mason-registry lock to 2025-10-16-faulty-pvc

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-16-nice-socks"
+M.registry_release = "github:mason-org/mason-registry@2025-10-16-faulty-pvc"
 
 return M
