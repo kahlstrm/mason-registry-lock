8ed456ecd5e53a1971049c2005c8d8a726fda82c
GitHub Action <action@github.com>
2025-10-17 00:39:24 +0000

    Update mason-registry lock to 2025-10-16-sunny-hub

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-16-brainy-editor"
+M.registry_release = "github:mason-org/mason-registry@2025-10-16-sunny-hub"
 
 return M
