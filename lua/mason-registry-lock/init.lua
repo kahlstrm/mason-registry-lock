e268c3fb899137bdcbab33b284c18f0694c746dd
GitHub Action <action@github.com>
2025-10-18 00:37:09 +0000

    Update mason-registry lock to 2025-10-17-unsung-beach

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-17-lovely-onion"
+M.registry_release = "github:mason-org/mason-registry@2025-10-17-unsung-beach"
 
 return M
