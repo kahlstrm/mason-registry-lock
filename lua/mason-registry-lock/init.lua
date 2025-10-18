03189db662e7b0909c5785ecb08e516a8d6d8cab
GitHub Action <action@github.com>
2025-10-18 00:37:09 +0000

    Update mason-registry lock to 2025-10-17-civil-remora

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-17-short-vimba"
+M.registry_release = "github:mason-org/mason-registry@2025-10-17-civil-remora"
 
 return M
