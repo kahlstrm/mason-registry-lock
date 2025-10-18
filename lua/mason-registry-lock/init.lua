53efd7900e9818002bb010adfca090297ad081e9
GitHub Action <action@github.com>
2025-10-18 00:37:09 +0000

    Update mason-registry lock to 2025-10-17-rival-island

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-17-oily-action"
+M.registry_release = "github:mason-org/mason-registry@2025-10-17-rival-island"
 
 return M
