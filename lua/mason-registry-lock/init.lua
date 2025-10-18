9016190babe142f8b21240f4a61dc222eb9ff32b
GitHub Action <action@github.com>
2025-10-18 00:37:09 +0000

    Update mason-registry lock to 2025-10-17-short-vimba

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-17-torn-soda"
+M.registry_release = "github:mason-org/mason-registry@2025-10-17-short-vimba"
 
 return M
