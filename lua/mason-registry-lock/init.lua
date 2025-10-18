fcf0d7fca5377228ebb536b22b9b33900de5d469
GitHub Action <action@github.com>
2025-10-18 00:37:09 +0000

    Update mason-registry lock to 2025-10-17-lonely-mark

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-17-round-stove"
+M.registry_release = "github:mason-org/mason-registry@2025-10-17-lonely-mark"
 
 return M
