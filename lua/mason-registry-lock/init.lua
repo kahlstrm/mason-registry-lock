28d54c5dbf03b8c42f216573b6fe29a2de3d8da6
GitHub Action <action@github.com>
2025-10-18 00:37:09 +0000

    Update mason-registry lock to 2025-10-17-round-stove

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-17-lush-fang"
+M.registry_release = "github:mason-org/mason-registry@2025-10-17-round-stove"
 
 return M
