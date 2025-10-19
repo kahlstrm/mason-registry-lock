eee3b8c3d34e4d0dff3923a6fd7cea359476fdd9
GitHub Action <action@github.com>
2025-10-19 00:44:25 +0000

    Update mason-registry lock to 2025-10-18-musty-bangle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-18-help-marlin"
+M.registry_release = "github:mason-org/mason-registry@2025-10-18-musty-bangle"
 
 return M
