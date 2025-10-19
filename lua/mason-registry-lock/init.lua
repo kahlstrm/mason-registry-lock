097192f03238d04d592da17082c815c8247fc3e8
GitHub Action <action@github.com>
2025-10-19 00:44:25 +0000

    Update mason-registry lock to 2025-10-18-lime-snook

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-18-rash-point"
+M.registry_release = "github:mason-org/mason-registry@2025-10-18-lime-snook"
 
 return M
