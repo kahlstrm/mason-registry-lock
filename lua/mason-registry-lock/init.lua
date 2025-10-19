ee05f270c070220782b6eae126af8c851ed2e563
GitHub Action <action@github.com>
2025-10-19 00:44:25 +0000

    Update mason-registry lock to 2025-10-18-help-marlin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-18-putrid-mullet"
+M.registry_release = "github:mason-org/mason-registry@2025-10-18-help-marlin"
 
 return M
