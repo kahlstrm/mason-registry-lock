1d60edc49ab4eab564d86a007ab3e44382af0204
GitHub Action <action@github.com>
2025-10-19 00:44:25 +0000

    Update mason-registry lock to 2025-10-18-icky-loss

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-17-steep-birth"
+M.registry_release = "github:mason-org/mason-registry@2025-10-18-icky-loss"
 
 return M
