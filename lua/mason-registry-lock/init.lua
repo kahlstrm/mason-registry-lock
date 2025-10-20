0081e37ddb41e98a765c32efdb1dddf9ea3ef65d
GitHub Action <action@github.com>
2025-10-20 00:42:59 +0000

    Update mason-registry lock to 2025-10-19-sordid-need

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-19-quain-zoo"
+M.registry_release = "github:mason-org/mason-registry@2025-10-19-sordid-need"
 
 return M
