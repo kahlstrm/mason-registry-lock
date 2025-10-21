59923e7ff7240600e3f4cd9085eb116911bc592b
GitHub Action <action@github.com>
2025-10-21 00:40:26 +0000

    Update mason-registry lock to 2025-10-20-direct-bench

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-20-sound-neck"
+M.registry_release = "github:mason-org/mason-registry@2025-10-20-direct-bench"
 
 return M
