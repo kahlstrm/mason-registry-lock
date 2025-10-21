a5b806855e18251f6092b3b7c7b58c4b1fffa28e
GitHub Action <action@github.com>
2025-10-21 00:40:26 +0000

    Update mason-registry lock to 2025-10-20-grateful-burbot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-20-steep-lock"
+M.registry_release = "github:mason-org/mason-registry@2025-10-20-grateful-burbot"
 
 return M
