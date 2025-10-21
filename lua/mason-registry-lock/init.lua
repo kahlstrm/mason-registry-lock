5c4da38b8ce287f1deadfe5e906a253799aa4131
GitHub Action <action@github.com>
2025-10-21 00:40:26 +0000

    Update mason-registry lock to 2025-10-21-zigzag-sight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-21-slender-washer"
+M.registry_release = "github:mason-org/mason-registry@2025-10-21-zigzag-sight"
 
 return M
