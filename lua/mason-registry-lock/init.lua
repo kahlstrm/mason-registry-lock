162c210c29c4feb7ae9e65c25af05509acdf8145
GitHub Action <action@github.com>
2025-10-21 00:40:26 +0000

    Update mason-registry lock to 2025-10-20-weak-ide

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-20-direct-bench"
+M.registry_release = "github:mason-org/mason-registry@2025-10-20-weak-ide"
 
 return M
