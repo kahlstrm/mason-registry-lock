7a4677e96e4a90fdda5ad880fd649a7c577d37ad
GitHub Action <action@github.com>
2025-10-22 00:41:26 +0000

    Update mason-registry lock to 2025-10-21-native-trumpet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-21-oval-stew"
+M.registry_release = "github:mason-org/mason-registry@2025-10-21-native-trumpet"
 
 return M
