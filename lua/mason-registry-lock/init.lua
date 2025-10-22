23266ebf4aa74383cd9819e49168f895140d4871
GitHub Action <action@github.com>
2025-10-22 00:41:26 +0000

    Update mason-registry lock to 2025-10-21-spicy-drone

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-21-ancient-fund"
+M.registry_release = "github:mason-org/mason-registry@2025-10-21-spicy-drone"
 
 return M
