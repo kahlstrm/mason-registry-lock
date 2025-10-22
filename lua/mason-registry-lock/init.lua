e68aa26816d2cb4bcbf9c24f49f1b42da34c0a60
GitHub Action <action@github.com>
2025-10-22 00:41:26 +0000

    Update mason-registry lock to 2025-10-21-casual-drake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-21-lazy-editor"
+M.registry_release = "github:mason-org/mason-registry@2025-10-21-casual-drake"
 
 return M
