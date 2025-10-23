3b7b07c39e89df53ddaeed3efd43914f62c0a3de
GitHub Action <action@github.com>
2025-10-23 00:40:09 +0000

    Update mason-registry lock to 2025-10-21-tame-throat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-21-urban-cap"
+M.registry_release = "github:mason-org/mason-registry@2025-10-21-tame-throat"
 
 return M
