96ec6e1c1027ef2f461ac5e59cc2e93e27456cb9
GitHub Action <action@github.com>
2025-10-23 00:40:09 +0000

    Update mason-registry lock to 2025-10-22-acrid-clover

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-22-weird-timer"
+M.registry_release = "github:mason-org/mason-registry@2025-10-22-acrid-clover"
 
 return M
