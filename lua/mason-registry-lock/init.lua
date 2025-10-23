31cb2f987fd6ceb404d5b3750c9cbae46dd8bff3
GitHub Action <action@github.com>
2025-10-23 00:40:10 +0000

    Update mason-registry lock to 2025-10-22-plump-doctor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-22-due-mist"
+M.registry_release = "github:mason-org/mason-registry@2025-10-22-plump-doctor"
 
 return M
