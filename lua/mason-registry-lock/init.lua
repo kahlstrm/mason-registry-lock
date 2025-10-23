0cc40fea17db15317bef10424e2f557e5361b963
GitHub Action <action@github.com>
2025-10-23 00:40:10 +0000

    Update mason-registry lock to 2025-10-22-tough-dish

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-22-stiff-match"
+M.registry_release = "github:mason-org/mason-registry@2025-10-22-tough-dish"
 
 return M
