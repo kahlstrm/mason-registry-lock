c60ab056a532360292d9f0e2159d8eaa5361fc4c
GitHub Action <action@github.com>
2025-10-24 00:37:23 +0000

    Update mason-registry lock to 2025-10-23-thin-size

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-23-zonked-subway"
+M.registry_release = "github:mason-org/mason-registry@2025-10-23-thin-size"
 
 return M
