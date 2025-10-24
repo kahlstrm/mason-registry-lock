afb659e0d28102fa6c0de7e8b6e762dc2768c02d
GitHub Action <action@github.com>
2025-10-24 00:37:23 +0000

    Update mason-registry lock to 2025-10-23-inborn-alto

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-23-numb-rabbit"
+M.registry_release = "github:mason-org/mason-registry@2025-10-23-inborn-alto"
 
 return M
