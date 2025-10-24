48d21a9d9b3b9df3859e196de9deeb474fe3a027
GitHub Action <action@github.com>
2025-10-24 00:37:23 +0000

    Update mason-registry lock to 2025-10-23-breeze-crown

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-23-runny-shop"
+M.registry_release = "github:mason-org/mason-registry@2025-10-23-breeze-crown"
 
 return M
