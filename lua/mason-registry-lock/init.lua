69a5eebf1fea9bca687b3643dcb18fb3a2233cc4
GitHub Action <action@github.com>
2025-10-25 00:38:54 +0000

    Update mason-registry lock to 2025-10-24-flawed-koodoo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-23-open-satin"
+M.registry_release = "github:mason-org/mason-registry@2025-10-24-flawed-koodoo"
 
 return M
