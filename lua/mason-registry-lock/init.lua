debc372cd7590f409097baa0cee58e953128af08
GitHub Action <action@github.com>
2025-10-25 00:38:54 +0000

    Update mason-registry lock to 2025-10-24-lazy-chrome

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-24-petty-drum"
+M.registry_release = "github:mason-org/mason-registry@2025-10-24-lazy-chrome"
 
 return M
