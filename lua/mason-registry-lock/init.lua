6fd804a314ad3adcbe4dee648f027ae4df93eb0b
GitHub Action <action@github.com>
2025-10-26 00:43:19 +0000

    Update mason-registry lock to 2025-10-25-nine-nature

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-25-able-meat"
+M.registry_release = "github:mason-org/mason-registry@2025-10-25-nine-nature"
 
 return M
