5c813aecb99951435acfd79512465d968fd57093
GitHub Action <action@github.com>
2025-10-26 00:43:19 +0000

    Update mason-registry lock to 2025-10-25-able-meat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-24-witty-switch"
+M.registry_release = "github:mason-org/mason-registry@2025-10-25-able-meat"
 
 return M
