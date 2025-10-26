a2c9b4d0640a12e769649509fc278f2691f26d6b
GitHub Action <action@github.com>
2025-10-26 00:43:19 +0000

    Update mason-registry lock to 2025-10-25-flimsy-pail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-25-nine-nature"
+M.registry_release = "github:mason-org/mason-registry@2025-10-25-flimsy-pail"
 
 return M
