ada3802e683db6f1baa6267fc0495de61f28aee8
GitHub Action <action@github.com>
2025-10-27 00:44:36 +0000

    Update mason-registry lock to 2025-10-26-wary-cell

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-26-nutty-tapir"
+M.registry_release = "github:mason-org/mason-registry@2025-10-26-wary-cell"
 
 return M
