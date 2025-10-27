aad25e0dfe0ef73703b6f373dd8ed53561a19779
GitHub Action <action@github.com>
2025-10-27 00:44:36 +0000

    Update mason-registry lock to 2025-10-26-nutty-tapir

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-26-purple-sulfur"
+M.registry_release = "github:mason-org/mason-registry@2025-10-26-nutty-tapir"
 
 return M
