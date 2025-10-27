65a5d11f667b2267d41e839eb1c22a523f12d3e7
GitHub Action <action@github.com>
2025-10-27 00:44:36 +0000

    Update mason-registry lock to 2025-10-26-purple-sulfur

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-25-wavy-drama"
+M.registry_release = "github:mason-org/mason-registry@2025-10-26-purple-sulfur"
 
 return M
