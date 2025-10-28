f870947c17b34b4b2de45a70a896b914431820b2
GitHub Action <action@github.com>
2025-10-28 00:39:17 +0000

    Update mason-registry lock to 2025-10-27-bored-basin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-27-yearly-comb"
+M.registry_release = "github:mason-org/mason-registry@2025-10-27-bored-basin"
 
 return M
