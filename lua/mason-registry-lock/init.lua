a3671bf69b2903d6423c9dc92af2deeeb3dfb9bb
GitHub Action <action@github.com>
2025-10-28 00:39:17 +0000

    Update mason-registry lock to 2025-10-27-second-frog

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-27-unfair-pest"
+M.registry_release = "github:mason-org/mason-registry@2025-10-27-second-frog"
 
 return M
