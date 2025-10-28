322cdd736d07663c4447f0e2af0c78c200311d58
GitHub Action <action@github.com>
2025-10-28 00:39:17 +0000

    Update mason-registry lock to 2025-10-27-webbed-elk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-27-green-hope"
+M.registry_release = "github:mason-org/mason-registry@2025-10-27-webbed-elk"
 
 return M
