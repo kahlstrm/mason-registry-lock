8f86fc0ddef717dff569a3de3481ccd74cd51160
GitHub Action <action@github.com>
2025-10-29 00:42:25 +0000

    Update mason-registry lock to 2025-10-29-ordinary-hand

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-28-bronze-vicuna"
+M.registry_release = "github:mason-org/mason-registry@2025-10-29-ordinary-hand"
 
 return M
