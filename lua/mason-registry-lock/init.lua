c6ec95cbc5042055d72673679a8b0d014c3b133c
GitHub Action <action@github.com>
2025-10-29 00:42:25 +0000

    Update mason-registry lock to 2025-10-27-yearly-comb

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-27-like-vein"
+M.registry_release = "github:mason-org/mason-registry@2025-10-27-yearly-comb"
 
 return M
