0c34a35299c73004610d94e6652e0f3c201fe46e
GitHub Action <action@github.com>
2025-10-29 00:42:25 +0000

    Update mason-registry lock to 2025-10-28-stale-ngine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-28-plump-guru"
+M.registry_release = "github:mason-org/mason-registry@2025-10-28-stale-ngine"
 
 return M
