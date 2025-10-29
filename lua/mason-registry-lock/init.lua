9a9dcd68c5c2e26cca3391f2d75a549081ed152a
GitHub Action <action@github.com>
2025-10-29 00:42:25 +0000

    Update mason-registry lock to 2025-10-28-soggy-wing

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-28-verbal-yogurt"
+M.registry_release = "github:mason-org/mason-registry@2025-10-28-soggy-wing"
 
 return M
