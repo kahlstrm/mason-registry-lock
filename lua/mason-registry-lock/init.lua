22c1e57729a0c1daf8430ef40020d047246e46c3
GitHub Action <action@github.com>
2025-10-29 00:42:25 +0000

    Update mason-registry lock to 2025-10-28-verbal-yogurt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-28-frozen-cerium"
+M.registry_release = "github:mason-org/mason-registry@2025-10-28-verbal-yogurt"
 
 return M
