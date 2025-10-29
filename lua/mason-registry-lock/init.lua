52cac520c2f8516924ec7820c26e11bbbe349621
GitHub Action <action@github.com>
2025-10-29 00:42:25 +0000

    Update mason-registry lock to 2025-10-28-skinny-ring

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-28-those-bison"
+M.registry_release = "github:mason-org/mason-registry@2025-10-28-skinny-ring"
 
 return M
