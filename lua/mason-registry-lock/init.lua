5bee3ec049b7bf4b6f694c2a52718d0c3aacc36d
GitHub Action <action@github.com>
2025-10-29 00:42:25 +0000

    Update mason-registry lock to 2025-10-28-frozen-cerium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-28-tinted-roughy"
+M.registry_release = "github:mason-org/mason-registry@2025-10-28-frozen-cerium"
 
 return M
