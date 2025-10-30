e9e8afefa027278529da28353afeebe8ddc954ad
GitHub Action <action@github.com>
2025-10-30 00:42:05 +0000

    Update mason-registry lock to 2025-10-29-greek-donna

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-29-superb-road"
+M.registry_release = "github:mason-org/mason-registry@2025-10-29-greek-donna"
 
 return M
