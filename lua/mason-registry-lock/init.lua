c3b35b3bc828cdfbfb7e743283a242c2543c61b7
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-30-rival-title

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-30-irish-node"
+M.registry_release = "github:mason-org/mason-registry@2025-10-30-rival-title"
 
 return M
