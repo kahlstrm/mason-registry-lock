d93ecb76a722ff794449305ae174ba7cd90eaddb
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-30-prime-tire

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-30-toxic-oboe"
+M.registry_release = "github:mason-org/mason-registry@2025-10-30-prime-tire"
 
 return M
