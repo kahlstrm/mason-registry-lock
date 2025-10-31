7764ea9cb24761ff42586aa5a5e6187287f0f33a
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-31-frigid-moon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-back-terrain"
+M.registry_release = "github:mason-org/mason-registry@2025-10-31-frigid-moon"
 
 return M
