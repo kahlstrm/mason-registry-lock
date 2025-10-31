0f1563e3ac29310124e820c21b66e6293ae824e7
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-30-zippy-bacon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-30-rival-title"
+M.registry_release = "github:mason-org/mason-registry@2025-10-30-zippy-bacon"
 
 return M
