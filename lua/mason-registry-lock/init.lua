49be0b5029faffada166e4732c2de604376252f3
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-30-causal-force

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-30-real-camp"
+M.registry_release = "github:mason-org/mason-registry@2025-10-30-causal-force"
 
 return M
