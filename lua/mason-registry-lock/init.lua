53d1c970ef854091455fcb3787f53c62985e199f
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-31-unique-india

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-ill-george"
+M.registry_release = "github:mason-org/mason-registry@2025-10-31-unique-india"
 
 return M
