65be138b7d392fd254a26d8afcbf24ecb0485854
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-31-back-terrain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-lively-heat"
+M.registry_release = "github:mason-org/mason-registry@2025-10-31-back-terrain"
 
 return M
