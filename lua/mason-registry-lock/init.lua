547e4789814c05cc069e6011d6e5343bfb2e0f50
GitHub Action <action@github.com>
2025-10-31 00:40:21 +0000

    Update mason-registry lock to 2025-10-30-irish-node

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-30-allied-fowl"
+M.registry_release = "github:mason-org/mason-registry@2025-10-30-irish-node"
 
 return M
