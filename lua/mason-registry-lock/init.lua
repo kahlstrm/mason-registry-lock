8c2274aaca30d53cfb1db98d344f73f9cf81f040
GitHub Action <action@github.com>
2025-11-01 00:43:57 +0000

    Update mason-registry lock to 2025-10-31-robust-pepper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-above-cycle"
+M.registry_release = "github:mason-org/mason-registry@2025-10-31-robust-pepper"
 
 return M
