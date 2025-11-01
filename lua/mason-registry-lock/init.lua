cd83eb98187e4a5e466c29174e06bdf3cfeade6c
GitHub Action <action@github.com>
2025-11-01 00:43:56 +0000

    Update mason-registry lock to 2025-10-31-bold-zinc

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-blonde-mole"
+M.registry_release = "github:mason-org/mason-registry@2025-10-31-bold-zinc"
 
 return M
