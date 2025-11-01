f0615f9c168e214c9fefad90f470493b8e909e65
GitHub Action <action@github.com>
2025-11-01 00:43:57 +0000

    Update mason-registry lock to 2025-10-31-wacky-shadow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-abaft-yarn"
+M.registry_release = "github:mason-org/mason-registry@2025-10-31-wacky-shadow"
 
 return M
