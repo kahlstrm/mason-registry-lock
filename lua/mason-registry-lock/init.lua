e9c186e7271683b8ba6cf54e11566c591bda2f53
GitHub Action <action@github.com>
2025-11-01 00:43:57 +0000

    Update mason-registry lock to 2025-10-31-slender-moat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-dapper-canada"
+M.registry_release = "github:mason-org/mason-registry@2025-10-31-slender-moat"
 
 return M
