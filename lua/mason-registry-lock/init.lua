fdcf0478865b9ac9608ee4cb9c1e9edbf46b5ed0
GitHub Action <action@github.com>
2025-11-02 00:44:09 +0000

    Update mason-registry lock to 2025-11-01-dusty-dryer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-01-those-video"
+M.registry_release = "github:mason-org/mason-registry@2025-11-01-dusty-dryer"
 
 return M
