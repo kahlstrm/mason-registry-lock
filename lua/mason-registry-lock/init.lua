95b56888d605eaf346b0983743258df7feb98806
GitHub Action <action@github.com>
2025-11-02 00:44:09 +0000

    Update mason-registry lock to 2025-11-01-round-impala

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-10-31-robust-pepper"
+M.registry_release = "github:mason-org/mason-registry@2025-11-01-round-impala"
 
 return M
