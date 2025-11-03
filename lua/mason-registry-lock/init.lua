68e2fb934938ece2a5d62787f658ba2cfe85b336
GitHub Action <action@github.com>
2025-11-03 00:43:40 +0000

    Update mason-registry lock to 2025-11-02-tacit-supper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-02-scarce-trowel"
+M.registry_release = "github:mason-org/mason-registry@2025-11-02-tacit-supper"
 
 return M
