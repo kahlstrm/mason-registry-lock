5ebdb2ba4e7a66fc71aaa3a5f7f1253e19a2f0dc
GitHub Action <action@github.com>
2025-11-03 00:43:40 +0000

    Update mason-registry lock to 2025-11-02-one-radio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-02-brash-rifle"
+M.registry_release = "github:mason-org/mason-registry@2025-11-02-one-radio"
 
 return M
