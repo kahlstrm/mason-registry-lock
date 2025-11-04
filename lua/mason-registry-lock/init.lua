b408ae0ac16f842b7066aca11ab7a936f97f08c2
GitHub Action <action@github.com>
2025-11-04 00:41:06 +0000

    Update mason-registry lock to 2025-11-03-inland-whiff

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-03-petite-roach"
+M.registry_release = "github:mason-org/mason-registry@2025-11-03-inland-whiff"
 
 return M
