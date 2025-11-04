e162af7530cda44d84dd4d5efb04fbba6e4e85c7
GitHub Action <action@github.com>
2025-11-04 00:41:06 +0000

    Update mason-registry lock to 2025-11-03-sunny-magic

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-03-mere-oven"
+M.registry_release = "github:mason-org/mason-registry@2025-11-03-sunny-magic"
 
 return M
