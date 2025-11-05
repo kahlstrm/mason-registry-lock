331b07b4c93f3246f64c6a087e67522ff24a71d9
GitHub Action <action@github.com>
2025-11-05 00:42:49 +0000

    Update mason-registry lock to 2025-11-04-solid-weapon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-04-lowly-angel"
+M.registry_release = "github:mason-org/mason-registry@2025-11-04-solid-weapon"
 
 return M
