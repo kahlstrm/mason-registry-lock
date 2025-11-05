db7961100eb2178576c837ff1ec31465b69f14c5
GitHub Action <action@github.com>
2025-11-05 00:42:49 +0000

    Update mason-registry lock to 2025-11-04-lanky-bun

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-04-portly-petrol"
+M.registry_release = "github:mason-org/mason-registry@2025-11-04-lanky-bun"
 
 return M
