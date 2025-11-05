4bbed7e7ea9ff911c33f2604c46cefa5787a0136
GitHub Action <action@github.com>
2025-11-05 00:42:49 +0000

    Update mason-registry lock to 2025-11-04-spicy-column

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-04-tory-lily"
+M.registry_release = "github:mason-org/mason-registry@2025-11-04-spicy-column"
 
 return M
