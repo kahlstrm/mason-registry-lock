f422b6b4eb368b2be467624d4c2c832238813508
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-flawed-lilac

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-aloof-clam"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-flawed-lilac"
 
 return M
