7264887d7e9c99740317e6aa7bca19a8f78a2e78
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-rubber-find

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-grown-helium"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-rubber-find"
 
 return M
