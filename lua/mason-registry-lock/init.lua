521d4309f7185b108f6c918889310835ec981b0c
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-dismal-tim

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-irish-snow"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-dismal-tim"
 
 return M
