013c22ce7bf3bb6dab061db416d49f8f87ce69d0
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-noted-crack

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-rubber-find"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-noted-crack"
 
 return M
