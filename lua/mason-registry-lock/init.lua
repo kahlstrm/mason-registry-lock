39ad879577c94159e161b42017224478651b3018
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-irish-snow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-agile-lemon"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-irish-snow"
 
 return M
