369bfd94ec604455c85fe1e4342559e140882edb
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-grown-helium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-damp-pony"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-grown-helium"
 
 return M
