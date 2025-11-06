319951a70cfa33aae90da1cafb871ec976415f9d
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-amused-ton

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-noted-crack"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-amused-ton"
 
 return M
