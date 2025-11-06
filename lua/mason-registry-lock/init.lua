93f7f4a8f7e128840e442764deca7936e97f789e
GitHub Action <action@github.com>
2025-11-06 00:41:26 +0000

    Update mason-registry lock to 2025-11-05-upper-flight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-fake-jaw"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-upper-flight"
 
 return M
