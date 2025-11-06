9811b5dfe2ba6bc9689aeabb5cb16fd1e6670dcf
GitHub Action <action@github.com>
2025-11-06 00:41:25 +0000

    Update mason-registry lock to 2025-11-05-clumsy-theory

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-05-chummy-iniom"
+M.registry_release = "github:mason-org/mason-registry@2025-11-05-clumsy-theory"
 
 return M
