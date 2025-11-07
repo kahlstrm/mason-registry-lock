71ac7f1978026708a756595e909810d9e1566383
GitHub Action <action@github.com>
2025-11-07 00:41:45 +0000

    Update mason-registry lock to 2025-11-06-remote-pacus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-06-dual-gender"
+M.registry_release = "github:mason-org/mason-registry@2025-11-06-remote-pacus"
 
 return M
