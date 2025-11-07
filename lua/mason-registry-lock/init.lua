1d4283e9e02c141ec4a1389ab69493855489cf78
GitHub Action <action@github.com>
2025-11-07 00:41:45 +0000

    Update mason-registry lock to 2025-11-06-kind-linda

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-06-measly-data"
+M.registry_release = "github:mason-org/mason-registry@2025-11-06-kind-linda"
 
 return M
