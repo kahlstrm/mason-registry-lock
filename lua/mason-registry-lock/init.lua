c19a1256b1d308dc1f7b2c4b56fd8bf1191e7d50
GitHub Action <action@github.com>
2025-11-07 00:41:45 +0000

    Update mason-registry lock to 2025-11-06-round-space

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-06-spotty-road"
+M.registry_release = "github:mason-org/mason-registry@2025-11-06-round-space"
 
 return M
