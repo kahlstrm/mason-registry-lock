26d8e11a0bc0b4d312d606df231634a0fcb7a78b
GitHub Action <action@github.com>
2025-11-07 00:41:45 +0000

    Update mason-registry lock to 2025-11-06-dual-gender

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-06-smart-help"
+M.registry_release = "github:mason-org/mason-registry@2025-11-06-dual-gender"
 
 return M
