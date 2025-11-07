14a889a442dd4b907505a1529fe5bb6ab49df6ab
GitHub Action <action@github.com>
2025-11-07 00:41:45 +0000

    Update mason-registry lock to 2025-11-06-frizzy-dew

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-06-frigid-ink"
+M.registry_release = "github:mason-org/mason-registry@2025-11-06-frizzy-dew"
 
 return M
