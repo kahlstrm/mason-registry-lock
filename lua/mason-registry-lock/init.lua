6967b57ffe867ec52eca87a00f62fa2ab1dd03ae
GitHub Action <action@github.com>
2025-11-07 00:41:45 +0000

    Update mason-registry lock to 2025-11-06-wiggly-beaver

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-06-forked-japan"
+M.registry_release = "github:mason-org/mason-registry@2025-11-06-wiggly-beaver"
 
 return M
