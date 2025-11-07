6ef4868f89579886ea5d8c113c268c0e316daf68
GitHub Action <action@github.com>
2025-11-07 00:41:45 +0000

    Update mason-registry lock to 2025-11-06-forked-japan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-06-frizzy-dew"
+M.registry_release = "github:mason-org/mason-registry@2025-11-06-forked-japan"
 
 return M
