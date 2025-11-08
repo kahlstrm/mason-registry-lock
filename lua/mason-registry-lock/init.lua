7079f19fd68a1a8e1934c7be6b144ee1dcca659c
GitHub Action <action@github.com>
2025-11-08 00:38:56 +0000

    Update mason-registry lock to 2025-11-07-sore-stream

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-07-sturdy-mine"
+M.registry_release = "github:mason-org/mason-registry@2025-11-07-sore-stream"
 
 return M
