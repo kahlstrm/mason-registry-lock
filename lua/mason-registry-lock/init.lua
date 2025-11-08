0721e8e520be0072f92949574e3f047e206981c5
GitHub Action <action@github.com>
2025-11-08 00:38:56 +0000

    Update mason-registry lock to 2025-11-07-middle-basin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-07-ablaze-minnow"
+M.registry_release = "github:mason-org/mason-registry@2025-11-07-middle-basin"
 
 return M
