976110eb1e3809acad4d73bb2cddd2755d15d775
GitHub Action <action@github.com>
2025-11-08 00:38:56 +0000

    Update mason-registry lock to 2025-11-07-tense-hair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-07-unusual-town"
+M.registry_release = "github:mason-org/mason-registry@2025-11-07-tense-hair"
 
 return M
