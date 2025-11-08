60301a48f00c59330f9927d494e5ead020a30914
GitHub Action <action@github.com>
2025-11-08 00:38:56 +0000

    Update mason-registry lock to 2025-11-07-ablaze-minnow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-07-rural-tongue"
+M.registry_release = "github:mason-org/mason-registry@2025-11-07-ablaze-minnow"
 
 return M
