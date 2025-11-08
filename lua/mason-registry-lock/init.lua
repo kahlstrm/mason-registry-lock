57e2ded0f0bbd9cf2194d83f1a1c618fb9930ff7
GitHub Action <action@github.com>
2025-11-08 00:38:56 +0000

    Update mason-registry lock to 2025-11-07-melodic-curler

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-07-sane-blouse"
+M.registry_release = "github:mason-org/mason-registry@2025-11-07-melodic-curler"
 
 return M
