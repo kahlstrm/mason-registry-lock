d376ef58f784a69384d218811058c17703ebf30a
GitHub Action <action@github.com>
2025-11-09 00:44:15 +0000

    Update mason-registry lock to 2025-11-08-white-swim

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-08-lone-coconut"
+M.registry_release = "github:mason-org/mason-registry@2025-11-08-white-swim"
 
 return M
