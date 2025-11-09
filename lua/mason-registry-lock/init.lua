ca982452bbfa733cfaa666016fdea6c2de2b9cfb
GitHub Action <action@github.com>
2025-11-09 00:44:15 +0000

    Update mason-registry lock to 2025-11-08-equal-value

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-08-vanilla-calf"
+M.registry_release = "github:mason-org/mason-registry@2025-11-08-equal-value"
 
 return M
