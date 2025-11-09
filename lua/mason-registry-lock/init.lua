4ea4a1ab4d16adc64e3e8d73649f023f943fb7f5
GitHub Action <action@github.com>
2025-11-09 00:44:15 +0000

    Update mason-registry lock to 2025-11-08-lone-coconut

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-08-female-jumper"
+M.registry_release = "github:mason-org/mason-registry@2025-11-08-lone-coconut"
 
 return M
