b4e33e22cc012125afcab4918666f7ffd53fcb90
GitHub Action <action@github.com>
2025-11-09 00:44:15 +0000

    Update mason-registry lock to 2025-11-08-female-jumper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-08-cruel-flavor"
+M.registry_release = "github:mason-org/mason-registry@2025-11-08-female-jumper"
 
 return M
