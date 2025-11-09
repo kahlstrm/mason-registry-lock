a1a73b9c533b723e7490b9230968ed2d24b0a48f
GitHub Action <action@github.com>
2025-11-09 00:44:15 +0000

    Update mason-registry lock to 2025-11-08-cruel-flavor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-08-lime-airplane"
+M.registry_release = "github:mason-org/mason-registry@2025-11-08-cruel-flavor"
 
 return M
