bd9923f7b0dd4f409071c2aeea5f9fce58b571c0
GitHub Action <action@github.com>
2025-11-11 00:42:36 +0000

    Update mason-registry lock to 2025-11-10-deaf-week

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-10-solar-crate"
+M.registry_release = "github:mason-org/mason-registry@2025-11-10-deaf-week"
 
 return M
