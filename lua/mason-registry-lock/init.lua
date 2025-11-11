ba3a3ae27766a0e4db5d36240186418432c10112
GitHub Action <action@github.com>
2025-11-11 00:42:36 +0000

    Update mason-registry lock to 2025-11-10-solar-crate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-10-sloppy-jumper"
+M.registry_release = "github:mason-org/mason-registry@2025-11-10-solar-crate"
 
 return M
