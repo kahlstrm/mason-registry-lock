9e1d392952213228979f578e55dee8a9d6acc44d
GitHub Action <action@github.com>
2025-11-12 00:42:28 +0000

    Update mason-registry lock to 2025-11-11-rotten-class

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-droopy-armor"
+M.registry_release = "github:mason-org/mason-registry@2025-11-11-rotten-class"
 
 return M
