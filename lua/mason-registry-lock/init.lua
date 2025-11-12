1ffcdf0f59414bc8fe6951e898e2630b7a0ffd3d
GitHub Action <action@github.com>
2025-11-12 00:42:28 +0000

    Update mason-registry lock to 2025-11-11-numb-fire

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-above-ounce"
+M.registry_release = "github:mason-org/mason-registry@2025-11-11-numb-fire"
 
 return M
