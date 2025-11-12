a48ba0811b52a0adabb4a031ac31d30b3ed0ddbb
GitHub Action <action@github.com>
2025-11-12 00:42:28 +0000

    Update mason-registry lock to 2025-11-11-square-gauge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-rowdy-hose"
+M.registry_release = "github:mason-org/mason-registry@2025-11-11-square-gauge"
 
 return M
