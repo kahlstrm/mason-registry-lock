ab8a9526dc2f1df88720cfd4588585c64473518f
GitHub Action <action@github.com>
2025-11-12 00:42:28 +0000

    Update mason-registry lock to 2025-11-11-direct-shame

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-macho-stew"
+M.registry_release = "github:mason-org/mason-registry@2025-11-11-direct-shame"
 
 return M
