adca07ef0611a70846e7a012660691258a451ef8
GitHub Action <action@github.com>
2025-11-12 00:42:28 +0000

    Update mason-registry lock to 2025-11-11-ready-danio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-rotten-class"
+M.registry_release = "github:mason-org/mason-registry@2025-11-11-ready-danio"
 
 return M
