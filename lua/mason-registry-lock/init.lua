1d770ee9825125cd789dd9eaff7f4357b7d27ffd
GitHub Action <action@github.com>
2025-11-12 00:42:28 +0000

    Update mason-registry lock to 2025-11-11-deep-chalk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-square-gauge"
+M.registry_release = "github:mason-org/mason-registry@2025-11-11-deep-chalk"
 
 return M
