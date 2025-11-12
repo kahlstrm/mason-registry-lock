dd3140c2b3709e3ed682618db80830357c767fd1
GitHub Action <action@github.com>
2025-11-12 00:42:28 +0000

    Update mason-registry lock to 2025-11-11-holy-beaver

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-fat-guilt"
+M.registry_release = "github:mason-org/mason-registry@2025-11-11-holy-beaver"
 
 return M
