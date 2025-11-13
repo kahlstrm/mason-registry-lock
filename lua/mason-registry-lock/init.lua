71cf8afba69bcae1ff3d64a2a5311ee68c61ec83
GitHub Action <action@github.com>
2025-11-13 00:42:11 +0000

    Update mason-registry lock to 2025-11-12-rear-stage

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-11-smelly-fight"
+M.registry_release = "github:mason-org/mason-registry@2025-11-12-rear-stage"
 
 return M
