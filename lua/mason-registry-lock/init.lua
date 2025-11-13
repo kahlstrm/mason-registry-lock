d805a1c2b82c2fdca7b502729dc5094580a21bc0
GitHub Action <action@github.com>
2025-11-13 00:42:11 +0000

    Update mason-registry lock to 2025-11-12-kosher-emery

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-12-prior-waiter"
+M.registry_release = "github:mason-org/mason-registry@2025-11-12-kosher-emery"
 
 return M
