039f6d2df8bedebc3e9f8e69ededb4e24844aecd
GitHub Action <action@github.com>
2025-11-13 00:42:11 +0000

    Update mason-registry lock to 2025-11-12-marked-chip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-12-bottled-kale"
+M.registry_release = "github:mason-org/mason-registry@2025-11-12-marked-chip"
 
 return M
