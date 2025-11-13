9515d7ff74113e8c4f65e6bafa72c3733c984687
GitHub Action <action@github.com>
2025-11-13 00:42:11 +0000

    Update mason-registry lock to 2025-11-12-impure-crush

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-12-fun-lemur"
+M.registry_release = "github:mason-org/mason-registry@2025-11-12-impure-crush"
 
 return M
