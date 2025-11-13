6a2463b8018b06f4f3120d419bac20c196880834
GitHub Action <action@github.com>
2025-11-13 00:42:11 +0000

    Update mason-registry lock to 2025-11-12-bottled-kale

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-12-full-crate"
+M.registry_release = "github:mason-org/mason-registry@2025-11-12-bottled-kale"
 
 return M
