5ef80cdbfdf3aeb98ae450b21f6c9e507345d559
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-deeply-switch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-jolly-porter"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-deeply-switch"
 
 return M
