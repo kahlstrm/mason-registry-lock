072f14336628a47cb395091495979cdbb12c89ac
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-joint-cerium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-dull-spork"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-joint-cerium"
 
 return M
