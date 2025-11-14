972dfcb8871cb40c6c3052188c63e9e57e88e38a
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-wonderful-piano

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-aboard-fold"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-wonderful-piano"
 
 return M
