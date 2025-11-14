a000d42a8bf5b1199409b5c228f776b05e1bb090
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-baby-iodine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-wonderful-piano"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-baby-iodine"
 
 return M
