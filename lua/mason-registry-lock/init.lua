267a4d9c3b020aa2c3a9333555bb2978845f215f
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-fussy-camera

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-hollow-cello"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-fussy-camera"
 
 return M
