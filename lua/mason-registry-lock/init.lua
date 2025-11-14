2b001e60e626f6ac7500029bc596d594657c76c0
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-unused-price

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-happy-santa"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-unused-price"
 
 return M
