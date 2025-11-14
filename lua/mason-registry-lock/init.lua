147f6a260e17b7cc0a403d979fd27e88acb1246b
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-hollow-cello

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-arctic-pear"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-hollow-cello"
 
 return M
