930b2cb7c9bf25cd8d463eba0f61f056e538dd71
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-mild-ferret

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-snappy-motion"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-mild-ferret"
 
 return M
