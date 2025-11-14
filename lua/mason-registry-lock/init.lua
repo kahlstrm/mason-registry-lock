5a250f61d5fa15f5e27f104044ee26d2a9f0b8c6
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-dull-spork

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-dense-family"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-dull-spork"
 
 return M
