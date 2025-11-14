2e34b773895b2beed96142aa491bf9a14d4ab10b
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-dense-family

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-deeply-switch"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-dense-family"
 
 return M
