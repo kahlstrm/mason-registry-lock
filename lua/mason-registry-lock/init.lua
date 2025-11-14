5ec946f7b1d02de9b53e638cb8cc68b2d9966e81
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-aboard-fold

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-13-mild-ferret"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-aboard-fold"
 
 return M
