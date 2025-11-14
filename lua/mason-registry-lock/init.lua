a4b2451c7589f2e79829c31620d946342d6ce1c0
GitHub Action <action@github.com>
2025-11-14 00:42:19 +0000

    Update mason-registry lock to 2025-11-13-double-move

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-12-marked-chip"
+M.registry_release = "github:mason-org/mason-registry@2025-11-13-double-move"
 
 return M
