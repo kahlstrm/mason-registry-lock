e595a0e3119b8c9c4e3ebcb56975edde6aa45df8
GitHub Action <action@github.com>
2025-11-15 00:40:40 +0000

    Update mason-registry lock to 2025-11-14-dense-record

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-14-rowdy-land"
+M.registry_release = "github:mason-org/mason-registry@2025-11-14-dense-record"
 
 return M
