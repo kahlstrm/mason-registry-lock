f153bddcd10e927721b2957e66c2a628e3d8b829
GitHub Action <action@github.com>
2025-11-15 00:40:41 +0000

    Update mason-registry lock to 2025-11-14-white-garden

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-14-scarce-cello"
+M.registry_release = "github:mason-org/mason-registry@2025-11-14-white-garden"
 
 return M
