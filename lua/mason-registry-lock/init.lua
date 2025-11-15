c5654bd1eb21c852b0e785ed0c71087cc9959f15
GitHub Action <action@github.com>
2025-11-15 00:40:40 +0000

    Update mason-registry lock to 2025-11-14-severe-moat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-14-real-bear"
+M.registry_release = "github:mason-org/mason-registry@2025-11-14-severe-moat"
 
 return M
