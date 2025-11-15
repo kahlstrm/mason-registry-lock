eea1f5826be01747a5ea2c242c221ff53987f85f
GitHub Action <action@github.com>
2025-11-15 00:40:41 +0000

    Update mason-registry lock to 2025-11-14-tender-copy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-14-wee-repair"
+M.registry_release = "github:mason-org/mason-registry@2025-11-14-tender-copy"
 
 return M
