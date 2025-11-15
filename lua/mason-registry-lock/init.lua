7d488a576ff744ac8dbab89ac5ee6a59281cf8e9
GitHub Action <action@github.com>
2025-11-15 00:40:41 +0000

    Update mason-registry lock to 2025-11-14-calm-diving

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-14-ragged-dongle"
+M.registry_release = "github:mason-org/mason-registry@2025-11-14-calm-diving"
 
 return M
