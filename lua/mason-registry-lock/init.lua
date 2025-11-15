e83cda1ed838d9c86f441afa3b8bbbd95f7cdff1
GitHub Action <action@github.com>
2025-11-15 00:40:41 +0000

    Update mason-registry lock to 2025-11-14-scarce-cello

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-14-calm-diving"
+M.registry_release = "github:mason-org/mason-registry@2025-11-14-scarce-cello"
 
 return M
