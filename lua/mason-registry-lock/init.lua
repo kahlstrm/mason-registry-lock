031807bfce53d83303c226c823fb7fba012b0ac5
GitHub Action <action@github.com>
2025-11-15 00:40:40 +0000

    Update mason-registry lock to 2025-11-14-weak-day

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-14-grand-appeal"
+M.registry_release = "github:mason-org/mason-registry@2025-11-14-weak-day"
 
 return M
