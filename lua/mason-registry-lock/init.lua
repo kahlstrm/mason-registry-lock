230f7a9373b0f567c9f8485ad47a6e851c2623a0
GitHub Action <action@github.com>
2025-11-16 00:45:08 +0000

    Update mason-registry lock to 2025-11-15-best-elver

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-15-giddy-loss"
+M.registry_release = "github:mason-org/mason-registry@2025-11-15-best-elver"
 
 return M
