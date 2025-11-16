f53bd2f2bcdbb755b8fba2418b4f7a2b60cefb76
GitHub Action <action@github.com>
2025-11-16 00:45:08 +0000

    Update mason-registry lock to 2025-11-15-handy-find

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-15-latin-button"
+M.registry_release = "github:mason-org/mason-registry@2025-11-15-handy-find"
 
 return M
