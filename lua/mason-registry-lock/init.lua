2119ceb745c729364440d127ffea635deff0ab35
GitHub Action <action@github.com>
2025-11-17 00:43:24 +0000

    Update mason-registry lock to 2025-11-16-ripe-tax

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-16-soviet-sponge"
+M.registry_release = "github:mason-org/mason-registry@2025-11-16-ripe-tax"
 
 return M
