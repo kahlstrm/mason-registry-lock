1eb399bf7644d238a1ddbc09e7e0f88dd4ca4a57
GitHub Action <action@github.com>
2025-11-18 00:41:46 +0000

    Update mason-registry lock to 2025-11-17-cyan-cony

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-17-gothic-molly"
+M.registry_release = "github:mason-org/mason-registry@2025-11-17-cyan-cony"
 
 return M
