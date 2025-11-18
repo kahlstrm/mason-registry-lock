41028ad8c0836ef04d8a1091f42a1c4c09e254ff
GitHub Action <action@github.com>
2025-11-18 00:41:46 +0000

    Update mason-registry lock to 2025-11-17-smug-able

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-17-double-crab"
+M.registry_release = "github:mason-org/mason-registry@2025-11-17-smug-able"
 
 return M
