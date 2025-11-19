52175f1a23c9184a56229c511f4f22ccc5d077da
GitHub Action <action@github.com>
2025-11-19 00:42:05 +0000

    Update mason-registry lock to 2025-11-18-fragile-coconut

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-17-third-shame"
+M.registry_release = "github:mason-org/mason-registry@2025-11-18-fragile-coconut"
 
 return M
