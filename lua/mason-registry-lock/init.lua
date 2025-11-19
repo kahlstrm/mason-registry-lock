e05014b83e769cc27d9508e7a7bae19e8e703d88
GitHub Action <action@github.com>
2025-11-19 00:42:05 +0000

    Update mason-registry lock to 2025-11-18-serene-wedge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-18-keen-edge"
+M.registry_release = "github:mason-org/mason-registry@2025-11-18-serene-wedge"
 
 return M
