415feceae8d10f7f2bb50dc9a5bad74ffe1a54dc
GitHub Action <action@github.com>
2025-11-19 00:42:05 +0000

    Update mason-registry lock to 2025-11-18-nice-soup

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-18-showy-debtor"
+M.registry_release = "github:mason-org/mason-registry@2025-11-18-nice-soup"
 
 return M
