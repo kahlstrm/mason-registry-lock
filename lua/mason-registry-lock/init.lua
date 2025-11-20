3354b2e5987d2a372557c75fd25c7121e0f12506
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-19-dotted-glove

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-19-somber-knight"
+M.registry_release = "github:mason-org/mason-registry@2025-11-19-dotted-glove"
 
 return M
