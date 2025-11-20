33f0509c557ba1fe54a4b9b428a9817f216c26a8
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-19-elite-bream

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-19-smug-maraca"
+M.registry_release = "github:mason-org/mason-registry@2025-11-19-elite-bream"
 
 return M
