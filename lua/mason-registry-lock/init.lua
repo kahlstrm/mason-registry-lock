49a5cf64315c467f168edcd7d2abe37b13a20f97
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-19-cyan-crust

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-19-sweet-branch"
+M.registry_release = "github:mason-org/mason-registry@2025-11-19-cyan-crust"
 
 return M
