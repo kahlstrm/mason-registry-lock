75e197193d1551f661b424adba1277d8d12fcfab
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-19-active-cherry

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-19-blind-earth"
+M.registry_release = "github:mason-org/mason-registry@2025-11-19-active-cherry"
 
 return M
