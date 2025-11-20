385ba5d3af7d9f9ad5ade3ce451a220521745005
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-19-smug-maraca

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-19-rusty-pruner"
+M.registry_release = "github:mason-org/mason-registry@2025-11-19-smug-maraca"
 
 return M
