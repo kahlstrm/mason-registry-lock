b1222a5c60c904ff5b7915b8dcd85cf8a5ff07e1
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-20-big-silica

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-20-versed-brown"
+M.registry_release = "github:mason-org/mason-registry@2025-11-20-big-silica"
 
 return M
