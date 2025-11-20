7f7b56c987b556713bed868778196f0d708d027d
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-19-icy-umbrella

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-19-elated-amount"
+M.registry_release = "github:mason-org/mason-registry@2025-11-19-icy-umbrella"
 
 return M
