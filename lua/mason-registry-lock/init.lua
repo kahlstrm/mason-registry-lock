d83735aaa9648037f987a46b22fc3b89e79558a7
GitHub Action <action@github.com>
2025-11-20 00:40:50 +0000

    Update mason-registry lock to 2025-11-19-savory-galley

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-19-flaky-smell"
+M.registry_release = "github:mason-org/mason-registry@2025-11-19-savory-galley"
 
 return M
