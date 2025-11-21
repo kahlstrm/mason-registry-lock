d2d953bd435890d0b12fdc1a2e13d9b26cdbe3c9
GitHub Action <action@github.com>
2025-11-21 00:41:47 +0000

    Update mason-registry lock to 2025-11-20-bossy-platy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-20-noted-blow"
+M.registry_release = "github:mason-org/mason-registry@2025-11-20-bossy-platy"
 
 return M
