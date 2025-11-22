fad1ec2cfe37366761d14713a33ad50132bab6ac
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-tight-dahlia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-asian-cymbal"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-tight-dahlia"
 
 return M
