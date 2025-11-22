1afcd268b605fbd823491968b4cb1f724130b6e0
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-tawdry-sodium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-tight-dahlia"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-tawdry-sodium"
 
 return M
