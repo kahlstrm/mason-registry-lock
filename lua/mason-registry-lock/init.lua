42ec23a890c19c2fc5646fa4a19f55e60d9d98df
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-oblong-soap

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-quaint-six"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-oblong-soap"
 
 return M
