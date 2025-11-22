d3c32fdc9baa3e0107410f45e74777af8a5d4019
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-fixed-world

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-worse-pan"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-fixed-world"
 
 return M
