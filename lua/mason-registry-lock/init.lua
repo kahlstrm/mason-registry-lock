1d589a3d015ee599d186f1dd9d340300563d93c1
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-edible-waiter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-ancient-ankle"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-edible-waiter"
 
 return M
