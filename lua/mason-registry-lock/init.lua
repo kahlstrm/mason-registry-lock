cbb560088923eb6a8f1d10ea674dcf79bba9d81d
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-worse-pan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-modern-plate"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-worse-pan"
 
 return M
