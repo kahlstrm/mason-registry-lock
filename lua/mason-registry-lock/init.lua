a920aa74224a066390001c0f1c4cabc668bbf7cd
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-quaint-six

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-second-soul"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-quaint-six"
 
 return M
