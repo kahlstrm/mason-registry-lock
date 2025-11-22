50576458b70dc506d43a6f3a999be281f4a84797
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-chosen-german

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-homely-comet"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-chosen-german"
 
 return M
