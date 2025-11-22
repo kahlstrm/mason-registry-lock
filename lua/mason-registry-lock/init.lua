d82fb319d2993370ec24b4c351a01952ab0a08f5
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-asian-cymbal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-bloody-brazil"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-asian-cymbal"
 
 return M
