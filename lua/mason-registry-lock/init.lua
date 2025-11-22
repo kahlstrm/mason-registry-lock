2837d5a0ee9fc684619818bec1c5c00ad4e32fe2
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-flimsy-dace

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-fixed-world"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-flimsy-dace"
 
 return M
