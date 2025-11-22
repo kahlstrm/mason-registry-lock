45543d3d7a05329e54a385953665e6a6534fb2bf
GitHub Action <action@github.com>
2025-11-22 00:39:55 +0000

    Update mason-registry lock to 2025-11-21-lost-gator

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-21-tawdry-sodium"
+M.registry_release = "github:mason-org/mason-registry@2025-11-21-lost-gator"
 
 return M
