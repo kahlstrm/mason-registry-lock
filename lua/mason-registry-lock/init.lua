05218fff3fffb26a28879dea385e61c1a284c7ab
GitHub Action <action@github.com>
2025-11-23 00:48:23 +0000

    Update mason-registry lock to 2025-11-22-unused-roast

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-22-raspy-ping"
+M.registry_release = "github:mason-org/mason-registry@2025-11-22-unused-roast"
 
 return M
