a1e5368c954fc4f7895e8bd99ff2662eb14d4164
GitHub Action <action@github.com>
2025-11-23 00:48:23 +0000

    Update mason-registry lock to 2025-11-22-next-grouse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-22-front-track"
+M.registry_release = "github:mason-org/mason-registry@2025-11-22-next-grouse"
 
 return M
