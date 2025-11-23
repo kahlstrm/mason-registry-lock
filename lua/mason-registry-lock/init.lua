a274196b53eba606fc0d31cab375077297208ca1
GitHub Action <action@github.com>
2025-11-23 00:48:23 +0000

    Update mason-registry lock to 2025-11-22-windy-tire

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-22-crabby-cart"
+M.registry_release = "github:mason-org/mason-registry@2025-11-22-windy-tire"
 
 return M
