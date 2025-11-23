2d75e3b1a299dbd74c02e3b2dbf7a13561f195e5
GitHub Action <action@github.com>
2025-11-23 00:48:23 +0000

    Update mason-registry lock to 2025-11-22-crabby-cart

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-22-unused-roast"
+M.registry_release = "github:mason-org/mason-registry@2025-11-22-crabby-cart"
 
 return M
