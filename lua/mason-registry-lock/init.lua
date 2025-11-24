44cad2e2fd6616b2145777148d653dee7859ea51
GitHub Action <action@github.com>
2025-11-24 00:44:59 +0000

    Update mason-registry lock to 2025-11-23-usable-true

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-23-aback-save"
+M.registry_release = "github:mason-org/mason-registry@2025-11-23-usable-true"
 
 return M
