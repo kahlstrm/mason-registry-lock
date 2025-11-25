bd0143247801dc775215d048226e202f22dffd04
GitHub Action <action@github.com>
2025-11-25 00:40:59 +0000

    Update mason-registry lock to 2025-11-24-lean-dace

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-24-asian-moose"
+M.registry_release = "github:mason-org/mason-registry@2025-11-24-lean-dace"
 
 return M
