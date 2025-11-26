2d70f206ef55718e5d0dcbd114fe3200f95b4e79
GitHub Action <action@github.com>
2025-11-26 00:42:12 +0000

    Update mason-registry lock to 2025-11-25-prior-taste

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-25-rich-robert"
+M.registry_release = "github:mason-org/mason-registry@2025-11-25-prior-taste"
 
 return M
