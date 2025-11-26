96a3f22d4ac6b2bb1e74c26501e5c3fddbec0f7f
GitHub Action <action@github.com>
2025-11-26 00:42:12 +0000

    Update mason-registry lock to 2025-11-25-awake-quart

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-25-down-vein"
+M.registry_release = "github:mason-org/mason-registry@2025-11-25-awake-quart"
 
 return M
