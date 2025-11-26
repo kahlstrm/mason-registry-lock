84f0b14f03d6ef08fe9f12ca05e6bd2a2fc767a2
GitHub Action <action@github.com>
2025-11-26 00:42:12 +0000

    Update mason-registry lock to 2025-11-25-silent-text

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-25-civic-calf"
+M.registry_release = "github:mason-org/mason-registry@2025-11-25-silent-text"
 
 return M
