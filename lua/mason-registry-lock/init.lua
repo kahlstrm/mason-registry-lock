5b20fb1d1bbebc230f36214f5f2e7ca2c9341514
GitHub Action <action@github.com>
2025-11-26 00:42:12 +0000

    Update mason-registry lock to 2025-11-25-long-crush

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-25-few-wonder"
+M.registry_release = "github:mason-org/mason-registry@2025-11-25-long-crush"
 
 return M
