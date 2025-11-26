eb0094dd848a7f92c8c1121f975afe23f10eb44e
GitHub Action <action@github.com>
2025-11-26 00:42:12 +0000

    Update mason-registry lock to 2025-11-25-civic-calf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-25-round-minute"
+M.registry_release = "github:mason-org/mason-registry@2025-11-25-civic-calf"
 
 return M
