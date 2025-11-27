6b046ad55e0fa0951be60f4b0784a5b8ef1b802c
GitHub Action <action@github.com>
2025-11-27 00:41:25 +0000

    Update mason-registry lock to 2025-11-26-alike-cedar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-26-moist-saury"
+M.registry_release = "github:mason-org/mason-registry@2025-11-26-alike-cedar"
 
 return M
