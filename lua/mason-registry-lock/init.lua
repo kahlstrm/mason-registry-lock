b4077365452c76ccf33bebc5eba141e825ddf395
GitHub Action <action@github.com>
2025-11-27 00:41:25 +0000

    Update mason-registry lock to 2025-11-27-charming-slip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-27-mixed-skiing"
+M.registry_release = "github:mason-org/mason-registry@2025-11-27-charming-slip"
 
 return M
