a9b64b54183d85421646204c8ce825741aa00d81
GitHub Action <action@github.com>
2025-11-27 00:41:25 +0000

    Update mason-registry lock to 2025-11-26-clean-mile

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-26-public-blenny"
+M.registry_release = "github:mason-org/mason-registry@2025-11-26-clean-mile"
 
 return M
