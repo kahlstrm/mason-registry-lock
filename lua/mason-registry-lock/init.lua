592054cf163c2e7bc49437a1b5b0f1e1bc3f1a88
GitHub Action <action@github.com>
2025-11-28 00:40:34 +0000

    Update mason-registry lock to 2025-11-27-milky-oval

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-27-equal-dace"
+M.registry_release = "github:mason-org/mason-registry@2025-11-27-milky-oval"
 
 return M
