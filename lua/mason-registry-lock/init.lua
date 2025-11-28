2fc5b69a7c9c8258117b40a1ecdc18316daae4a7
GitHub Action <action@github.com>
2025-11-28 00:40:34 +0000

    Update mason-registry lock to 2025-11-27-static-profit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-27-fancy-stop"
+M.registry_release = "github:mason-org/mason-registry@2025-11-27-static-profit"
 
 return M
