e543f3380ed78c08b90fd6255fd28de044581744
GitHub Action <action@github.com>
2025-11-28 00:40:34 +0000

    Update mason-registry lock to 2025-11-27-robust-jump

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-27-queasy-ilisha"
+M.registry_release = "github:mason-org/mason-registry@2025-11-27-robust-jump"
 
 return M
