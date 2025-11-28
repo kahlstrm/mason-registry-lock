68181c01dd37a159d3374450b6d02abdfcee982f
GitHub Action <action@github.com>
2025-11-28 00:40:34 +0000

    Update mason-registry lock to 2025-11-27-equal-dace

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-27-charming-slip"
+M.registry_release = "github:mason-org/mason-registry@2025-11-27-equal-dace"
 
 return M
