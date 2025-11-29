2fd2aef70c94ca9227ae3d99eb758a2f0aa2df07
GitHub Action <action@github.com>
2025-11-29 00:40:35 +0000

    Update mason-registry lock to 2025-11-28-nine-field

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-28-ugly-break"
+M.registry_release = "github:mason-org/mason-registry@2025-11-28-nine-field"
 
 return M
