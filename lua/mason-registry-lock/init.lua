f7042571a7b76454c04a5b9b8400649445ae7262
GitHub Action <action@github.com>
2025-11-29 00:40:35 +0000

    Update mason-registry lock to 2025-11-28-bleak-rise

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-28-batty-bongo"
+M.registry_release = "github:mason-org/mason-registry@2025-11-28-bleak-rise"
 
 return M
