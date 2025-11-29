11fb09904f29b40ef73347d8ca3ed6c0fd4ecdb1
GitHub Action <action@github.com>
2025-11-29 00:40:35 +0000

    Update mason-registry lock to 2025-11-28-fiscal-cinema

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-28-rosy-oat"
+M.registry_release = "github:mason-org/mason-registry@2025-11-28-fiscal-cinema"
 
 return M
