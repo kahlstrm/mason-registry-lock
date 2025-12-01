5b896acf30f1ae3e3448aadeba680e85f439dbc0
GitHub Action <action@github.com>
2025-12-01 00:53:12 +0000

    Update mason-registry lock to 2025-11-30-shiny-clave

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-11-30-early-pass"
+M.registry_release = "github:mason-org/mason-registry@2025-11-30-shiny-clave"
 
 return M
