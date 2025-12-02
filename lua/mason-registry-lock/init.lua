8b834ec23d21587c576981c830ba7abf613210da
GitHub Action <action@github.com>
2025-12-02 00:43:03 +0000

    Update mason-registry lock to 2025-12-01-half-frame

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-01-kindly-silica"
+M.registry_release = "github:mason-org/mason-registry@2025-12-01-half-frame"
 
 return M
