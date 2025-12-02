75457dc1f456f7d75423460b30bde669cc3ccdff
GitHub Action <action@github.com>
2025-12-02 00:43:03 +0000

    Update mason-registry lock to 2025-12-01-stupid-forest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-01-prior-flash"
+M.registry_release = "github:mason-org/mason-registry@2025-12-01-stupid-forest"
 
 return M
