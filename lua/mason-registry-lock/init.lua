b06b7274345256f61ed3756ca71085d77481d71f
GitHub Action <action@github.com>
2025-12-02 00:43:03 +0000

    Update mason-registry lock to 2025-12-01-kindly-silica

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-01-tense-ice"
+M.registry_release = "github:mason-org/mason-registry@2025-12-01-kindly-silica"
 
 return M
