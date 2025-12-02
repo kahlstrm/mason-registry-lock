6e7ef38a5b7ace9da1c52ebc0f5834f77c3001ba
GitHub Action <action@github.com>
2025-12-02 00:43:03 +0000

    Update mason-registry lock to 2025-12-01-shaggy-debt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-01-aged-hamlet"
+M.registry_release = "github:mason-org/mason-registry@2025-12-01-shaggy-debt"
 
 return M
