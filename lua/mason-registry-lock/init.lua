d632bcc5f4241821045e9d369c4c51f2d54bc509
GitHub Action <action@github.com>
2025-12-03 00:42:31 +0000

    Update mason-registry lock to 2025-12-02-big-syrup

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-02-trashy-cloud"
+M.registry_release = "github:mason-org/mason-registry@2025-12-02-big-syrup"
 
 return M
