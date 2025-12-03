6a33e06e6f6d047dee0a709a718ba72931f164b4
GitHub Action <action@github.com>
2025-12-03 00:42:31 +0000

    Update mason-registry lock to 2025-12-02-noisy-dry

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-02-knotty-area"
+M.registry_release = "github:mason-org/mason-registry@2025-12-02-noisy-dry"
 
 return M
