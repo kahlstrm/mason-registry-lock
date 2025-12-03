aba38afb276dab1ed7e13058710ce28c86ae63b9
GitHub Action <action@github.com>
2025-12-03 00:42:31 +0000

    Update mason-registry lock to 2025-12-02-faint-shad

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-02-dense-money"
+M.registry_release = "github:mason-org/mason-registry@2025-12-02-faint-shad"
 
 return M
