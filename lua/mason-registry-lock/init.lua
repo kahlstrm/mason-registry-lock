fd05b71fef221a7e931dd8155434cc6f3200c424
GitHub Action <action@github.com>
2025-12-03 00:42:31 +0000

    Update mason-registry lock to 2025-12-02-pricey-pitch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-02-thirsty-meter"
+M.registry_release = "github:mason-org/mason-registry@2025-12-02-pricey-pitch"
 
 return M
