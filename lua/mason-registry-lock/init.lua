4f360cbe61a21feea173441125d532f8abb6fbf8
GitHub Action <action@github.com>
2025-12-03 00:42:31 +0000

    Update mason-registry lock to 2025-12-02-snoopy-filly

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-02-open-fact"
+M.registry_release = "github:mason-org/mason-registry@2025-12-02-snoopy-filly"
 
 return M
