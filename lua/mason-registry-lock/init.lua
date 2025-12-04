837e4cadb7863c6eef975af8d3afb3a7542d42f4
GitHub Action <action@github.com>
2025-12-04 00:42:53 +0000

    Update mason-registry lock to 2025-12-03-copper-pansy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-03-rebel-possum"
+M.registry_release = "github:mason-org/mason-registry@2025-12-03-copper-pansy"
 
 return M
