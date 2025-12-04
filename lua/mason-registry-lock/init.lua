faca0da990d019748f3fdb553af19215dfa72487
GitHub Action <action@github.com>
2025-12-04 00:42:52 +0000

    Update mason-registry lock to 2025-12-03-north-reason

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-03-bubbly-jungle"
+M.registry_release = "github:mason-org/mason-registry@2025-12-03-north-reason"
 
 return M
