9f931cf31b80a358d36d94fcd30d08385ce7a283
GitHub Action <action@github.com>
2025-12-04 00:42:53 +0000

    Update mason-registry lock to 2025-12-03-gigantic-socks

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-03-steel-chair"
+M.registry_release = "github:mason-org/mason-registry@2025-12-03-gigantic-socks"
 
 return M
