4b578b7c7c5bccae64d1dfdea7547bbdba15160e
GitHub Action <action@github.com>
2025-12-04 00:42:53 +0000

    Update mason-registry lock to 2025-12-03-steel-chair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-03-north-reason"
+M.registry_release = "github:mason-org/mason-registry@2025-12-03-steel-chair"
 
 return M
