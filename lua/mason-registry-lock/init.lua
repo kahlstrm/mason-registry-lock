c906daa0a731d52f3c51e633634df6bc26b69cc7
GitHub Action <action@github.com>
2025-12-04 00:42:53 +0000

    Update mason-registry lock to 2025-12-03-putrid-loss

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-03-copper-pansy"
+M.registry_release = "github:mason-org/mason-registry@2025-12-03-putrid-loss"
 
 return M
