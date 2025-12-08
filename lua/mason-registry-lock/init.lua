d8a598cbdd670dca1b9b14cc8faf594740ce073e
GitHub Action <action@github.com>
2025-12-08 00:43:50 +0000

    Update mason-registry lock to 2025-12-07-sharp-cello

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-07-jaunty-legal"
+M.registry_release = "github:mason-org/mason-registry@2025-12-07-sharp-cello"
 
 return M
