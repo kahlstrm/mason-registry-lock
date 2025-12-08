9d526e97733302a3feabb019d6696a8f09eb74b6
GitHub Action <action@github.com>
2025-12-08 00:43:50 +0000

    Update mason-registry lock to 2025-12-07-sane-rose

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-07-stuck-flute"
+M.registry_release = "github:mason-org/mason-registry@2025-12-07-sane-rose"
 
 return M
