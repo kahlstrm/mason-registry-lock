db38544b14b02b8a5aca11edea2901201fbaaa1c
GitHub Action <action@github.com>
2025-12-08 00:43:50 +0000

    Update mason-registry lock to 2025-12-07-ruby-louvar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-07-husky-debt"
+M.registry_release = "github:mason-org/mason-registry@2025-12-07-ruby-louvar"
 
 return M
