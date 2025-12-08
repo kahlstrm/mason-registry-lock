207064994cb7af8aad8d82585ef038198d977be8
GitHub Action <action@github.com>
2025-12-08 00:43:50 +0000

    Update mason-registry lock to 2025-12-07-shaggy-indium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-07-ruby-louvar"
+M.registry_release = "github:mason-org/mason-registry@2025-12-07-shaggy-indium"
 
 return M
