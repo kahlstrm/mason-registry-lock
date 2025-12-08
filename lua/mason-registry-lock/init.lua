f5dbd3c09fb4708c5dcef7608ec380ecf8067c23
GitHub Action <action@github.com>
2025-12-08 00:43:50 +0000

    Update mason-registry lock to 2025-12-07-husky-debt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-07-wobbly-coil"
+M.registry_release = "github:mason-org/mason-registry@2025-12-07-husky-debt"
 
 return M
