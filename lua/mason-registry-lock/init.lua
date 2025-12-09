7f078e7ecf4992024757d592f15eacf3a1ebf2ac
GitHub Action <action@github.com>
2025-12-09 00:43:16 +0000

    Update mason-registry lock to 2025-12-08-queasy-rotate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-07-steel-angora"
+M.registry_release = "github:mason-org/mason-registry@2025-12-08-queasy-rotate"
 
 return M
