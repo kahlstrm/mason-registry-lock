6f7ba919aac0615c6a8d9bc32dfb810a23977b62
GitHub Action <action@github.com>
2025-12-09 00:43:16 +0000

    Update mason-registry lock to 2025-12-08-green-socks

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-08-queasy-rotate"
+M.registry_release = "github:mason-org/mason-registry@2025-12-08-green-socks"
 
 return M
