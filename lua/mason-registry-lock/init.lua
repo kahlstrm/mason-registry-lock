65eb1c3af61700b368d8eed9ceb04cf23e9fb269
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-weird-chrome

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-portly-cry"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-weird-chrome"
 
 return M
