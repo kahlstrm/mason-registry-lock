22a6128a24d3875763c4cf925655f33e8f31b0e4
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-deep-quilt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-testy-legal"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-deep-quilt"
 
 return M
