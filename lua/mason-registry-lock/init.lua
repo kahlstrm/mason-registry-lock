ca36174f158ea4a0377c25b0dddad1e1676625b4
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-cooing-metal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-wet-plow"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-cooing-metal"
 
 return M
