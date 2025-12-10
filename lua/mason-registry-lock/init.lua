f8a60ef1c2838b063e52351c91c79f4053c4d16d
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-giddy-flood

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-inland-king"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-giddy-flood"
 
 return M
