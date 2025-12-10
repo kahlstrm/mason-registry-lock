a154c24dd3f2913df5e558ea3f4c75e363e4d876
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-plush-seat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-cut-waiter"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-plush-seat"
 
 return M
