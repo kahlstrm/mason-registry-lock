9e9d7ac8aa1c80dea993e8baa95ded11c3f85a52
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-cleaver-ruby

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-giddy-flood"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-cleaver-ruby"
 
 return M
