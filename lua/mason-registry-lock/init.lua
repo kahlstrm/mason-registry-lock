81c9810df9a8b8697fd9881a4296abb5ee08e33c
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-mobile-shame

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-plush-seat"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-mobile-shame"
 
 return M
