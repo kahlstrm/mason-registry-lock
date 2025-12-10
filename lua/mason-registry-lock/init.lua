8b6f1b223b75cad8020b089281336b152bcdd119
GitHub Action <action@github.com>
2025-12-10 00:44:35 +0000

    Update mason-registry lock to 2025-12-09-cut-waiter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-09-dotted-torque"
+M.registry_release = "github:mason-org/mason-registry@2025-12-09-cut-waiter"
 
 return M
