0cbaf91d9313c02583e7d02e057c076cffc5079a
GitHub Action <action@github.com>
2025-12-11 00:45:21 +0000

    Update mason-registry lock to 2025-12-10-asleep-issue

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-10-fiery-click"
+M.registry_release = "github:mason-org/mason-registry@2025-12-10-asleep-issue"
 
 return M
