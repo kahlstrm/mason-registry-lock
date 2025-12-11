f8c948c4f217e26076e7ab6184de98e0feeeba93
GitHub Action <action@github.com>
2025-12-11 00:45:20 +0000

    Update mason-registry lock to 2025-12-10-gifted-bar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-10-bubbly-crate"
+M.registry_release = "github:mason-org/mason-registry@2025-12-10-gifted-bar"
 
 return M
