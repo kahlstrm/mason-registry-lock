a46cf90a30cae7253f45cce39f238dae0e233b94
GitHub Action <action@github.com>
2025-12-11 00:45:20 +0000

    Update mason-registry lock to 2025-12-10-civic-taimen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-10-faded-shad"
+M.registry_release = "github:mason-org/mason-registry@2025-12-10-civic-taimen"
 
 return M
