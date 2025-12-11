bf9379591a67751be773adbc4a45cfd6de8914e6
GitHub Action <action@github.com>
2025-12-11 00:45:20 +0000

    Update mason-registry lock to 2025-12-10-dizzy-word

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-10-lean-brazil"
+M.registry_release = "github:mason-org/mason-registry@2025-12-10-dizzy-word"
 
 return M
