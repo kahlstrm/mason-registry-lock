c10fcd89fd2746b5bd62f147818cefdde407c898
GitHub Action <action@github.com>
2025-12-11 00:45:20 +0000

    Update mason-registry lock to 2025-12-10-melodic-fence

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-10-dizzy-word"
+M.registry_release = "github:mason-org/mason-registry@2025-12-10-melodic-fence"
 
 return M
