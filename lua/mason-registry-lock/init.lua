50751351cc8d476b1a09825d8c85ded266afed3e
GitHub Action <action@github.com>
2025-12-11 00:45:21 +0000

    Update mason-registry lock to 2025-12-10-rear-sitter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-10-subtle-tomato"
+M.registry_release = "github:mason-org/mason-registry@2025-12-10-rear-sitter"
 
 return M
