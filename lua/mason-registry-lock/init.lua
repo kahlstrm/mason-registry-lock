2839c0f7c27c2305b0f412281e881d38747c6f35
GitHub Action <action@github.com>
2025-12-12 00:44:08 +0000

    Update mason-registry lock to 2025-12-11-marked-sort

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-11-droopy-lamb"
+M.registry_release = "github:mason-org/mason-registry@2025-12-11-marked-sort"
 
 return M
