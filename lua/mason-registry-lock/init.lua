51ea0b62f22c38854f5d798411884b6965a44124
GitHub Action <action@github.com>
2025-12-12 00:44:08 +0000

    Update mason-registry lock to 2025-12-11-sneaky-heap

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-11-precious-spring"
+M.registry_release = "github:mason-org/mason-registry@2025-12-11-sneaky-heap"
 
 return M
