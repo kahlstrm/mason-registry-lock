ec5f2e7f50f606d8fd5c291178accd7c9c82c55b
GitHub Action <action@github.com>
2025-12-12 00:44:08 +0000

    Update mason-registry lock to 2025-12-11-easy-heron

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-11-spare-trail"
+M.registry_release = "github:mason-org/mason-registry@2025-12-11-easy-heron"
 
 return M
