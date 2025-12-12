c6adf6f69cef76d056cd160377ab192c08180b5a
GitHub Action <action@github.com>
2025-12-12 00:44:09 +0000

    Update mason-registry lock to 2025-12-11-frugal-knot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-11-crispy-hawk"
+M.registry_release = "github:mason-org/mason-registry@2025-12-11-frugal-knot"
 
 return M
