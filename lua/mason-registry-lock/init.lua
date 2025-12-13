68af7450725938ef88e3dc4fa0ae503f76fa647a
GitHub Action <action@github.com>
2025-12-13 00:41:45 +0000

    Update mason-registry lock to 2025-12-12-spooky-bridge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-12-bold-beet"
+M.registry_release = "github:mason-org/mason-registry@2025-12-12-spooky-bridge"
 
 return M
