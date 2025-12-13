34ee12277ee4bf73ef2258017c894577a8d4b515
GitHub Action <action@github.com>
2025-12-13 00:41:45 +0000

    Update mason-registry lock to 2025-12-12-ok-vermin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-12-lowly-store"
+M.registry_release = "github:mason-org/mason-registry@2025-12-12-ok-vermin"
 
 return M
