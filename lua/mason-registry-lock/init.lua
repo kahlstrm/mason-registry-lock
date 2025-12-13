fdc772f21882a8fee63ad02a80371a5d44bf3833
GitHub Action <action@github.com>
2025-12-13 00:41:45 +0000

    Update mason-registry lock to 2025-12-12-lowly-store

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-12-magic-poison"
+M.registry_release = "github:mason-org/mason-registry@2025-12-12-lowly-store"
 
 return M
