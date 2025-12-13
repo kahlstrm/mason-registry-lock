06e68bc7b372ab67b3148c026d57b16f63bbda79
GitHub Action <action@github.com>
2025-12-13 00:41:45 +0000

    Update mason-registry lock to 2025-12-11-wrong-ox

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-11-uneven-owner"
+M.registry_release = "github:mason-org/mason-registry@2025-12-11-wrong-ox"
 
 return M
