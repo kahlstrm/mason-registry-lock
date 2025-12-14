c4267a8efbbfa75614c8225717ca80bbf3ddb67a
GitHub Action <action@github.com>
2025-12-14 00:48:46 +0000

    Update mason-registry lock to 2025-12-13-lost-catsup

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-13-lanky-spain"
+M.registry_release = "github:mason-org/mason-registry@2025-12-13-lost-catsup"
 
 return M
