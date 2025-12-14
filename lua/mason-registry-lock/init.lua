b018770cec6a98e5737b5bd42aa41e8fcb33f6be
GitHub Action <action@github.com>
2025-12-14 00:48:46 +0000

    Update mason-registry lock to 2025-12-13-flat-mallet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-12-visual-agency"
+M.registry_release = "github:mason-org/mason-registry@2025-12-13-flat-mallet"
 
 return M
