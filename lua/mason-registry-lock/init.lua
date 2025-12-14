6a6b38c4f9b9e40339e5015eaae7302f9736d3db
GitHub Action <action@github.com>
2025-12-14 00:48:46 +0000

    Update mason-registry lock to 2025-12-13-soviet-jargon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-13-legal-turnip"
+M.registry_release = "github:mason-org/mason-registry@2025-12-13-soviet-jargon"
 
 return M
