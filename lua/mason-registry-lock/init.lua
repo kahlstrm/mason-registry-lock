8b5cfb07352175c02608b37811b5428159e48333
GitHub Action <action@github.com>
2025-12-14 00:48:46 +0000

    Update mason-registry lock to 2025-12-13-swift-gun

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-13-strange-gauge"
+M.registry_release = "github:mason-org/mason-registry@2025-12-13-swift-gun"
 
 return M
