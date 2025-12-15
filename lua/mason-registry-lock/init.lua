3e88e85aca42fce25aaac796307dee10b6745c0a
GitHub Action <action@github.com>
2025-12-15 00:47:05 +0000

    Update mason-registry lock to 2025-12-14-gritty-fender

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-14-barren-queen"
+M.registry_release = "github:mason-org/mason-registry@2025-12-14-gritty-fender"
 
 return M
