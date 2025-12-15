bf2f5de3ef6393e3f8534998b5675770f01ac0fc
GitHub Action <action@github.com>
2025-12-15 00:47:05 +0000

    Update mason-registry lock to 2025-12-14-adored-tie

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-14-gritty-fender"
+M.registry_release = "github:mason-org/mason-registry@2025-12-14-adored-tie"
 
 return M
