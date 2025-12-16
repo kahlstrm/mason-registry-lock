0389440c08cd2c07d4b6aee122c084463fba9864
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-middle-dollar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-sturdy-bubble"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-middle-dollar"
 
 return M
