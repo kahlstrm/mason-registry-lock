6d9211fdb26989cea3882b3600c6e65bbb0dbf44
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-aching-answer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-mutual-gold"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-aching-answer"
 
 return M
