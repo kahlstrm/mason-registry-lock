923ebeaf1132bd591bb983fc76dd1a693eb6f3c7
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-boring-boga

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-middle-dollar"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-boring-boga"
 
 return M
