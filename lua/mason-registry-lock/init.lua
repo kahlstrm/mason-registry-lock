7c217057581849a2d431a95aee03d545b7899b12
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-lewd-swim

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-festive-saw"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-lewd-swim"
 
 return M
