3819526042ed927438cbf3b89d1524ba3e21de58
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-orange-avenue

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-blond-yew"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-orange-avenue"
 
 return M
