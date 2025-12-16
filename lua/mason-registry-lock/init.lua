9e5da1e4a9d9605d9be4107c6d36d3554eecc493
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-prize-sycamore

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-early-shorts"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-prize-sycamore"
 
 return M
