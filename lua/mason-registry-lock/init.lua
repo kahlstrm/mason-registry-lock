da2a055c199ce8f890cba8eba0cb44eb2d5f27fc
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-pretty-cobalt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-aural-field"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-pretty-cobalt"
 
 return M
