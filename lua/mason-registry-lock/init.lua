a673a1d435bcbd578861c80be60004f17aefceab
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-muted-leaf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-orange-avenue"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-muted-leaf"
 
 return M
