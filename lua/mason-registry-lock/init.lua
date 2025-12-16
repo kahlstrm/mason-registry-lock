1ee3ea08de6a933718d36d390ed0a0a4e89751ad
GitHub Action <action@github.com>
2025-12-16 00:44:48 +0000

    Update mason-registry lock to 2025-12-15-living-green

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-15-aching-answer"
+M.registry_release = "github:mason-org/mason-registry@2025-12-15-living-green"
 
 return M
