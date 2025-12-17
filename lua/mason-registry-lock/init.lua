cc89e96a4d3818a5156aaf778ab39baad870b3c3
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-knotty-oat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-bawdy-handle"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-knotty-oat"
 
 return M
