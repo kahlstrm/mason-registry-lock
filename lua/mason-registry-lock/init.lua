e467bde6e52991de8c7f7af222bfae3667006e06
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-moldy-letter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-abrupt-circle"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-moldy-letter"
 
 return M
