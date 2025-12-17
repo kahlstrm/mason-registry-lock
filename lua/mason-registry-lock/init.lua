74eb06baa2f76c1b9160f0716fe557078b892e01
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-weekly-theory

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-knotty-oat"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-weekly-theory"
 
 return M
