c6a520406ca6a23303439620c9576e19f59dbafc
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-fat-zephyr

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-mellow-cartoon"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-fat-zephyr"
 
 return M
