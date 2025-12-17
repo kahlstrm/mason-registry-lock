2ca351c8819c35839ba5862f6591197c7dc1376e
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-untidy-seven

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-tiny-comb"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-untidy-seven"
 
 return M
