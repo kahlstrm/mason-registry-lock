7a9fc34da1c1e6ae43990c03ed9e0a5bea195c4b
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-abrupt-circle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-small-tang"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-abrupt-circle"
 
 return M
