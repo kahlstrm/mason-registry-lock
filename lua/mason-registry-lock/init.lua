4aa6daa1e383d25076df9237adcd5aa1ea9cfaa6
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-gentle-course

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-wiry-boga"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-gentle-course"
 
 return M
