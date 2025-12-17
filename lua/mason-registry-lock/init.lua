44952a15b89b0d4807ca90d34e02c40a75c48349
GitHub Action <action@github.com>
2025-12-17 00:41:24 +0000

    Update mason-registry lock to 2025-12-16-mellow-cartoon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-untidy-seven"
+M.registry_release = "github:mason-org/mason-registry@2025-12-16-mellow-cartoon"
 
 return M
