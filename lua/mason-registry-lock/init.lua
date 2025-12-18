de20176c7202468b6d0bb857f24623363e16bebc
GitHub Action <action@github.com>
2025-12-18 00:41:02 +0000

    Update mason-registry lock to 2025-12-17-evil-audio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-16-gentle-course"
+M.registry_release = "github:mason-org/mason-registry@2025-12-17-evil-audio"
 
 return M
