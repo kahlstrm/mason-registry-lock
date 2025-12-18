186ebaf2bc00d0973bdc01d661915d3c80e9b3af
GitHub Action <action@github.com>
2025-12-18 00:41:02 +0000

    Update mason-registry lock to 2025-12-17-ahead-butane

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-17-evil-audio"
+M.registry_release = "github:mason-org/mason-registry@2025-12-17-ahead-butane"
 
 return M
