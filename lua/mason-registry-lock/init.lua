0741c83a181bf6a0c92e9627a44187a21623dbc2
GitHub Action <action@github.com>
2025-12-18 00:41:02 +0000

    Update mason-registry lock to 2025-12-17-aged-lightning

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-17-noisy-horror"
+M.registry_release = "github:mason-org/mason-registry@2025-12-17-aged-lightning"
 
 return M
