cf3c5bc3c9293622e48d1e56475c8345854879a2
GitHub Action <action@github.com>
2025-12-19 00:44:29 +0000

    Update mason-registry lock to 2025-12-18-head-output

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-18-steady-card"
+M.registry_release = "github:mason-org/mason-registry@2025-12-18-head-output"
 
 return M
