1af5fcc64db04481e7fcaddc3c12e4b6c22af6dd
GitHub Action <action@github.com>
2025-12-19 00:44:28 +0000

    Update mason-registry lock to 2025-12-18-irate-route

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-18-webbed-bank"
+M.registry_release = "github:mason-org/mason-registry@2025-12-18-irate-route"
 
 return M
