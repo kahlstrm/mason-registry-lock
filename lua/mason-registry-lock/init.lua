8087f70ad54222a137478ee9a55b2be81782a3b9
GitHub Action <action@github.com>
2025-12-19 00:44:28 +0000

    Update mason-registry lock to 2025-12-18-vain-mother

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-18-foamy-comb"
+M.registry_release = "github:mason-org/mason-registry@2025-12-18-vain-mother"
 
 return M
