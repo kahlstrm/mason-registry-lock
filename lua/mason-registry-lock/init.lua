2c4f298a263588132ba6fd96e66522d333bdee3a
GitHub Action <action@github.com>
2025-12-19 00:44:28 +0000

    Update mason-registry lock to 2025-12-18-frayed-nickel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-18-knotty-chill"
+M.registry_release = "github:mason-org/mason-registry@2025-12-18-frayed-nickel"
 
 return M
