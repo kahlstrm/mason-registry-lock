a271a612171875977d18bd6dde9dd95561a78738
GitHub Action <action@github.com>
2025-12-19 00:44:29 +0000

    Update mason-registry lock to 2025-12-18-steady-card

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-18-breezy-idea"
+M.registry_release = "github:mason-org/mason-registry@2025-12-18-steady-card"
 
 return M
