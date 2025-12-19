3f7fd0d5191cb492dd7bf6600800238b0c3654ae
GitHub Action <action@github.com>
2025-12-19 00:44:29 +0000

    Update mason-registry lock to 2025-12-18-half-vacuum

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-18-head-output"
+M.registry_release = "github:mason-org/mason-registry@2025-12-18-half-vacuum"
 
 return M
