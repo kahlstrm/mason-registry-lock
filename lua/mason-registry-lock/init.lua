a6041855121d85704d22e9f0aa57f987da4b7518
GitHub Action <action@github.com>
2025-12-19 00:44:28 +0000

    Update mason-registry lock to 2025-12-18-bony-output

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-18-faint-crowd"
+M.registry_release = "github:mason-org/mason-registry@2025-12-18-bony-output"
 
 return M
