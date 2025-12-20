a0d554f59c8636bdef3c328df2167606e379139b
GitHub Action <action@github.com>
2025-12-20 00:41:44 +0000

    Update mason-registry lock to 2025-12-19-dapper-leek

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-19-icky-korean"
+M.registry_release = "github:mason-org/mason-registry@2025-12-19-dapper-leek"
 
 return M
