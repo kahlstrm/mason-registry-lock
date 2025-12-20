12b1a77db90530bd7521b00f292baa0d3725d6ad
GitHub Action <action@github.com>
2025-12-20 00:41:44 +0000

    Update mason-registry lock to 2025-12-19-limp-quiver

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-19-elated-weasel"
+M.registry_release = "github:mason-org/mason-registry@2025-12-19-limp-quiver"
 
 return M
