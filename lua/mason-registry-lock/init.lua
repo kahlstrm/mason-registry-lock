dbebddc13714e53043b4601fa05c00e000ab44a3
GitHub Action <action@github.com>
2025-12-20 00:41:44 +0000

    Update mason-registry lock to 2025-12-19-somber-bow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-19-dapper-leek"
+M.registry_release = "github:mason-org/mason-registry@2025-12-19-somber-bow"
 
 return M
