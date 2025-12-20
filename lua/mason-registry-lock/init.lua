0a76ac77bc6f2fb759bdfaf4b819e9926e6afdc1
GitHub Action <action@github.com>
2025-12-20 00:41:44 +0000

    Update mason-registry lock to 2025-12-19-perky-camp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-19-tart-match"
+M.registry_release = "github:mason-org/mason-registry@2025-12-19-perky-camp"
 
 return M
