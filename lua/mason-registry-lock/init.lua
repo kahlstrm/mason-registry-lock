d74a9bc83d42cd0e9f93a916b4d3d2b008877459
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-tense-group

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-blind-toilet"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-tense-group"
 
 return M
