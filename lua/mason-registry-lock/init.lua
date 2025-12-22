8eb9087fe1ed6dc2b931588c6b568604d989e4c7
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-thick-boot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-plain-view"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-thick-boot"
 
 return M
