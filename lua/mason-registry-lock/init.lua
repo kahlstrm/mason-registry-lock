1f08480e2a58eeefe20fc6c56a22cd7047454d48
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-swift-bobcat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-thick-boot"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-swift-bobcat"
 
 return M
