af12d58b1a2b887c78d1abefe85a87ab16fbec0e
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-tired-quilt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-grubby-nose"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-tired-quilt"
 
 return M
