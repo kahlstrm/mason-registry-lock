ad7f63b5297ea358981393abaf3ad81ffe7380bb
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-plain-view

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-winged-mouth"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-plain-view"
 
 return M
