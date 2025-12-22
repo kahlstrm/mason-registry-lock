5e03f49be2c7651bea7d6a0451c67cdf87c7b299
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-weird-opah

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-calm-timer"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-weird-opah"
 
 return M
