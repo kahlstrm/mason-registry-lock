850cc421253adc272c47de370c3f83cf4efa4387
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-grubby-nose

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-weird-opah"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-grubby-nose"
 
 return M
