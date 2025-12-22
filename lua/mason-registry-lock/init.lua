801e928f0f008b54607d24469fc04c8e3e225f02
GitHub Action <action@github.com>
2025-12-22 00:47:08 +0000

    Update mason-registry lock to 2025-12-21-blind-toilet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-21-bony-air"
+M.registry_release = "github:mason-org/mason-registry@2025-12-21-blind-toilet"
 
 return M
