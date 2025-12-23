5de520c1ddf98da303933eb4519bb8adf0cf7a82
GitHub Action <action@github.com>
2025-12-23 00:44:24 +0000

    Update mason-registry lock to 2025-12-22-overt-mary

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-22-snoopy-bank"
+M.registry_release = "github:mason-org/mason-registry@2025-12-22-overt-mary"
 
 return M
