dfe85d492ec1b9895eb5d25dadbebcc406639576
GitHub Action <action@github.com>
2025-12-23 00:44:24 +0000

    Update mason-registry lock to 2025-12-22-open-danger

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-22-golden-ruffe"
+M.registry_release = "github:mason-org/mason-registry@2025-12-22-open-danger"
 
 return M
