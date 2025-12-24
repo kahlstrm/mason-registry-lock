fd600b0e88b46615aba739f69ba8021ecfb39747
GitHub Action <action@github.com>
2025-12-24 00:44:06 +0000

    Update mason-registry lock to 2025-12-24-exact-viola

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-24-glass-deal"
+M.registry_release = "github:mason-org/mason-registry@2025-12-24-exact-viola"
 
 return M
