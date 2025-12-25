5229d86a0b082b4cf4f5d14a84501de27e91e742
GitHub Action <action@github.com>
2025-12-25 00:44:22 +0000

    Update mason-registry lock to 2025-12-24-used-brandy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-24-queasy-sandra"
+M.registry_release = "github:mason-org/mason-registry@2025-12-24-used-brandy"
 
 return M
