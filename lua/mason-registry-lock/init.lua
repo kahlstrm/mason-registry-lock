742be83f0b4e9c68a28b7c41e531edb803b7a8a1
GitHub Action <action@github.com>
2025-12-26 00:44:28 +0000

    Update mason-registry lock to 2025-12-25-quie-shelf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-25-joyous-ounce"
+M.registry_release = "github:mason-org/mason-registry@2025-12-25-quie-shelf"
 
 return M
