40db5112f310889ab3d793113dd8a188fdca6ce6
GitHub Action <action@github.com>
2025-12-26 00:44:28 +0000

    Update mason-registry lock to 2025-12-25-needy-test

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-25-sulky-anime"
+M.registry_release = "github:mason-org/mason-registry@2025-12-25-needy-test"
 
 return M
