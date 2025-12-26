2fee1fe86e8d6dced8b08316949b9a38a548acc0
GitHub Action <action@github.com>
2025-12-26 00:44:28 +0000

    Update mason-registry lock to 2025-12-25-sugary-move

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-25-scary-delete"
+M.registry_release = "github:mason-org/mason-registry@2025-12-25-sugary-move"
 
 return M
