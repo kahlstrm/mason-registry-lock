fd70ede7b1dd5120a6a95b9356e1726336cec43e
GitHub Action <action@github.com>
2025-12-28 00:51:54 +0000

    Update mason-registry lock to 2025-12-27-subtle-oat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-27-moral-bird"
+M.registry_release = "github:mason-org/mason-registry@2025-12-27-subtle-oat"
 
 return M
