6f7b1cf4eee998f9e8f8c8ab73d09400e395b18e
GitHub Action <action@github.com>
2025-12-28 00:51:54 +0000

    Update mason-registry lock to 2025-12-27-small-finger

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-27-annual-powder"
+M.registry_release = "github:mason-org/mason-registry@2025-12-27-small-finger"
 
 return M
