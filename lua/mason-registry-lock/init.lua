41b7d75ecc3c51cf7ed95c5cc156351e58de9ad7
GitHub Action <action@github.com>
2025-12-28 00:51:54 +0000

    Update mason-registry lock to 2025-12-27-broken-home

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-26-gritty-option"
+M.registry_release = "github:mason-org/mason-registry@2025-12-27-broken-home"
 
 return M
