b702c0f8a3e427ae0d001d1b2e92a6a6cded27af
GitHub Action <action@github.com>
2025-12-29 00:50:06 +0000

    Update mason-registry lock to 2025-12-29-phony-carrot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-28-frayed-steam"
+M.registry_release = "github:mason-org/mason-registry@2025-12-29-phony-carrot"
 
 return M
