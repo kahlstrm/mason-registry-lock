33975d90ef239ae4219a397b2121d63319acb6a8
GitHub Action <action@github.com>
2025-12-30 00:44:31 +0000

    Update mason-registry lock to 2025-12-29-unfit-memory

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-29-portly-kokopu"
+M.registry_release = "github:mason-org/mason-registry@2025-12-29-unfit-memory"
 
 return M
