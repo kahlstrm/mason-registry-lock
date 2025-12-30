9ece20a91b6a689c81fae2c64fa18db8defbd87a
GitHub Action <action@github.com>
2025-12-30 00:44:31 +0000

    Update mason-registry lock to 2025-12-29-lean-wrench

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-29-corny-appeal"
+M.registry_release = "github:mason-org/mason-registry@2025-12-29-lean-wrench"
 
 return M
