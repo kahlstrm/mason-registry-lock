d7246ed69d054fc07892f3682de4695009e8687b
GitHub Action <action@github.com>
2025-12-30 00:44:31 +0000

    Update mason-registry lock to 2025-12-29-corny-appeal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-29-fake-bed"
+M.registry_release = "github:mason-org/mason-registry@2025-12-29-corny-appeal"
 
 return M
