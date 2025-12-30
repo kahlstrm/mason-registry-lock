bf99c4ab04ff239710033962adf4be03852713bc
GitHub Action <action@github.com>
2025-12-30 00:44:31 +0000

    Update mason-registry lock to 2025-12-29-elated-mice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-29-phony-carrot"
+M.registry_release = "github:mason-org/mason-registry@2025-12-29-elated-mice"
 
 return M
