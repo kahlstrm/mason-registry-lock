c567fee04a808159c702987e04e24b556c654ce2
GitHub Action <action@github.com>
2025-12-31 00:45:16 +0000

    Update mason-registry lock to 2025-12-30-last-dragon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-30-vague-gnu"
+M.registry_release = "github:mason-org/mason-registry@2025-12-30-last-dragon"
 
 return M
