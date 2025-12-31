2d39716693b394ad5ad468ab55e9a704ef77ba51
GitHub Action <action@github.com>
2025-12-31 00:45:16 +0000

    Update mason-registry lock to 2025-12-30-deep-plier

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-30-rural-pigeon"
+M.registry_release = "github:mason-org/mason-registry@2025-12-30-deep-plier"
 
 return M
