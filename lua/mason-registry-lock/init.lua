709d868ec8b63139c5f5b70e54a228e3fa75f863
GitHub Action <action@github.com>
2025-12-31 00:45:16 +0000

    Update mason-registry lock to 2025-12-30-tired-neon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-30-jaunty-tendon"
+M.registry_release = "github:mason-org/mason-registry@2025-12-30-tired-neon"
 
 return M
