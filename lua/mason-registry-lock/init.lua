df5f1a069acf44f12e7ace657f4317813eb66c36
GitHub Action <action@github.com>
2025-12-31 00:45:15 +0000

    Update mason-registry lock to 2025-12-30-old-bugle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-29-unfit-memory"
+M.registry_release = "github:mason-org/mason-registry@2025-12-30-old-bugle"
 
 return M
