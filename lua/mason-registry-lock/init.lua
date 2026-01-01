1d610c17c91780e1b39946b5dcbfd18480b63432
GitHub Action <action@github.com>
2026-01-01 00:52:11 +0000

    Update mason-registry lock to 2025-12-31-tragic-case

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-31-ok-server"
+M.registry_release = "github:mason-org/mason-registry@2025-12-31-tragic-case"
 
 return M
