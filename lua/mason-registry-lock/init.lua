6d758d7f0f00a922593f5d078fdd6c873538cf8b
GitHub Action <action@github.com>
2026-01-01 00:52:11 +0000

    Update mason-registry lock to 2025-12-31-strong-window

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-31-silver-bar"
+M.registry_release = "github:mason-org/mason-registry@2025-12-31-strong-window"
 
 return M
