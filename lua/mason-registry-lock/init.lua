ef2e72e174cad75c0f7f0508051a2f572fdb0610
GitHub Action <action@github.com>
2026-01-01 00:52:11 +0000

    Update mason-registry lock to 2025-12-31-friendly-street

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-31-snappy-bugle"
+M.registry_release = "github:mason-org/mason-registry@2025-12-31-friendly-street"
 
 return M
