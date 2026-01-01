13b899aee7b2c4246db41a1b9fdae247733c6ed3
GitHub Action <action@github.com>
2026-01-01 00:52:11 +0000

    Update mason-registry lock to 2025-12-31-dirty-denim

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2025-12-31-tragic-case"
+M.registry_release = "github:mason-org/mason-registry@2025-12-31-dirty-denim"
 
 return M
