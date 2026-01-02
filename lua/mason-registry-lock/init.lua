2f955157a2a7b290ee2bc52901509431f8d679af
GitHub Action <action@github.com>
2026-01-02 00:45:28 +0000

    Update mason-registry lock to 2026-01-01-tough-aruana

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-01-fancy-kayak"
+M.registry_release = "github:mason-org/mason-registry@2026-01-01-tough-aruana"
 
 return M
