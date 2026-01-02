d8029777a01600d8b190950cc8f28205a7e20325
GitHub Action <action@github.com>
2026-01-02 00:45:28 +0000

    Update mason-registry lock to 2026-01-01-innate-option

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-01-broad-can"
+M.registry_release = "github:mason-org/mason-registry@2026-01-01-innate-option"
 
 return M
