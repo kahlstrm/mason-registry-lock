f1fd06a945196a07b4278a8aa6a916a12551375a
GitHub Action <action@github.com>
2026-01-02 00:45:28 +0000

    Update mason-registry lock to 2026-01-01-sedate-leg

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-01-innate-option"
+M.registry_release = "github:mason-org/mason-registry@2026-01-01-sedate-leg"
 
 return M
