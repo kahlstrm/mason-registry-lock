6be66695579a3bed1390b398fb149e0c974d95d6
GitHub Action <action@github.com>
2026-01-04 00:52:06 +0000

    Update mason-registry lock to 2026-01-03-swift-node

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-03-gamy-owner"
+M.registry_release = "github:mason-org/mason-registry@2026-01-03-swift-node"
 
 return M
