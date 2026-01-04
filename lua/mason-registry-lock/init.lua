446c0e2a0a23144a64ecefab7ae855ded0baae3b
GitHub Action <action@github.com>
2026-01-04 00:52:06 +0000

    Update mason-registry lock to 2026-01-03-smooth-input

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-03-flimsy-carbon"
+M.registry_release = "github:mason-org/mason-registry@2026-01-03-smooth-input"
 
 return M
