4d632d9577053aa4fd1710d46ebbe914c46ae122
GitHub Action <action@github.com>
2026-01-04 00:52:06 +0000

    Update mason-registry lock to 2026-01-03-hollow-robin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-03-smooth-input"
+M.registry_release = "github:mason-org/mason-registry@2026-01-03-hollow-robin"
 
 return M
