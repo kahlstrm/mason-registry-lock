0a7c225d03782a327cd01aef51eb3f75d45137e7
GitHub Action <action@github.com>
2026-01-04 00:52:06 +0000

    Update mason-registry lock to 2026-01-03-mushy-cornet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-03-swift-node"
+M.registry_release = "github:mason-org/mason-registry@2026-01-03-mushy-cornet"
 
 return M
