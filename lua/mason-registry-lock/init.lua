03110780574d78ec0d8bffff8e01cffdd3b9e20a
GitHub Action <action@github.com>
2026-01-04 00:52:06 +0000

    Update mason-registry lock to 2026-01-03-gamy-owner

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-03-hon-pimple"
+M.registry_release = "github:mason-org/mason-registry@2026-01-03-gamy-owner"
 
 return M
