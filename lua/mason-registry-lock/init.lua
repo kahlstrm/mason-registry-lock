5146f3624cdb3ca6229ebb51068ee1f3ca3d3ebc
GitHub Action <action@github.com>
2026-01-04 00:52:06 +0000

    Update mason-registry lock to 2026-01-03-tawdry-whiff

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-03-knobby-input"
+M.registry_release = "github:mason-org/mason-registry@2026-01-03-tawdry-whiff"
 
 return M
