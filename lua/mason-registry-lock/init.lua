8041b8c63757b8248f9d6e328df04d0089eaea29
GitHub Action <action@github.com>
2026-01-05 00:51:20 +0000

    Update mason-registry lock to 2026-01-04-mean-yarn

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-04-naive-shrine"
+M.registry_release = "github:mason-org/mason-registry@2026-01-04-mean-yarn"
 
 return M
