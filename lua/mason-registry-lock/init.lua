63f4149ab6932044e31fae57aeaf123f6239b4bd
GitHub Action <action@github.com>
2026-01-05 00:51:20 +0000

    Update mason-registry lock to 2026-01-04-naive-shrine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-04-zany-angel"
+M.registry_release = "github:mason-org/mason-registry@2026-01-04-naive-shrine"
 
 return M
