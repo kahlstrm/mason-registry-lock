a523c4c400a2991f371dca128d69c44976f953dd
GitHub Action <action@github.com>
2026-01-05 00:51:20 +0000

    Update mason-registry lock to 2026-01-04-zany-angel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-04-barren-effect"
+M.registry_release = "github:mason-org/mason-registry@2026-01-04-zany-angel"
 
 return M
