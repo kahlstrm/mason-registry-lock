1b1f6188bf9a306eda7f844902d83bfc4acba676
GitHub Action <action@github.com>
2026-01-06 00:45:44 +0000

    Update mason-registry lock to 2026-01-05-brave-card

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-05-charming-linen"
+M.registry_release = "github:mason-org/mason-registry@2026-01-05-brave-card"
 
 return M
