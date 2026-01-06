b21c17a360a79aa74070a3d3a1c849c3388e96c1
GitHub Action <action@github.com>
2026-01-06 00:45:44 +0000

    Update mason-registry lock to 2026-01-05-junior-moat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-05-cold-alley"
+M.registry_release = "github:mason-org/mason-registry@2026-01-05-junior-moat"
 
 return M
