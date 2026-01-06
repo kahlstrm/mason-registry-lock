cc79f625837089480fff0923c68b4a10b3909276
GitHub Action <action@github.com>
2026-01-06 00:45:44 +0000

    Update mason-registry lock to 2026-01-05-tough-spot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-05-weekly-grill"
+M.registry_release = "github:mason-org/mason-registry@2026-01-05-tough-spot"
 
 return M
