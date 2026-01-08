f68df8dcd7b8576f9bb53cc1cb3c9b113fe738c5
GitHub Action <action@github.com>
2026-01-08 00:45:43 +0000

    Update mason-registry lock to 2026-01-07-faded-frown

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-07-senior-cerium"
+M.registry_release = "github:mason-org/mason-registry@2026-01-07-faded-frown"
 
 return M
