de38454a25da85e1b62c741b9bdcee4d2c24bfec
GitHub Action <action@github.com>
2026-01-08 00:45:43 +0000

    Update mason-registry lock to 2026-01-07-formal-input

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-07-faded-secret"
+M.registry_release = "github:mason-org/mason-registry@2026-01-07-formal-input"
 
 return M
