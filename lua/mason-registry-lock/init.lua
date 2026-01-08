ccd26a73653020a094405174f993f6e5ab249aa5
GitHub Action <action@github.com>
2026-01-08 00:45:43 +0000

    Update mason-registry lock to 2026-01-07-swanky-jeep

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-07-faded-frown"
+M.registry_release = "github:mason-org/mason-registry@2026-01-07-swanky-jeep"
 
 return M
