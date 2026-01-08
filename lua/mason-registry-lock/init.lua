53bf073e0aaa65b9c376397623c44a6a22fd8764
GitHub Action <action@github.com>
2026-01-08 00:45:43 +0000

    Update mason-registry lock to 2026-01-07-dismal-rotate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-07-blue-press"
+M.registry_release = "github:mason-org/mason-registry@2026-01-07-dismal-rotate"
 
 return M
