d861a9cfccdd7bb3ffce2e98136e84876c5afa82
GitHub Action <action@github.com>
2026-01-09 00:45:35 +0000

    Update mason-registry lock to 2026-01-07-naive-desk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-07-magical-court"
+M.registry_release = "github:mason-org/mason-registry@2026-01-07-naive-desk"
 
 return M
