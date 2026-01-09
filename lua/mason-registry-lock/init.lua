0b5b3a36cd933f7d3034d4f450d8922529222642
GitHub Action <action@github.com>
2026-01-09 00:45:35 +0000

    Update mason-registry lock to 2026-01-08-warm-canary

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-08-eight-hedge"
+M.registry_release = "github:mason-org/mason-registry@2026-01-08-warm-canary"
 
 return M
