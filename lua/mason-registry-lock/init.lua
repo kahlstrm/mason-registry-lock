31f7e5f25a368cbb66db5030c964f531d3a215a4
GitHub Action <action@github.com>
2026-01-09 00:45:35 +0000

    Update mason-registry lock to 2026-01-08-hard-pocket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-08-unable-feet"
+M.registry_release = "github:mason-org/mason-registry@2026-01-08-hard-pocket"
 
 return M
