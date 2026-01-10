af3a6026de12a46ec4f8608c57fb66171d3913e8
GitHub Action <action@github.com>
2026-01-10 00:44:26 +0000

    Update mason-registry lock to 2026-01-09-trusty-crib

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-09-spry-belief"
+M.registry_release = "github:mason-org/mason-registry@2026-01-09-trusty-crib"
 
 return M
