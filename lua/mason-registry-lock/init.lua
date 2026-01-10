fcfe829d71332ae398b0108f12e4f0e74fed02e9
GitHub Action <action@github.com>
2026-01-10 00:44:25 +0000

    Update mason-registry lock to 2026-01-09-solid-size

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-08-evil-velvet"
+M.registry_release = "github:mason-org/mason-registry@2026-01-09-solid-size"
 
 return M
