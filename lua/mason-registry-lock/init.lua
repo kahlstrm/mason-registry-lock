ce91ce9748afe2493151a7d54ccd2c616f25a3e1
GitHub Action <action@github.com>
2026-01-10 00:44:26 +0000

    Update mason-registry lock to 2026-01-09-snug-dimple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-09-windy-leaf"
+M.registry_release = "github:mason-org/mason-registry@2026-01-09-snug-dimple"
 
 return M
