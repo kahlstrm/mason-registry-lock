d482cd7736c4fbd7e5eaf3a35846cbf46ceb720e
GitHub Action <action@github.com>
2026-01-11 00:51:50 +0000

    Update mason-registry lock to 2026-01-10-frugal-pencil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-10-proud-cymbal"
+M.registry_release = "github:mason-org/mason-registry@2026-01-10-frugal-pencil"
 
 return M
