c756f54d3261c035936489956ad69788d5f91c9d
GitHub Action <action@github.com>
2026-01-11 00:51:50 +0000

    Update mason-registry lock to 2026-01-10-native-month

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-10-gifted-growth"
+M.registry_release = "github:mason-org/mason-registry@2026-01-10-native-month"
 
 return M
