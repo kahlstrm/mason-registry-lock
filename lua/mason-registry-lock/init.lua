122a482abbafc8f901a23c534e8f897ce164e879
GitHub Action <action@github.com>
2026-01-11 00:51:50 +0000

    Update mason-registry lock to 2026-01-10-ample-drum

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-10-early-dove"
+M.registry_release = "github:mason-org/mason-registry@2026-01-10-ample-drum"
 
 return M
