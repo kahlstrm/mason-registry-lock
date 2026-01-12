f5dd0fc305d5b202efa68c867007de7b1f085ce1
GitHub Action <action@github.com>
2026-01-12 00:49:33 +0000

    Update mason-registry lock to 2026-01-11-flaky-error

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-11-delightful-fetish"
+M.registry_release = "github:mason-org/mason-registry@2026-01-11-flaky-error"
 
 return M
