ecafed295c9b3134de30316f46f93d85f88af6b4
GitHub Action <action@github.com>
2026-01-13 00:41:46 +0000

    Update mason-registry lock to 2026-01-12-sparse-steel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-12-nearby-guru"
+M.registry_release = "github:mason-org/mason-registry@2026-01-12-sparse-steel"
 
 return M
