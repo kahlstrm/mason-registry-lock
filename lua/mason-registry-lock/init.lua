b4f50cf60033bc0727861cd450de3dfd02813e3e
GitHub Action <action@github.com>
2026-01-13 00:41:46 +0000

    Update mason-registry lock to 2026-01-12-raspy-edward

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-12-inner-four"
+M.registry_release = "github:mason-org/mason-registry@2026-01-12-raspy-edward"
 
 return M
