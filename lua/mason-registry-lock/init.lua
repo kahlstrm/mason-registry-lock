877f0e851b830497373d0069e6a1c09841749812
GitHub Action <action@github.com>
2026-01-13 00:41:46 +0000

    Update mason-registry lock to 2026-01-12-gross-pilot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-12-stiff-place"
+M.registry_release = "github:mason-org/mason-registry@2026-01-12-gross-pilot"
 
 return M
