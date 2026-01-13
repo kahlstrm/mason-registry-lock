3573b0fa0168dee45eb1d62288fcd1326b61a420
GitHub Action <action@github.com>
2026-01-13 00:41:46 +0000

    Update mason-registry lock to 2026-01-12-nearby-guru

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-11-flaky-error"
+M.registry_release = "github:mason-org/mason-registry@2026-01-12-nearby-guru"
 
 return M
