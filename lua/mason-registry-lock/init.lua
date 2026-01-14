31622ff92fd47ad1341f696cf447cf97d7dc5806
GitHub Action <action@github.com>
2026-01-14 00:47:45 +0000

    Update mason-registry lock to 2026-01-13-vast-window

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-13-noisy-hyena"
+M.registry_release = "github:mason-org/mason-registry@2026-01-13-vast-window"
 
 return M
