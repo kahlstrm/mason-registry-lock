35b1c24e641d342686ee0163e5c182ff9f473574
GitHub Action <action@github.com>
2026-01-14 00:47:45 +0000

    Update mason-registry lock to 2026-01-13-best-ruffe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-13-purple-mob"
+M.registry_release = "github:mason-org/mason-registry@2026-01-13-best-ruffe"
 
 return M
