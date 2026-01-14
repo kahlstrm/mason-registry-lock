91d57f685fe8795aa47d7102a359df8f44d5c906
GitHub Action <action@github.com>
2026-01-14 00:47:45 +0000

    Update mason-registry lock to 2026-01-13-fierce-day

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-13-leafy-family"
+M.registry_release = "github:mason-org/mason-registry@2026-01-13-fierce-day"
 
 return M
