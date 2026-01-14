74bbfc02cbd4b60a2a34805495958575b9074701
GitHub Action <action@github.com>
2026-01-14 00:47:45 +0000

    Update mason-registry lock to 2026-01-13-superb-flash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-13-wordy-bakery"
+M.registry_release = "github:mason-org/mason-registry@2026-01-13-superb-flash"
 
 return M
