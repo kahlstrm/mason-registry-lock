21747f4374898071ee9ec5197fa63f4a1f6a1b2a
GitHub Action <action@github.com>
2026-01-14 00:47:45 +0000

    Update mason-registry lock to 2026-01-13-purple-mob

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-13-superb-flash"
+M.registry_release = "github:mason-org/mason-registry@2026-01-13-purple-mob"
 
 return M
