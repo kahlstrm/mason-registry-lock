984a91e1ee81ec2445d63807948764c9da9c3a49
GitHub Action <action@github.com>
2026-01-15 00:43:33 +0000

    Update mason-registry lock to 2026-01-14-shoddy-slash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-14-mutual-roll"
+M.registry_release = "github:mason-org/mason-registry@2026-01-14-shoddy-slash"
 
 return M
