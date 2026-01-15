1e420b304b9cc3a80d9667383a6169d20e29c834
GitHub Action <action@github.com>
2026-01-15 00:43:33 +0000

    Update mason-registry lock to 2026-01-14-rustic-space

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-14-absent-vision"
+M.registry_release = "github:mason-org/mason-registry@2026-01-14-rustic-space"
 
 return M
