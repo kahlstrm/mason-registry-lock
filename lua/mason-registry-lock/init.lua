4ac1e3f597bd9ab5245ae2754c6dd8a97992bb37
GitHub Action <action@github.com>
2026-01-15 00:43:33 +0000

    Update mason-registry lock to 2026-01-14-mutual-roll

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-14-east-automobile"
+M.registry_release = "github:mason-org/mason-registry@2026-01-14-mutual-roll"
 
 return M
