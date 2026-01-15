dd70f637e9ff34b082c6c3c9fed62bf93cf06624
GitHub Action <action@github.com>
2026-01-15 00:43:33 +0000

    Update mason-registry lock to 2026-01-14-weird-crowd

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-14-rustic-space"
+M.registry_release = "github:mason-org/mason-registry@2026-01-14-weird-crowd"
 
 return M
