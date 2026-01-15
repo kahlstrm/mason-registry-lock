67de6b3976a610c3303905c1368db71936d5f20a
GitHub Action <action@github.com>
2026-01-15 00:43:33 +0000

    Update mason-registry lock to 2026-01-14-quick-beech

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-14-shoddy-slash"
+M.registry_release = "github:mason-org/mason-registry@2026-01-14-quick-beech"
 
 return M
