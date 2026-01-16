261476123e0865b8410dcf473107461ea34e2e7d
GitHub Action <action@github.com>
2026-01-16 00:46:28 +0000

    Update mason-registry lock to 2026-01-15-bronze-piano

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-15-neat-safe"
+M.registry_release = "github:mason-org/mason-registry@2026-01-15-bronze-piano"
 
 return M
