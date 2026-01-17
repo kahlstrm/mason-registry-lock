2f80c604823ad59420050a628ba653adcd6e49f2
GitHub Action <action@github.com>
2026-01-17 00:43:57 +0000

    Update mason-registry lock to 2026-01-16-divine-lead

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-16-warped-penny"
+M.registry_release = "github:mason-org/mason-registry@2026-01-16-divine-lead"
 
 return M
