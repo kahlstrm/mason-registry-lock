10eef8814141c2d64ce5ca2e3a29acb656722792
GitHub Action <action@github.com>
2026-01-17 00:43:56 +0000

    Update mason-registry lock to 2026-01-16-nearby-win

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-15-portly-route"
+M.registry_release = "github:mason-org/mason-registry@2026-01-16-nearby-win"
 
 return M
