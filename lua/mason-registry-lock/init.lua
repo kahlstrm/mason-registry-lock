ce32e1f3fea6627b423ef72a2ce3f22421274895
GitHub Action <action@github.com>
2026-01-17 00:43:57 +0000

    Update mason-registry lock to 2026-01-16-queasy-koi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-16-lame-shelf"
+M.registry_release = "github:mason-org/mason-registry@2026-01-16-queasy-koi"
 
 return M
