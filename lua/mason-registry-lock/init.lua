6dd8db57b4845e23cc62c5bf2db6f789bab1c292
GitHub Action <action@github.com>
2026-01-17 00:43:57 +0000

    Update mason-registry lock to 2026-01-16-elite-able

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-16-joyous-dish"
+M.registry_release = "github:mason-org/mason-registry@2026-01-16-elite-able"
 
 return M
