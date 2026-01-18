d28fe54f122f29287292c79f4fb1ef97341e56aa
GitHub Action <action@github.com>
2026-01-18 00:51:07 +0000

    Update mason-registry lock to 2026-01-17-grim-cable

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-17-shrill-sleep"
+M.registry_release = "github:mason-org/mason-registry@2026-01-17-grim-cable"
 
 return M
