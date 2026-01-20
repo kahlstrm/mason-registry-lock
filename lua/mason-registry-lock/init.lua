be6f3f9c8add47cc32cb29a9df5a15978d4371a1
GitHub Action <action@github.com>
2026-01-20 00:44:59 +0000

    Update mason-registry lock to 2026-01-19-lying-carrot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-18-first-hacker"
+M.registry_release = "github:mason-org/mason-registry@2026-01-19-lying-carrot"
 
 return M
