f7888330932174c39ef03966f164150711826e36
GitHub Action <action@github.com>
2026-01-20 00:44:59 +0000

    Update mason-registry lock to 2026-01-19-normal-walu

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-19-active-pair"
+M.registry_release = "github:mason-org/mason-registry@2026-01-19-normal-walu"
 
 return M
