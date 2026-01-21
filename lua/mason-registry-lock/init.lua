420af409d8ef1183f30391f76161ca6c3353f04d
GitHub Action <action@github.com>
2026-01-21 00:47:01 +0000

    Update mason-registry lock to 2026-01-20-near-swing

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-20-quick-bull"
+M.registry_release = "github:mason-org/mason-registry@2026-01-20-near-swing"
 
 return M
