63af796ecd0bc84dc0243b554a1b283d48bff09a
GitHub Action <action@github.com>
2026-01-21 00:47:01 +0000

    Update mason-registry lock to 2026-01-20-causal-hub

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-20-aural-pansy"
+M.registry_release = "github:mason-org/mason-registry@2026-01-20-causal-hub"
 
 return M
