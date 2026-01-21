c2eac14a915f60449447f240d1a02e16c0121365
GitHub Action <action@github.com>
2026-01-21 00:47:01 +0000

    Update mason-registry lock to 2026-01-20-dusty-decade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-20-robust-scup"
+M.registry_release = "github:mason-org/mason-registry@2026-01-20-dusty-decade"
 
 return M
