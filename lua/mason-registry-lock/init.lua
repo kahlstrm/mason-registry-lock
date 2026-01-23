e3e7cdbb27550e50ce86d0b8e19f4fdfa6abdefe
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-ordinary-turret

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-perfect-eight"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-ordinary-turret"
 
 return M
