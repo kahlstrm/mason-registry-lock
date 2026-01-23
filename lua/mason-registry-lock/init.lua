0f55cb39c357c6aa9f21632f208a5edc0489a368
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-secret-turbot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-square-thread"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-secret-turbot"
 
 return M
