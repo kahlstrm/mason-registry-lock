947c7d57140639a1ca9f847e92789420a2682cba
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-perfect-eight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-secret-turbot"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-perfect-eight"
 
 return M
