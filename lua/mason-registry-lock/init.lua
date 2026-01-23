a9ce21e3201a8a3baf318ba0ed613de14b5b0b6b
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-other-corner

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-friendly-fence"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-other-corner"
 
 return M
