4359ba821a62e8b4e0801e31e902d88371d98116
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-causal-danger

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-ordinary-turret"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-causal-danger"
 
 return M
