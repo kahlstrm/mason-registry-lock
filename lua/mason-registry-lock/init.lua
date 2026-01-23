1bddb2cd5a915243c15260314365941494615092
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-friendly-fence

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-mean-spoon"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-friendly-fence"
 
 return M
