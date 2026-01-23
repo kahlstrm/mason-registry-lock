f3c693693a3a046d3b24dfc31ce0552a5299ead2
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-liable-tench

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-cuddly-linen"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-liable-tench"
 
 return M
