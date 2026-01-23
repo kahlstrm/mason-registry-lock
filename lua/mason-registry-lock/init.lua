8e1437e79b35c8a0d27bad28974f41e8618888a0
GitHub Action <action@github.com>
2026-01-23 00:46:57 +0000

    Update mason-registry lock to 2026-01-22-rainy-tongue

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-22-fair-pluto"
+M.registry_release = "github:mason-org/mason-registry@2026-01-22-rainy-tongue"
 
 return M
