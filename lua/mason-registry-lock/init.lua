ef3401d4e2fae8f48c05dc3200172b6eb29d98e2
GitHub Action <action@github.com>
2026-01-24 00:44:45 +0000

    Update mason-registry lock to 2026-01-23-vast-pastor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-23-dual-thomas"
+M.registry_release = "github:mason-org/mason-registry@2026-01-23-vast-pastor"
 
 return M
