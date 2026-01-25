f51d23bf73878b0de3f42b7a455235b775768395
GitHub Action <action@github.com>
2026-01-25 00:52:39 +0000

    Update mason-registry lock to 2026-01-24-smooth-tapir

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-24-frothy-tenor"
+M.registry_release = "github:mason-org/mason-registry@2026-01-24-smooth-tapir"
 
 return M
