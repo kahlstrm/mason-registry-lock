7cc59d3bdfae6784f883ff948a8683f9529ba9c0
GitHub Action <action@github.com>
2026-01-25 00:52:39 +0000

    Update mason-registry lock to 2026-01-24-moody-hook

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-24-rugged-gaze"
+M.registry_release = "github:mason-org/mason-registry@2026-01-24-moody-hook"
 
 return M
