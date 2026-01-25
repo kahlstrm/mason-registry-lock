695530a515eb33237642e75c21ed5113af5f181d
GitHub Action <action@github.com>
2026-01-25 00:52:39 +0000

    Update mason-registry lock to 2026-01-24-quaint-minnow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-24-moody-hook"
+M.registry_release = "github:mason-org/mason-registry@2026-01-24-quaint-minnow"
 
 return M
