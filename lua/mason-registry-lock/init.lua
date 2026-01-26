982559b866d8912218a9b61fa688ed21f66739f2
GitHub Action <action@github.com>
2026-01-26 00:51:51 +0000

    Update mason-registry lock to 2026-01-25-best-chord

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-25-adept-organ"
+M.registry_release = "github:mason-org/mason-registry@2026-01-25-best-chord"
 
 return M
