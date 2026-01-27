d38149da84a448e07946cda0ed237014c85548d5
GitHub Action <action@github.com>
2026-01-27 00:49:56 +0000

    Update mason-registry lock to 2026-01-26-junior-powder

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-26-keen-hot"
+M.registry_release = "github:mason-org/mason-registry@2026-01-26-junior-powder"
 
 return M
