03dd0a52a7913d021e76213e51e578cef7211c01
GitHub Action <action@github.com>
2026-01-28 00:46:23 +0000

    Update mason-registry lock to 2026-01-27-magical-egg

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-27-damp-event"
+M.registry_release = "github:mason-org/mason-registry@2026-01-27-magical-egg"
 
 return M
