8ef5582ca7514dbb3de7a4bd4571c309ab9a0d5a
GitHub Action <action@github.com>
2026-01-28 00:46:23 +0000

    Update mason-registry lock to 2026-01-27-pretty-audio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-27-magical-egg"
+M.registry_release = "github:mason-org/mason-registry@2026-01-27-pretty-audio"
 
 return M
