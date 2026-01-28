733b66ff3974b1ade236679c9c10b7df324db1b8
GitHub Action <action@github.com>
2026-01-28 00:46:23 +0000

    Update mason-registry lock to 2026-01-27-fatal-socks

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-27-neat-juice"
+M.registry_release = "github:mason-org/mason-registry@2026-01-27-fatal-socks"
 
 return M
