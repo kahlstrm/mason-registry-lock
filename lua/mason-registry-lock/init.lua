60463709c424fbaeca5655ba62b6a6510947919b
GitHub Action <action@github.com>
2026-01-29 00:54:47 +0000

    Update mason-registry lock to 2026-01-28-fried-skin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-28-thin-smash"
+M.registry_release = "github:mason-org/mason-registry@2026-01-28-fried-skin"
 
 return M
