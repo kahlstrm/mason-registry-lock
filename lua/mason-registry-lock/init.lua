dc941ebfe607530754e9ef462402e4b52a5ad0b4
GitHub Action <action@github.com>
2026-01-30 00:55:19 +0000

    Update mason-registry lock to 2026-01-29-rough-double

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-29-dense-tiger"
+M.registry_release = "github:mason-org/mason-registry@2026-01-29-rough-double"
 
 return M
