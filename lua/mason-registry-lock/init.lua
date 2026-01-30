aac6dc4768f4b17de0d530789b3893b7eddeb7e2
GitHub Action <action@github.com>
2026-01-30 00:55:19 +0000

    Update mason-registry lock to 2026-01-29-top-crab

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-29-steady-smell"
+M.registry_release = "github:mason-org/mason-registry@2026-01-29-top-crab"
 
 return M
