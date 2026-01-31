284c2b9ddb157e79a0c56e5366c677f36a2e76cd
GitHub Action <action@github.com>
2026-01-31 00:52:41 +0000

    Update mason-registry lock to 2026-01-30-strong-image

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-30-bleak-jet"
+M.registry_release = "github:mason-org/mason-registry@2026-01-30-strong-image"
 
 return M
