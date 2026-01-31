7a7d6a552e1cedd9a9fbd39fdabd8847249399f8
GitHub Action <action@github.com>
2026-01-31 00:52:41 +0000

    Update mason-registry lock to 2026-01-30-vital-key

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-30-gritty-sack"
+M.registry_release = "github:mason-org/mason-registry@2026-01-30-vital-key"
 
 return M
