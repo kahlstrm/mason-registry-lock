a9ee2e3aafddf155e222355341f87ec06d919c08
GitHub Action <action@github.com>
2026-01-31 00:52:41 +0000

    Update mason-registry lock to 2026-01-30-innate-carrot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-30-baked-era"
+M.registry_release = "github:mason-org/mason-registry@2026-01-30-innate-carrot"
 
 return M
