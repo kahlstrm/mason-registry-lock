1e1b187ffedfae75001ca7bb23698fa371e89908
GitHub Action <action@github.com>
2026-01-31 00:52:41 +0000

    Update mason-registry lock to 2026-01-30-fit-crush

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-30-direct-witch"
+M.registry_release = "github:mason-org/mason-registry@2026-01-30-fit-crush"
 
 return M
