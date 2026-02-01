46cc90082e1fdc88d56fd3e447b9e61fc3a48df9
GitHub Action <action@github.com>
2026-02-01 01:04:56 +0000

    Update mason-registry lock to 2026-01-31-thoughtful-pull

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-01-31-frugal-kayak"
+M.registry_release = "github:mason-org/mason-registry@2026-01-31-thoughtful-pull"
 
 return M
