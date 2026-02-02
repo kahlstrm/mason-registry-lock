1fb28ffdfa2994d2683d0e54aca582dd9c94d607
GitHub Action <action@github.com>
2026-02-02 00:58:46 +0000

    Update mason-registry lock to 2026-02-01-abject-insect

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-01-hoarse-wolf"
+M.registry_release = "github:mason-org/mason-registry@2026-02-01-abject-insect"
 
 return M
