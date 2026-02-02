779fb280fb78ccd2b75ce9b977a1cc935169845a
GitHub Action <action@github.com>
2026-02-02 00:58:46 +0000

    Update mason-registry lock to 2026-02-01-ratty-slide

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-01-abject-insect"
+M.registry_release = "github:mason-org/mason-registry@2026-02-01-ratty-slide"
 
 return M
