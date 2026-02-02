d356283a5727d0963e670db363d1807fede21a97
GitHub Action <action@github.com>
2026-02-02 00:58:46 +0000

    Update mason-registry lock to 2026-02-01-final-alibi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-01-grubby-elbow"
+M.registry_release = "github:mason-org/mason-registry@2026-02-01-final-alibi"
 
 return M
