9e4d0e9ac9ab7b15b11edd508e518f7864cc3ef0
GitHub Action <action@github.com>
2026-02-03 00:58:57 +0000

    Update mason-registry lock to 2026-02-02-dusty-opah

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-01-net-motion"
+M.registry_release = "github:mason-org/mason-registry@2026-02-02-dusty-opah"
 
 return M
