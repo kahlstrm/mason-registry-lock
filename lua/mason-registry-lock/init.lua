2da4b2065659e5d675b03119ca588f650a6b58f7
GitHub Action <action@github.com>
2026-02-03 00:58:57 +0000

    Update mason-registry lock to 2026-02-02-gritty-poison

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-02-acidic-maple"
+M.registry_release = "github:mason-org/mason-registry@2026-02-02-gritty-poison"
 
 return M
