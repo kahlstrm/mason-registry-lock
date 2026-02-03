8a3a696e366b8dcff16e2b6d79e5f1d5ee227b53
GitHub Action <action@github.com>
2026-02-03 00:58:57 +0000

    Update mason-registry lock to 2026-02-02-stable-cony

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-02-absent-lycra"
+M.registry_release = "github:mason-org/mason-registry@2026-02-02-stable-cony"
 
 return M
