4d0fbf888c84e697cc28d3389badd27e40af4256
GitHub Action <action@github.com>
2026-02-03 00:58:57 +0000

    Update mason-registry lock to 2026-02-02-absent-lycra

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-02-dusty-opah"
+M.registry_release = "github:mason-org/mason-registry@2026-02-02-absent-lycra"
 
 return M
