30265d07678f2237592abc558154fa9dd2855947
GitHub Action <action@github.com>
2026-02-03 00:58:57 +0000

    Update mason-registry lock to 2026-02-02-rotund-pan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-02-uppity-office"
+M.registry_release = "github:mason-org/mason-registry@2026-02-02-rotund-pan"
 
 return M
