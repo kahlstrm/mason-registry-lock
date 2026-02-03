f4944e717210cd56ed7478df5a67fab41701f01e
GitHub Action <action@github.com>
2026-02-03 00:58:57 +0000

    Update mason-registry lock to 2026-02-02-odd-toad

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-02-manual-jason"
+M.registry_release = "github:mason-org/mason-registry@2026-02-02-odd-toad"
 
 return M
