adf08b4f77db9774d1d4d2ba8ad2d6294ba7ec8e
GitHub Action <action@github.com>
2026-02-03 00:58:57 +0000

    Update mason-registry lock to 2026-02-02-tory-zingel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-02-fluid-fuel"
+M.registry_release = "github:mason-org/mason-registry@2026-02-02-tory-zingel"
 
 return M
