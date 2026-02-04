06ed0314538cb6d59715eadcb2dcd1fb7308c094
GitHub Action <action@github.com>
2026-02-04 00:54:35 +0000

    Update mason-registry lock to 2026-02-03-lonely-colt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-03-glossy-exit"
+M.registry_release = "github:mason-org/mason-registry@2026-02-03-lonely-colt"
 
 return M
