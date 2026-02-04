3de8b40add54cc9b929ce22ae47fe440669e9e40
GitHub Action <action@github.com>
2026-02-04 00:54:35 +0000

    Update mason-registry lock to 2026-02-03-excess-brush

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-03-basic-ape"
+M.registry_release = "github:mason-org/mason-registry@2026-02-03-excess-brush"
 
 return M
