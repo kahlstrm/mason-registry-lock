e0d2e07d0c57def9cd6d8a44d034e4197b2e3fb3
GitHub Action <action@github.com>
2026-02-04 00:54:35 +0000

    Update mason-registry lock to 2026-02-04-arctic-thread

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-03-stingy-sight"
+M.registry_release = "github:mason-org/mason-registry@2026-02-04-arctic-thread"
 
 return M
