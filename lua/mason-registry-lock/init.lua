be46bc683237ae46dcba4ffeff3ff779c83deb18
GitHub Action <action@github.com>
2026-02-05 00:57:30 +0000

    Update mason-registry lock to 2026-02-04-french-turtle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-04-warm-gas"
+M.registry_release = "github:mason-org/mason-registry@2026-02-04-french-turtle"
 
 return M
