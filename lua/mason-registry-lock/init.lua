36ae1acde5d886b2474d891c261ef6105096f575
GitHub Action <action@github.com>
2026-02-05 00:57:30 +0000

    Update mason-registry lock to 2026-02-04-daring-bell

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-04-arctic-thread"
+M.registry_release = "github:mason-org/mason-registry@2026-02-04-daring-bell"
 
 return M
