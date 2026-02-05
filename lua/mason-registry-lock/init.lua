efbfd9553434e893790a1ea141295978d21d1f42
GitHub Action <action@github.com>
2026-02-05 00:57:30 +0000

    Update mason-registry lock to 2026-02-04-tiny-appeal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-04-crafty-lump"
+M.registry_release = "github:mason-org/mason-registry@2026-02-04-tiny-appeal"
 
 return M
