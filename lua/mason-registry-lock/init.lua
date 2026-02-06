9954e525e0adb303e1bcfb9f69c01b55c6420f0e
GitHub Action <action@github.com>
2026-02-06 00:54:24 +0000

    Update mason-registry lock to 2026-02-05-rotten-rotate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-05-friendly-win"
+M.registry_release = "github:mason-org/mason-registry@2026-02-05-rotten-rotate"
 
 return M
