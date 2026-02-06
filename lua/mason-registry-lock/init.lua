3d65b0345f97139e0f242e0343b46ff3bdd1b420
GitHub Action <action@github.com>
2026-02-06 00:54:24 +0000

    Update mason-registry lock to 2026-02-05-normal-oryx

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-05-keen-plane"
+M.registry_release = "github:mason-org/mason-registry@2026-02-05-normal-oryx"
 
 return M
