590ee1f1bcabff2448762523278f289838b767b3
GitHub Action <action@github.com>
2026-02-06 00:54:24 +0000

    Update mason-registry lock to 2026-02-05-green-pacus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-04-tiny-appeal"
+M.registry_release = "github:mason-org/mason-registry@2026-02-05-green-pacus"
 
 return M
