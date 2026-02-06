3dd1bb34cf86630dbd843e564f3ac24308902756
GitHub Action <action@github.com>
2026-02-06 00:54:24 +0000

    Update mason-registry lock to 2026-02-05-friendly-win

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-05-fried-guru"
+M.registry_release = "github:mason-org/mason-registry@2026-02-05-friendly-win"
 
 return M
