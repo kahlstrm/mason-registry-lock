dfd8cc2b67ffb272f26caf4f9f23ad8b38740efe
GitHub Action <action@github.com>
2026-02-06 00:54:24 +0000

    Update mason-registry lock to 2026-02-05-down-galaxy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-05-ordinary-white"
+M.registry_release = "github:mason-org/mason-registry@2026-02-05-down-galaxy"
 
 return M
