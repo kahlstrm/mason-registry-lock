7b063987507cb27c3ee8482770dafe28f49bb720
GitHub Action <action@github.com>
2026-02-06 00:54:24 +0000

    Update mason-registry lock to 2026-02-05-keen-plane

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-05-jumpy-bridge"
+M.registry_release = "github:mason-org/mason-registry@2026-02-05-keen-plane"
 
 return M
