b4f0977f68dd464a8fa1b706aaaf2d3c9c5dbbff
GitHub Action <action@github.com>
2026-02-07 00:54:23 +0000

    Update mason-registry lock to 2026-02-06-icy-curve

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-05-living-collar"
+M.registry_release = "github:mason-org/mason-registry@2026-02-06-icy-curve"
 
 return M
