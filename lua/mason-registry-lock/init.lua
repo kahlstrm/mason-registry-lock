d0eb4feaa75894457e3d65cb4082fb74b81d94cc
GitHub Action <action@github.com>
2026-02-07 00:54:23 +0000

    Update mason-registry lock to 2026-02-06-vanilla-gar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-06-frigid-small"
+M.registry_release = "github:mason-org/mason-registry@2026-02-06-vanilla-gar"
 
 return M
