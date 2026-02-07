64b978ff196e9dfcfbdddb6fa3e66bdd334f4326
GitHub Action <action@github.com>
2026-02-07 00:54:23 +0000

    Update mason-registry lock to 2026-02-06-trashy-spot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-06-tense-muscle"
+M.registry_release = "github:mason-org/mason-registry@2026-02-06-trashy-spot"
 
 return M
