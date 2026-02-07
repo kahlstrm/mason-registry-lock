c4789a1201a64b556d1851b63fc5d12faef4756d
GitHub Action <action@github.com>
2026-02-07 00:54:23 +0000

    Update mason-registry lock to 2026-02-06-jazzy-anime

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-06-sticky-season"
+M.registry_release = "github:mason-org/mason-registry@2026-02-06-jazzy-anime"
 
 return M
