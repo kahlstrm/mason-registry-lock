bb23c3f1b57e825e31bcfc225126116188746365
GitHub Action <action@github.com>
2026-02-08 01:13:39 +0000

    Update mason-registry lock to 2026-02-07-thin-sheet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-07-sorry-joke"
+M.registry_release = "github:mason-org/mason-registry@2026-02-07-thin-sheet"
 
 return M
