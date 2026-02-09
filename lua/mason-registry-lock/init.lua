efcaa92abb2f045e20d7777d34ae6aa1a2748d0c
GitHub Action <action@github.com>
2026-02-09 01:00:23 +0000

    Update mason-registry lock to 2026-02-08-fake-study

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-08-cleaver-ticket"
+M.registry_release = "github:mason-org/mason-registry@2026-02-08-fake-study"
 
 return M
