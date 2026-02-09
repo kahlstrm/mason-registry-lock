39277e969a538e2dece99efac0e7aecac45caaf0
GitHub Action <action@github.com>
2026-02-09 01:00:23 +0000

    Update mason-registry lock to 2026-02-08-cleaver-ticket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-08-asian-slash"
+M.registry_release = "github:mason-org/mason-registry@2026-02-08-cleaver-ticket"
 
 return M
