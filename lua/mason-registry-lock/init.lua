9f6f92fbb8cbb65131e4500859042e69797ff178
GitHub Action <action@github.com>
2026-02-09 01:00:23 +0000

    Update mason-registry lock to 2026-02-08-asian-slash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-08-candid-knight"
+M.registry_release = "github:mason-org/mason-registry@2026-02-08-asian-slash"
 
 return M
