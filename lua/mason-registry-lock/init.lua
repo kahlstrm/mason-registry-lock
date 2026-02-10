ee60caa4c0ef981513a9b284066b0d48e760598e
GitHub Action <action@github.com>
2026-02-10 01:08:39 +0000

    Update mason-registry lock to 2026-02-09-mighty-pleco

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-tangy-danio"
+M.registry_release = "github:mason-org/mason-registry@2026-02-09-mighty-pleco"
 
 return M
