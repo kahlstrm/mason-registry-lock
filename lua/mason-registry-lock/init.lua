ed10aca9319977ccc2dc94e22f2fef8840484552
GitHub Action <action@github.com>
2026-02-10 01:08:39 +0000

    Update mason-registry lock to 2026-02-09-hearty-plant

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-jagged-pile"
+M.registry_release = "github:mason-org/mason-registry@2026-02-09-hearty-plant"
 
 return M
