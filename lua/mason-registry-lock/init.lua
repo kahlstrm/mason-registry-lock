26a3c03fc51537553d50b60826ce351e2c8c50ed
GitHub Action <action@github.com>
2026-02-10 01:08:39 +0000

    Update mason-registry lock to 2026-02-09-rowdy-pansy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-broad-cost"
+M.registry_release = "github:mason-org/mason-registry@2026-02-09-rowdy-pansy"
 
 return M
