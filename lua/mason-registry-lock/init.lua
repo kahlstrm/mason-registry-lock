599003450ae2330f8c12da3a691824c002aacc5a
GitHub Action <action@github.com>
2026-02-10 01:08:39 +0000

    Update mason-registry lock to 2026-02-09-creamy-timer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-deep-twist"
+M.registry_release = "github:mason-org/mason-registry@2026-02-09-creamy-timer"
 
 return M
