9f1c3ecd99d964c360f92b79a250ce465ddc0115
GitHub Action <action@github.com>
2026-02-10 01:08:39 +0000

    Update mason-registry lock to 2026-02-09-total-metal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-native-era"
+M.registry_release = "github:mason-org/mason-registry@2026-02-09-total-metal"
 
 return M
