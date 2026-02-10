09c7b0e8ab9d4fc8f3a18f049d0f0ae366ed200a
GitHub Action <action@github.com>
2026-02-10 01:08:39 +0000

    Update mason-registry lock to 2026-02-09-long-map

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-timely-route"
+M.registry_release = "github:mason-org/mason-registry@2026-02-09-long-map"
 
 return M
