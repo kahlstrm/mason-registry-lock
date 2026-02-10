59bb74cb47f67ea66a8126452642100df7a54615
GitHub Action <action@github.com>
2026-02-10 01:08:39 +0000

    Update mason-registry lock to 2026-02-09-deep-twist

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-rowdy-pickle"
+M.registry_release = "github:mason-org/mason-registry@2026-02-09-deep-twist"
 
 return M
