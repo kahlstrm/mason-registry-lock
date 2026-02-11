e365a3753892a27471a466cc27b918176e9a19cd
GitHub Action <action@github.com>
2026-02-11 01:06:30 +0000

    Update mason-registry lock to 2026-02-10-upset-fate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-10-dapper-virgo"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-upset-fate"
 
 return M
