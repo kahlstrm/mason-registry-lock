b35d8c9f472ec2684a9a10f9ee62f7a30143f102
GitHub Action <action@github.com>
2026-02-11 01:06:30 +0000

    Update mason-registry lock to 2026-02-10-burly-hill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-10-white-parcel"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-burly-hill"
 
 return M
