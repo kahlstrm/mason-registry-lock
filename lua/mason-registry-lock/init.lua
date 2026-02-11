cd4e506fa0eea2505353d650c5ca7bed506089c8
GitHub Action <action@github.com>
2026-02-11 01:06:30 +0000

    Update mason-registry lock to 2026-02-10-petty-bear

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-10-fixed-dollar"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-petty-bear"
 
 return M
