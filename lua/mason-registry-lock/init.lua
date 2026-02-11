376b802c5a2a12956b2af0a72a33a1905085cc73
GitHub Action <action@github.com>
2026-02-11 01:06:30 +0000

    Update mason-registry lock to 2026-02-10-humble-stone

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-10-down-colony"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-humble-stone"
 
 return M
