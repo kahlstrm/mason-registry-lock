e2253757fb41154ac254118026a62386b3b02e46
GitHub Action <action@github.com>
2026-02-11 01:06:30 +0000

    Update mason-registry lock to 2026-02-10-dapper-virgo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-10-humble-stone"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-dapper-virgo"
 
 return M
