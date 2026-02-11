d53fb46872863402219876ac9f5a1eb2cd89fb00
GitHub Action <action@github.com>
2026-02-11 01:06:29 +0000

    Update mason-registry lock to 2026-02-10-raspy-clave

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-09-ugly-shrine"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-raspy-clave"
 
 return M
