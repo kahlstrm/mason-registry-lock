19e16c809e43f64b349e51409272bb96645532cc
GitHub Action <action@github.com>
2026-02-11 01:06:29 +0000

    Update mason-registry lock to 2026-02-10-south-maid

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-10-raspy-clave"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-south-maid"
 
 return M
