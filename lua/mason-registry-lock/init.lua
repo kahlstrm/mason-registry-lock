59cddd2828008e243f8cb0892f5aeb7d3de266de
GitHub Action <action@github.com>
2026-02-11 01:06:30 +0000

    Update mason-registry lock to 2026-02-10-slimy-single

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-10-raw-bangle"
+M.registry_release = "github:mason-org/mason-registry@2026-02-10-slimy-single"
 
 return M
