0214b398046440f792201aa7a81e2056e6418f7a
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-rare-nation

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-webbed-plier"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-rare-nation"
 
 return M
