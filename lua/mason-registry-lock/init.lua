086f5902c16256a700bd21b49827ac737b436cb8
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-usual-murder

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-phobic-guilty"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-usual-murder"
 
 return M
