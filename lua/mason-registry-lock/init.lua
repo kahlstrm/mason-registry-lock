7fe8233c28e9a2a2907cae91cea2fb085980b4ef
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-friendly-check

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-weary-gut"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-friendly-check"
 
 return M
