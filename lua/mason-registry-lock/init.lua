2fe21af0c649a7cb6bc67a1cd649a7ae73fc4a30
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-dismal-betta

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-prize-shock"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-dismal-betta"
 
 return M
