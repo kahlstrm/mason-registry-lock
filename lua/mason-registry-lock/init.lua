17486162b9e62e487244efc481bc791654a4245e
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-phobic-guilty

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-warped-china"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-phobic-guilty"
 
 return M
