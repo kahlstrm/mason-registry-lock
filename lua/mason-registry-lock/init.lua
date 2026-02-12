b1dc411e0a77472ffe4485f9830128acf08f7657
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-spiffy-editor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-dismal-betta"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-spiffy-editor"
 
 return M
