62cc57237f0efca9d24e6b4262c47e5bd1451e68
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-busy-italy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-rare-nation"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-busy-italy"
 
 return M
