cacfc213ff210e3242279c4f6c547c314d35f04e
GitHub Action <action@github.com>
2026-02-12 00:58:59 +0000

    Update mason-registry lock to 2026-02-11-tangy-dingo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-11-friendly-check"
+M.registry_release = "github:mason-org/mason-registry@2026-02-11-tangy-dingo"
 
 return M
