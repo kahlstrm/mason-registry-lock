1744819112d1ad1c2881f3fe06820af513db5136
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-clear-caucus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-baby-trowel"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-clear-caucus"
 
 return M
