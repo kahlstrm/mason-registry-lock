dee791aef370d69ff8949112dd860e6aefdd2e41
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-moody-stem

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-flashy-fuel"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-moody-stem"
 
 return M
