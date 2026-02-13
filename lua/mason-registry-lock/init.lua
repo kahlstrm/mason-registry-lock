5bfb5317cec2169d5efca3fee11aa500884ee729
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-giddy-border

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-bronze-hour"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-giddy-border"
 
 return M
