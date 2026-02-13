da259cf25f4cefaeef206da2693bb86585338369
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-tricky-base

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-unruly-trip"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-tricky-base"
 
 return M
