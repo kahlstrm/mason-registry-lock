d60e063aa20ebebf62a388cff0f964d8529944d5
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-many-cannon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-swiss-grunt"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-many-cannon"
 
 return M
