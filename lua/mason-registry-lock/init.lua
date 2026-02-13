a03be90e49c80b99acdd59bbbc7234ddbe2232cf
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-unruly-trip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-clear-caucus"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-unruly-trip"
 
 return M
