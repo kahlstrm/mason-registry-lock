b51c7c556c73ec5349fcb3911c1ed321465c4fd8
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-flashy-fuel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-tricky-base"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-flashy-fuel"
 
 return M
