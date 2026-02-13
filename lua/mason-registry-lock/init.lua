8e17b51c6f3283d219df5b928a5adcfd4bc18867
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-sugary-cloudy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-spotty-mitten"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-sugary-cloudy"
 
 return M
