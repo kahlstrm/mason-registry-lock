dbb87a229ef18b898af70c7378a89d87e1ad4d45
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-bronze-hour

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-sugary-cloudy"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-bronze-hour"
 
 return M
