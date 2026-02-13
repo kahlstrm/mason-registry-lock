5fe799588e3a362e71877f9d2da84f9e5a177e4e
GitHub Action <action@github.com>
2026-02-13 01:02:57 +0000

    Update mason-registry lock to 2026-02-12-solar-swamp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-12-sedate-hog"
+M.registry_release = "github:mason-org/mason-registry@2026-02-12-solar-swamp"
 
 return M
