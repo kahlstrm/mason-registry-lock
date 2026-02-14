b8fb53991744a0efcdeff774f230ec054c03f8c4
GitHub Action <action@github.com>
2026-02-14 00:56:28 +0000

    Update mason-registry lock to 2026-02-13-glossy-bill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-13-blue-store"
+M.registry_release = "github:mason-org/mason-registry@2026-02-13-glossy-bill"
 
 return M
