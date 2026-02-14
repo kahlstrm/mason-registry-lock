163f452220315576f1ed68e772e7f8710e3a3c84
GitHub Action <action@github.com>
2026-02-14 00:56:28 +0000

    Update mason-registry lock to 2026-02-13-astral-trail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-13-winged-cattle"
+M.registry_release = "github:mason-org/mason-registry@2026-02-13-astral-trail"
 
 return M
