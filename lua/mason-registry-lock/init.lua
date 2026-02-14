22d41e1a557413d3eee10acb101f2811c543b2ff
GitHub Action <action@github.com>
2026-02-14 00:56:28 +0000

    Update mason-registry lock to 2026-02-13-mixed-dot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-13-awake-ziege"
+M.registry_release = "github:mason-org/mason-registry@2026-02-13-mixed-dot"
 
 return M
