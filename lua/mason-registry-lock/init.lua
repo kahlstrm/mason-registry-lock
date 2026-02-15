4fb86add4785a881c08a92df0915569fad4661ed
GitHub Action <action@github.com>
2026-02-15 01:01:28 +0000

    Update mason-registry lock to 2026-02-14-aching-sock

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-14-chosen-land"
+M.registry_release = "github:mason-org/mason-registry@2026-02-14-aching-sock"
 
 return M
