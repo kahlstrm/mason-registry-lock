7c755e2f00bc5ec060167a733476535d446ed7c4
GitHub Action <action@github.com>
2026-02-15 01:01:28 +0000

    Update mason-registry lock to 2026-02-15-secret-crown

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-14-fixed-fender"
+M.registry_release = "github:mason-org/mason-registry@2026-02-15-secret-crown"
 
 return M
