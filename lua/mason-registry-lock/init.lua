07cbd3c8ec23fbd2c5d844252df6681ddc874855
GitHub Action <action@github.com>
2026-02-15 01:01:28 +0000

    Update mason-registry lock to 2026-02-14-ornate-rudd

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-14-aching-sock"
+M.registry_release = "github:mason-org/mason-registry@2026-02-14-ornate-rudd"
 
 return M
