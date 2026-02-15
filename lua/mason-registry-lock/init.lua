e31797edf8fc8b50eb0e8530baa62b45cac83f72
GitHub Action <action@github.com>
2026-02-15 01:01:28 +0000

    Update mason-registry lock to 2026-02-14-steep-tooth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-14-ornate-rudd"
+M.registry_release = "github:mason-org/mason-registry@2026-02-14-steep-tooth"
 
 return M
