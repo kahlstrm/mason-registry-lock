f2b8a042ab0b73ac93e5b305b5f10a15e065d098
GitHub Action <action@github.com>
2026-02-16 00:59:20 +0000

    Update mason-registry lock to 2026-02-15-proper-badger

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-15-polite-fifth"
+M.registry_release = "github:mason-org/mason-registry@2026-02-15-proper-badger"
 
 return M
