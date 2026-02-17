b462724b35761bf5d2923e23f96d30bb26f5d93b
GitHub Action <action@github.com>
2026-02-17 00:58:31 +0000

    Update mason-registry lock to 2026-02-16-snotty-event

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-16-next-meat"
+M.registry_release = "github:mason-org/mason-registry@2026-02-16-snotty-event"
 
 return M
