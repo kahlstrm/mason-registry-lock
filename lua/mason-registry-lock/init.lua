2b408fe8c3f888a6c23a7bb10ae6d016e901b618
GitHub Action <action@github.com>
2026-02-17 00:58:31 +0000

    Update mason-registry lock to 2026-02-16-godly-wall

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-16-dotted-sprout"
+M.registry_release = "github:mason-org/mason-registry@2026-02-16-godly-wall"
 
 return M
