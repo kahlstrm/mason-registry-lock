1e5ab2c7470c61b3cc1e93300cc494fa09c5a612
GitHub Action <action@github.com>
2026-02-17 00:58:31 +0000

    Update mason-registry lock to 2026-02-16-exact-grape

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-16-each-addax"
+M.registry_release = "github:mason-org/mason-registry@2026-02-16-exact-grape"
 
 return M
