ec5614353dac9d77d8863ad17e969bd68b00dab7
GitHub Action <action@github.com>
2026-02-17 00:58:31 +0000

    Update mason-registry lock to 2026-02-16-dotted-sprout

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-16-retail-weed"
+M.registry_release = "github:mason-org/mason-registry@2026-02-16-dotted-sprout"
 
 return M
