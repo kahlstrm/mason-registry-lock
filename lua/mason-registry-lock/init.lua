375da0508344e40865f1bfb8fc07c74424e39187
GitHub Action <action@github.com>
2026-02-18 01:00:22 +0000

    Update mason-registry lock to 2026-02-17-sick-zipper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-17-steep-spread"
+M.registry_release = "github:mason-org/mason-registry@2026-02-17-sick-zipper"
 
 return M
