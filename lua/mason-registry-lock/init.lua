629de34a124b42bb50320f5b5d7c676bbd7807b4
GitHub Action <action@github.com>
2026-02-18 01:00:22 +0000

    Update mason-registry lock to 2026-02-17-deeply-fund

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-17-pink-file"
+M.registry_release = "github:mason-org/mason-registry@2026-02-17-deeply-fund"
 
 return M
