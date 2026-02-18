e7c605e567204d678bf6e8852de439579cd1220f
GitHub Action <action@github.com>
2026-02-18 01:00:22 +0000

    Update mason-registry lock to 2026-02-17-ahead-shadow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-17-fiery-head"
+M.registry_release = "github:mason-org/mason-registry@2026-02-17-ahead-shadow"
 
 return M
