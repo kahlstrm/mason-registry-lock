a78c998732fc972b850cbad8f0c121ee940f3f52
GitHub Action <action@github.com>
2026-02-18 01:00:22 +0000

    Update mason-registry lock to 2026-02-17-brave-slope

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-17-mass-perch"
+M.registry_release = "github:mason-org/mason-registry@2026-02-17-brave-slope"
 
 return M
