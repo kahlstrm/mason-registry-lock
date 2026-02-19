91421080d6bbbf4b79116f322f4fb87af068ca3a
GitHub Action <action@github.com>
2026-02-19 00:59:36 +0000

    Update mason-registry lock to 2026-02-17-pink-file

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-17-messy-fridge"
+M.registry_release = "github:mason-org/mason-registry@2026-02-17-pink-file"
 
 return M
