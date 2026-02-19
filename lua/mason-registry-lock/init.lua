cf1ca58b4fdbdcd6a2ca33f3755e1535b494d47a
GitHub Action <action@github.com>
2026-02-19 00:59:36 +0000

    Update mason-registry lock to 2026-02-18-spooky-router

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-18-oily-robert"
+M.registry_release = "github:mason-org/mason-registry@2026-02-18-spooky-router"
 
 return M
