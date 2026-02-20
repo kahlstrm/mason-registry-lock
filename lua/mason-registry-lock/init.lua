fb63a1986d8dfd0999b1654f2826c4dd3e45ca6e
GitHub Action <action@github.com>
2026-02-20 00:55:59 +0000

    Update mason-registry lock to 2026-02-19-crude-sandra

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-19-rustic-year"
+M.registry_release = "github:mason-org/mason-registry@2026-02-19-crude-sandra"
 
 return M
