513b301b5607ea462f6325a909c1ed319c3813e9
GitHub Action <action@github.com>
2026-02-20 00:55:59 +0000

    Update mason-registry lock to 2026-02-19-salty-team

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-19-civil-slide"
+M.registry_release = "github:mason-org/mason-registry@2026-02-19-salty-team"
 
 return M
