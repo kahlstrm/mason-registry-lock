ed23a409dfe66114bf0dd8aaa10752d1e245d6be
GitHub Action <action@github.com>
2026-02-21 00:54:54 +0000

    Update mason-registry lock to 2026-02-20-modest-grade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-20-wide-liver"
+M.registry_release = "github:mason-org/mason-registry@2026-02-20-modest-grade"
 
 return M
