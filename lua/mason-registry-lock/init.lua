a4cb2a60a5dfed7c3c6c7ebdcd0ef1d09f78527f
GitHub Action <action@github.com>
2026-02-22 00:58:45 +0000

    Update mason-registry lock to 2026-02-21-shrill-root

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-21-oblong-base"
+M.registry_release = "github:mason-org/mason-registry@2026-02-21-shrill-root"
 
 return M
