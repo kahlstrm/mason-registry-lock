4ee888a75e7dcfd7ce398bb3cbb995a0d81f467f
GitHub Action <action@github.com>
2026-02-22 00:58:45 +0000

    Update mason-registry lock to 2026-02-21-mobile-stick

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-21-precious-moose"
+M.registry_release = "github:mason-org/mason-registry@2026-02-21-mobile-stick"
 
 return M
