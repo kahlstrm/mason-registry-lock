b0fe98654b0a9a30fce8d8806b7983734a72e0aa
GitHub Action <action@github.com>
2026-02-22 00:58:45 +0000

    Update mason-registry lock to 2026-02-21-real-dahlia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-21-mute-tetra"
+M.registry_release = "github:mason-org/mason-registry@2026-02-21-real-dahlia"
 
 return M
