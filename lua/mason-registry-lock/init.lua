02aa0eb1f433eb248fae30e9a48053a6477117d1
GitHub Action <action@github.com>
2026-02-24 00:56:49 +0000

    Update mason-registry lock to 2026-02-23-blue-musk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-23-swanky-result"
+M.registry_release = "github:mason-org/mason-registry@2026-02-23-blue-musk"
 
 return M
