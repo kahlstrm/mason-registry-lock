0fa28d864d059779b051994514d46306fbbcf820
GitHub Action <action@github.com>
2026-02-25 01:02:26 +0000

    Update mason-registry lock to 2026-02-24-spiffy-cub

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-24-eerie-pack"
+M.registry_release = "github:mason-org/mason-registry@2026-02-24-spiffy-cub"
 
 return M
