3a274ea961f9334a51085c07b725f664a59ad6ab
GitHub Action <action@github.com>
2026-02-25 01:02:26 +0000

    Update mason-registry lock to 2026-02-24-shy-erbium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-24-magic-weapon"
+M.registry_release = "github:mason-org/mason-registry@2026-02-24-shy-erbium"
 
 return M
