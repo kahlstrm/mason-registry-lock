553fa97e387f1336cb95fea2bfaccf69fb8ad9ef
GitHub Action <action@github.com>
2026-02-26 00:56:08 +0000

    Update mason-registry lock to 2026-02-25-proper-seed

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-25-smarmy-greek"
+M.registry_release = "github:mason-org/mason-registry@2026-02-25-proper-seed"
 
 return M
