c9dd351fa1af4297a42b2bf54e74492f67262100
GitHub Action <action@github.com>
2026-02-26 00:56:08 +0000

    Update mason-registry lock to 2026-02-25-rapid-show

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-25-proper-seed"
+M.registry_release = "github:mason-org/mason-registry@2026-02-25-rapid-show"
 
 return M
