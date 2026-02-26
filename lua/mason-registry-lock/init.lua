1bf37a8444eb3dac688dbcba326475dcef27e2f5
GitHub Action <action@github.com>
2026-02-26 00:56:08 +0000

    Update mason-registry lock to 2026-02-25-fuzzy-plot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-25-cool-driver"
+M.registry_release = "github:mason-org/mason-registry@2026-02-25-fuzzy-plot"
 
 return M
