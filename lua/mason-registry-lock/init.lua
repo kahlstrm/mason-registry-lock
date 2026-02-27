152c8f57ff4ae9dfd0003230d3fcfdabe36b2f64
GitHub Action <action@github.com>
2026-02-27 00:57:29 +0000

    Update mason-registry lock to 2026-02-26-perfect-tarpon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-26-korean-event"
+M.registry_release = "github:mason-org/mason-registry@2026-02-26-perfect-tarpon"
 
 return M
