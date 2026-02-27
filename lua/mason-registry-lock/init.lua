0b4d04a2a979b9e148ab0840362788b7c92ffaae
GitHub Action <action@github.com>
2026-02-27 00:57:29 +0000

    Update mason-registry lock to 2026-02-26-posh-camera

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-26-lame-value"
+M.registry_release = "github:mason-org/mason-registry@2026-02-26-posh-camera"
 
 return M
