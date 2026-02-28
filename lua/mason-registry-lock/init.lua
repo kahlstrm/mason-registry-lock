3fde0c046567f5994143811c201fae2b6631bc2d
GitHub Action <action@github.com>
2026-02-28 00:50:24 +0000

    Update mason-registry lock to 2026-02-27-strict-spear

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-27-solar-dock"
+M.registry_release = "github:mason-org/mason-registry@2026-02-27-strict-spear"
 
 return M
