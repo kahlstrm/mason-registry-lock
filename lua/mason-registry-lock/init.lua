d17a2261b87761c032c660a1bd8cdad20b33afd9
GitHub Action <action@github.com>
2026-02-28 00:50:24 +0000

    Update mason-registry lock to 2026-02-27-flawed-creek

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-27-busy-soccer"
+M.registry_release = "github:mason-org/mason-registry@2026-02-27-flawed-creek"
 
 return M
