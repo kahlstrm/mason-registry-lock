d543cb4ba266e66df15f49693cf7190c8d23ef00
GitHub Action <action@github.com>
2026-02-28 00:50:24 +0000

    Update mason-registry lock to 2026-02-27-innate-laura

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-27-nearby-dime"
+M.registry_release = "github:mason-org/mason-registry@2026-02-27-innate-laura"
 
 return M
