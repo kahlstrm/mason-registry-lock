b7a6f4540b0d94318ff7c7d43e63c1e0880d7539
GitHub Action <action@github.com>
2026-02-28 00:50:24 +0000

    Update mason-registry lock to 2026-02-27-solar-dock

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-27-aware-lagena"
+M.registry_release = "github:mason-org/mason-registry@2026-02-27-solar-dock"
 
 return M
