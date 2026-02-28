a07408baa69459f0a6efc10b360cb9fe2c40f879
GitHub Action <action@github.com>
2026-02-28 00:50:24 +0000

    Update mason-registry lock to 2026-02-27-common-rate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-27-direct-link"
+M.registry_release = "github:mason-org/mason-registry@2026-02-27-common-rate"
 
 return M
