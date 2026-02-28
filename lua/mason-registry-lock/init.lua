52bdb68ce961b46702b490e527f5f11aacb466c8
GitHub Action <action@github.com>
2026-02-28 00:50:24 +0000

    Update mason-registry lock to 2026-02-27-moral-oxygen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-27-strict-spear"
+M.registry_release = "github:mason-org/mason-registry@2026-02-27-moral-oxygen"
 
 return M
