672cb9e2e832bc1d38f0caead34319bc3e3f3f05
GitHub Action <action@github.com>
2026-02-28 00:50:24 +0000

    Update mason-registry lock to 2026-02-27-active-siren

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-02-27-drafty-curler"
+M.registry_release = "github:mason-org/mason-registry@2026-02-27-active-siren"
 
 return M
