683d88a1963f5d8425c6d88f4c93a43b4416966b
GitHub Action <action@github.com>
2026-03-02 00:57:21 +0000

    Update mason-registry lock to 2026-03-01-secret-bill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-01-thin-fence"
+M.registry_release = "github:mason-org/mason-registry@2026-03-01-secret-bill"
 
 return M
