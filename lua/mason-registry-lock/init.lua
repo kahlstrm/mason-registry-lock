7f3aca3c2fcaf0f581853b92810db80faaec215b
GitHub Action <action@github.com>
2026-03-03 00:59:29 +0000

    Update mason-registry lock to 2026-03-02-greedy-beauty

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-02-atomic-random"
+M.registry_release = "github:mason-org/mason-registry@2026-03-02-greedy-beauty"
 
 return M
