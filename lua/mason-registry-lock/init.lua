ed1fbf640450d7c8ec84cda53025acbaedfd30d1
GitHub Action <action@github.com>
2026-03-03 00:59:29 +0000

    Update mason-registry lock to 2026-03-02-knotty-silver

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-02-zany-lemur"
+M.registry_release = "github:mason-org/mason-registry@2026-03-02-knotty-silver"
 
 return M
