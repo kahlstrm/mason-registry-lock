4c9d91cac0e52a8e101e4f0d160cb472da3cbe2b
GitHub Action <action@github.com>
2026-03-03 00:59:29 +0000

    Update mason-registry lock to 2026-03-02-linear-cornet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-02-plump-james"
+M.registry_release = "github:mason-org/mason-registry@2026-03-02-linear-cornet"
 
 return M
