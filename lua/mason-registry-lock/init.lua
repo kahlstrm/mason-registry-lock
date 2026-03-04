fa5abb999af8d49749ce7b1d03bc8d6d21607fe5
GitHub Action <action@github.com>
2026-03-04 00:55:58 +0000

    Update mason-registry lock to 2026-03-03-antsy-fax

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-03-oblong-steam"
+M.registry_release = "github:mason-org/mason-registry@2026-03-03-antsy-fax"
 
 return M
