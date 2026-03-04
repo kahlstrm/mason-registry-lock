45894c7153873603be532bf9cf208f584869efa7
GitHub Action <action@github.com>
2026-03-04 00:55:58 +0000

    Update mason-registry lock to 2026-03-03-speedy-refund

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-03-steep-pear"
+M.registry_release = "github:mason-org/mason-registry@2026-03-03-speedy-refund"
 
 return M
