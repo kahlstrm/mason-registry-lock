16063e47eb4dfb5ff86e0bb8bcf56f4b854f1b8b
GitHub Action <action@github.com>
2026-03-04 00:55:58 +0000

    Update mason-registry lock to 2026-03-03-lazy-layer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-02-linear-cornet"
+M.registry_release = "github:mason-org/mason-registry@2026-03-03-lazy-layer"
 
 return M
