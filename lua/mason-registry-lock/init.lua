994797c657892331828d687827a8aa5a5ff9cb66
GitHub Action <action@github.com>
2026-03-05 00:58:05 +0000

    Update mason-registry lock to 2026-03-04-cut-faith

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-04-neat-cerium"
+M.registry_release = "github:mason-org/mason-registry@2026-03-04-cut-faith"
 
 return M
