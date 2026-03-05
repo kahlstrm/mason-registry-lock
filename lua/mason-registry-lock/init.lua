48a43c295a549509ac8d4c5cf76f2b0db40eefe9
GitHub Action <action@github.com>
2026-03-05 00:58:05 +0000

    Update mason-registry lock to 2026-03-04-wicked-self

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-04-lesser-bubble"
+M.registry_release = "github:mason-org/mason-registry@2026-03-04-wicked-self"
 
 return M
