128ba69e960dcefa0ea410134c72e7b593777864
GitHub Action <action@github.com>
2026-03-05 00:58:05 +0000

    Update mason-registry lock to 2026-03-04-left-loan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-04-labour-exit"
+M.registry_release = "github:mason-org/mason-registry@2026-03-04-left-loan"
 
 return M
