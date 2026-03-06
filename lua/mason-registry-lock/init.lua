d7c792622a5c43f5d94fb12c6f193a1f603a1534
GitHub Action <action@github.com>
2026-03-06 01:02:34 +0000

    Update mason-registry lock to 2026-03-05-french-judo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-05-aware-brace"
+M.registry_release = "github:mason-org/mason-registry@2026-03-05-french-judo"
 
 return M
