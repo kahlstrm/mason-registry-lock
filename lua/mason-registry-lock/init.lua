7400d19a218e008f60edcee4cc129e40859966c5
GitHub Action <action@github.com>
2026-03-06 01:02:34 +0000

    Update mason-registry lock to 2026-03-05-onyx-tope

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-05-pumped-wallet"
+M.registry_release = "github:mason-org/mason-registry@2026-03-05-onyx-tope"
 
 return M
