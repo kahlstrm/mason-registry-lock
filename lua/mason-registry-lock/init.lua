3ec8f66d25ad56d335fe1fbec1fa851d614b4d60
GitHub Action <action@github.com>
2026-03-06 01:02:34 +0000

    Update mason-registry lock to 2026-03-05-aware-brace

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-05-long-koala"
+M.registry_release = "github:mason-org/mason-registry@2026-03-05-aware-brace"
 
 return M
