8452cc11fd218307c6587a4f7497120a373496cb
GitHub Action <action@github.com>
2026-03-06 01:02:34 +0000

    Update mason-registry lock to 2026-03-05-melodic-mist

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-05-third-deity"
+M.registry_release = "github:mason-org/mason-registry@2026-03-05-melodic-mist"
 
 return M
