2f4ce5690eb48d08fbf3626f510d3d65951bb581
GitHub Action <action@github.com>
2026-03-06 01:02:34 +0000

    Update mason-registry lock to 2026-03-05-tough-chive

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-05-elegant-fan"
+M.registry_release = "github:mason-org/mason-registry@2026-03-05-tough-chive"
 
 return M
