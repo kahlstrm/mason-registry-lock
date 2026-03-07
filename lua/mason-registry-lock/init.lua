5b50b1d1c5206b8d4aa8e4b5a8d8ba483105d477
GitHub Action <action@github.com>
2026-03-07 00:54:05 +0000

    Update mason-registry lock to 2026-03-06-blank-switch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-06-recent-straw"
+M.registry_release = "github:mason-org/mason-registry@2026-03-06-blank-switch"
 
 return M
