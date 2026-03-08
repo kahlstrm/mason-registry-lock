64ff4a68d56436a2255e261f72e85c784fdedc5a
GitHub Action <action@github.com>
2026-03-08 00:58:00 +0000

    Update mason-registry lock to 2026-03-07-icy-degree

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-07-ruddy-touch"
+M.registry_release = "github:mason-org/mason-registry@2026-03-07-icy-degree"
 
 return M
