88bc91c921b781f5ef5bbfdd2593695c5bde940b
GitHub Action <action@github.com>
2026-03-09 00:58:03 +0000

    Update mason-registry lock to 2026-03-08-loving-hate

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-08-arab-tyvek"
+M.registry_release = "github:mason-org/mason-registry@2026-03-08-loving-hate"
 
 return M
