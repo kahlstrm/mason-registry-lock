7af3baa619902f88d1be06e47724aafa6f01971b
GitHub Action <action@github.com>
2026-03-09 00:58:03 +0000

    Update mason-registry lock to 2026-03-08-fair-kalium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-08-dutch-task"
+M.registry_release = "github:mason-org/mason-registry@2026-03-08-fair-kalium"
 
 return M
