03a35171dea5d0f244f1c8318e772d2597a34422
GitHub Action <action@github.com>
2026-03-10 00:53:12 +0000

    Update mason-registry lock to 2026-03-09-nutty-bonsai

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-09-greek-sea"
+M.registry_release = "github:mason-org/mason-registry@2026-03-09-nutty-bonsai"
 
 return M
