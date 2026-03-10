803e6c78f11733628277df7a8627008e640b6ac9
GitHub Action <action@github.com>
2026-03-10 00:53:12 +0000

    Update mason-registry lock to 2026-03-09-hard-gold

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-09-actual-catsup"
+M.registry_release = "github:mason-org/mason-registry@2026-03-09-hard-gold"
 
 return M
