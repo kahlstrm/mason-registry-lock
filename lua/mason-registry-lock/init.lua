5e7ac696d42fe4a9efa3994895b4ef3f61cd197c
GitHub Action <action@github.com>
2026-03-10 00:53:12 +0000

    Update mason-registry lock to 2026-03-09-steep-map

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-09-stormy-raft"
+M.registry_release = "github:mason-org/mason-registry@2026-03-09-steep-map"
 
 return M
