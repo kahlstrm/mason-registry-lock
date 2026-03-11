820e7483682e22e6e904e9441980328520b05a11
GitHub Action <action@github.com>
2026-03-11 00:54:08 +0000

    Update mason-registry lock to 2026-03-10-sedate-idol

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-meek-slip"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-sedate-idol"
 
 return M
