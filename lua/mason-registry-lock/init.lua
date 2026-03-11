7e04bb2f0bde989367e50c9a67cc0ee861fb4249
GitHub Action <action@github.com>
2026-03-11 00:54:07 +0000

    Update mason-registry lock to 2026-03-10-giddy-cover

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-irate-bonsai"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-giddy-cover"
 
 return M
