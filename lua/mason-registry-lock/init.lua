11aa6e0f047fa350348245c87daa1cf28b191b1d
GitHub Action <action@github.com>
2026-03-11 00:54:08 +0000

    Update mason-registry lock to 2026-03-10-misty-drill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-fine-moat"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-misty-drill"
 
 return M
