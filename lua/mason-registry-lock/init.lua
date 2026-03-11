8c3ae6cbc68e9e784a59b52c12a7f4aa4be65ffe
GitHub Action <action@github.com>
2026-03-11 00:54:07 +0000

    Update mason-registry lock to 2026-03-10-irate-bonsai

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-right-seeder"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-irate-bonsai"
 
 return M
