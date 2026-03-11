82aea6daf305289f6a62655dba177da6a8020cc0
GitHub Action <action@github.com>
2026-03-11 00:54:08 +0000

    Update mason-registry lock to 2026-03-10-alert-grasshopper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-best-body"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-alert-grasshopper"
 
 return M
