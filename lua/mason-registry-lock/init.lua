238debb057325440a6e26279a5fb0e9c6279ce06
GitHub Action <action@github.com>
2026-03-11 00:54:07 +0000

    Update mason-registry lock to 2026-03-09-rising-osmium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-09-living-rose"
+M.registry_release = "github:mason-org/mason-registry@2026-03-09-rising-osmium"
 
 return M
