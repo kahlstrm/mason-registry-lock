9dda41d62711761ed7fcf6c89a9a7b85ce000e23
GitHub Action <action@github.com>
2026-03-11 00:54:08 +0000

    Update mason-registry lock to 2026-03-10-amused-sword

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-aural-fence"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-amused-sword"
 
 return M
