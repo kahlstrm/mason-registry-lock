563a6a56c77046d2c014795450a7172e3402e1e8
GitHub Action <action@github.com>
2026-03-11 00:54:08 +0000

    Update mason-registry lock to 2026-03-10-calm-zebra

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-baked-action"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-calm-zebra"
 
 return M
