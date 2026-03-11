e8ddd1cef0f854557bfa2f5c546bdd3cc1eefefb
GitHub Action <action@github.com>
2026-03-11 00:54:07 +0000

    Update mason-registry lock to 2026-03-09-stormy-raft

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-09-rapid-sight"
+M.registry_release = "github:mason-org/mason-registry@2026-03-09-stormy-raft"
 
 return M
