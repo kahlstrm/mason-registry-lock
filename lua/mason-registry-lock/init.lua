d6e210c01bd554d46df3246731084e0d083b83fb
GitHub Action <action@github.com>
2026-03-11 00:54:07 +0000

    Update mason-registry lock to 2026-03-10-kindly-paste

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-10-awake-tea"
+M.registry_release = "github:mason-org/mason-registry@2026-03-10-kindly-paste"
 
 return M
