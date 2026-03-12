ddbabe2b1832160e227054ce6f540554da50ecaa
GitHub Action <action@github.com>
2026-03-12 00:50:45 +0000

    Update mason-registry lock to 2026-03-11-ratty-hemp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-11-dental-home"
+M.registry_release = "github:mason-org/mason-registry@2026-03-11-ratty-hemp"
 
 return M
