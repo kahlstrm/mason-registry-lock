3c63a2d4ec5b0a02960dc677c72d38634f7ac398
GitHub Action <action@github.com>
2026-03-12 00:50:45 +0000

    Update mason-registry lock to 2026-03-11-valid-cork

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-11-tired-impala"
+M.registry_release = "github:mason-org/mason-registry@2026-03-11-valid-cork"
 
 return M
