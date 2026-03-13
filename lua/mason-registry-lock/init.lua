02fc374e0b9d9b38645411832ad2b11558588b83
GitHub Action <action@github.com>
2026-03-13 00:56:33 +0000

    Update mason-registry lock to 2026-03-12-lined-coast

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-12-sedate-limia"
+M.registry_release = "github:mason-org/mason-registry@2026-03-12-lined-coast"
 
 return M
