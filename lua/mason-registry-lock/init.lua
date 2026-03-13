34fac193372b37483966fb007347200531437397
GitHub Action <action@github.com>
2026-03-13 00:56:33 +0000

    Update mason-registry lock to 2026-03-12-wild-lyric

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-12-bawdy-betty"
+M.registry_release = "github:mason-org/mason-registry@2026-03-12-wild-lyric"
 
 return M
