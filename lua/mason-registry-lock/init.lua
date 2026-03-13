d1c964173672e76cfdcd1ecd288ad0f1a7e100be
GitHub Action <action@github.com>
2026-03-13 00:56:33 +0000

    Update mason-registry lock to 2026-03-12-ruling-hall

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-12-imaginary-addax"
+M.registry_release = "github:mason-org/mason-registry@2026-03-12-ruling-hall"
 
 return M
