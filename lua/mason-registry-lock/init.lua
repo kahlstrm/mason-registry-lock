eb321b53274dc369177d806a58093e673e322694
GitHub Action <action@github.com>
2026-03-13 00:56:33 +0000

    Update mason-registry lock to 2026-03-12-sedate-limia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-12-short-place"
+M.registry_release = "github:mason-org/mason-registry@2026-03-12-sedate-limia"
 
 return M
