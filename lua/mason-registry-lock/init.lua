919a4ae5e9cac55d9a4cf9edc23039342f58e259
GitHub Action <action@github.com>
2026-03-14 00:55:49 +0000

    Update mason-registry lock to 2026-03-13-helpful-bugle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-13-barren-milk"
+M.registry_release = "github:mason-org/mason-registry@2026-03-13-helpful-bugle"
 
 return M
