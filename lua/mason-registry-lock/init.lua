d4613e46c2743809229a63b8ab29cc8d47b4c048
GitHub Action <action@github.com>
2026-03-16 01:05:45 +0000

    Update mason-registry lock to 2026-03-15-absent-design

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-15-sharp-meal"
+M.registry_release = "github:mason-org/mason-registry@2026-03-15-absent-design"
 
 return M
