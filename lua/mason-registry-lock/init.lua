8f573cfc17e5189b232436fb78a3a357bef2e210
GitHub Action <action@github.com>
2026-03-17 00:58:05 +0000

    Update mason-registry lock to 2026-03-16-ablaze-liquid

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-15-tangy-server"
+M.registry_release = "github:mason-org/mason-registry@2026-03-16-ablaze-liquid"
 
 return M
