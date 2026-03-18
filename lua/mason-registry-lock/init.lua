6ca4fef612fdc3ed2d9a04c4719e9dacb2995a41
GitHub Action <action@github.com>
2026-03-18 01:02:17 +0000

    Update mason-registry lock to 2026-03-17-honest-match

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-17-pumped-bow"
+M.registry_release = "github:mason-org/mason-registry@2026-03-17-honest-match"
 
 return M
