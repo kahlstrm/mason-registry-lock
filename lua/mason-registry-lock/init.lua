6b881fa8691857040245146c754d391c36e82682
GitHub Action <action@github.com>
2026-03-18 01:02:17 +0000

    Update mason-registry lock to 2026-03-17-even-trial

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-17-wicked-object"
+M.registry_release = "github:mason-org/mason-registry@2026-03-17-even-trial"
 
 return M
