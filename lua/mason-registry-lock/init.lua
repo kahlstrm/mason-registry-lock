caa668bd784b358254b81b7031a7c9a7bbc1928d
GitHub Action <action@github.com>
2026-03-18 01:02:17 +0000

    Update mason-registry lock to 2026-03-17-brisk-market

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-17-honest-match"
+M.registry_release = "github:mason-org/mason-registry@2026-03-17-brisk-market"
 
 return M
