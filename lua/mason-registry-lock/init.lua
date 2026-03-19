e550ae6fe1a0c8ffcd5adb730b672ced94c0aaa0
GitHub Action <action@github.com>
2026-03-19 01:01:15 +0000

    Update mason-registry lock to 2026-03-18-absent-value

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-18-tawdry-cartoon"
+M.registry_release = "github:mason-org/mason-registry@2026-03-18-absent-value"
 
 return M
