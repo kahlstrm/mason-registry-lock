5c0bc4db21635909341d255130769604cab71ccb
GitHub Action <action@github.com>
2026-03-19 01:01:15 +0000

    Update mason-registry lock to 2026-03-18-square-grasshopper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-18-livid-puffin"
+M.registry_release = "github:mason-org/mason-registry@2026-03-18-square-grasshopper"
 
 return M
