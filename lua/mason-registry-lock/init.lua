f0dd3e62d2f1f447f8d8b968d89e8abb979e6a83
GitHub Action <action@github.com>
2026-03-20 00:58:22 +0000

    Update mason-registry lock to 2026-03-19-quiet-fired

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-19-onyx-week"
+M.registry_release = "github:mason-org/mason-registry@2026-03-19-quiet-fired"
 
 return M
