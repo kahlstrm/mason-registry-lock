90200d519b80a1fa79464eec4e49c3eae830179e
GitHub Action <action@github.com>
2026-03-20 00:58:22 +0000

    Update mason-registry lock to 2026-03-19-astral-soil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-19-swanky-debtor"
+M.registry_release = "github:mason-org/mason-registry@2026-03-19-astral-soil"
 
 return M
