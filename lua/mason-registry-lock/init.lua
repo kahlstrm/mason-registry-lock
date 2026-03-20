659e5c03a77d763a8f74be2f0a1eccfe5dcbfa32
GitHub Action <action@github.com>
2026-03-20 00:58:22 +0000

    Update mason-registry lock to 2026-03-19-major-output

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-19-large-bone"
+M.registry_release = "github:mason-org/mason-registry@2026-03-19-major-output"
 
 return M
