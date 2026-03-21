91796772db2a6816388913bc8a46e10d6a73b4cc
GitHub Action <action@github.com>
2026-03-21 00:54:28 +0000

    Update mason-registry lock to 2026-03-20-wobbly-lung

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-20-tense-weight"
+M.registry_release = "github:mason-org/mason-registry@2026-03-20-wobbly-lung"
 
 return M
