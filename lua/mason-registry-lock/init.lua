7cf34010ee76ce741f6f0a5dfcdccfd2c14dc9d4
GitHub Action <action@github.com>
2026-03-21 00:54:28 +0000

    Update mason-registry lock to 2026-03-20-giddy-camera

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-20-ripe-celery"
+M.registry_release = "github:mason-org/mason-registry@2026-03-20-giddy-camera"
 
 return M
