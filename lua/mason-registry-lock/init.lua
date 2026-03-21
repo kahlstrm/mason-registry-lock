ee20253a1333d95a79f3c8e877b124d614a9757e
GitHub Action <action@github.com>
2026-03-21 00:54:28 +0000

    Update mason-registry lock to 2026-03-20-tense-weight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-20-dim-roach"
+M.registry_release = "github:mason-org/mason-registry@2026-03-20-tense-weight"
 
 return M
