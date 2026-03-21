1865b4354105d06d7df817d50cdf9e0ec2942219
GitHub Action <action@github.com>
2026-03-21 00:54:28 +0000

    Update mason-registry lock to 2026-03-20-calm-alibi

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-20-dusty-pipe"
+M.registry_release = "github:mason-org/mason-registry@2026-03-20-calm-alibi"
 
 return M
