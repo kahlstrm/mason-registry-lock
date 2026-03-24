4dad60caaa83e9183dea082a30f2cb7cb0c5312b
GitHub Action <action@github.com>
2026-03-24 00:55:32 +0000

    Update mason-registry lock to 2026-03-24-smart-tank

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-23-greek-blue"
+M.registry_release = "github:mason-org/mason-registry@2026-03-24-smart-tank"
 
 return M
