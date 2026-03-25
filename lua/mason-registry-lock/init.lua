904fb4965f9398a078ab7d0848f5cc9de5427124
GitHub Action <action@github.com>
2026-03-25 01:00:25 +0000

    Update mason-registry lock to 2026-03-24-faint-pull

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-24-frizzy-moat"
+M.registry_release = "github:mason-org/mason-registry@2026-03-24-faint-pull"
 
 return M
