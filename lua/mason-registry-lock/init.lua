1cc4774437c1aef9c840312104d0c8001948bc4a
GitHub Action <action@github.com>
2026-03-25 01:00:25 +0000

    Update mason-registry lock to 2026-03-24-pale-mouse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-24-faint-pull"
+M.registry_release = "github:mason-org/mason-registry@2026-03-24-pale-mouse"
 
 return M
