99b9fc70d77066b601f74b1537a776b897a2bccf
GitHub Action <action@github.com>
2026-03-26 01:05:27 +0000

    Update mason-registry lock to 2026-03-25-pink-brake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-25-sheer-canvas"
+M.registry_release = "github:mason-org/mason-registry@2026-03-25-pink-brake"
 
 return M
