0584f52a533c29dca02910ecf992940d03cddad2
GitHub Action <action@github.com>
2026-03-26 01:05:27 +0000

    Update mason-registry lock to 2026-03-26-ruby-answer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-26-ample-cotton"
+M.registry_release = "github:mason-org/mason-registry@2026-03-26-ruby-answer"
 
 return M
