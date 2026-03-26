0ea598d5456173e9fbda4dedd967decdeac60f67
GitHub Action <action@github.com>
2026-03-26 01:05:27 +0000

    Update mason-registry lock to 2026-03-25-small-ping

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-25-quain-person"
+M.registry_release = "github:mason-org/mason-registry@2026-03-25-small-ping"
 
 return M
