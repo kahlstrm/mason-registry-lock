f6ecc1ee24bfd5f25c7c08ac85df9ecccc35fcf2
GitHub Action <action@github.com>
2026-03-26 01:05:27 +0000

    Update mason-registry lock to 2026-03-25-adhoc-ground

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-25-small-ping"
+M.registry_release = "github:mason-org/mason-registry@2026-03-25-adhoc-ground"
 
 return M
