2c3bcca5c04b27953696bdfd074bb7ad1bb348fe
GitHub Action <action@github.com>
2026-03-28 00:59:22 +0000

    Update mason-registry lock to 2026-03-27-icy-unicorn

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-27-raspy-freeze"
+M.registry_release = "github:mason-org/mason-registry@2026-03-27-icy-unicorn"
 
 return M
