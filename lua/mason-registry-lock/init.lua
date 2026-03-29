2ba0056af4b0e8ae453de9b77768169e6e1a80cb
GitHub Action <action@github.com>
2026-03-29 01:08:06 +0000

    Update mason-registry lock to 2026-03-28-narrow-world

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-28-annual-pepper"
+M.registry_release = "github:mason-org/mason-registry@2026-03-28-narrow-world"
 
 return M
