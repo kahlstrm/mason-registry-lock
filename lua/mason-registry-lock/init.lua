e75699862ab05a71e764f2503ec6ce6e514b01d5
GitHub Action <action@github.com>
2026-03-29 01:08:06 +0000

    Update mason-registry lock to 2026-03-28-acid-mole

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-28-kooky-oyster"
+M.registry_release = "github:mason-org/mason-registry@2026-03-28-acid-mole"
 
 return M
