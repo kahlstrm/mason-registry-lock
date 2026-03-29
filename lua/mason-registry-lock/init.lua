4649760170aa6d325f3a5c541271a6ae2c990de5
GitHub Action <action@github.com>
2026-03-29 01:08:06 +0000

    Update mason-registry lock to 2026-03-28-usual-phlox

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-28-copper-amago"
+M.registry_release = "github:mason-org/mason-registry@2026-03-28-usual-phlox"
 
 return M
