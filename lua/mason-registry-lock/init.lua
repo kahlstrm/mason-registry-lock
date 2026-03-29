01672026ff9a1dd1686bdee75728a42a0014a4a2
GitHub Action <action@github.com>
2026-03-29 01:08:06 +0000

    Update mason-registry lock to 2026-03-28-nine-wrasse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-28-humble-age"
+M.registry_release = "github:mason-org/mason-registry@2026-03-28-nine-wrasse"
 
 return M
