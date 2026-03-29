3ef3c9b4af5ec03545e141bf65d33dd6cb9c93e7
GitHub Action <action@github.com>
2026-03-29 01:08:06 +0000

    Update mason-registry lock to 2026-03-28-copper-amago

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-28-nine-wrasse"
+M.registry_release = "github:mason-org/mason-registry@2026-03-28-copper-amago"
 
 return M
