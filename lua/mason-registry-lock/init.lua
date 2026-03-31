64342d145635715c9f4ed031f675bdbabcb9adba
GitHub Action <action@github.com>
2026-03-31 01:06:56 +0000

    Update mason-registry lock to 2026-03-30-livid-group

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-28-narrow-world"
+M.registry_release = "github:mason-org/mason-registry@2026-03-30-livid-group"
 
 return M
