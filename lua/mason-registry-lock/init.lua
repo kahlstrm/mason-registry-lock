baa2669293050875f81d287ec7db5092625a5d25
GitHub Action <action@github.com>
2026-04-01 01:13:41 +0000

    Update mason-registry lock to 2026-03-31-fixed-flight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-31-visual-truck"
+M.registry_release = "github:mason-org/mason-registry@2026-03-31-fixed-flight"
 
 return M
