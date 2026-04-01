e4dcd590798139e66e19276a232d7604d59ed658
GitHub Action <action@github.com>
2026-04-01 01:13:41 +0000

    Update mason-registry lock to 2026-03-31-strict-cymbal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-31-quirky-vision"
+M.registry_release = "github:mason-org/mason-registry@2026-03-31-strict-cymbal"
 
 return M
