b5eb8129aac9bda3392068411917046d72ef9991
GitHub Action <action@github.com>
2026-04-01 01:13:41 +0000

    Update mason-registry lock to 2026-03-31-scant-banjo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-31-seemly-train"
+M.registry_release = "github:mason-org/mason-registry@2026-03-31-scant-banjo"
 
 return M
