09fc6163855a6da5861ff8e73c1523d0e4df5a51
GitHub Action <action@github.com>
2026-04-01 01:13:41 +0000

    Update mason-registry lock to 2026-03-31-flat-tooth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-31-given-thumb"
+M.registry_release = "github:mason-org/mason-registry@2026-03-31-flat-tooth"
 
 return M
