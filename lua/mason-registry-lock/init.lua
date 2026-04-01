10c105212dea1033c0371b9e13bacbcb34c1d84b
GitHub Action <action@github.com>
2026-04-01 01:13:41 +0000

    Update mason-registry lock to 2026-03-31-seemly-train

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-31-small-summer"
+M.registry_release = "github:mason-org/mason-registry@2026-03-31-seemly-train"
 
 return M
