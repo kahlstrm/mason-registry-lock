042a22c8441b4da3965db75d8a49a4c330d18175
GitHub Action <action@github.com>
2026-04-01 01:13:41 +0000

    Update mason-registry lock to 2026-03-31-free-dolphin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-31-grim-blouse"
+M.registry_release = "github:mason-org/mason-registry@2026-03-31-free-dolphin"
 
 return M
