e660d73c32ea5436a50698fcf17cb87d44e2c5f6
GitHub Action <action@github.com>
2026-04-01 01:13:41 +0000

    Update mason-registry lock to 2026-03-31-latin-dress

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-03-31-drab-wheel"
+M.registry_release = "github:mason-org/mason-registry@2026-03-31-latin-dress"
 
 return M
