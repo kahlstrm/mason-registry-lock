ce4833e670ce168ec0cb249fc65ab39839c574ad
GitHub Action <action@github.com>
2026-04-02 01:03:39 +0000

    Update mason-registry lock to 2026-04-01-mixed-temple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-01-gusty-alley"
+M.registry_release = "github:mason-org/mason-registry@2026-04-01-mixed-temple"
 
 return M
