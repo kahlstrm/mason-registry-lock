1045ee08701fd7690f7e26e947017864644087b4
GitHub Action <action@github.com>
2026-04-02 01:03:39 +0000

    Update mason-registry lock to 2026-04-01-cool-plaice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-01-polite-audio"
+M.registry_release = "github:mason-org/mason-registry@2026-04-01-cool-plaice"
 
 return M
