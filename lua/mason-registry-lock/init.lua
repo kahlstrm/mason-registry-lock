8ed8a5d66286aeb369f77baebffd9888e790049b
GitHub Action <action@github.com>
2026-04-02 01:03:40 +0000

    Update mason-registry lock to 2026-04-01-vain-dirt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-01-rustic-snoek"
+M.registry_release = "github:mason-org/mason-registry@2026-04-01-vain-dirt"
 
 return M
