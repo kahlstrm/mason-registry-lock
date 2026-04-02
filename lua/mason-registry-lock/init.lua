da8ec5e868de2775399171da0ebdbbdf967e03cd
GitHub Action <action@github.com>
2026-04-02 01:03:39 +0000

    Update mason-registry lock to 2026-04-01-precious-celery

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-01-mixed-temple"
+M.registry_release = "github:mason-org/mason-registry@2026-04-01-precious-celery"
 
 return M
