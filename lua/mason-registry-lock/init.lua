a64fdafa8afe7859c514744175fd461131632052
GitHub Action <action@github.com>
2026-04-02 01:03:39 +0000

    Update mason-registry lock to 2026-04-01-retail-mass

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-01-safe-xenon"
+M.registry_release = "github:mason-org/mason-registry@2026-04-01-retail-mass"
 
 return M
