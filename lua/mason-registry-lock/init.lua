e6f8aa21b98c135a355fbab5e3d1c8ff32f38064
GitHub Action <action@github.com>
2026-04-03 01:06:04 +0000

    Update mason-registry lock to 2026-04-03-tight-nurse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-03-funny-park"
+M.registry_release = "github:mason-org/mason-registry@2026-04-03-tight-nurse"
 
 return M
