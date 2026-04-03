3afb662da2a4ce954a935aafcae8961a747a4c21
GitHub Action <action@github.com>
2026-04-03 01:06:04 +0000

    Update mason-registry lock to 2026-04-02-abrupt-dew

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-02-welsh-oryx"
+M.registry_release = "github:mason-org/mason-registry@2026-04-02-abrupt-dew"
 
 return M
