5206ddc3bc620e95b764b345c3dffbd40406554d
GitHub Action <action@github.com>
2026-04-03 01:06:04 +0000

    Update mason-registry lock to 2026-04-02-hasty-zipper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-02-vain-sprout"
+M.registry_release = "github:mason-org/mason-registry@2026-04-02-hasty-zipper"
 
 return M
