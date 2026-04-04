3fc2c72afaf1806f2418a37a6bd684e0175edfcb
GitHub Action <action@github.com>
2026-04-04 01:00:43 +0000

    Update mason-registry lock to 2026-04-03-woeful-column

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-03-known-spear"
+M.registry_release = "github:mason-org/mason-registry@2026-04-03-woeful-column"
 
 return M
