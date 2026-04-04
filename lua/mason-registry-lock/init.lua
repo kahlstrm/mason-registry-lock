86288d5f239edcadd219a39e8eabde66478a1a9d
GitHub Action <action@github.com>
2026-04-04 01:00:43 +0000

    Update mason-registry lock to 2026-04-03-gamy-space

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-03-harsh-galaxy"
+M.registry_release = "github:mason-org/mason-registry@2026-04-03-gamy-space"
 
 return M
