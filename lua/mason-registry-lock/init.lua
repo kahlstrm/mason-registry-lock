d5dc84761731897bb820acec83aa2218faaff005
GitHub Action <action@github.com>
2026-04-05 01:09:14 +0000

    Update mason-registry lock to 2026-04-04-aural-tongue

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-04-pushy-tanker"
+M.registry_release = "github:mason-org/mason-registry@2026-04-04-aural-tongue"
 
 return M
