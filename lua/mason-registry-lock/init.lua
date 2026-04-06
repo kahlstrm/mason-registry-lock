1ad2ce6f993d7e2e4e8e8e8dc815c06773a3e806
GitHub Action <action@github.com>
2026-04-06 01:09:14 +0000

    Update mason-registry lock to 2026-04-05-forked-bacon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-05-added-zipper"
+M.registry_release = "github:mason-org/mason-registry@2026-04-05-forked-bacon"
 
 return M
