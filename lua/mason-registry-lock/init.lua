e07dbeaac7131104a591c962742e7a99956f65ac
GitHub Action <action@github.com>
2026-04-07 01:07:07 +0000

    Update mason-registry lock to 2026-04-06-tough-rubber

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-06-helpful-bangu"
+M.registry_release = "github:mason-org/mason-registry@2026-04-06-tough-rubber"
 
 return M
