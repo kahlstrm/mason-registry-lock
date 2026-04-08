54d8e300c9aa662b30806db81ba93d86d44427a7
GitHub Action <action@github.com>
2026-04-08 01:08:10 +0000

    Update mason-registry lock to 2026-04-07-steady-oxygen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-07-sparse-iron"
+M.registry_release = "github:mason-org/mason-registry@2026-04-07-steady-oxygen"
 
 return M
