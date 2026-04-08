727fc7d28c398e9620d826d17a60bff10889a6c7
GitHub Action <action@github.com>
2026-04-08 01:08:10 +0000

    Update mason-registry lock to 2026-04-07-sparse-iron

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-07-kind-flare"
+M.registry_release = "github:mason-org/mason-registry@2026-04-07-sparse-iron"
 
 return M
