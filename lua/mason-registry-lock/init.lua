5935a7694efa9d73c851f02a76553008fee33ff2
GitHub Action <action@github.com>
2026-04-08 01:08:10 +0000

    Update mason-registry lock to 2026-04-07-bright-comb

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-07-bumpy-meal"
+M.registry_release = "github:mason-org/mason-registry@2026-04-07-bright-comb"
 
 return M
