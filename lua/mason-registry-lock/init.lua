9f00ddae0f591bcb5003a2d2b99b85f8617cce95
GitHub Action <action@github.com>
2026-04-08 01:08:10 +0000

    Update mason-registry lock to 2026-04-07-eager-decade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-07-alike-giraffe"
+M.registry_release = "github:mason-org/mason-registry@2026-04-07-eager-decade"
 
 return M
