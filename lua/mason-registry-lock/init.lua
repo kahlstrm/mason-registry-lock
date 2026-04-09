a7f0095e3346cbf9ee83d50a86babcfd92cae5e6
GitHub Action <action@github.com>
2026-04-09 00:58:28 +0000

    Update mason-registry lock to 2026-04-09-toxic-mirror

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-08-aloof-rocket"
+M.registry_release = "github:mason-org/mason-registry@2026-04-09-toxic-mirror"
 
 return M
