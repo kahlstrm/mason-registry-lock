9f95f298f1697199478aeca11e95629e12c39625
GitHub Action <action@github.com>
2026-04-09 00:58:28 +0000

    Update mason-registry lock to 2026-04-08-clean-part

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-08-select-zero"
+M.registry_release = "github:mason-org/mason-registry@2026-04-08-clean-part"
 
 return M
