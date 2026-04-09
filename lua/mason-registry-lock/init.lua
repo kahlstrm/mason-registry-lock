a646bafb4c7ea1641b751199fbd0a0aae0b5d2d8
GitHub Action <action@github.com>
2026-04-09 00:58:28 +0000

    Update mason-registry lock to 2026-04-08-aloof-rocket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-08-mixed-sycamore"
+M.registry_release = "github:mason-org/mason-registry@2026-04-08-aloof-rocket"
 
 return M
