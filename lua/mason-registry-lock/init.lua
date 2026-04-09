8b51a8598580d7dcc4fdeb054567d1467d4c8371
GitHub Action <action@github.com>
2026-04-09 00:58:28 +0000

    Update mason-registry lock to 2026-04-08-goofy-part

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-08-dutch-audio"
+M.registry_release = "github:mason-org/mason-registry@2026-04-08-goofy-part"
 
 return M
