6ac2a2a772594af0c240e61f0c6ddec9a6592a14
GitHub Action <action@github.com>
2026-04-09 00:58:28 +0000

    Update mason-registry lock to 2026-04-08-keen-nail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-08-verbal-tetra"
+M.registry_release = "github:mason-org/mason-registry@2026-04-08-keen-nail"
 
 return M
