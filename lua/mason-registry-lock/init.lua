f707e4a0782364c80a9a7003440a211b9ecfc49d
GitHub Action <action@github.com>
2026-04-10 01:08:55 +0000

    Update mason-registry lock to 2026-04-09-pale-edge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-09-magical-reward"
+M.registry_release = "github:mason-org/mason-registry@2026-04-09-pale-edge"
 
 return M
