1ce4ff16eb11c53bbcd0d7552045f7a3599d14c3
GitHub Action <action@github.com>
2026-04-10 01:08:55 +0000

    Update mason-registry lock to 2026-04-09-ruby-grease

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-09-creepy-orange"
+M.registry_release = "github:mason-org/mason-registry@2026-04-09-ruby-grease"
 
 return M
