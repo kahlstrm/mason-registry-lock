80a2538507515234263e555c6d32f9b94bbbd8bb
GitHub Action <action@github.com>
2026-04-11 01:04:19 +0000

    Update mason-registry lock to 2026-04-10-live-slave

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-10-added-reward"
+M.registry_release = "github:mason-org/mason-registry@2026-04-10-live-slave"
 
 return M
