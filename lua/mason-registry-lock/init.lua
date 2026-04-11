d94a19981b7ef1a69494b315a902d6d5b525bf40
GitHub Action <action@github.com>
2026-04-11 01:04:19 +0000

    Update mason-registry lock to 2026-04-10-drafty-stitch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-10-live-slave"
+M.registry_release = "github:mason-org/mason-registry@2026-04-10-drafty-stitch"
 
 return M
