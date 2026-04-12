62987861669f0b927788ff980d1bd5847d72a5ca
GitHub Action <action@github.com>
2026-04-12 01:12:40 +0000

    Update mason-registry lock to 2026-04-11-south-tenor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-11-obese-self"
+M.registry_release = "github:mason-org/mason-registry@2026-04-11-south-tenor"
 
 return M
