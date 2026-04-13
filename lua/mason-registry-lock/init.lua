6b98fafea2764b9e867056c7936710ff845945e5
GitHub Action <action@github.com>
2026-04-13 01:14:28 +0000

    Update mason-registry lock to 2026-04-12-vital-column

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-12-livid-goose"
+M.registry_release = "github:mason-org/mason-registry@2026-04-12-vital-column"
 
 return M
