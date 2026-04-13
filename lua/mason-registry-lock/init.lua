5d5bd5cd219cc2132ac8454f4b2a1a8d8b961a11
GitHub Action <action@github.com>
2026-04-13 01:14:28 +0000

    Update mason-registry lock to 2026-04-12-livid-goose

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-12-live-friend"
+M.registry_release = "github:mason-org/mason-registry@2026-04-12-livid-goose"
 
 return M
