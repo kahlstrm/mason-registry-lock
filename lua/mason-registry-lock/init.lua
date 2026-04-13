6c3d5435403682434939a07fb7a4180759ba61ee
GitHub Action <action@github.com>
2026-04-13 01:14:28 +0000

    Update mason-registry lock to 2026-04-12-rural-cirrus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-12-vital-column"
+M.registry_release = "github:mason-org/mason-registry@2026-04-12-rural-cirrus"
 
 return M
