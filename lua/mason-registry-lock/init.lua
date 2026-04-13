a85cea8e2ede876e9cadc1cbc45c4bf4d90804f0
GitHub Action <action@github.com>
2026-04-13 01:14:28 +0000

    Update mason-registry lock to 2026-04-12-acute-tower

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-12-aback-slime"
+M.registry_release = "github:mason-org/mason-registry@2026-04-12-acute-tower"
 
 return M
