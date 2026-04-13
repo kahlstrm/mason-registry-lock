6139cd66ac9407991a8053ea0089995928249014
GitHub Action <action@github.com>
2026-04-13 01:14:28 +0000

    Update mason-registry lock to 2026-04-12-joyous-lily

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-12-acute-tower"
+M.registry_release = "github:mason-org/mason-registry@2026-04-12-joyous-lily"
 
 return M
