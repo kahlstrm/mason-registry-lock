475fe6859eb4f94c2a8a1f7fa121fc195aff962a
GitHub Action <action@github.com>
2026-04-14 01:13:15 +0000

    Update mason-registry lock to 2026-04-13-dry-wheat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-13-livid-crack"
+M.registry_release = "github:mason-org/mason-registry@2026-04-13-dry-wheat"
 
 return M
