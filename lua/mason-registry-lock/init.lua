ba31f2e861518223a789b5982d516bf8d019f61c
GitHub Action <action@github.com>
2026-04-14 01:13:15 +0000

    Update mason-registry lock to 2026-04-13-mean-coil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-13-subtle-porgy"
+M.registry_release = "github:mason-org/mason-registry@2026-04-13-mean-coil"
 
 return M
