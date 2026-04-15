8d755b424826f2f22d08defba9e07cc37d3588e8
GitHub Action <action@github.com>
2026-04-15 01:11:37 +0000

    Update mason-registry lock to 2026-04-14-unruly-color

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-14-sweaty-witch"
+M.registry_release = "github:mason-org/mason-registry@2026-04-14-unruly-color"
 
 return M
