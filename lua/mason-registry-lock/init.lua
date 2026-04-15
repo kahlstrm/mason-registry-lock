f37126193bcfe6ceacae528371ea0b021b941a81
GitHub Action <action@github.com>
2026-04-15 01:11:37 +0000

    Update mason-registry lock to 2026-04-14-fair-steel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-14-hot-koodoo"
+M.registry_release = "github:mason-org/mason-registry@2026-04-14-fair-steel"
 
 return M
