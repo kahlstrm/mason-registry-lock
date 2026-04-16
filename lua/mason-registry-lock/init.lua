60ee767bd4fb99ef4d4f325dd62c73001ca0332b
GitHub Action <action@github.com>
2026-04-16 01:16:01 +0000

    Update mason-registry lock to 2026-04-15-stiff-chain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-15-dark-coach"
+M.registry_release = "github:mason-org/mason-registry@2026-04-15-stiff-chain"
 
 return M
