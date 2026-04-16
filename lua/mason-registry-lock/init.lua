6c1c464204e0917e6b0940e9b4a80979e28af052
GitHub Action <action@github.com>
2026-04-16 01:16:01 +0000

    Update mason-registry lock to 2026-04-15-shaky-cast

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-15-sick-cereal"
+M.registry_release = "github:mason-org/mason-registry@2026-04-15-shaky-cast"
 
 return M
