a934784c3f88153a6bd35592886f188477279a57
GitHub Action <action@github.com>
2026-04-17 01:13:37 +0000

    Update mason-registry lock to 2026-04-15-wrong-bison

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-15-actual-deck"
+M.registry_release = "github:mason-org/mason-registry@2026-04-15-wrong-bison"
 
 return M
