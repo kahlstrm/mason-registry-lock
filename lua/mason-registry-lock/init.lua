43bb92c6f033903d6d0206904de32b1e72d72558
GitHub Action <action@github.com>
2026-04-17 01:13:37 +0000

    Update mason-registry lock to 2026-04-16-female-spot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-16-lame-mars"
+M.registry_release = "github:mason-org/mason-registry@2026-04-16-female-spot"
 
 return M
