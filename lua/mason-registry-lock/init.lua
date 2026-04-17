4ed6e9c8d0a97dcc1834a7b369391a37ebc68101
GitHub Action <action@github.com>
2026-04-17 01:13:37 +0000

    Update mason-registry lock to 2026-04-16-scared-bucket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-16-female-spot"
+M.registry_release = "github:mason-org/mason-registry@2026-04-16-scared-bucket"
 
 return M
