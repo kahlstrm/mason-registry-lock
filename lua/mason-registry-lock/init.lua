cdbf7ce50cb7a7030bfea34ae1cb420bbdfb66aa
GitHub Action <action@github.com>
2026-04-17 01:13:37 +0000

    Update mason-registry lock to 2026-04-16-antiquet-sheet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-16-melted-core"
+M.registry_release = "github:mason-org/mason-registry@2026-04-16-antiquet-sheet"
 
 return M
