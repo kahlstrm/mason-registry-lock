426e560af57b543c2c69bd08f3962727ab2b9e90
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-17-aware-mynah

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-17-cloudy-danio"
+M.registry_release = "github:mason-org/mason-registry@2026-04-17-aware-mynah"
 
 return M
