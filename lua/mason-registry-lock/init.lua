6b572c3ad482db82a6bd30c50d8a87a6eb7d22c4
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-17-doting-motion

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-17-elite-lightning"
+M.registry_release = "github:mason-org/mason-registry@2026-04-17-doting-motion"
 
 return M
