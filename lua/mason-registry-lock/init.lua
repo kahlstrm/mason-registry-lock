64cd211d1bba5f5e018181dffdc14d1a284672d2
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-17-open-trunk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-17-mere-answer"
+M.registry_release = "github:mason-org/mason-registry@2026-04-17-open-trunk"
 
 return M
