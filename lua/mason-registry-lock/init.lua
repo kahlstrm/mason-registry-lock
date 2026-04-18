16591f60e6ff2fc344bf3b6f1bddb1ac21fee998
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-16-slimy-chef

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-16-wide-chrome"
+M.registry_release = "github:mason-org/mason-registry@2026-04-16-slimy-chef"
 
 return M
