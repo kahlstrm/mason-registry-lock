44f6c9e4215db047a82559ab7ecc33537a7f4676
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-16-ideal-loan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-16-recent-chime"
+M.registry_release = "github:mason-org/mason-registry@2026-04-16-ideal-loan"
 
 return M
