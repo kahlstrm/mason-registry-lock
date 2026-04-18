2e936a85f70ce4bca5ca428081ce9d9932330513
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-17-mere-answer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-17-aware-bail"
+M.registry_release = "github:mason-org/mason-registry@2026-04-17-mere-answer"
 
 return M
