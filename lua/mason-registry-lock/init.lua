6d13760366a42e7ce0d62e722400619a60c92be0
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-17-glass-level

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-17-sole-goose"
+M.registry_release = "github:mason-org/mason-registry@2026-04-17-glass-level"
 
 return M
