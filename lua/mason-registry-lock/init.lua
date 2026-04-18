827cf07b9cc5651aee44ae22981290e49ba1b739
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-17-elite-lightning

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-17-greek-belief"
+M.registry_release = "github:mason-org/mason-registry@2026-04-17-elite-lightning"
 
 return M
