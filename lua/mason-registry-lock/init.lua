9b769b43e03b0f9c334f3296c438a2d5ac47f036
GitHub Action <action@github.com>
2026-04-18 01:07:09 +0000

    Update mason-registry lock to 2026-04-17-kosher-cover

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-17-knobby-angler"
+M.registry_release = "github:mason-org/mason-registry@2026-04-17-kosher-cover"
 
 return M
