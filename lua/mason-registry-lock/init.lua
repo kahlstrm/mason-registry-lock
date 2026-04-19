33143d7cd109e1e5e5e632a34f79aa53dfc3bd0d
GitHub Action <action@github.com>
2026-04-19 01:15:33 +0000

    Update mason-registry lock to 2026-04-18-polish-unit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-18-vast-pump"
+M.registry_release = "github:mason-org/mason-registry@2026-04-18-polish-unit"
 
 return M
