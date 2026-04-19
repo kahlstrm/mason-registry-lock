540c451708b4cedffd3d844d7c3fb12ab876affd
GitHub Action <action@github.com>
2026-04-19 01:15:33 +0000

    Update mason-registry lock to 2026-04-18-grey-trance

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-18-rough-find"
+M.registry_release = "github:mason-org/mason-registry@2026-04-18-grey-trance"
 
 return M
