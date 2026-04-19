ad2423886c5a8b0d447cccd9a4dd16c166639de8
GitHub Action <action@github.com>
2026-04-19 01:15:33 +0000

    Update mason-registry lock to 2026-04-18-rough-find

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-18-bottled-point"
+M.registry_release = "github:mason-org/mason-registry@2026-04-18-rough-find"
 
 return M
