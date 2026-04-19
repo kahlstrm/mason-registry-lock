dcbcf388046ca6afaa1d839ecf53481e8a9851b1
GitHub Action <action@github.com>
2026-04-19 01:15:33 +0000

    Update mason-registry lock to 2026-04-18-bottled-point

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-18-lesser-soccer"
+M.registry_release = "github:mason-org/mason-registry@2026-04-18-bottled-point"
 
 return M
