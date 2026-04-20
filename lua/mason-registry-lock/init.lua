179b0aa866742bb615858a06b6b33411012fa29d
GitHub Action <action@github.com>
2026-04-20 01:15:31 +0000

    Update mason-registry lock to 2026-04-19-welsh-spade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-19-nutty-parcel"
+M.registry_release = "github:mason-org/mason-registry@2026-04-19-welsh-spade"
 
 return M
