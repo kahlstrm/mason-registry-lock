8cf5a192f3dd0a5da454f1fceddf62c65a749e4e
GitHub Action <action@github.com>
2026-04-20 01:15:31 +0000

    Update mason-registry lock to 2026-04-19-bent-look

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-19-sulky-plier"
+M.registry_release = "github:mason-org/mason-registry@2026-04-19-bent-look"
 
 return M
