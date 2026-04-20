9885d6730ab1a60342c5c9517a5eb7049aef36c2
GitHub Action <action@github.com>
2026-04-20 01:15:31 +0000

    Update mason-registry lock to 2026-04-19-sulky-plier

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-19-hasty-route"
+M.registry_release = "github:mason-org/mason-registry@2026-04-19-sulky-plier"
 
 return M
