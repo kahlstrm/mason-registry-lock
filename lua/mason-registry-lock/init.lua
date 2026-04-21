a9d8afc3b08c898d69a297afc4a3031c68ac998d
GitHub Action <action@github.com>
2026-04-21 01:13:00 +0000

    Update mason-registry lock to 2026-04-20-major-cellar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-20-secure-way"
+M.registry_release = "github:mason-org/mason-registry@2026-04-20-major-cellar"
 
 return M
