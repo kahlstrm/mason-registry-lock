d285b8d51eefe3aa61304651b9b12849d69b6416
GitHub Action <action@github.com>
2026-04-21 01:13:00 +0000

    Update mason-registry lock to 2026-04-20-abrupt-shop

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-20-honest-fiber"
+M.registry_release = "github:mason-org/mason-registry@2026-04-20-abrupt-shop"
 
 return M
