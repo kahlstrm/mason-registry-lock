06463522cad4113d3f10692adfaea7bb51f91d4d
GitHub Action <action@github.com>
2026-04-22 01:11:23 +0000

    Update mason-registry lock to 2026-04-21-sloppy-dime

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-21-tough-nase"
+M.registry_release = "github:mason-org/mason-registry@2026-04-21-sloppy-dime"
 
 return M
