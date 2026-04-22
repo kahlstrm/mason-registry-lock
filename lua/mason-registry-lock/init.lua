ac09d53c7f6912a247e0b625047fa22cfdd8b7eb
GitHub Action <action@github.com>
2026-04-22 01:11:23 +0000

    Update mason-registry lock to 2026-04-21-unsung-margin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-21-glossy-risk"
+M.registry_release = "github:mason-org/mason-registry@2026-04-21-unsung-margin"
 
 return M
