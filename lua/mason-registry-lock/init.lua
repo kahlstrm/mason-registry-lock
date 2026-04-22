64b2f3f0154b8602008849264bccb91664130379
GitHub Action <action@github.com>
2026-04-22 01:11:23 +0000

    Update mason-registry lock to 2026-04-21-tough-nase

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-21-quie-kitty"
+M.registry_release = "github:mason-org/mason-registry@2026-04-21-tough-nase"
 
 return M
