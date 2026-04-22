559fe96bfe520efb3279a71a41d8490dda7d717d
GitHub Action <action@github.com>
2026-04-22 01:11:23 +0000

    Update mason-registry lock to 2026-04-21-merry-domain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-21-light-drop"
+M.registry_release = "github:mason-org/mason-registry@2026-04-21-merry-domain"
 
 return M
