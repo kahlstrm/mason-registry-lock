e84b34f587b7c67c1c24c1a9eacef658d7589564
GitHub Action <action@github.com>
2026-04-22 01:11:23 +0000

    Update mason-registry lock to 2026-04-21-light-drop

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-21-super-grass"
+M.registry_release = "github:mason-org/mason-registry@2026-04-21-light-drop"
 
 return M
