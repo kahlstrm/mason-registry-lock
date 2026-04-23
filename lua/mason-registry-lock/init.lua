7774f976839e4a4592919bd856e0d916f70aaab1
GitHub Action <action@github.com>
2026-04-23 01:16:55 +0000

    Update mason-registry lock to 2026-04-22-harsh-single

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-22-bitter-damage"
+M.registry_release = "github:mason-org/mason-registry@2026-04-22-harsh-single"
 
 return M
