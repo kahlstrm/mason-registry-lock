5ddbe6db465442ab9a117f5f3bc15a7428ac15d3
GitHub Action <action@github.com>
2026-04-23 01:16:55 +0000

    Update mason-registry lock to 2026-04-22-slimy-hash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-22-gross-state"
+M.registry_release = "github:mason-org/mason-registry@2026-04-22-slimy-hash"
 
 return M
