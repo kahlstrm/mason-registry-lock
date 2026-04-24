e934161c99e80ff62e5d08812e8b840e893cfbc7
GitHub Action <action@github.com>
2026-04-24 01:16:31 +0000

    Update mason-registry lock to 2026-04-23-great-scarf

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-23-icky-menu"
+M.registry_release = "github:mason-org/mason-registry@2026-04-23-great-scarf"
 
 return M
