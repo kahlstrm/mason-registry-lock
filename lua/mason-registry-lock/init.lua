31f2e06f5bc2a57dc56aef8a6ce39ec3417be402
GitHub Action <action@github.com>
2026-04-24 01:16:31 +0000

    Update mason-registry lock to 2026-04-23-quain-timer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-23-ruby-prose"
+M.registry_release = "github:mason-org/mason-registry@2026-04-23-quain-timer"
 
 return M
