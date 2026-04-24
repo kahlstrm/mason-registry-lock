f76d26c9483bf6310b7bf4c0d6eebe68746b976d
GitHub Action <action@github.com>
2026-04-24 01:16:31 +0000

    Update mason-registry lock to 2026-04-23-icky-menu

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-22-tasty-growth"
+M.registry_release = "github:mason-org/mason-registry@2026-04-23-icky-menu"
 
 return M
