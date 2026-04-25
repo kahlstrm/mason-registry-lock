7ddeb6bf1cf9b6ec0d77f145d5bdea74de3a5df5
GitHub Action <action@github.com>
2026-04-25 01:09:39 +0000

    Update mason-registry lock to 2026-04-24-jaunty-level

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-24-copper-socks"
+M.registry_release = "github:mason-org/mason-registry@2026-04-24-jaunty-level"
 
 return M
