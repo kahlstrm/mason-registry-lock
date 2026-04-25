0dabc406ee7e0e16ac8bfeb9dba5dfaaf34f856c
GitHub Action <action@github.com>
2026-04-25 01:09:39 +0000

    Update mason-registry lock to 2026-04-24-okay-octopus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-24-frayed-dibble"
+M.registry_release = "github:mason-org/mason-registry@2026-04-24-okay-octopus"
 
 return M
