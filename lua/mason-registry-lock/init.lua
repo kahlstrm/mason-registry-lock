9af6180f3fdee75f99e6d955b46d9e9bb76112bd
GitHub Action <action@github.com>
2026-04-25 01:09:39 +0000

    Update mason-registry lock to 2026-04-24-wiry-self

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-24-okay-octopus"
+M.registry_release = "github:mason-org/mason-registry@2026-04-24-wiry-self"
 
 return M
