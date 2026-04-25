184d8e7b898541fd5d884b6b10e57b25a3a0b790
GitHub Action <action@github.com>
2026-04-25 01:09:39 +0000

    Update mason-registry lock to 2026-04-25-pushy-barium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-25-broad-cerium"
+M.registry_release = "github:mason-org/mason-registry@2026-04-25-pushy-barium"
 
 return M
