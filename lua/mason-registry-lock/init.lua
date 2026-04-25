3bd370e8afbab1957213872c0618cf1356ad7888
GitHub Action <action@github.com>
2026-04-25 01:09:39 +0000

    Update mason-registry lock to 2026-04-24-copper-socks

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-24-like-zephyr"
+M.registry_release = "github:mason-org/mason-registry@2026-04-24-copper-socks"
 
 return M
