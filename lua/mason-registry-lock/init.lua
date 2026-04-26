ef207f5a68dd16d54e73fe42cc09cbe2800254e8
GitHub Action <action@github.com>
2026-04-26 01:18:17 +0000

    Update mason-registry lock to 2026-04-25-down-carrot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-25-pushy-barium"
+M.registry_release = "github:mason-org/mason-registry@2026-04-25-down-carrot"
 
 return M
