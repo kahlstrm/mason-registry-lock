06ed66b7bbe662a4df6e4a9022966840fbbb9863
GitHub Action <action@github.com>
2026-04-26 01:18:17 +0000

    Update mason-registry lock to 2026-04-25-marked-jitter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-25-harsh-brake"
+M.registry_release = "github:mason-org/mason-registry@2026-04-25-marked-jitter"
 
 return M
