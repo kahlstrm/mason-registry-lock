5a567cf630d14c4c2f88e463ef0718869f2ce5d1
GitHub Action <action@github.com>
2026-04-28 01:26:04 +0000

    Update mason-registry lock to 2026-04-27-smart-oxygen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-27-shiny-sudan"
+M.registry_release = "github:mason-org/mason-registry@2026-04-27-smart-oxygen"
 
 return M
