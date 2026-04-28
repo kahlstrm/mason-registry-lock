b5291c2fba40d0d7278ec25e00327f2b92d46b80
GitHub Action <action@github.com>
2026-04-28 01:26:04 +0000

    Update mason-registry lock to 2026-04-27-cooked-pot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-27-quirky-bacon"
+M.registry_release = "github:mason-org/mason-registry@2026-04-27-cooked-pot"
 
 return M
