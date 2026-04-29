bebca4387215d70ed706dcad562094b87383919b
GitHub Action <action@github.com>
2026-04-29 01:28:47 +0000

    Update mason-registry lock to 2026-04-28-poised-folder

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-28-aquatic-hacker"
+M.registry_release = "github:mason-org/mason-registry@2026-04-28-poised-folder"
 
 return M
