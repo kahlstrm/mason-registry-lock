99cc5c9f234eea55c2109575d8f98bccbad26ab7
GitHub Action <action@github.com>
2026-04-29 01:28:48 +0000

    Update mason-registry lock to 2026-04-28-spicy-arrow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-28-short-sphere"
+M.registry_release = "github:mason-org/mason-registry@2026-04-28-spicy-arrow"
 
 return M
