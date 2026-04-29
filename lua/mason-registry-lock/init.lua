c7b838bfbd608738248871bfcc4b8dac18da68f5
GitHub Action <action@github.com>
2026-04-29 01:28:47 +0000

    Update mason-registry lock to 2026-04-28-this-join

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-28-flashy-exit"
+M.registry_release = "github:mason-org/mason-registry@2026-04-28-this-join"
 
 return M
