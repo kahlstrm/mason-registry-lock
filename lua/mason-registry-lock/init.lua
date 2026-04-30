82afd4dff07037c64c685e39be161911f7cef920
GitHub Action <action@github.com>
2026-04-30 01:27:53 +0000

    Update mason-registry lock to 2026-04-29-plump-asia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-29-noted-smoke"
+M.registry_release = "github:mason-org/mason-registry@2026-04-29-plump-asia"
 
 return M
