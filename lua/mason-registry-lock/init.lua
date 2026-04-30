83431dc7c7818788469753126648199bdc6924db
GitHub Action <action@github.com>
2026-04-30 01:27:53 +0000

    Update mason-registry lock to 2026-04-29-breeze-lemur

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-29-fixed-zero"
+M.registry_release = "github:mason-org/mason-registry@2026-04-29-breeze-lemur"
 
 return M
