15b793074cb94d84364c28c418449b3e6e96680f
GitHub Action <action@github.com>
2026-04-30 01:27:53 +0000

    Update mason-registry lock to 2026-04-29-muted-home

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-29-solar-shield"
+M.registry_release = "github:mason-org/mason-registry@2026-04-29-muted-home"
 
 return M
