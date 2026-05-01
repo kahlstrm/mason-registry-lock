d96de9209a742a2ae6c9b3bc6031943aa9efe941
GitHub Action <action@github.com>
2026-05-01 01:30:30 +0000

    Update mason-registry lock to 2026-04-30-uneven-balloon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-30-floppy-pail"
+M.registry_release = "github:mason-org/mason-registry@2026-04-30-uneven-balloon"
 
 return M
