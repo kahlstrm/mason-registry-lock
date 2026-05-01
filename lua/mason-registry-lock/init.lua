12ed0234b883b4c4f3d59802e894295e9dc69887
GitHub Action <action@github.com>
2026-05-01 01:30:30 +0000

    Update mason-registry lock to 2026-04-30-floppy-pail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-04-30-dental-hail"
+M.registry_release = "github:mason-org/mason-registry@2026-04-30-floppy-pail"
 
 return M
