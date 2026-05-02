e26ca9a0e40e4bf5a32149799605b42470b0ef64
GitHub Action <action@github.com>
2026-05-02 01:21:34 +0000

    Update mason-registry lock to 2026-05-01-small-season

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-01-fiery-range"
+M.registry_release = "github:mason-org/mason-registry@2026-05-01-small-season"
 
 return M
