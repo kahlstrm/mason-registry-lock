7c4dbbbdd786a6398085aa3c44fd31f14f75f249
GitHub Action <action@github.com>
2026-05-02 01:21:34 +0000

    Update mason-registry lock to 2026-05-01-snoopy-trowel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-01-best-oreo"
+M.registry_release = "github:mason-org/mason-registry@2026-05-01-snoopy-trowel"
 
 return M
