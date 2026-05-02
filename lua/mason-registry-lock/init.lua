e3e577902148e953f5afe5e33c6df4d7fbcdeea6
GitHub Action <action@github.com>
2026-05-02 01:21:34 +0000

    Update mason-registry lock to 2026-05-01-frayed-push

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-01-pumped-boron"
+M.registry_release = "github:mason-org/mason-registry@2026-05-01-frayed-push"
 
 return M
