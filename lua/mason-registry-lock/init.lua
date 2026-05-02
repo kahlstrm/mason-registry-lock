4abdfe06cf4d3e364606b15675bcee03aa8113a7
GitHub Action <action@github.com>
2026-05-02 01:21:34 +0000

    Update mason-registry lock to 2026-05-01-spry-flash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-01-remote-gemini"
+M.registry_release = "github:mason-org/mason-registry@2026-05-01-spry-flash"
 
 return M
