e92e37349fbaf579b9c0524190c061094ab34c39
GitHub Action <action@github.com>
2026-05-03 01:24:58 +0000

    Update mason-registry lock to 2026-05-02-junior-newt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-02-portly-nova"
+M.registry_release = "github:mason-org/mason-registry@2026-05-02-junior-newt"
 
 return M
