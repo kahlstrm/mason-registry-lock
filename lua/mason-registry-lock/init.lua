063256d615dcc1e34dcddb4866c6f38b96e6565b
GitHub Action <action@github.com>
2026-05-03 01:24:58 +0000

    Update mason-registry lock to 2026-05-02-rustic-fawn

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-02-wobbly-zinc"
+M.registry_release = "github:mason-org/mason-registry@2026-05-02-rustic-fawn"
 
 return M
