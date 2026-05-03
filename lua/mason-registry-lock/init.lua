66364ad29ead0eea212abe7c304dd32d88b794c4
GitHub Action <action@github.com>
2026-05-03 01:24:58 +0000

    Update mason-registry lock to 2026-05-02-skinny-loach

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-02-dotted-greek"
+M.registry_release = "github:mason-org/mason-registry@2026-05-02-skinny-loach"
 
 return M
