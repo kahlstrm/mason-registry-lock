fcbb91ac9442c166018af9cd9c9d9aa932725270
GitHub Action <action@github.com>
2026-05-05 01:23:44 +0000

    Update mason-registry lock to 2026-05-05-peach-bunny

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-04-large-editor"
+M.registry_release = "github:mason-org/mason-registry@2026-05-05-peach-bunny"
 
 return M
