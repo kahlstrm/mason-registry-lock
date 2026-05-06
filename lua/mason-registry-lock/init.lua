fa06798b0b86071f480a54e99624af10db6f7a96
GitHub Action <action@github.com>
2026-05-06 01:21:51 +0000

    Update mason-registry lock to 2026-05-05-kooky-point

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-05-untidy-class"
+M.registry_release = "github:mason-org/mason-registry@2026-05-05-kooky-point"
 
 return M
