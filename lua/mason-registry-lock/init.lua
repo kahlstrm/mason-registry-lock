435752dd643c5a71e5bb6a2398aed6ab0253c1fd
GitHub Action <action@github.com>
2026-05-06 01:21:51 +0000

    Update mason-registry lock to 2026-05-05-untidy-class

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-05-closed-format"
+M.registry_release = "github:mason-org/mason-registry@2026-05-05-untidy-class"
 
 return M
