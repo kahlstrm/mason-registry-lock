db3f2ea00c41191fb70c916278c9d29ca83917d4
GitHub Action <action@github.com>
2026-05-06 01:21:51 +0000

    Update mason-registry lock to 2026-05-05-closed-format

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-05-ajar-turnip"
+M.registry_release = "github:mason-org/mason-registry@2026-05-05-closed-format"
 
 return M
