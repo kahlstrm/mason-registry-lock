3c1afd9b41d42596c979c0a1d4204d9a9750fca0
GitHub Action <action@github.com>
2026-05-06 01:21:51 +0000

    Update mason-registry lock to 2026-05-06-aware-gut

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-05-kooky-point"
+M.registry_release = "github:mason-org/mason-registry@2026-05-06-aware-gut"
 
 return M
