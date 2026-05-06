9543808300eb91fa1af9fda8c03aeeb92e67555e
GitHub Action <action@github.com>
2026-05-06 01:21:51 +0000

    Update mason-registry lock to 2026-05-05-inc-fibre

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-05-rusty-koodoo"
+M.registry_release = "github:mason-org/mason-registry@2026-05-05-inc-fibre"
 
 return M
