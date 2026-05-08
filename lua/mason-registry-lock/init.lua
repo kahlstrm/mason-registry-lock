facabb722bcb17b1a3b1abc35e1e003310ae645e
GitHub Action <action@github.com>
2026-05-08 01:30:15 +0000

    Update mason-registry lock to 2026-05-07-lean-parade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-07-fluffy-desire"
+M.registry_release = "github:mason-org/mason-registry@2026-05-07-lean-parade"
 
 return M
