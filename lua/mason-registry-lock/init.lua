bd4d007cc0aaa09cbd66ec19e9a36eff4393f5db
GitHub Action <action@github.com>
2026-05-08 01:30:15 +0000

    Update mason-registry lock to 2026-05-07-jagged-sphere

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-07-filthy-beer"
+M.registry_release = "github:mason-org/mason-registry@2026-05-07-jagged-sphere"
 
 return M
