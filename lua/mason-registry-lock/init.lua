79fc4630ded7ac0aae579a9ec4295d83b2f52821
GitHub Action <action@github.com>
2026-05-08 01:30:15 +0000

    Update mason-registry lock to 2026-05-07-lowly-pruner

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-07-jagged-sphere"
+M.registry_release = "github:mason-org/mason-registry@2026-05-07-lowly-pruner"
 
 return M
