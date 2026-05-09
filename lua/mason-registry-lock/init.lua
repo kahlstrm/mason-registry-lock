d14878bb85420f9c4cdda462e1b41ea03f5d678d
GitHub Action <action@github.com>
2026-05-09 01:27:10 +0000

    Update mason-registry lock to 2026-05-08-alike-camp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-08-exotic-aruana"
+M.registry_release = "github:mason-org/mason-registry@2026-05-08-alike-camp"
 
 return M
