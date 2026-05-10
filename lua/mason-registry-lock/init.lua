ce45e3429ed9bf53520ad53b3e705da543a63bb8
GitHub Action <action@github.com>
2026-05-10 01:28:45 +0000

    Update mason-registry lock to 2026-05-09-modern-knot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-09-near-panty"
+M.registry_release = "github:mason-org/mason-registry@2026-05-09-modern-knot"
 
 return M
