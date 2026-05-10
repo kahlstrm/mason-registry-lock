018772be681833919a9e01b1a4d985f3c11af6e8
GitHub Action <action@github.com>
2026-05-10 01:28:45 +0000

    Update mason-registry lock to 2026-05-09-webbed-season

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-09-modern-knot"
+M.registry_release = "github:mason-org/mason-registry@2026-05-09-webbed-season"
 
 return M
