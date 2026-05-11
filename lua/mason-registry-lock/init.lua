a973ee765cf4135476d7ff67226620342b6952ae
GitHub Action <action@github.com>
2026-05-11 01:47:13 +0000

    Update mason-registry lock to 2026-05-11-charming-swamp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-10-painless-char"
+M.registry_release = "github:mason-org/mason-registry@2026-05-11-charming-swamp"
 
 return M
