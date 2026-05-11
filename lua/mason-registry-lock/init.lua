1e75eb47646f53dc35ce248c9e1a7762c3a54584
GitHub Action <action@github.com>
2026-05-11 01:47:13 +0000

    Update mason-registry lock to 2026-05-10-clumsy-quail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-10-scant-ring"
+M.registry_release = "github:mason-org/mason-registry@2026-05-10-clumsy-quail"
 
 return M
