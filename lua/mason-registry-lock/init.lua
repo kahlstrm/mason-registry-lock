9a1788d7bcc549eeda06ab1ac4c32b21160cdff2
GitHub Action <action@github.com>
2026-05-11 01:47:13 +0000

    Update mason-registry lock to 2026-05-10-scant-ring

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-10-tangy-sack"
+M.registry_release = "github:mason-org/mason-registry@2026-05-10-scant-ring"
 
 return M
