5b886c50c6a91da543105814eb13f1e8cb4fe4df
GitHub Action <action@github.com>
2026-05-12 01:29:07 +0000

    Update mason-registry lock to 2026-05-11-sulky-zipper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-11-like-banjo"
+M.registry_release = "github:mason-org/mason-registry@2026-05-11-sulky-zipper"
 
 return M
