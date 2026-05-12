cfcfa3b1fd031d30080ab31f852d20ed217ddfb4
GitHub Action <action@github.com>
2026-05-12 01:29:07 +0000

    Update mason-registry lock to 2026-05-11-placid-lunch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-11-lethal-star"
+M.registry_release = "github:mason-org/mason-registry@2026-05-11-placid-lunch"
 
 return M
