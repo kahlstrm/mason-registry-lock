4bfd4799d380e08bc40881923448e8932ae556c3
GitHub Action <action@github.com>
2026-05-12 01:29:07 +0000

    Update mason-registry lock to 2026-05-11-unused-wombat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-11-lumpy-cinema"
+M.registry_release = "github:mason-org/mason-registry@2026-05-11-unused-wombat"
 
 return M
