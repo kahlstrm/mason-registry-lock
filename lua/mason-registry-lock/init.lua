a8e6acfafb19881a21d7dc2da5e544ae9f80cfcd
GitHub Action <action@github.com>
2026-05-12 01:29:07 +0000

    Update mason-registry lock to 2026-05-11-entire-brow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-11-unused-wombat"
+M.registry_release = "github:mason-org/mason-registry@2026-05-11-entire-brow"
 
 return M
