fbd165b364e03cc075de98652db954d95d50c4b3
GitHub Action <action@github.com>
2026-05-13 01:47:41 +0000

    Update mason-registry lock to 2026-05-12-future-aster

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-12-bold-area"
+M.registry_release = "github:mason-org/mason-registry@2026-05-12-future-aster"
 
 return M
