a81b383109b9d1d21f060e3c058a56c454e8a4b6
GitHub Action <action@github.com>
2026-05-13 01:47:41 +0000

    Update mason-registry lock to 2026-05-12-mute-galaxy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-12-future-aster"
+M.registry_release = "github:mason-org/mason-registry@2026-05-12-mute-galaxy"
 
 return M
