58606d8ff72c196f73a496444e06cb0a8f8c5980
GitHub Action <action@github.com>
2026-05-13 01:47:41 +0000

    Update mason-registry lock to 2026-05-12-gamy-marble

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-12-given-coyote"
+M.registry_release = "github:mason-org/mason-registry@2026-05-12-gamy-marble"
 
 return M
