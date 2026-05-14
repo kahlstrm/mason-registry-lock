8e08cf28d98bf9c6d1f6da957f480e376a52fd1e
GitHub Action <action@github.com>
2026-05-14 01:50:30 +0000

    Update mason-registry lock to 2026-05-13-overt-erbium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-13-paltry-appeal"
+M.registry_release = "github:mason-org/mason-registry@2026-05-13-overt-erbium"
 
 return M
