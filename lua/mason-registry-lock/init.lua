5cec22e350ff9c0728f280248b9a0b376ae557e5
GitHub Action <action@github.com>
2026-05-14 01:50:30 +0000

    Update mason-registry lock to 2026-05-13-unsung-stew

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-13-round-kendo"
+M.registry_release = "github:mason-org/mason-registry@2026-05-13-unsung-stew"
 
 return M
