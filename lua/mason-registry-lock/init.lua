64f6917eac875548ca39fc42a503b13aa90b2963
GitHub Action <action@github.com>
2026-05-15 01:47:34 +0000

    Update mason-registry lock to 2026-05-14-queasy-nic

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-14-ready-beach"
+M.registry_release = "github:mason-org/mason-registry@2026-05-14-queasy-nic"
 
 return M
