8e2bbe70f63a6504da2b9c48969305fcdc0e5302
GitHub Action <action@github.com>
2026-05-15 01:47:35 +0000

    Update mason-registry lock to 2026-05-14-pale-gray

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-14-silky-nerve"
+M.registry_release = "github:mason-org/mason-registry@2026-05-14-pale-gray"
 
 return M
