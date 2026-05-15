5b693ff8b8c615fa320cded329e3ae804575deb6
GitHub Action <action@github.com>
2026-05-15 01:47:34 +0000

    Update mason-registry lock to 2026-05-14-lonely-sloth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-14-legal-decade"
+M.registry_release = "github:mason-org/mason-registry@2026-05-14-lonely-sloth"
 
 return M
