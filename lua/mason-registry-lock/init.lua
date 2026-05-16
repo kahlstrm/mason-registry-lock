20950f39f7ab97bb46a4eb2985a91fcdd2c410a0
GitHub Action <action@github.com>
2026-05-16 01:28:17 +0000

    Update mason-registry lock to 2026-05-15-likely-airplane

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-15-slender-lamp"
+M.registry_release = "github:mason-org/mason-registry@2026-05-15-likely-airplane"
 
 return M
