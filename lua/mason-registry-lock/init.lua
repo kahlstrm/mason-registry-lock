3ac14d153a5023b1009086f5895f68b8c45c1e4a
GitHub Action <action@github.com>
2026-05-16 01:28:17 +0000

    Update mason-registry lock to 2026-05-15-eldest-cedar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-15-moral-comb"
+M.registry_release = "github:mason-org/mason-registry@2026-05-15-eldest-cedar"
 
 return M
