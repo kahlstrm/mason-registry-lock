288848041fd9d3823c781ccaceddfe4761f91c67
GitHub Action <action@github.com>
2026-05-16 01:28:17 +0000

    Update mason-registry lock to 2026-05-15-untidy-party

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-15-heady-grunt"
+M.registry_release = "github:mason-org/mason-registry@2026-05-15-untidy-party"
 
 return M
