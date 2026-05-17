91ba0366a3058829ab9470385c9fa1a729a54fec
GitHub Action <action@github.com>
2026-05-17 01:45:16 +0000

    Update mason-registry lock to 2026-05-16-civic-puppy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-16-pastel-lynx"
+M.registry_release = "github:mason-org/mason-registry@2026-05-16-civic-puppy"
 
 return M
