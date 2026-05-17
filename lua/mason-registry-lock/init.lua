640e028d378773c0347e32675440d4536b2ea9d0
GitHub Action <action@github.com>
2026-05-17 01:45:16 +0000

    Update mason-registry lock to 2026-05-16-pastel-lynx

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-16-furry-botany"
+M.registry_release = "github:mason-org/mason-registry@2026-05-16-pastel-lynx"
 
 return M
