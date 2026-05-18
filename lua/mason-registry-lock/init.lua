3e297c616c73ddc8d29a1c4f31e2088edf820e74
GitHub Action <action@github.com>
2026-05-18 01:54:19 +0000

    Update mason-registry lock to 2026-05-17-bent-seat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-17-scarce-sugar"
+M.registry_release = "github:mason-org/mason-registry@2026-05-17-bent-seat"
 
 return M
