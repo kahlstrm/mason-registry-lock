49e1f2b3a986541db70c7c6ae96df69ce19b397d
GitHub Action <action@github.com>
2026-05-18 01:54:19 +0000

    Update mason-registry lock to 2026-05-17-scarce-sugar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-17-swift-swim"
+M.registry_release = "github:mason-org/mason-registry@2026-05-17-scarce-sugar"
 
 return M
