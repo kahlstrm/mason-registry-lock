713b7afc43a4efe0c49ef7fe81a232d97d6292f3
GitHub Action <action@github.com>
2026-05-19 01:54:09 +0000

    Update mason-registry lock to 2026-05-18-ideal-seven

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-17-bent-seat"
+M.registry_release = "github:mason-org/mason-registry@2026-05-18-ideal-seven"
 
 return M
