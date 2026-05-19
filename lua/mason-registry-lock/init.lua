fce60050176b3d8fee67cbbb7d0db5eda8620f79
GitHub Action <action@github.com>
2026-05-19 01:54:09 +0000

    Update mason-registry lock to 2026-05-19-grubby-map

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-19-second-hawk"
+M.registry_release = "github:mason-org/mason-registry@2026-05-19-grubby-map"
 
 return M
