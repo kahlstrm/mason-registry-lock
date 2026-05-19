c883ab0c33c245ac86654d58670227d5ba631517
GitHub Action <action@github.com>
2026-05-19 01:54:09 +0000

    Update mason-registry lock to 2026-05-18-husky-bank

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-18-ideal-seven"
+M.registry_release = "github:mason-org/mason-registry@2026-05-18-husky-bank"
 
 return M
