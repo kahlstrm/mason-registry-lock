73fb73664815c4da2f191bd0aa2fac5627479d8e
GitHub Action <action@github.com>
2026-05-20 01:53:57 +0000

    Update mason-registry lock to 2026-05-19-scarce-pleco

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-19-daily-platy"
+M.registry_release = "github:mason-org/mason-registry@2026-05-19-scarce-pleco"
 
 return M
