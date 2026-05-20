9b53ea62224e79781d6ae57b3200e40dbcaba520
GitHub Action <action@github.com>
2026-05-20 01:53:57 +0000

    Update mason-registry lock to 2026-05-19-barren-stitch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-19-grubby-map"
+M.registry_release = "github:mason-org/mason-registry@2026-05-19-barren-stitch"
 
 return M
