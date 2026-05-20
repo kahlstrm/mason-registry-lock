f4184b183bc3f2f88a44712de21e6bee5451bfe1
GitHub Action <action@github.com>
2026-05-20 01:53:57 +0000

    Update mason-registry lock to 2026-05-19-antiquet-bleak

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-19-mealy-glue"
+M.registry_release = "github:mason-org/mason-registry@2026-05-19-antiquet-bleak"
 
 return M
