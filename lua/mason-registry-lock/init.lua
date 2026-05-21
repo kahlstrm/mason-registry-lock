78dbfc1751af3aa7ac141dc3c4420bfe3649a8d0
GitHub Action <action@github.com>
2026-05-21 01:53:06 +0000

    Update mason-registry lock to 2026-05-21-third-map

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-21-naive-value"
+M.registry_release = "github:mason-org/mason-registry@2026-05-21-third-map"
 
 return M
