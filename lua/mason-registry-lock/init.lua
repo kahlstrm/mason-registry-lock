d16efa630375027629d13ae6b26028360750c4b0
GitHub Action <action@github.com>
2026-05-21 01:53:06 +0000

    Update mason-registry lock to 2026-05-21-naive-value

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-20-rotten-brick"
+M.registry_release = "github:mason-org/mason-registry@2026-05-21-naive-value"
 
 return M
