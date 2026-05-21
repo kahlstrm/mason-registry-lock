c7f1ef4f08038c56b2d11080db071fe6dda870af
GitHub Action <action@github.com>
2026-05-21 01:53:06 +0000

    Update mason-registry lock to 2026-05-20-adept-cold

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-20-manic-lagena"
+M.registry_release = "github:mason-org/mason-registry@2026-05-20-adept-cold"
 
 return M
