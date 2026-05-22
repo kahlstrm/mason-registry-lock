94554592313dcbc8afc9174dd43981c540639c0c
GitHub Action <action@github.com>
2026-05-22 01:54:14 +0000

    Update mason-registry lock to 2026-05-21-edible-osmium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-21-angry-drum"
+M.registry_release = "github:mason-org/mason-registry@2026-05-21-edible-osmium"
 
 return M
