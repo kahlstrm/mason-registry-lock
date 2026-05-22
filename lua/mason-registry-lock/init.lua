4925ff112fec0b6c66e2153cd22fb1ef42578df2
GitHub Action <action@github.com>
2026-05-22 01:54:14 +0000

    Update mason-registry lock to 2026-05-21-charming-band

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-21-edible-osmium"
+M.registry_release = "github:mason-org/mason-registry@2026-05-21-charming-band"
 
 return M
