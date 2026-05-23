8658dbcfb5d4db8dd547e30e15b9182310ea1852
GitHub Action <action@github.com>
2026-05-23 01:45:36 +0000

    Update mason-registry lock to 2026-05-22-coarse-swan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-22-fiscal-eon"
+M.registry_release = "github:mason-org/mason-registry@2026-05-22-coarse-swan"
 
 return M
