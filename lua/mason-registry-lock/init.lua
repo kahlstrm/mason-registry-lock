938c8ce4769a4958d841e142a36d4b864c446849
GitHub Action <action@github.com>
2026-05-23 01:45:37 +0000

    Update mason-registry lock to 2026-05-22-elite-pie

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-22-latin-pistol"
+M.registry_release = "github:mason-org/mason-registry@2026-05-22-elite-pie"
 
 return M
