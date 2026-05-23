a10d3ac1cf2bc8c57fb75efeb45e1e8ad3c0fa9a
GitHub Action <action@github.com>
2026-05-23 01:45:36 +0000

    Update mason-registry lock to 2026-05-22-past-plain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-22-zippy-push"
+M.registry_release = "github:mason-org/mason-registry@2026-05-22-past-plain"
 
 return M
