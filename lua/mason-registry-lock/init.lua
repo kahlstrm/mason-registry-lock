a02bcd63ab8bbfb4fa17da94c52b3af49846e091
GitHub Action <action@github.com>
2026-05-23 01:45:37 +0000

    Update mason-registry lock to 2026-05-22-latin-pistol

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-22-fit-crib"
+M.registry_release = "github:mason-org/mason-registry@2026-05-22-latin-pistol"
 
 return M
