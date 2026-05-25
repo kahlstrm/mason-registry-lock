55de38f58b4d86ac20d3a589fb3e2bc098d1164c
GitHub Action <action@github.com>
2026-05-25 01:57:53 +0000

    Update mason-registry lock to 2026-05-24-broad-wallet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-24-open-grill"
+M.registry_release = "github:mason-org/mason-registry@2026-05-24-broad-wallet"
 
 return M
