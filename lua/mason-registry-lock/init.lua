353da6ef41a1b418686349b6614e45aa28c125f1
GitHub Action <action@github.com>
2026-05-25 01:57:53 +0000

    Update mason-registry lock to 2026-05-24-steady-stage

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-24-last-dugong"
+M.registry_release = "github:mason-org/mason-registry@2026-05-24-steady-stage"
 
 return M
