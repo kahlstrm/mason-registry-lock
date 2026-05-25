471489bf2541ad476595ed430109109d46a7cbf8
GitHub Action <action@github.com>
2026-05-25 01:57:53 +0000

    Update mason-registry lock to 2026-05-24-watery-fog

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-24-loyal-mantra"
+M.registry_release = "github:mason-org/mason-registry@2026-05-24-watery-fog"
 
 return M
