9dcce80bb6d393a8794358e7c1c5bc4c9f78b26d
GitHub Action <action@github.com>
2026-05-27 01:58:02 +0000

    Update mason-registry lock to 2026-05-26-soft-velvet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-26-hearty-soul"
+M.registry_release = "github:mason-org/mason-registry@2026-05-26-soft-velvet"
 
 return M
