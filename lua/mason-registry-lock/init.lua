22caebbed3e0b92cf39a8b2dd789db814191fd31
GitHub Action <action@github.com>
2026-05-27 01:58:02 +0000

    Update mason-registry lock to 2026-05-26-timely-locket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-25-frilly-bait"
+M.registry_release = "github:mason-org/mason-registry@2026-05-26-timely-locket"
 
 return M
