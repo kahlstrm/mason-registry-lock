2598619159e4dfb12c77f39cb5c31fe60c486fa4
GitHub Action <action@github.com>
2026-05-27 01:58:02 +0000

    Update mason-registry lock to 2026-05-26-needy-order

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-26-fried-waiter"
+M.registry_release = "github:mason-org/mason-registry@2026-05-26-needy-order"
 
 return M
