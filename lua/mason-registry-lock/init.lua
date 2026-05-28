e354a2102ea95e0f2303917c28ecfeca935397d8
GitHub Action <action@github.com>
2026-05-28 01:29:53 +0000

    Update mason-registry lock to 2026-05-27-melodic-turbot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-27-lavish-town"
+M.registry_release = "github:mason-org/mason-registry@2026-05-27-melodic-turbot"
 
 return M
