e4f37a85bb6d5453c8d71bd353aa6f9fc896623e
GitHub Action <action@github.com>
2026-05-28 01:29:53 +0000

    Update mason-registry lock to 2026-05-27-asian-robin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-27-melodic-turbot"
+M.registry_release = "github:mason-org/mason-registry@2026-05-27-asian-robin"
 
 return M
