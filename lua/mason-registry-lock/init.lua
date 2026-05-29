c0b4c9a8e094485e9ebcebeddf44d941ebbe0162
GitHub Action <action@github.com>
2026-05-29 01:51:25 +0000

    Update mason-registry lock to 2026-05-28-asleep-pole

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-28-glad-quiver"
+M.registry_release = "github:mason-org/mason-registry@2026-05-28-asleep-pole"
 
 return M
