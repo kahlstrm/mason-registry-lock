feedc96dc097435af6cbbf4990165871227cdec7
GitHub Action <action@github.com>
2026-05-29 01:51:25 +0000

    Update mason-registry lock to 2026-05-28-jazzy-nine

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-28-dynamic-tire"
+M.registry_release = "github:mason-org/mason-registry@2026-05-28-jazzy-nine"
 
 return M
