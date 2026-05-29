458906bdff5e41ab75c4880092a76c4fa7814f7a
GitHub Action <action@github.com>
2026-05-29 01:51:25 +0000

    Update mason-registry lock to 2026-05-28-itchy-swamp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-28-jazzy-nine"
+M.registry_release = "github:mason-org/mason-registry@2026-05-28-itchy-swamp"
 
 return M
