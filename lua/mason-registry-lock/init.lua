4beeae858a6b19402602b77c4a9ef4bcb541c89b
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-snappy-crack

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-rigid-sandra"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-snappy-crack"
 
 return M
