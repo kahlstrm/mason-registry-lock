04976ce24adad690fc50318405ccdbc0c9145cdd
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-super-drake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-great-crack"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-super-drake"
 
 return M
