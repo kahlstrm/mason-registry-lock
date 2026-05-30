b008930e07f9fca0777a26a1be23d53a3d0c59f6
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-30-alike-slide

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-30-common-jump"
+M.registry_release = "github:mason-org/mason-registry@2026-05-30-alike-slide"
 
 return M
