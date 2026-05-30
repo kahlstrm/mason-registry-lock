1ff405f8ea785e0505b2493be0d0552b7d037e56
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-net-nest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-lethal-color"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-net-nest"
 
 return M
