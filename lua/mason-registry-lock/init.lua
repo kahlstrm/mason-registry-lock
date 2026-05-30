fba9ffc88d6da6510187b3310ce54b9cbc7ff689
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-public-roughy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-super-drake"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-public-roughy"
 
 return M
