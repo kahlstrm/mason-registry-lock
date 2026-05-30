8b8db368f4adaf591500fd958b4e0e5725277895
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-imaginary-bone

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-minor-shape"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-imaginary-bone"
 
 return M
