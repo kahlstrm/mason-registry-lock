f876ced7b08cf7c90fda451f4cf09a5dbb774d37
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-minor-shape

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-loose-sail"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-minor-shape"
 
 return M
