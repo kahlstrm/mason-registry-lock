7962a414d3292f25828b8408e75b143e17d9315b
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-shut-sulfur

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-peach-arrow"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-shut-sulfur"
 
 return M
