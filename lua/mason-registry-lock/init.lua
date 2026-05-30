efc56e37a24ef03c936845fc4a400612f03d73a6
GitHub Action <action@github.com>
2026-05-30 01:47:38 +0000

    Update mason-registry lock to 2026-05-29-melted-poppy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-29-net-nest"
+M.registry_release = "github:mason-org/mason-registry@2026-05-29-melted-poppy"
 
 return M
