523c327f8909677ffbf0a600eef8da108138b5e9
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-31-pastel-rise

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-31-cloudy-sponge"
+M.registry_release = "github:mason-org/mason-registry@2026-05-31-pastel-rise"
 
 return M
