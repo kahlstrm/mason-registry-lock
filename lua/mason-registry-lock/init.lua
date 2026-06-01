172a962f1cad9cd8658e3a7430bf34ae38cd6d33
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-30-placid-hair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-30-entire-anime"
+M.registry_release = "github:mason-org/mason-registry@2026-05-30-placid-hair"
 
 return M
