0cd8d5b41635f545e702b776aa996eed25e19b7d
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-30-trashy-glove

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-30-speedy-gauge"
+M.registry_release = "github:mason-org/mason-registry@2026-05-30-trashy-glove"
 
 return M
