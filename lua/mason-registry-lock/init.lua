adccd6d89c4ce7e2ecd6d1bea1dd8eb75f162a56
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-31-excess-diamond

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-31-ahead-link"
+M.registry_release = "github:mason-org/mason-registry@2026-05-31-excess-diamond"
 
 return M
