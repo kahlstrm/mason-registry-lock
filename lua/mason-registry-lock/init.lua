ea1cc83ff51c02c7fc242f4e3b86a027fd83e4c0
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-30-valid-banker

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-30-frilly-stork"
+M.registry_release = "github:mason-org/mason-registry@2026-05-30-valid-banker"
 
 return M
