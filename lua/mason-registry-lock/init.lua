0b207a2df84937a0355bf6fedbfa1abf3a2887ed
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-31-ready-bolt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-31-woozy-wallet"
+M.registry_release = "github:mason-org/mason-registry@2026-05-31-ready-bolt"
 
 return M
