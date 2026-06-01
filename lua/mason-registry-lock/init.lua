5f8c01442bb42a44fb0164cc4d8d0d49c078dfd7
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-30-jagged-clerk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-30-alike-slide"
+M.registry_release = "github:mason-org/mason-registry@2026-05-30-jagged-clerk"
 
 return M
