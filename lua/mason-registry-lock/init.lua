4f17c0e2139b7a29e0d1919c0030bc9b4e77e567
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-31-cloudy-sponge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-31-frizzy-angler"
+M.registry_release = "github:mason-org/mason-registry@2026-05-31-cloudy-sponge"
 
 return M
