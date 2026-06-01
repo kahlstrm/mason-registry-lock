b94c4c7a99893e3cd6d5f9aa76ddfa2c2a8384f1
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-31-plush-rhino

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-31-pastel-tooth"
+M.registry_release = "github:mason-org/mason-registry@2026-05-31-plush-rhino"
 
 return M
