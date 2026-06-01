dcbbbd2191878b3b8412159715123318a1971d1d
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-31-swiss-summer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-31-third-plate"
+M.registry_release = "github:mason-org/mason-registry@2026-05-31-swiss-summer"
 
 return M
