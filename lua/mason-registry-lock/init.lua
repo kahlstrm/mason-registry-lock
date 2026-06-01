55de1ec04071ef9d6411d05c911a367c1ed52857
GitHub Action <action@github.com>
2026-06-01 02:07:38 +0000

    Update mason-registry lock to 2026-05-31-ahead-link

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-05-31-hefty-glider"
+M.registry_release = "github:mason-org/mason-registry@2026-05-31-ahead-link"
 
 return M
