5889c3d83c3325b8919e6bf43f8495a6e15cca19
GitHub Action <action@github.com>
2026-06-03 02:26:16 +0000

    Update mason-registry lock to 2026-06-03-guilty-desert

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-02-flashy-record"
+M.registry_release = "github:mason-org/mason-registry@2026-06-03-guilty-desert"
 
 return M
