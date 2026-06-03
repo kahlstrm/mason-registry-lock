6aae3cef9bc696ac7f725a7b007c4bba9a03ec08
GitHub Action <action@github.com>
2026-06-03 02:26:16 +0000

    Update mason-registry lock to 2026-06-02-rustic-tray

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-02-cool-face"
+M.registry_release = "github:mason-org/mason-registry@2026-06-02-rustic-tray"
 
 return M
