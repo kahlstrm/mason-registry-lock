2e0c693b59fa65c6facee4016ce3a3c7a6672d51
GitHub Action <action@github.com>
2026-06-03 02:26:16 +0000

    Update mason-registry lock to 2026-06-02-acute-jumper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-02-faulty-whale"
+M.registry_release = "github:mason-org/mason-registry@2026-06-02-acute-jumper"
 
 return M
