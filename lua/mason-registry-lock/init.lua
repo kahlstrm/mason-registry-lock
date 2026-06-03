d58100313aa9c053f065603855b083c52c28d183
GitHub Action <action@github.com>
2026-06-03 02:26:16 +0000

    Update mason-registry lock to 2026-06-02-faulty-whale

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-02-south-hash"
+M.registry_release = "github:mason-org/mason-registry@2026-06-02-faulty-whale"
 
 return M
