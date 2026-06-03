da94d45c09c703efec3110b63773ce0f71087e34
GitHub Action <action@github.com>
2026-06-03 02:26:16 +0000

    Update mason-registry lock to 2026-06-02-quain-velvet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-02-help-wire"
+M.registry_release = "github:mason-org/mason-registry@2026-06-02-quain-velvet"
 
 return M
