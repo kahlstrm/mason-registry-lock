b4204ec40c9bb4fc6e2bc8bbb6c75469bebbd074
GitHub Action <action@github.com>
2026-06-04 02:14:01 +0000

    Update mason-registry lock to 2026-06-03-quirky-cereal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-03-many-nase"
+M.registry_release = "github:mason-org/mason-registry@2026-06-03-quirky-cereal"
 
 return M
