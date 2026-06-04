db5eb4e594ac568c79746521cedb93a046cf42d4
GitHub Action <action@github.com>
2026-06-04 02:14:01 +0000

    Update mason-registry lock to 2026-06-03-expert-yellow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-03-wiry-trance"
+M.registry_release = "github:mason-org/mason-registry@2026-06-03-expert-yellow"
 
 return M
