c70143fb39445636986dfa6c3ae78498b32e363d
GitHub Action <action@github.com>
2026-06-04 02:14:01 +0000

    Update mason-registry lock to 2026-06-03-modest-rhythm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-03-untidy-herd"
+M.registry_release = "github:mason-org/mason-registry@2026-06-03-modest-rhythm"
 
 return M
