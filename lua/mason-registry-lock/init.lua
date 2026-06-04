dffd35ff9910cf17494ed94df4091ce26d6a3678
GitHub Action <action@github.com>
2026-06-04 02:14:01 +0000

    Update mason-registry lock to 2026-06-04-tight-sloth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-03-orange-sponge"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-tight-sloth"
 
 return M
