a4f87b7bf6ead6ab83f329977debbba996ee51a0
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-05-last-white

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-fluffy-comet"
+M.registry_release = "github:mason-org/mason-registry@2026-06-05-last-white"
 
 return M
