cb5c40034c064cc9e3a7ce3de61b8bf8e237c440
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-04-fluffy-comet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-spooky-loach"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-fluffy-comet"
 
 return M
