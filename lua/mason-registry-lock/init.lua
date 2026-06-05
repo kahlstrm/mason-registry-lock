3d0bd77d018508d399a3b3360131656c8da78f62
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-04-flashy-file

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-bubbly-ping"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-flashy-file"
 
 return M
