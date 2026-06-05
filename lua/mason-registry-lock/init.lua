ffe752cd9ad87f4e96e07059773f0defe3dfe087
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-04-fuzzy-hair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-bored-nose"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-fuzzy-hair"
 
 return M
