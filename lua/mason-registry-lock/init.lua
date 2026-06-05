0a3632bdfe8ea0f44069a075612b162c5f468f12
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-04-tacky-modem

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-okay-slum"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-tacky-modem"
 
 return M
