9b659b57a87dffa572e18dfceaa0644d330cbe31
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-04-sloppy-locket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-smelly-zipper"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-sloppy-locket"
 
 return M
