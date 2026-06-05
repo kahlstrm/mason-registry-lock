408a28590cd3eccf8f80f2d16b249b670267ea19
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-04-bubbly-ping

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-fuzzy-hair"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-bubbly-ping"
 
 return M
