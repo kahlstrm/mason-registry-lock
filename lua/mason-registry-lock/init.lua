1702fb5df1e0fa9fe47ad178ca880e87db4d2c5a
GitHub Action <action@github.com>
2026-06-05 01:58:19 +0000

    Update mason-registry lock to 2026-06-04-usual-cell

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-04-flashy-file"
+M.registry_release = "github:mason-org/mason-registry@2026-06-04-usual-cell"
 
 return M
