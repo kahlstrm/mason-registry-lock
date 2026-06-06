78b53f3258649b0cbf1d425a07da56c27b22026b
GitHub Action <action@github.com>
2026-06-06 01:50:14 +0000

    Update mason-registry lock to 2026-06-05-cleaver-smile

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-05-woozy-arch"
+M.registry_release = "github:mason-org/mason-registry@2026-06-05-cleaver-smile"
 
 return M
