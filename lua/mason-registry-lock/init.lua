31827b42c4aec69b48d9c69adfac6a0f31e4975d
GitHub Action <action@github.com>
2026-06-06 01:50:14 +0000

    Update mason-registry lock to 2026-06-05-bad-crocus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-05-cleaver-smile"
+M.registry_release = "github:mason-org/mason-registry@2026-06-05-bad-crocus"
 
 return M
