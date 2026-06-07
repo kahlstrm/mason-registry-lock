bc7242db0f47d8d1fd185305bc45a476aefbc84e
GitHub Action <action@github.com>
2026-06-07 02:03:37 +0000

    Update mason-registry lock to 2026-06-06-lavish-income

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-06-glass-desire"
+M.registry_release = "github:mason-org/mason-registry@2026-06-06-lavish-income"
 
 return M
