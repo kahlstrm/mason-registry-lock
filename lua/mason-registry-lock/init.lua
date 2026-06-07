5acd34bfa240f88a2e832778fbf1dd017f4b0f8e
GitHub Action <action@github.com>
2026-06-07 02:03:37 +0000

    Update mason-registry lock to 2026-06-06-lunar-catsup

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-06-lonely-ram"
+M.registry_release = "github:mason-org/mason-registry@2026-06-06-lunar-catsup"
 
 return M
