88af6ff4deeed87cbb45934e0b514ab417c7c5ba
GitHub Action <action@github.com>
2026-06-07 02:03:37 +0000

    Update mason-registry lock to 2026-06-06-glass-desire

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-06-lunar-catsup"
+M.registry_release = "github:mason-org/mason-registry@2026-06-06-glass-desire"
 
 return M
