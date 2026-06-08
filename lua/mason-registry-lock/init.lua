9315b0bc181c45529ae751005706e8329a137608
GitHub Action <action@github.com>
2026-06-08 02:08:37 +0000

    Update mason-registry lock to 2026-06-07-tested-turnip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-06-strict-jeff"
+M.registry_release = "github:mason-org/mason-registry@2026-06-07-tested-turnip"
 
 return M
