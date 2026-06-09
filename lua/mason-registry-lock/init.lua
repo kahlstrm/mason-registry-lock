4a25ed456efd2b2f436c3ee09072cf3cb4a63d97
GitHub Action <action@github.com>
2026-06-09 01:46:42 +0000

    Update mason-registry lock to 2026-06-08-above-chip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-08-sulky-groove"
+M.registry_release = "github:mason-org/mason-registry@2026-06-08-above-chip"
 
 return M
