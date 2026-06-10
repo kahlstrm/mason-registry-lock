71bc8ae2c7955a248ad1c11c5808a26b1c744ec9
GitHub Action <action@github.com>
2026-06-10 01:57:13 +0000

    Update mason-registry lock to 2026-06-09-mushy-pike

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-09-gothic-taimen"
+M.registry_release = "github:mason-org/mason-registry@2026-06-09-mushy-pike"
 
 return M
