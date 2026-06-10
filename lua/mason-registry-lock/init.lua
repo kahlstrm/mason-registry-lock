fafd504dc14d011e41ebbd34191f57c862a9a488
GitHub Action <action@github.com>
2026-06-10 01:57:13 +0000

    Update mason-registry lock to 2026-06-09-gothic-taimen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-09-steamy-wallet"
+M.registry_release = "github:mason-org/mason-registry@2026-06-09-gothic-taimen"
 
 return M
