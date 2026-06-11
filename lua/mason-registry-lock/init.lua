492fbfff1f066038ff371e0a20e765b7c9f56cae
GitHub Action <action@github.com>
2026-06-11 02:08:33 +0000

    Update mason-registry lock to 2026-06-10-uppity-wire

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-10-steel-bonsai"
+M.registry_release = "github:mason-org/mason-registry@2026-06-10-uppity-wire"
 
 return M
