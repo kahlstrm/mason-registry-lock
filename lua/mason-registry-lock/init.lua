b713fd0ac13cd0cef38cd0ac94cb9bd017c84e1c
GitHub Action <action@github.com>
2026-06-11 02:08:33 +0000

    Update mason-registry lock to 2026-06-10-flashy-seal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-10-three-grill"
+M.registry_release = "github:mason-org/mason-registry@2026-06-10-flashy-seal"
 
 return M
