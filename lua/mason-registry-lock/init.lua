5980233f7a4e62e9e8b6a876e86808c3823fa9b3
GitHub Action <action@github.com>
2026-06-11 02:08:33 +0000

    Update mason-registry lock to 2026-06-10-steel-bonsai

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-10-moldy-weever"
+M.registry_release = "github:mason-org/mason-registry@2026-06-10-steel-bonsai"
 
 return M
