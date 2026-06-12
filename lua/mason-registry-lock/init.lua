899fef0bb17ba13fdd946fc041887ea252a663f2
GitHub Action <action@github.com>
2026-06-12 02:04:44 +0000

    Update mason-registry lock to 2026-06-11-impure-cause

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-smug-goose"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-impure-cause"
 
 return M
