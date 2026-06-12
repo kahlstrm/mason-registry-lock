9d6f57528ecce1f69eb865f97def0dbadd0d81c2
GitHub Action <action@github.com>
2026-06-12 02:04:44 +0000

    Update mason-registry lock to 2026-06-11-pushy-copper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-impure-liquor"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-pushy-copper"
 
 return M
