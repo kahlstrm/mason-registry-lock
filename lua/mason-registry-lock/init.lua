290aec5cedc185e361ce68ca84ea325285351647
GitHub Action <action@github.com>
2026-06-12 02:04:44 +0000

    Update mason-registry lock to 2026-06-11-thorny-valley

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-swanky-india"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-thorny-valley"
 
 return M
