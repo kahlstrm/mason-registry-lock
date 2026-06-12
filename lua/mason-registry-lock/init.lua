3fe7081e644a9be0146d60777cecfe324138d57e
GitHub Action <action@github.com>
2026-06-12 02:04:43 +0000

    Update mason-registry lock to 2026-06-11-quaint-hole

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-near-state"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-quaint-hole"
 
 return M
