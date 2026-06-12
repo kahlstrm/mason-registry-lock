7843be059402d0e4ce6b56d1f3f354c4435629fc
GitHub Action <action@github.com>
2026-06-12 02:04:43 +0000

    Update mason-registry lock to 2026-06-11-near-state

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-rebel-petrol"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-near-state"
 
 return M
