ae7553d2846f41e67060efa2b12d1bf3bdd7592d
GitHub Action <action@github.com>
2026-06-12 02:04:44 +0000

    Update mason-registry lock to 2026-06-11-very-pair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-jaunty-height"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-very-pair"
 
 return M
