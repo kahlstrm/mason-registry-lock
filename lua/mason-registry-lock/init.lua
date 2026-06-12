8f3da015501f869049ea5e1a08e37c45de889728
GitHub Action <action@github.com>
2026-06-12 02:04:43 +0000

    Update mason-registry lock to 2026-06-11-rebel-petrol

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-spooky-plain"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-rebel-petrol"
 
 return M
