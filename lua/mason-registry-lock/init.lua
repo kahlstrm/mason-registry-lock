1ab8a40509b7c1e28655f95529b1b339c588936e
GitHub Action <action@github.com>
2026-06-12 02:04:44 +0000

    Update mason-registry lock to 2026-06-11-harsh-list

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-large-canada"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-harsh-list"
 
 return M
