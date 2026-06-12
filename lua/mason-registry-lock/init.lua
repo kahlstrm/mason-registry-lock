b641069fae2b90d18770d49b5d933bf6abd0d7a6
GitHub Action <action@github.com>
2026-06-12 02:04:43 +0000

    Update mason-registry lock to 2026-06-11-spooky-plain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-11-used-stick"
+M.registry_release = "github:mason-org/mason-registry@2026-06-11-spooky-plain"
 
 return M
