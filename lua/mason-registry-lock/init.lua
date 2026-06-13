ae1bd269fb826fbb406982f3b3ecd97bf2ce854f
GitHub Action <action@github.com>
2026-06-13 01:57:46 +0000

    Update mason-registry lock to 2026-06-12-fast-tuna

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-12-aquatic-plier"
+M.registry_release = "github:mason-org/mason-registry@2026-06-12-fast-tuna"
 
 return M
