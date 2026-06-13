643a8cd639f6e493259b1b38a5c6a1f7d5cd13c5
GitHub Action <action@github.com>
2026-06-13 01:57:46 +0000

    Update mason-registry lock to 2026-06-12-stable-coach

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-12-fried-turbot"
+M.registry_release = "github:mason-org/mason-registry@2026-06-12-stable-coach"
 
 return M
