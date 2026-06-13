d205a4e2444250b44d7198b7179ee0af57081b4c
GitHub Action <action@github.com>
2026-06-13 01:57:46 +0000

    Update mason-registry lock to 2026-06-12-fried-turbot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-12-aged-lunge"
+M.registry_release = "github:mason-org/mason-registry@2026-06-12-fried-turbot"
 
 return M
