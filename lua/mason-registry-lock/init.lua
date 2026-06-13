4afb734f5a63e58d44294f0791b44a703e507b69
GitHub Action <action@github.com>
2026-06-13 01:57:46 +0000

    Update mason-registry lock to 2026-06-12-rival-ship

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-12-recent-breeze"
+M.registry_release = "github:mason-org/mason-registry@2026-06-12-rival-ship"
 
 return M
