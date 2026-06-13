76350f571d4f734f26f17f483d26580c07760f8e
GitHub Action <action@github.com>
2026-06-13 01:57:46 +0000

    Update mason-registry lock to 2026-06-12-thoughtful-zombie

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-12-bulky-palm"
+M.registry_release = "github:mason-org/mason-registry@2026-06-12-thoughtful-zombie"
 
 return M
