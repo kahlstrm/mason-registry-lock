6ec18ee08f0497b874caba006cea78fe363f24cf
GitHub Action <action@github.com>
2026-06-13 01:57:46 +0000

    Update mason-registry lock to 2026-06-12-real-trail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-12-thoughtful-zombie"
+M.registry_release = "github:mason-org/mason-registry@2026-06-12-real-trail"
 
 return M
