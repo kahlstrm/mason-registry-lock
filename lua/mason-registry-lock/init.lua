ea1973483ad90ab658349cc9b01244ed59f044b8
GitHub Action <action@github.com>
2026-06-14 02:07:26 +0000

    Update mason-registry lock to 2026-06-13-robust-wheel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-13-aboard-fruit"
+M.registry_release = "github:mason-org/mason-registry@2026-06-13-robust-wheel"
 
 return M
