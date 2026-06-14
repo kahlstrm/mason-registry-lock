6ba28ac67bca8d88fc93ea7ba9c3866c448a3d20
GitHub Action <action@github.com>
2026-06-14 02:07:26 +0000

    Update mason-registry lock to 2026-06-13-godly-beam

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-13-liquid-cock"
+M.registry_release = "github:mason-org/mason-registry@2026-06-13-godly-beam"
 
 return M
