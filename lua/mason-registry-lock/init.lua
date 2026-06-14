9504907b82d489c501afd67013c0058324327112
GitHub Action <action@github.com>
2026-06-14 02:07:26 +0000

    Update mason-registry lock to 2026-06-13-astral-cloth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-13-robust-wheel"
+M.registry_release = "github:mason-org/mason-registry@2026-06-13-astral-cloth"
 
 return M
