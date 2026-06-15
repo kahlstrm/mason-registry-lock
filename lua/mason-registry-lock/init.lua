08182b922c76678ab9ea478369c11e66f52c2188
GitHub Action <action@github.com>
2026-06-15 02:11:31 +0000

    Update mason-registry lock to 2026-06-14-full-vessel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-14-bloody-shoe"
+M.registry_release = "github:mason-org/mason-registry@2026-06-14-full-vessel"
 
 return M
