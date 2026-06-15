b2e525abfb51d963805c1127449a705762353f5b
GitHub Action <action@github.com>
2026-06-15 02:11:31 +0000

    Update mason-registry lock to 2026-06-14-bottled-target

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-14-spicy-radio"
+M.registry_release = "github:mason-org/mason-registry@2026-06-14-bottled-target"
 
 return M
