0c7eabe84537e90a7cc393203fd52a5c67a17a9b
GitHub Action <action@github.com>
2026-06-15 02:11:31 +0000

    Update mason-registry lock to 2026-06-14-spicy-radio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-14-full-vessel"
+M.registry_release = "github:mason-org/mason-registry@2026-06-14-spicy-radio"
 
 return M
