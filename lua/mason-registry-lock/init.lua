4ac8c27a8142d8058ca392c318d3787ed88dc8c9
GitHub Action <action@github.com>
2026-06-15 02:11:31 +0000

    Update mason-registry lock to 2026-06-14-invisible-trip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-14-overt-peanut"
+M.registry_release = "github:mason-org/mason-registry@2026-06-14-invisible-trip"
 
 return M
