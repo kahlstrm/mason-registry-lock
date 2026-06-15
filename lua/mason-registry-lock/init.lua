838035f173c60caf0446560ef81486cb0dbe8c70
GitHub Action <action@github.com>
2026-06-15 02:11:31 +0000

    Update mason-registry lock to 2026-06-14-overt-peanut

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-14-bottled-target"
+M.registry_release = "github:mason-org/mason-registry@2026-06-14-overt-peanut"
 
 return M
