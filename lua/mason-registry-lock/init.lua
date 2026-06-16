6e7b3ee04aed75b39482bfd0d06d99ef910822b9
GitHub Action <action@github.com>
2026-06-16 02:15:19 +0000

    Update mason-registry lock to 2026-06-15-yellow-amago

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-15-recent-curler"
+M.registry_release = "github:mason-org/mason-registry@2026-06-15-yellow-amago"
 
 return M
