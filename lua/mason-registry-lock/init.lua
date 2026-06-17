81348e90b44a6481fd85a77892ca41e9d8427c9a
GitHub Action <action@github.com>
2026-06-17 02:11:01 +0000

    Update mason-registry lock to 2026-06-16-added-sponge

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-16-sheer-eagle"
+M.registry_release = "github:mason-org/mason-registry@2026-06-16-added-sponge"
 
 return M
