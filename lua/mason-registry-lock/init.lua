afb65ba583c126cad10b3166e16cfb2643280010
GitHub Action <action@github.com>
2026-06-17 02:11:01 +0000

    Update mason-registry lock to 2026-06-16-oily-server

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-16-basic-plow"
+M.registry_release = "github:mason-org/mason-registry@2026-06-16-oily-server"
 
 return M
