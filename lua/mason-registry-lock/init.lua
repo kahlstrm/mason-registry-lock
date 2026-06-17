b2131537492e91f66bebe9696a0214c3c069bc4a
GitHub Action <action@github.com>
2026-06-17 02:11:01 +0000

    Update mason-registry lock to 2026-06-16-corny-skill

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-16-next-ruffe"
+M.registry_release = "github:mason-org/mason-registry@2026-06-16-corny-skill"
 
 return M
