475fc774608dc32f293b318a5c611d98813510f9
GitHub Action <action@github.com>
2026-06-17 02:11:01 +0000

    Update mason-registry lock to 2026-06-16-civic-knee

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-16-silent-slide"
+M.registry_release = "github:mason-org/mason-registry@2026-06-16-civic-knee"
 
 return M
