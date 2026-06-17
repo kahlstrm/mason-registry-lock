e148628f5682cec05fdbe480bf9bebf488759c97
GitHub Action <action@github.com>
2026-06-17 02:11:01 +0000

    Update mason-registry lock to 2026-06-16-sturdy-nuke

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-16-bawdy-bottom"
+M.registry_release = "github:mason-org/mason-registry@2026-06-16-sturdy-nuke"
 
 return M
