30768f3231df2d2a903bf0a3e4be06ce4a271d38
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-17-excess-racing

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-17-eager-trade"
+M.registry_release = "github:mason-org/mason-registry@2026-06-17-excess-racing"
 
 return M
