caa9d6e3b0af8cc66884cd93059391f2ccc34fec
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-18-sonic-sail

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-17-elated-airbus"
+M.registry_release = "github:mason-org/mason-registry@2026-06-18-sonic-sail"
 
 return M
