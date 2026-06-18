647ffc166e757229a7cb6feee722f49155c3621e
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-17-moral-veil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-17-modern-grip"
+M.registry_release = "github:mason-org/mason-registry@2026-06-17-moral-veil"
 
 return M
