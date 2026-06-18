db547a26b24ce6f71c07212ef93dacea751f25a3
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-17-bony-ruby

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-16-sturdy-nuke"
+M.registry_release = "github:mason-org/mason-registry@2026-06-17-bony-ruby"
 
 return M
