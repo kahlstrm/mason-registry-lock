95c40aae7e8c8d8f44d597c078f10224a581bad2
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-17-bare-icicle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-17-excess-racing"
+M.registry_release = "github:mason-org/mason-registry@2026-06-17-bare-icicle"
 
 return M
