448dce481de4e272db49a8b6f10b2632d579cc75
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-18-delightful-check

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-18-sonic-sail"
+M.registry_release = "github:mason-org/mason-registry@2026-06-18-delightful-check"
 
 return M
