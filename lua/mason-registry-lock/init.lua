d6467ae1a70ccfd59f07c0f0159fad9204b7bc0e
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-17-modern-grip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-17-bare-icicle"
+M.registry_release = "github:mason-org/mason-registry@2026-06-17-modern-grip"
 
 return M
