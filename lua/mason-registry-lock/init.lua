4392a7c060e11dddd13d8f705d5179b5ddc97df5
GitHub Action <action@github.com>
2026-06-18 02:08:55 +0000

    Update mason-registry lock to 2026-06-17-jaded-spike

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-17-dreary-dress"
+M.registry_release = "github:mason-org/mason-registry@2026-06-17-jaded-spike"
 
 return M
