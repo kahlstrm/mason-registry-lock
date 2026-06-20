24b1d960d50504ed95dbed85460d394ecb554dfd
GitHub Action <action@github.com>
2026-06-20 01:58:19 +0000

    Update mason-registry lock to 2026-06-19-rigid-stew

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-19-feline-screen"
+M.registry_release = "github:mason-org/mason-registry@2026-06-19-rigid-stew"
 
 return M
