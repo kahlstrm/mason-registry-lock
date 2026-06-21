02ed2e14ad9887703f3232b1161f65e22323b909
GitHub Action <action@github.com>
2026-06-21 02:10:05 +0000

    Update mason-registry lock to 2026-06-20-timely-yoke

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-20-broad-flame"
+M.registry_release = "github:mason-org/mason-registry@2026-06-20-timely-yoke"
 
 return M
