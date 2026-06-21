73fb3bd135c027e3c700719a423f003ab9c5c9b2
GitHub Action <action@github.com>
2026-06-21 02:10:05 +0000

    Update mason-registry lock to 2026-06-20-vague-kangaroo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-20-rapid-card"
+M.registry_release = "github:mason-org/mason-registry@2026-06-20-vague-kangaroo"
 
 return M
