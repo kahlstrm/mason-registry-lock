7dfbdf3932f964e1aaebd83cf3d83c07e290e218
GitHub Action <action@github.com>
2026-06-21 02:10:05 +0000

    Update mason-registry lock to 2026-06-20-apt-tim

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-20-vague-kangaroo"
+M.registry_release = "github:mason-org/mason-registry@2026-06-20-apt-tim"
 
 return M
