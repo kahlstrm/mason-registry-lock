6f5074fd2c063ea5dbd2e11dc64f888163b46312
GitHub Action <action@github.com>
2026-06-22 02:13:40 +0000

    Update mason-registry lock to 2026-06-21-minute-bird

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-21-actual-vise"
+M.registry_release = "github:mason-org/mason-registry@2026-06-21-minute-bird"
 
 return M
