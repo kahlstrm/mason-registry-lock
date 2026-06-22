088dbec45481f25cc89f5fa2a560a6f44ee72fad
GitHub Action <action@github.com>
2026-06-22 02:13:40 +0000

    Update mason-registry lock to 2026-06-21-young-yellow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-21-minute-bird"
+M.registry_release = "github:mason-org/mason-registry@2026-06-21-young-yellow"
 
 return M
