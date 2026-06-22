170991063aa51f35fc23b005a4582bdf766f10fc
GitHub Action <action@github.com>
2026-06-22 02:13:40 +0000

    Update mason-registry lock to 2026-06-22-precious-potato

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-21-dizzy-swing"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-precious-potato"
 
 return M
