d2b95df8d165bacb3c372afe41f84a8e202c1659
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-22-purple-step

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-bloody-bait"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-purple-step"
 
 return M
