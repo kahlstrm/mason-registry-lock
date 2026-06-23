3044a7c7c6578247188ebe51e90e92a0c7a8122f
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-23-human-rust

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-cooing-fibre"
+M.registry_release = "github:mason-org/mason-registry@2026-06-23-human-rust"
 
 return M
