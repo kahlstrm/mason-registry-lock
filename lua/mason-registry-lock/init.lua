8520d38a6590a3c53b6c60640cd2e51837ee0f52
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-22-fluid-torque

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-atomic-close"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-fluid-torque"
 
 return M
