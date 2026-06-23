948a78c5ee463afc6505073ef8df1b90953258b4
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-22-sordid-pole

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-precious-potato"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-sordid-pole"
 
 return M
