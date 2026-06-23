3cd41d48e4dda440101925c4f749c53e4801eff0
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-22-single-dancer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-basic-jewel"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-single-dancer"
 
 return M
