a9c21e7fcc4a2b0915a0fbc102a751f20f3b97c0
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-22-worst-lark

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-fluid-torque"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-worst-lark"
 
 return M
