56eb8c4caed1d38f599f3f55637c480342279b35
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-22-fancy-event

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-sordid-pole"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-fancy-event"
 
 return M
