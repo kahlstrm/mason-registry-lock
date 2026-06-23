a4cfe8119ede013db0d24b9ad91d525a1981e75d
GitHub Action <action@github.com>
2026-06-23 01:52:54 +0000

    Update mason-registry lock to 2026-06-22-versed-basket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-22-fancy-event"
+M.registry_release = "github:mason-org/mason-registry@2026-06-22-versed-basket"
 
 return M
