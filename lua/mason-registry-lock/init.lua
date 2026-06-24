b22503cd1f3a214fe599ef222344a147a6dea0a5
GitHub Action <action@github.com>
2026-06-24 01:51:14 +0000

    Update mason-registry lock to 2026-06-23-plain-spike

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-23-worse-shore"
+M.registry_release = "github:mason-org/mason-registry@2026-06-23-plain-spike"
 
 return M
