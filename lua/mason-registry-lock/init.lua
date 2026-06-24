37b2fcb9d9696b1d549ee3cdd405d1c7a77784db
GitHub Action <action@github.com>
2026-06-24 01:51:14 +0000

    Update mason-registry lock to 2026-06-23-verbal-dinner

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-23-flashy-beam"
+M.registry_release = "github:mason-org/mason-registry@2026-06-23-verbal-dinner"
 
 return M
