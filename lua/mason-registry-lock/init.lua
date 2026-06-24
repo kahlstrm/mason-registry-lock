3c4311ebf18f2f91da654b5e4f598626702c87d6
GitHub Action <action@github.com>
2026-06-24 01:51:14 +0000

    Update mason-registry lock to 2026-06-23-ultra-timer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-23-hairy-shake"
+M.registry_release = "github:mason-org/mason-registry@2026-06-23-ultra-timer"
 
 return M
