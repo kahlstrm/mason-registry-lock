14b0fbf83f5f64f47d97634a40d53d58d0a65f9d
GitHub Action <action@github.com>
2026-06-25 01:55:08 +0000

    Update mason-registry lock to 2026-06-24-sound-brow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-24-flawed-credit"
+M.registry_release = "github:mason-org/mason-registry@2026-06-24-sound-brow"
 
 return M
