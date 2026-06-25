c7916ea08e1130dd5e2d93881a7a6a9b06ad29f5
GitHub Action <action@github.com>
2026-06-25 01:55:08 +0000

    Update mason-registry lock to 2026-06-24-hollow-safe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-24-sound-brow"
+M.registry_release = "github:mason-org/mason-registry@2026-06-24-hollow-safe"
 
 return M
