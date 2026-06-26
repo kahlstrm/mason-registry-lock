0ca4a1de672c94f26ebe9adfd73d4a54cb58c316
GitHub Action <action@github.com>
2026-06-26 01:57:02 +0000

    Update mason-registry lock to 2026-06-25-marked-koala

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-25-ample-verse"
+M.registry_release = "github:mason-org/mason-registry@2026-06-25-marked-koala"
 
 return M
