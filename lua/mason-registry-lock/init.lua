2379e740df726712cea103b812b76cbc8e6d0843
GitHub Action <action@github.com>
2026-06-26 01:57:02 +0000

    Update mason-registry lock to 2026-06-25-senior-quit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-25-marked-koala"
+M.registry_release = "github:mason-org/mason-registry@2026-06-25-senior-quit"
 
 return M
