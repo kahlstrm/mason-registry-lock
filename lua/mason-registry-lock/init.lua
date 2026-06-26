87d9c21bfcae30521016328ea11de111ea45baa5
GitHub Action <action@github.com>
2026-06-26 01:57:02 +0000

    Update mason-registry lock to 2026-06-25-gold-juice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-25-manual-arch"
+M.registry_release = "github:mason-org/mason-registry@2026-06-25-gold-juice"
 
 return M
