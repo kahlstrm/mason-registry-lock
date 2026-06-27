5c004a58f985a81e21c1197d42f78c6b79e70add
GitHub Action <action@github.com>
2026-06-27 01:50:20 +0000

    Update mason-registry lock to 2026-06-26-acrid-faucet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-26-bottled-height"
+M.registry_release = "github:mason-org/mason-registry@2026-06-26-acrid-faucet"
 
 return M
