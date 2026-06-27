5d17cadb4399f6b2b37142566fc48b792c6d261e
GitHub Action <action@github.com>
2026-06-27 01:50:20 +0000

    Update mason-registry lock to 2026-06-26-vacant-cello

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-26-rude-frown"
+M.registry_release = "github:mason-org/mason-registry@2026-06-26-vacant-cello"
 
 return M
