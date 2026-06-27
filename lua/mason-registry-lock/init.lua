762ac19383a1bd9452b7d5bf964c63d71a6d7817
GitHub Action <action@github.com>
2026-06-27 01:50:20 +0000

    Update mason-registry lock to 2026-06-26-knotty-wave

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-26-major-cub"
+M.registry_release = "github:mason-org/mason-registry@2026-06-26-knotty-wave"
 
 return M
