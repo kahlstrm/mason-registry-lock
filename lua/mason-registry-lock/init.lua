60c8c6aa508d2df30d238d2e7dc7818aca37668b
GitHub Action <action@github.com>
2026-06-28 02:00:27 +0000

    Update mason-registry lock to 2026-06-27-vague-crow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-27-weepy-stem"
+M.registry_release = "github:mason-org/mason-registry@2026-06-27-vague-crow"
 
 return M
