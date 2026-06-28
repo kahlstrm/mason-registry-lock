fd599efde88614498624d9ad062fe887c2e5bb66
GitHub Action <action@github.com>
2026-06-28 02:00:27 +0000

    Update mason-registry lock to 2026-06-27-invisible-plant

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-27-abject-pineapple"
+M.registry_release = "github:mason-org/mason-registry@2026-06-27-invisible-plant"
 
 return M
