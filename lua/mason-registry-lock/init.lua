7afd23d884e43ce39291eac10517012fcc4d7d72
GitHub Action <action@github.com>
2026-06-29 02:01:21 +0000

    Update mason-registry lock to 2026-06-28-far-plier

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-28-stupid-tongue"
+M.registry_release = "github:mason-org/mason-registry@2026-06-28-far-plier"
 
 return M
