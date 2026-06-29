3c84b39cc56e12e212c721ceb031aaf589e2e395
GitHub Action <action@github.com>
2026-06-29 02:01:21 +0000

    Update mason-registry lock to 2026-06-28-cleaver-fax

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-28-posh-brick"
+M.registry_release = "github:mason-org/mason-registry@2026-06-28-cleaver-fax"
 
 return M
