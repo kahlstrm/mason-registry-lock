67947d26b17716efba01fc2056e95645ad71b51e
GitHub Action <action@github.com>
2026-06-30 01:55:53 +0000

    Update mason-registry lock to 2026-06-29-dry-tooth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-29-dense-tooth"
+M.registry_release = "github:mason-org/mason-registry@2026-06-29-dry-tooth"
 
 return M
