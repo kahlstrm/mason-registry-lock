4000b4da738b897bcd997083564691241b9e371e
GitHub Action <action@github.com>
2026-06-30 01:55:54 +0000

    Update mason-registry lock to 2026-06-29-left-sight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-29-frail-pigeon"
+M.registry_release = "github:mason-org/mason-registry@2026-06-29-left-sight"
 
 return M
