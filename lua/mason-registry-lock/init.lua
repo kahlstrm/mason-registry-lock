2392c04488352dda0727b734782377e77015f808
GitHub Action <action@github.com>
2026-06-30 01:55:54 +0000

    Update mason-registry lock to 2026-06-29-cut-agency

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-29-expert-helium"
+M.registry_release = "github:mason-org/mason-registry@2026-06-29-cut-agency"
 
 return M
