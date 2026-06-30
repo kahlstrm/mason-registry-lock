5d01e8eec97810893cff6965e84f6d8195270adf
GitHub Action <action@github.com>
2026-06-30 01:55:54 +0000

    Update mason-registry lock to 2026-06-29-awake-order

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-29-evil-kaluga"
+M.registry_release = "github:mason-org/mason-registry@2026-06-29-awake-order"
 
 return M
