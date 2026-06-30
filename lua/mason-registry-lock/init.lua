692a7907a40ae0c83e654a65a25f508ca1695ac7
GitHub Action <action@github.com>
2026-06-30 01:55:54 +0000

    Update mason-registry lock to 2026-06-29-expert-helium

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-29-brave-plant"
+M.registry_release = "github:mason-org/mason-registry@2026-06-29-expert-helium"
 
 return M
