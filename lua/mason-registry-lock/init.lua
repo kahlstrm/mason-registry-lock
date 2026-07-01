a3eea6a87b53a190f579fe46e7c3f250008e6ead
GitHub Action <action@github.com>
2026-07-01 02:01:16 +0000

    Update mason-registry lock to 2026-06-30-brash-slide

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-30-dynamic-pack"
+M.registry_release = "github:mason-org/mason-registry@2026-06-30-brash-slide"
 
 return M
