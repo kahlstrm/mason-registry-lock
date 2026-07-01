3a54c8ddb6ea3a04d9e19a716d1f59a96aa98555
GitHub Action <action@github.com>
2026-07-01 02:01:16 +0000

    Update mason-registry lock to 2026-06-30-active-sled

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-30-noisy-belt"
+M.registry_release = "github:mason-org/mason-registry@2026-06-30-active-sled"
 
 return M
