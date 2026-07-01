270fd08d22a4d427b0c9908760ed89997bae7668
GitHub Action <action@github.com>
2026-07-01 02:01:16 +0000

    Update mason-registry lock to 2026-06-30-humble-sight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-06-30-active-sled"
+M.registry_release = "github:mason-org/mason-registry@2026-06-30-humble-sight"
 
 return M
