b30dd14229c38f96f3f4e669f77036e38e54b183
GitHub Action <action@github.com>
2026-07-02 01:54:57 +0000

    Update mason-registry lock to 2026-07-01-frilly-draw

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-01-spotty-pvc"
+M.registry_release = "github:mason-org/mason-registry@2026-07-01-frilly-draw"
 
 return M
