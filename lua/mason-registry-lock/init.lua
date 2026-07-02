244944c624e3d583e89c31b9f443c61db54ab804
GitHub Action <action@github.com>
2026-07-02 01:54:57 +0000

    Update mason-registry lock to 2026-07-01-urgent-zinc

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-01-frilly-draw"
+M.registry_release = "github:mason-org/mason-registry@2026-07-01-urgent-zinc"
 
 return M
