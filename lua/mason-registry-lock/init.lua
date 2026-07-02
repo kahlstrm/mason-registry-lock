e51b9f564abbf27f57dc096e364d9aa93beca84c
GitHub Action <action@github.com>
2026-07-02 01:54:57 +0000

    Update mason-registry lock to 2026-07-02-middle-fuscia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-01-forked-band"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-middle-fuscia"
 
 return M
