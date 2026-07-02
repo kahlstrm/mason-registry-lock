82787726836b8cf7dece0146e245b4aaaa645582
GitHub Action <action@github.com>
2026-07-02 01:54:57 +0000

    Update mason-registry lock to 2026-07-01-rotund-sleet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-01-imaginary-plier"
+M.registry_release = "github:mason-org/mason-registry@2026-07-01-rotund-sleet"
 
 return M
