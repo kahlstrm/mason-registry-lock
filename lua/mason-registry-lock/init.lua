2616d7e89925f94f37163b1541b71f888600bdc0
GitHub Action <action@github.com>
2026-07-03 01:25:01 +0000

    Update mason-registry lock to 2026-07-02-lush-door

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-slushy-grasshopper"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-lush-door"
 
 return M
