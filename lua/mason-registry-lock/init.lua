b5826c1d237a8470c1042c4398278e1f8568d5cf
GitHub Action <action@github.com>
2026-07-03 01:25:01 +0000

    Update mason-registry lock to 2026-07-02-aloof-patio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-gentle-handle"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-aloof-patio"
 
 return M
