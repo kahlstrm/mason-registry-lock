b4a215b9d3459ffa53d6ffc08766b270ad95e4d8
GitHub Action <action@github.com>
2026-07-03 01:25:01 +0000

    Update mason-registry lock to 2026-07-02-gentle-handle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-narrow-skates"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-gentle-handle"
 
 return M
