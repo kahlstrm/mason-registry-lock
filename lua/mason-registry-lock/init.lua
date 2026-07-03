77aeceecbbf50e66f3add9617348966682d9929e
GitHub Action <action@github.com>
2026-07-03 01:25:01 +0000

    Update mason-registry lock to 2026-07-02-edible-sauce

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-dual-dugout"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-edible-sauce"
 
 return M
