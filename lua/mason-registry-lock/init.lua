50698d3e2d365932881fdada5333a8997a6c2ca3
GitHub Action <action@github.com>
2026-07-03 01:25:01 +0000

    Update mason-registry lock to 2026-07-02-ordinary-drum

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-total-pound"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-ordinary-drum"
 
 return M
