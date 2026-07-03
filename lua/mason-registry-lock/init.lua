0192db53d4f144f614fabe335d2dd709a23abd2f
GitHub Action <action@github.com>
2026-07-03 01:25:01 +0000

    Update mason-registry lock to 2026-07-02-narrow-skates

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-only-james"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-narrow-skates"
 
 return M
