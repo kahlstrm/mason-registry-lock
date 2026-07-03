93bb714e9eb6b0572d1680e29e27a3c3b43c1b7c
GitHub Action <action@github.com>
2026-07-03 01:25:01 +0000

    Update mason-registry lock to 2026-07-02-valid-brass

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-rotten-glider"
+M.registry_release = "github:mason-org/mason-registry@2026-07-02-valid-brass"
 
 return M
