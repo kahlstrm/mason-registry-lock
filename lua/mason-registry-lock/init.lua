4c25f64871be1ab3e9fe0390e238009b63f5a86e
GitHub Action <action@github.com>
2026-07-04 01:24:45 +0000

    Update mason-registry lock to 2026-07-03-paltry-tapir

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-03-barren-find"
+M.registry_release = "github:mason-org/mason-registry@2026-07-03-paltry-tapir"
 
 return M
