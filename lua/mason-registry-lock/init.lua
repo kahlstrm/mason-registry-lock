35e4bd09c514d8f9f772cbecede9595643310d53
GitHub Action <action@github.com>
2026-07-04 01:24:45 +0000

    Update mason-registry lock to 2026-07-03-funny-noise

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-03-mean-insect"
+M.registry_release = "github:mason-org/mason-registry@2026-07-03-funny-noise"
 
 return M
