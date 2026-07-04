2cef497c9f2ba325694cdb18bb38ad7d15aab16b
GitHub Action <action@github.com>
2026-07-04 01:24:45 +0000

    Update mason-registry lock to 2026-07-03-shaggy-wrasse

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-03-open-birch"
+M.registry_release = "github:mason-org/mason-registry@2026-07-03-shaggy-wrasse"
 
 return M
