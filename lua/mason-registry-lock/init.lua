3b92bbc1144c9ddd6b632637b6b26d707ca4305a
GitHub Action <action@github.com>
2026-07-04 01:24:45 +0000

    Update mason-registry lock to 2026-07-03-baked-diving

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-03-paltry-tapir"
+M.registry_release = "github:mason-org/mason-registry@2026-07-03-baked-diving"
 
 return M
