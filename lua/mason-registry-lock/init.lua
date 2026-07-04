de14842a5c40008c0ea7f0299b721773b7f37dfc
GitHub Action <action@github.com>
2026-07-04 01:24:45 +0000

    Update mason-registry lock to 2026-07-03-slender-cork

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-03-mute-coast"
+M.registry_release = "github:mason-org/mason-registry@2026-07-03-slender-cork"
 
 return M
