37395f52ab5ee4a0ce22aad44a04040cf2597e05
GitHub Action <action@github.com>
2026-07-04 01:24:45 +0000

    Update mason-registry lock to 2026-07-03-barren-find

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-02-sonic-lamp"
+M.registry_release = "github:mason-org/mason-registry@2026-07-03-barren-find"
 
 return M
