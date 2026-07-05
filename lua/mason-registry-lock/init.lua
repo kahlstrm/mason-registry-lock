65bba8054f7f1f07c6c528744c522dab91efdded
GitHub Action <action@github.com>
2026-07-05 01:30:28 +0000

    Update mason-registry lock to 2026-07-04-frizzy-range

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-04-inborn-store"
+M.registry_release = "github:mason-org/mason-registry@2026-07-04-frizzy-range"
 
 return M
