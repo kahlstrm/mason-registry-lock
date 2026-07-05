437556063efc12ad69803ea2d6ab020bc57dbfaf
GitHub Action <action@github.com>
2026-07-05 01:30:28 +0000

    Update mason-registry lock to 2026-07-04-worst-case

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-03-wide-xenon"
+M.registry_release = "github:mason-org/mason-registry@2026-07-04-worst-case"
 
 return M
