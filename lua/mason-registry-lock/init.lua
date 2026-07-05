1f463c62ced363733701e5e433e37d07ca3d1927
GitHub Action <action@github.com>
2026-07-05 01:30:28 +0000

    Update mason-registry lock to 2026-07-04-messy-bit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-04-oval-letter"
+M.registry_release = "github:mason-org/mason-registry@2026-07-04-messy-bit"
 
 return M
