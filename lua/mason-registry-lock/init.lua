1c63986d6f26cae401a16d5910d3af3db58d4941
GitHub Action <action@github.com>
2026-07-05 01:30:28 +0000

    Update mason-registry lock to 2026-07-04-soupy-parade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-04-worst-case"
+M.registry_release = "github:mason-org/mason-registry@2026-07-04-soupy-parade"
 
 return M
