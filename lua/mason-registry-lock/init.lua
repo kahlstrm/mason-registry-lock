e10fb7f4936f9d7cecd1c6a22e7c0a6a10b6f558
GitHub Action <action@github.com>
2026-07-06 01:44:30 +0000

    Update mason-registry lock to 2026-07-05-frigid-thread

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-05-last-musk"
+M.registry_release = "github:mason-org/mason-registry@2026-07-05-frigid-thread"
 
 return M
