57939ba645f9fc022ad1b0c2d56b0f31157b693b
GitHub Action <action@github.com>
2026-07-07 01:42:29 +0000

    Update mason-registry lock to 2026-07-06-odd-saury

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-06-next-glider"
+M.registry_release = "github:mason-org/mason-registry@2026-07-06-odd-saury"
 
 return M
