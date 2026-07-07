1e53b27e6eadb7a5792e395d2ded8b79777a8ee7
GitHub Action <action@github.com>
2026-07-07 01:42:29 +0000

    Update mason-registry lock to 2026-07-06-next-glider

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-06-daring-winter"
+M.registry_release = "github:mason-org/mason-registry@2026-07-06-next-glider"
 
 return M
