a3a2dc9a673410e1ed5c32245f6c094377541029
GitHub Action <action@github.com>
2026-07-08 01:16:19 +0000

    Update mason-registry lock to 2026-07-08-bouncy-sun

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-08-gold-kendo"
+M.registry_release = "github:mason-org/mason-registry@2026-07-08-bouncy-sun"
 
 return M
