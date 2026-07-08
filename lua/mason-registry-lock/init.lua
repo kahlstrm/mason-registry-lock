18355fb5939e56bc72c727b37dafd1c00ccd19c2
GitHub Action <action@github.com>
2026-07-08 01:16:19 +0000

    Update mason-registry lock to 2026-07-07-versed-mitten

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-07-watery-birth"
+M.registry_release = "github:mason-org/mason-registry@2026-07-07-versed-mitten"
 
 return M
