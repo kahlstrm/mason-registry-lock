70e475e980c8a410eab5ae49fa66592d0992a5eb
GitHub Action <action@github.com>
2026-07-09 01:24:19 +0000

    Update mason-registry lock to 2026-07-08-brown-celery

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-08-grumpy-self"
+M.registry_release = "github:mason-org/mason-registry@2026-07-08-brown-celery"
 
 return M
