c7166fdd6262e2bf1d5c1b002459560a1f672608
GitHub Action <action@github.com>
2026-07-09 01:24:19 +0000

    Update mason-registry lock to 2026-07-08-vexed-line

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-08-cleaver-cloud"
+M.registry_release = "github:mason-org/mason-registry@2026-07-08-vexed-line"
 
 return M
