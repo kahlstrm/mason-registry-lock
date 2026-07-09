2f6127a8808016820d80d2184985f213ff73bb94
GitHub Action <action@github.com>
2026-07-09 01:24:19 +0000

    Update mason-registry lock to 2026-07-08-cleaver-cloud

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-08-giddy-comb"
+M.registry_release = "github:mason-org/mason-registry@2026-07-08-cleaver-cloud"
 
 return M
