80da8c986b64c02671b418858c9a765069fece42
GitHub Action <action@github.com>
2026-07-10 01:21:28 +0000

    Update mason-registry lock to 2026-07-09-cute-timer

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-09-curly-break"
+M.registry_release = "github:mason-org/mason-registry@2026-07-09-cute-timer"
 
 return M
