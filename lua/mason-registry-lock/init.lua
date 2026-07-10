39b19554901743debdad6bbe974b691e25f081e8
GitHub Action <action@github.com>
2026-07-10 01:21:28 +0000

    Update mason-registry lock to 2026-07-09-sparse-book

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-09-webbed-lemon"
+M.registry_release = "github:mason-org/mason-registry@2026-07-09-sparse-book"
 
 return M
