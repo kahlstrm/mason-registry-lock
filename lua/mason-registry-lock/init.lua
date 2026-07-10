f705908d2c0c285d2238c4a7802349ca0c7f174e
GitHub Action <action@github.com>
2026-07-10 01:21:28 +0000

    Update mason-registry lock to 2026-07-09-frosty-join

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-09-unfair-beetle"
+M.registry_release = "github:mason-org/mason-registry@2026-07-09-frosty-join"
 
 return M
