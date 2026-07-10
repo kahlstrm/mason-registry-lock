ff35eb7a9ea011943ab67fa03ecaf2101efa4582
GitHub Action <action@github.com>
2026-07-10 01:21:28 +0000

    Update mason-registry lock to 2026-07-09-regal-air

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-09-agile-louvar"
+M.registry_release = "github:mason-org/mason-registry@2026-07-09-regal-air"
 
 return M
