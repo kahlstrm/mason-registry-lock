eddd2dac78411f21d3faf95d884f2754ce89ab75
GitHub Action <action@github.com>
2026-07-10 01:21:28 +0000

    Update mason-registry lock to 2026-07-09-six-falce

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-09-secure-taurus"
+M.registry_release = "github:mason-org/mason-registry@2026-07-09-six-falce"
 
 return M
