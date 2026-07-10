a33d078f2d9b481b6d5a3f354b34dac7e7e4deb9
GitHub Action <action@github.com>
2026-07-10 01:21:28 +0000

    Update mason-registry lock to 2026-07-09-jumbo-fibre

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-09-stiff-canvas"
+M.registry_release = "github:mason-org/mason-registry@2026-07-09-jumbo-fibre"
 
 return M
