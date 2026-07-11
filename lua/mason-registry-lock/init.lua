11bd6025946a53e06af4a67382d5150e529665a5
GitHub Action <action@github.com>
2026-07-11 01:15:06 +0000

    Update mason-registry lock to 2026-07-10-strange-carbon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-10-likely-router"
+M.registry_release = "github:mason-org/mason-registry@2026-07-10-strange-carbon"
 
 return M
