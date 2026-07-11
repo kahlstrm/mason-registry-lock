262225237df7ffceb19da86e0e558d7ff4d417e3
GitHub Action <action@github.com>
2026-07-11 01:15:06 +0000

    Update mason-registry lock to 2026-07-10-tepid-coat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-10-flashy-canoe"
+M.registry_release = "github:mason-org/mason-registry@2026-07-10-tepid-coat"
 
 return M
