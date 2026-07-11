18e81553bad87c2398d082334e8fe78510d415ed
GitHub Action <action@github.com>
2026-07-11 01:15:06 +0000

    Update mason-registry lock to 2026-07-10-likely-router

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-10-trusty-stone"
+M.registry_release = "github:mason-org/mason-registry@2026-07-10-likely-router"
 
 return M
