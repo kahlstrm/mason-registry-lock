e5304da3ff3dc40845bdad6d24b083df160862ec
GitHub Action <action@github.com>
2026-07-11 01:15:06 +0000

    Update mason-registry lock to 2026-07-10-sacred-crop

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-10-fearless-device"
+M.registry_release = "github:mason-org/mason-registry@2026-07-10-sacred-crop"
 
 return M
