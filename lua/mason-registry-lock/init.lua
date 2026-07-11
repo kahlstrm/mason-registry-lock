02d224adccb4f26e27f45245e190f1739b06fea1
GitHub Action <action@github.com>
2026-07-11 01:15:06 +0000

    Update mason-registry lock to 2026-07-10-high-porter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-10-nearby-adult"
+M.registry_release = "github:mason-org/mason-registry@2026-07-10-high-porter"
 
 return M
