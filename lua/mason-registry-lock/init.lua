863bd3ae7e485a6724eeb16671cb95ca36f5d3fb
GitHub Action <action@github.com>
2026-07-11 01:15:06 +0000

    Update mason-registry lock to 2026-07-10-unusual-smoke

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-10-high-porter"
+M.registry_release = "github:mason-org/mason-registry@2026-07-10-unusual-smoke"
 
 return M
