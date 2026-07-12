daba0109172e155c3466ee10b6d19c45430ad19e
GitHub Action <action@github.com>
2026-07-12 01:17:03 +0000

    Update mason-registry lock to 2026-07-10-unusual-spot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-10-asleep-line"
+M.registry_release = "github:mason-org/mason-registry@2026-07-10-unusual-spot"
 
 return M
