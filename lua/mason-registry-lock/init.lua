03ce9b53614dc70a8aede09375bc7451e8114947
GitHub Action <action@github.com>
2026-07-12 01:17:03 +0000

    Update mason-registry lock to 2026-07-11-mute-bottle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-11-ruddy-chord"
+M.registry_release = "github:mason-org/mason-registry@2026-07-11-mute-bottle"
 
 return M
