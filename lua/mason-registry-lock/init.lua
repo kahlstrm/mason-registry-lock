0df49ad6d8fa382c2e32f8c365944023c4113571
GitHub Action <action@github.com>
2026-07-12 01:17:04 +0000

    Update mason-registry lock to 2026-07-11-frugal-towel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-11-icky-rap"
+M.registry_release = "github:mason-org/mason-registry@2026-07-11-frugal-towel"
 
 return M
