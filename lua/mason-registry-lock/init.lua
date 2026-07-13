b1a9001a3bbc0ac07532f508b418a123dcf98ca7
GitHub Action <action@github.com>
2026-07-13 01:18:10 +0000

    Update mason-registry lock to 2026-07-12-used-random

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-12-zigzag-coke"
+M.registry_release = "github:mason-org/mason-registry@2026-07-12-used-random"
 
 return M
