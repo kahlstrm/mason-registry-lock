8da616be124c9b267203a7bd8ecb2080b49669c9
GitHub Action <action@github.com>
2026-07-13 01:18:10 +0000

    Update mason-registry lock to 2026-07-12-kosher-stream

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-12-ornate-theory"
+M.registry_release = "github:mason-org/mason-registry@2026-07-12-kosher-stream"
 
 return M
