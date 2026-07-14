7191e04f7eb7e3f36f374fe28552bab56cb7ab5b
GitHub Action <action@github.com>
2026-07-14 01:07:48 +0000

    Update mason-registry lock to 2026-07-13-outer-brandy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-13-bawdy-unit"
+M.registry_release = "github:mason-org/mason-registry@2026-07-13-outer-brandy"
 
 return M
