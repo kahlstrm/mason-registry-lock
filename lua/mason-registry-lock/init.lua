be1ec0d69cc0fcc3d1ffd18d768497b79bd41c21
GitHub Action <action@github.com>
2026-07-14 01:07:48 +0000

    Update mason-registry lock to 2026-07-13-quaint-stick

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-13-impure-nurse"
+M.registry_release = "github:mason-org/mason-registry@2026-07-13-quaint-stick"
 
 return M
