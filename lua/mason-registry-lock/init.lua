0fe6a53489b9b3f6e118040cf90d8161fd83044e
GitHub Action <action@github.com>
2026-07-14 01:07:48 +0000

    Update mason-registry lock to 2026-07-13-paltry-flute

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-13-cool-spleen"
+M.registry_release = "github:mason-org/mason-registry@2026-07-13-paltry-flute"
 
 return M
