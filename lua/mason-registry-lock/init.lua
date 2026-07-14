8d59946c181ca806f0de214dd3b20ee55606d5bf
GitHub Action <action@github.com>
2026-07-14 01:07:48 +0000

    Update mason-registry lock to 2026-07-13-cool-spleen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-13-gigantic-flag"
+M.registry_release = "github:mason-org/mason-registry@2026-07-13-cool-spleen"
 
 return M
