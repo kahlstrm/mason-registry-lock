aaeabffa4bc42f458635e869d606be92f140f0bc
GitHub Action <action@github.com>
2026-07-14 01:07:48 +0000

    Update mason-registry lock to 2026-07-13-bawdy-unit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-13-quaint-stick"
+M.registry_release = "github:mason-org/mason-registry@2026-07-13-bawdy-unit"
 
 return M
