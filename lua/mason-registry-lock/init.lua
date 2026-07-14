18a5d13570852d357ca56028f24118184da340e4
GitHub Action <action@github.com>
2026-07-14 01:07:48 +0000

    Update mason-registry lock to 2026-07-13-gigantic-flag

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-13-outer-brandy"
+M.registry_release = "github:mason-org/mason-registry@2026-07-13-gigantic-flag"
 
 return M
