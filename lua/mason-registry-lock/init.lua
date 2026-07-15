8f01ff7acfc79a38602fe5f5779fc0f0efcdb86a
GitHub Action <action@github.com>
2026-07-15 01:05:26 +0000

    Update mason-registry lock to 2026-07-14-scaly-digger

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-14-vapid-moose"
+M.registry_release = "github:mason-org/mason-registry@2026-07-14-scaly-digger"
 
 return M
