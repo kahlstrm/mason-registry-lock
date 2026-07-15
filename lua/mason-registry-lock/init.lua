2e1c2ce53aed3c451d3183424a98ee694aa0d2d9
GitHub Action <action@github.com>
2026-07-15 01:05:26 +0000

    Update mason-registry lock to 2026-07-14-handy-cobweb

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-14-bouncy-locust"
+M.registry_release = "github:mason-org/mason-registry@2026-07-14-handy-cobweb"
 
 return M
