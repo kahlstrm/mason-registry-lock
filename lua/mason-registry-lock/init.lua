e246e62caa6a43b28df44e9cefa174783b447829
GitHub Action <action@github.com>
2026-07-15 01:05:26 +0000

    Update mason-registry lock to 2026-07-14-likely-freeze

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-14-sulky-libra"
+M.registry_release = "github:mason-org/mason-registry@2026-07-14-likely-freeze"
 
 return M
