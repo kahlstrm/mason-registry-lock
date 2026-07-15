0af67fee0599e40693f53f0261369ffb71d5c201
GitHub Action <action@github.com>
2026-07-15 01:05:26 +0000

    Update mason-registry lock to 2026-07-14-slim-quince

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-14-scaly-digger"
+M.registry_release = "github:mason-org/mason-registry@2026-07-14-slim-quince"
 
 return M
