3c89398ecf6caa8c209a6523980e10b5f95917f0
GitHub Action <action@github.com>
2026-07-16 01:13:39 +0000

    Update mason-registry lock to 2026-07-15-broken-bottle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-15-pink-egg"
+M.registry_release = "github:mason-org/mason-registry@2026-07-15-broken-bottle"
 
 return M
