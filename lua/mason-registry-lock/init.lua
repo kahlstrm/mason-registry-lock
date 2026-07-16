88295ecc38fd808688c56264c054f60f70a4ce3a
GitHub Action <action@github.com>
2026-07-16 01:13:39 +0000

    Update mason-registry lock to 2026-07-15-close-digger

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-15-head-photo"
+M.registry_release = "github:mason-org/mason-registry@2026-07-15-close-digger"
 
 return M
