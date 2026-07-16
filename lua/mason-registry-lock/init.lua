23f6cf7426ac9911b79985b1c6e4f72fd45eff57
GitHub Action <action@github.com>
2026-07-16 01:13:39 +0000

    Update mason-registry lock to 2026-07-15-mobile-vest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-15-magic-risk"
+M.registry_release = "github:mason-org/mason-registry@2026-07-15-mobile-vest"
 
 return M
