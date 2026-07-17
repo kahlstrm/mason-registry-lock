85ad7c998143c3b9e41e9b122a443f6db1cf0d32
GitHub Action <action@github.com>
2026-07-17 01:17:28 +0000

    Update mason-registry lock to 2026-07-16-noisy-heat

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-16-bowed-guide"
+M.registry_release = "github:mason-org/mason-registry@2026-07-16-noisy-heat"
 
 return M
