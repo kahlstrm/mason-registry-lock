828176386aacc801b091029fcdf2e4c5acfce622
GitHub Action <action@github.com>
2026-07-18 01:09:40 +0000

    Update mason-registry lock to 2026-07-18-skinny-grape

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-17-weekly-flight"
+M.registry_release = "github:mason-org/mason-registry@2026-07-18-skinny-grape"
 
 return M
