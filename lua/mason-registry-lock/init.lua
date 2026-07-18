01269f175d830afdf3861ec61bc29687b0e75b2d
GitHub Action <action@github.com>
2026-07-18 01:09:39 +0000

    Update mason-registry lock to 2026-07-17-worst-monday

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-17-early-lily"
+M.registry_release = "github:mason-org/mason-registry@2026-07-17-worst-monday"
 
 return M
