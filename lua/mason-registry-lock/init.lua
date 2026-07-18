1a695f73f67f446ffcfad8f0562a622f9e6ad3f5
GitHub Action <action@github.com>
2026-07-18 01:09:40 +0000

    Update mason-registry lock to 2026-07-17-scant-pineapple

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-17-spry-period"
+M.registry_release = "github:mason-org/mason-registry@2026-07-17-scant-pineapple"
 
 return M
