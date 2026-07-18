783e844827cfa20b6432c0d6b5747788b8c3270e
GitHub Action <action@github.com>
2026-07-18 01:09:39 +0000

    Update mason-registry lock to 2026-07-17-stable-gray

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-17-soupy-jitter"
+M.registry_release = "github:mason-org/mason-registry@2026-07-17-stable-gray"
 
 return M
