3a26e0281fb15b81925ec07adc265aa1b034b70f
GitHub Action <action@github.com>
2026-07-18 01:09:40 +0000

    Update mason-registry lock to 2026-07-17-spry-period

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-17-back-rudd"
+M.registry_release = "github:mason-org/mason-registry@2026-07-17-spry-period"
 
 return M
