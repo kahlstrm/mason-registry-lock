23611a86e2f113adfaa7e6e48f0259f69b6b10d3
GitHub Action <action@github.com>
2026-07-19 01:14:28 +0000

    Update mason-registry lock to 2026-07-18-normal-manx

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-18-armed-herd"
+M.registry_release = "github:mason-org/mason-registry@2026-07-18-normal-manx"
 
 return M
