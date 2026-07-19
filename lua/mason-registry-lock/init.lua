dcd7995efa04246e7d0a9f118c8ac4ab42f027f8
GitHub Action <action@github.com>
2026-07-19 01:14:28 +0000

    Update mason-registry lock to 2026-07-18-worthy-lemon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-18-abrupt-space"
+M.registry_release = "github:mason-org/mason-registry@2026-07-18-worthy-lemon"
 
 return M
