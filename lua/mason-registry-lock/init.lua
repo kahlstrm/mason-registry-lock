db603f797c2abf1bcbb447b71e848ce6121391aa
GitHub Action <action@github.com>
2026-07-20 01:17:51 +0000

    Update mason-registry lock to 2026-07-19-daring-icicle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-19-mellow-coil"
+M.registry_release = "github:mason-org/mason-registry@2026-07-19-daring-icicle"
 
 return M
