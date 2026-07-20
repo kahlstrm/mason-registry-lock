09cf9c910dd822336ef7a36d63db863abae5ed15
GitHub Action <action@github.com>
2026-07-20 01:17:51 +0000

    Update mason-registry lock to 2026-07-19-mellow-coil

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-19-klutzy-lift"
+M.registry_release = "github:mason-org/mason-registry@2026-07-19-mellow-coil"
 
 return M
