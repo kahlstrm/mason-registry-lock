95d25a0c8f9a22b0eec09726e2a9adfbabbd37f4
GitHub Action <action@github.com>
2026-07-21 01:14:56 +0000

    Update mason-registry lock to 2026-07-20-precious-hemp

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-20-dim-motion"
+M.registry_release = "github:mason-org/mason-registry@2026-07-20-precious-hemp"
 
 return M
