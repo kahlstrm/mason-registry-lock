933043c759c624a77f5831bf39d4abe3200cb3aa
GitHub Action <action@github.com>
2026-07-21 01:14:56 +0000

    Update mason-registry lock to 2026-07-20-graceful-kaluga

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-20-petty-margin"
+M.registry_release = "github:mason-org/mason-registry@2026-07-20-graceful-kaluga"
 
 return M
