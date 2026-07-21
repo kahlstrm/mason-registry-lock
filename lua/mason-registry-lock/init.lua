08c0839e7ce7651d830fb8ba5c3ab4592895a92c
GitHub Action <action@github.com>
2026-07-21 01:14:56 +0000

    Update mason-registry lock to 2026-07-21-aboard-memory

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-kooky-sprat"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-aboard-memory"
 
 return M
