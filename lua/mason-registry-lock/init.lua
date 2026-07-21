6340af111796905e91fe896251b5a10ecaac8f10
GitHub Action <action@github.com>
2026-07-21 01:14:56 +0000

    Update mason-registry lock to 2026-07-21-long-pluto

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-20-lunar-date"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-long-pluto"
 
 return M
