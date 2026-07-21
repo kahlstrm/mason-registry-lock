cba869077e4d83af3e480ae9bef72f4153408c9a
GitHub Action <action@github.com>
2026-07-21 01:14:56 +0000

    Update mason-registry lock to 2026-07-20-dim-motion

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-20-graceful-kaluga"
+M.registry_release = "github:mason-org/mason-registry@2026-07-20-dim-motion"
 
 return M
