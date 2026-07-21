2f0accf6aaa0821f8cf17ba7384aa18a56561274
GitHub Action <action@github.com>
2026-07-21 01:14:56 +0000

    Update mason-registry lock to 2026-07-21-queasy-butter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-long-pluto"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-queasy-butter"
 
 return M
