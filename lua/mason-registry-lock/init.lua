acff5f5760526b7832584ff6c33fa241d022b588
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-open-volume

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-vast-squirrel"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-open-volume"
 
 return M
