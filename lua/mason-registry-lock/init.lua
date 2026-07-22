d9e50f70c68a62ab0c109de4fed2dd6d6e1f674c
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-many-bleak

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-fresh-file"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-many-bleak"
 
 return M
