fb30e78b4d5844cab03c57da168142042b2a007e
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-past-ticket

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-open-volume"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-past-ticket"
 
 return M
