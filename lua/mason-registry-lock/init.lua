707b9dce567bd4cef80cb1883d964b003d6e3c7c
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-lazy-margin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-frigid-slice"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-lazy-margin"
 
 return M
