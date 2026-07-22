416925825225c0f52d4cc65878534e3ffec12c3e
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-vast-squirrel

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-sheer-old"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-vast-squirrel"
 
 return M
