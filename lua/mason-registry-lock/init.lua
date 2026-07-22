691590a18723ada8ba61eab69878f723eaf69aad
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-bottom-fibre

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-many-bleak"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-bottom-fibre"
 
 return M
