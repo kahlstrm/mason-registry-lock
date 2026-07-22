ff138f8d0d168726c41497abf5fa69a683cccc4f
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-unused-walrus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-bottom-fibre"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-unused-walrus"
 
 return M
