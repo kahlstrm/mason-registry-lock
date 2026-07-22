2c710629e612f9097121ca0bc08ab4ededa566cd
GitHub Action <action@github.com>
2026-07-22 01:13:17 +0000

    Update mason-registry lock to 2026-07-21-sheer-old

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-21-lazy-margin"
+M.registry_release = "github:mason-org/mason-registry@2026-07-21-sheer-old"
 
 return M
