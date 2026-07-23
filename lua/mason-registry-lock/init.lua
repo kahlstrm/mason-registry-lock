7be66e923751ad0e84c16af7b495750012c57391
GitHub Action <action@github.com>
2026-07-23 01:20:52 +0000

    Update mason-registry lock to 2026-07-22-graceful-mall

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-22-steep-pull"
+M.registry_release = "github:mason-org/mason-registry@2026-07-22-graceful-mall"
 
 return M
