beab9d56b84cd24bb9cf35cfcf1d52969cf5ae26
GitHub Action <action@github.com>
2026-07-23 01:20:52 +0000

    Update mason-registry lock to 2026-07-22-brawny-audio

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-22-filthy-output"
+M.registry_release = "github:mason-org/mason-registry@2026-07-22-brawny-audio"
 
 return M
