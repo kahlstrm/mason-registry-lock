f94c15180ec0ffbec5e05bdcd4e8bd80179ad51b
GitHub Action <action@github.com>
2026-07-23 01:20:52 +0000

    Update mason-registry lock to 2026-07-22-creepy-twist

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-22-marine-herd"
+M.registry_release = "github:mason-org/mason-registry@2026-07-22-creepy-twist"
 
 return M
