d492ea95b164fa2d1c237f2413f7445b49897bb2
GitHub Action <action@github.com>
2026-07-23 01:20:52 +0000

    Update mason-registry lock to 2026-07-22-robust-hope

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-22-marine-mark"
+M.registry_release = "github:mason-org/mason-registry@2026-07-22-robust-hope"
 
 return M
