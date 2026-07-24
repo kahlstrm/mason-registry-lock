fe09ce135e4ef61649a7d7e44168c5ea4292c9b6
GitHub Action <action@github.com>
2026-07-24 01:16:48 +0000

    Update mason-registry lock to 2026-07-23-dusty-record

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-23-weak-work"
+M.registry_release = "github:mason-org/mason-registry@2026-07-23-dusty-record"
 
 return M
