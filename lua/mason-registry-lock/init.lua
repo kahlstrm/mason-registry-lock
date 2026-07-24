843198ba36c8ac3cf2623155ceb8000ebd71467f
GitHub Action <action@github.com>
2026-07-24 01:16:48 +0000

    Update mason-registry lock to 2026-07-23-secure-eagle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-23-awake-washer"
+M.registry_release = "github:mason-org/mason-registry@2026-07-23-secure-eagle"
 
 return M
