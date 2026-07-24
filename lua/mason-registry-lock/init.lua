914140b4274250280844a9914cf9a562364a3105
GitHub Action <action@github.com>
2026-07-24 01:16:48 +0000

    Update mason-registry lock to 2026-07-23-bowed-exit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-23-soupy-curtain"
+M.registry_release = "github:mason-org/mason-registry@2026-07-23-bowed-exit"
 
 return M
