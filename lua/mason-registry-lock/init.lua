0b1cbbce01831ffae654e57334f5e758b75dedb7
GitHub Action <action@github.com>
2026-07-24 01:16:48 +0000

    Update mason-registry lock to 2026-07-22-steep-pull

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-22-single-hair"
+M.registry_release = "github:mason-org/mason-registry@2026-07-22-steep-pull"
 
 return M
