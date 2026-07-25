da359074d08e6511e8d9a5026f5709c175863b0e
GitHub Action <action@github.com>
2026-07-25 01:17:44 +0000

    Update mason-registry lock to 2026-07-23-tart-able

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-23-naval-flour"
+M.registry_release = "github:mason-org/mason-registry@2026-07-23-tart-able"
 
 return M
