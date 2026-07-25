f523513e8f0f460a6eb442fce8daf366dc46916e
GitHub Action <action@github.com>
2026-07-25 01:17:44 +0000

    Update mason-registry lock to 2026-07-23-naval-flour

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-23-secure-eagle"
+M.registry_release = "github:mason-org/mason-registry@2026-07-23-naval-flour"
 
 return M
