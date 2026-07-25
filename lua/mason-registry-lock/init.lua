641bc10328f24f65b0c17fde61f3125d82faa913
GitHub Action <action@github.com>
2026-07-25 01:17:44 +0000

    Update mason-registry lock to 2026-07-24-dotted-bass

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-24-good-barbel"
+M.registry_release = "github:mason-org/mason-registry@2026-07-24-dotted-bass"
 
 return M
