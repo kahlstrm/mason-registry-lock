3cbdac782133245807a04ad45aa37d7f5e39c0ad
GitHub Action <action@github.com>
2026-07-25 01:17:44 +0000

    Update mason-registry lock to 2026-07-24-inborn-hakea

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-24-awful-trial"
+M.registry_release = "github:mason-org/mason-registry@2026-07-24-inborn-hakea"
 
 return M
