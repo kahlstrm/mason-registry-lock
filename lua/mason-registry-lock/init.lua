d194a2bdf21d5e647834eec8f5452ca8939c6142
GitHub Action <action@github.com>
2026-07-25 01:17:44 +0000

    Update mason-registry lock to 2026-07-23-soupy-curtain

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-23-stingy-teeth"
+M.registry_release = "github:mason-org/mason-registry@2026-07-23-soupy-curtain"
 
 return M
