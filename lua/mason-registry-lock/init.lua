ac4f8d9944cc17dcf10ac57016337614ad690c78
GitHub Action <action@github.com>
2026-07-25 01:17:44 +0000

    Update mason-registry lock to 2026-07-23-rigid-mimosa

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-23-slushy-llama"
+M.registry_release = "github:mason-org/mason-registry@2026-07-23-rigid-mimosa"
 
 return M
