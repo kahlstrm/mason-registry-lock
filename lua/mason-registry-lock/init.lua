ddf34515c1289121d3f33414e4aea06c83b7377f
GitHub Action <action@github.com>
2026-07-25 01:17:44 +0000

    Update mason-registry lock to 2026-07-24-rubber-trumpet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-24-inborn-hakea"
+M.registry_release = "github:mason-org/mason-registry@2026-07-24-rubber-trumpet"
 
 return M
