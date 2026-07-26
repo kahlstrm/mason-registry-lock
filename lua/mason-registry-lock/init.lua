1b7067416d74a8dda4b28b1932d29bd268dfe2d8
GitHub Action <action@github.com>
2026-07-26 01:20:48 +0000

    Update mason-registry lock to 2026-07-25-joint-swan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-25-cooked-needle"
+M.registry_release = "github:mason-org/mason-registry@2026-07-25-joint-swan"
 
 return M
