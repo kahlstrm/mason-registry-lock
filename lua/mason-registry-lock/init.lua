794aebd4cdde9665a4875841f7c311b0b26c3b74
GitHub Action <action@github.com>
2026-07-26 01:20:48 +0000

    Update mason-registry lock to 2026-07-25-zonked-appeal

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-25-single-skirt"
+M.registry_release = "github:mason-org/mason-registry@2026-07-25-zonked-appeal"
 
 return M
