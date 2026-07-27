5daee88720ae6019d3466b67393a70b6de978b23
GitHub Action <action@github.com>
2026-07-27 01:24:58 +0000

    Update mason-registry lock to 2026-07-26-shoddy-state

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-26-steamy-mist"
+M.registry_release = "github:mason-org/mason-registry@2026-07-26-shoddy-state"
 
 return M
