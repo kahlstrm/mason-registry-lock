caedcaaaa78f973ff1a52644409b20449b05a0fb
GitHub Action <action@github.com>
2026-07-29 01:15:30 +0000

    Update mason-registry lock to 2026-07-28-flimsy-faucet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-28-far-skill"
+M.registry_release = "github:mason-org/mason-registry@2026-07-28-flimsy-faucet"
 
 return M
