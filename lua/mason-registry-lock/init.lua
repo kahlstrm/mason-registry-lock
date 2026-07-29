4a1532c89ca46f2ba3a11db2ee24d16b4e6e8f54
GitHub Action <action@github.com>
2026-07-29 01:15:30 +0000

    Update mason-registry lock to 2026-07-28-pale-button

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-28-superb-quiet"
+M.registry_release = "github:mason-org/mason-registry@2026-07-28-pale-button"
 
 return M
