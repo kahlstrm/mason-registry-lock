d9ed079dac4b9aa781bf798f350cb19dba473274
GitHub Action <action@github.com>
2026-07-30 01:08:10 +0000

    Update mason-registry lock to 2026-07-30-dismal-fall

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-30-hard-hyena"
+M.registry_release = "github:mason-org/mason-registry@2026-07-30-dismal-fall"
 
 return M
