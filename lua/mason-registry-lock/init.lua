c4b708e682101f37527286078643c93366273798
GitHub Action <action@github.com>
2026-07-30 01:08:10 +0000

    Update mason-registry lock to 2026-07-29-large-route

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-29-magic-three"
+M.registry_release = "github:mason-org/mason-registry@2026-07-29-large-route"
 
 return M
