6af4498414e79f2588418dacc8846e80d4893e5b
GitHub Action <action@github.com>
2026-07-30 01:08:10 +0000

    Update mason-registry lock to 2026-07-30-hard-hyena

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-30-six-curler"
+M.registry_release = "github:mason-org/mason-registry@2026-07-30-hard-hyena"
 
 return M
