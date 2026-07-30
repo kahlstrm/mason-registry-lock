257af349879b9ac04818b3874a92db6bcfa096dc
GitHub Action <action@github.com>
2026-07-30 01:08:10 +0000

    Update mason-registry lock to 2026-07-29-grave-sleep

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-29-narrow-mirror"
+M.registry_release = "github:mason-org/mason-registry@2026-07-29-grave-sleep"
 
 return M
