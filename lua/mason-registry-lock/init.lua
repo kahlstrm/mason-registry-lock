6f201ba86d4258b0e7285405bd165ceec5692dea
GitHub Action <action@github.com>
2026-07-30 01:08:10 +0000

    Update mason-registry lock to 2026-07-29-rapid-margin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-29-lame-care"
+M.registry_release = "github:mason-org/mason-registry@2026-07-29-rapid-margin"
 
 return M
