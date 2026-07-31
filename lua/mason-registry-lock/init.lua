852493efe22086e1b8a67859253563c9993e706d
GitHub Action <action@github.com>
2026-07-31 01:20:53 +0000

    Update mason-registry lock to 2026-07-30-ringed-yam

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-30-jovial-pepper"
+M.registry_release = "github:mason-org/mason-registry@2026-07-30-ringed-yam"
 
 return M
