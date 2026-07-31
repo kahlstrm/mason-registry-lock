dfe7855554712729784108a1a095bd038e7bb66d
GitHub Action <action@github.com>
2026-07-31 01:20:53 +0000

    Update mason-registry lock to 2026-07-30-furry-border

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-30-festive-desert"
+M.registry_release = "github:mason-org/mason-registry@2026-07-30-furry-border"
 
 return M
