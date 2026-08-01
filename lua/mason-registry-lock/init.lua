5f05780cb692175ed0bb1c185b7cbaa90cf4edc7
GitHub Action <action@github.com>
2026-08-01 01:22:09 +0000

    Update mason-registry lock to 2026-07-31-melted-image

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-31-short-day"
+M.registry_release = "github:mason-org/mason-registry@2026-07-31-melted-image"
 
 return M
