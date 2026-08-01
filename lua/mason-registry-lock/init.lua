25322c0e2a8f3327836ff4574ae5e6482f5ab4b5
GitHub Action <action@github.com>
2026-08-01 01:22:09 +0000

    Update mason-registry lock to 2026-07-31-manic-wallet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-07-31-worse-fall"
+M.registry_release = "github:mason-org/mason-registry@2026-07-31-manic-wallet"
 
 return M
