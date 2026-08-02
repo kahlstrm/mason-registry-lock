117f26d4f9fef5018c46e99e1fd0c0993d5f395e
GitHub Action <action@github.com>
2026-08-02 01:19:49 +0000

    Update mason-registry lock to 2026-08-01-cyan-era

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-01-sudden-elbow"
+M.registry_release = "github:mason-org/mason-registry@2026-08-01-cyan-era"
 
 return M
