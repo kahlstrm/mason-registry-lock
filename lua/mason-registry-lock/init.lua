018cdb5638fabf21601ccd1dec3845ba9782e89d
GitHub Action <action@github.com>
2026-08-02 01:19:49 +0000

    Update mason-registry lock to 2026-08-01-head-prairie

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-01-copper-fog"
+M.registry_release = "github:mason-org/mason-registry@2026-08-01-head-prairie"
 
 return M
