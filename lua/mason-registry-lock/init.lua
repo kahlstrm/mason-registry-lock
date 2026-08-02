1f792e32caf952be4dba4c50b1c0b57368b3227e
GitHub Action <action@github.com>
2026-08-02 01:19:49 +0000

    Update mason-registry lock to 2026-08-01-famous-factor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-01-funny-pirate"
+M.registry_release = "github:mason-org/mason-registry@2026-08-01-famous-factor"
 
 return M
