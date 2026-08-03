21170c9b052b9d062cce3c717fa3780ad3519609
GitHub Action <action@github.com>
2026-08-03 01:20:38 +0000

    Update mason-registry lock to 2026-08-02-simple-skin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-02-far-tailor"
+M.registry_release = "github:mason-org/mason-registry@2026-08-02-simple-skin"
 
 return M
