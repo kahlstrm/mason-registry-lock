9944b5a96f79bf416976fcc671eda05273ae28fa
GitHub Action <action@github.com>
2026-08-03 01:20:38 +0000

    Update mason-registry lock to 2026-08-02-solid-trip

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-02-harsh-brick"
+M.registry_release = "github:mason-org/mason-registry@2026-08-02-solid-trip"
 
 return M
