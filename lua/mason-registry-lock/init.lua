35602bd3be4dade958afb8762a34885dea1fe861
GitHub Action <action@github.com>
2026-08-03 01:20:38 +0000

    Update mason-registry lock to 2026-08-02-baby-rake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-02-elegant-wahoo"
+M.registry_release = "github:mason-org/mason-registry@2026-08-02-baby-rake"
 
 return M
