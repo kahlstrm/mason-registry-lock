bbd00f2db227758cb02c9c5ba6f06b100a751f42
GitHub Action <action@github.com>
2026-08-04 01:12:25 +0000

    Update mason-registry lock to 2026-08-03-humble-parent

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-03-grumpy-susan"
+M.registry_release = "github:mason-org/mason-registry@2026-08-03-humble-parent"
 
 return M
