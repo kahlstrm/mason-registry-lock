ccc74230c825eef59744abebb88f8e0dc3427dac
GitHub Action <action@github.com>
2026-08-04 01:12:26 +0000

    Update mason-registry lock to 2026-08-03-shy-supply

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-03-actual-shad"
+M.registry_release = "github:mason-org/mason-registry@2026-08-03-shy-supply"
 
 return M
