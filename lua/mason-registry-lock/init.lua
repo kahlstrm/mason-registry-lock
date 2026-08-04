e0ce7857920df8476bc1caf7a04c55ce75f0d7a6
GitHub Action <action@github.com>
2026-08-04 01:12:25 +0000

    Update mason-registry lock to 2026-08-03-boring-option

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-02-third-pass"
+M.registry_release = "github:mason-org/mason-registry@2026-08-03-boring-option"
 
 return M
