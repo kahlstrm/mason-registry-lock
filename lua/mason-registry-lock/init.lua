3a320bb4125f78aab6994c5c93e0d4137f74842a
GitHub Action <action@github.com>
2026-08-04 01:12:26 +0000

    Update mason-registry lock to 2026-08-03-lucky-wax

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-03-dotted-cymbal"
+M.registry_release = "github:mason-org/mason-registry@2026-08-03-lucky-wax"
 
 return M
