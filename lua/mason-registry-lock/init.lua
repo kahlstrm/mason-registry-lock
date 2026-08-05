48179d05d489cc53c528f19e2477a229727281c3
GitHub Action <action@github.com>
2026-08-05 01:13:31 +0000

    Update mason-registry lock to 2026-08-04-formal-cake

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-04-broad-opah"
+M.registry_release = "github:mason-org/mason-registry@2026-08-04-formal-cake"
 
 return M
