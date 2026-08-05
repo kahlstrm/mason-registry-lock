0186d08a2325331add599362a54121d57c344f60
GitHub Action <action@github.com>
2026-08-05 01:13:31 +0000

    Update mason-registry lock to 2026-08-04-broad-opah

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-04-bright-print"
+M.registry_release = "github:mason-org/mason-registry@2026-08-04-broad-opah"
 
 return M
