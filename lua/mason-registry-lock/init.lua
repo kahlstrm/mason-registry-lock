f58dac8564e473ce9487156bf5a275fb45ab25e2
GitHub Action <action@github.com>
2026-08-05 01:13:31 +0000

    Update mason-registry lock to 2026-08-04-bright-print

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-03-urgent-blast"
+M.registry_release = "github:mason-org/mason-registry@2026-08-04-bright-print"
 
 return M
