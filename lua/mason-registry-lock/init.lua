b235f925d11bea777cda2c2530b664a832e34f43
GitHub Action <action@github.com>
2026-08-05 01:13:31 +0000

    Update mason-registry lock to 2026-08-04-poised-table

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-04-raw-octave"
+M.registry_release = "github:mason-org/mason-registry@2026-08-04-poised-table"
 
 return M
