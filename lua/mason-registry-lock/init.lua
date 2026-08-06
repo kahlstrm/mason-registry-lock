902dbda62b1ad2cbe191398def9ffcbbcc048660
GitHub Action <action@github.com>
2026-08-06 01:10:51 +0000

    Update mason-registry lock to 2026-08-05-random-caucus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-04-corny-hole"
+M.registry_release = "github:mason-org/mason-registry@2026-08-05-random-caucus"
 
 return M
