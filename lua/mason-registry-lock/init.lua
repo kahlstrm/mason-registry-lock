5496dad42c2572dd33a1b9b3c0c001c306e157e3
GitHub Action <action@github.com>
2026-08-07 01:54:35 +0000

    Update mason-registry lock to 2026-08-07-obese-saliva

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-07-aquatic-lightning"
+M.registry_release = "github:mason-org/mason-registry@2026-08-07-obese-saliva"
 
 return M
