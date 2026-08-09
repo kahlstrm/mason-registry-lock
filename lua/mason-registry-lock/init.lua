f111d771968c50c409f99cb3ea55d42020827d9b
GitHub Action <action@github.com>
2026-08-09 00:39:22 +0000

    Update mason-registry lock to 2026-08-08-uppity-minnow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-08-abrupt-dolphin"
+M.registry_release = "github:mason-org/mason-registry@2026-08-08-uppity-minnow"
 
 return M
