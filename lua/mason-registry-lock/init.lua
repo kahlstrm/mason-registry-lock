a5ed5ffd75a8948dabefdf76779f0dbb9096814e
GitHub Action <action@github.com>
2026-08-09 00:39:22 +0000

    Update mason-registry lock to 2026-08-07-robust-hubcap

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-07-valid-liver"
+M.registry_release = "github:mason-org/mason-registry@2026-08-07-robust-hubcap"
 
 return M
