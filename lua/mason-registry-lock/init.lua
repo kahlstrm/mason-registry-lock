e4b0dc240d795956b4281639d9874bb25ca2ed10
GitHub Action <action@github.com>
2026-08-09 00:39:22 +0000

    Update mason-registry lock to 2026-08-08-untrue-flock

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-08-sane-rice"
+M.registry_release = "github:mason-org/mason-registry@2026-08-08-untrue-flock"
 
 return M
