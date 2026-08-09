5506ceda864ab32927b40a9f58c154ccf7ae15a9
GitHub Action <action@github.com>
2026-08-09 00:39:22 +0000

    Update mason-registry lock to 2026-08-08-tart-use

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-08-weekly-shorts"
+M.registry_release = "github:mason-org/mason-registry@2026-08-08-tart-use"
 
 return M
