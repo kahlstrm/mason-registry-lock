5007c9c27d531abbf12985ad8e902b87a18bc5ab
GitHub Action <action@github.com>
2026-08-09 00:39:22 +0000

    Update mason-registry lock to 2026-08-08-brave-octopus

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-08-lowly-east"
+M.registry_release = "github:mason-org/mason-registry@2026-08-08-brave-octopus"
 
 return M
