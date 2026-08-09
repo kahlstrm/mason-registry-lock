f6073f3d37583684999acd3ee65c8ac4dd7579ab
GitHub Action <action@github.com>
2026-08-09 00:39:22 +0000

    Update mason-registry lock to 2026-08-08-faded-olive

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-08-worse-border"
+M.registry_release = "github:mason-org/mason-registry@2026-08-08-faded-olive"
 
 return M
