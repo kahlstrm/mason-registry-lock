12c238e877a1e1072ef19472109558f08201e104
GitHub Action <action@github.com>
2026-08-10 00:41:03 +0000

    Update mason-registry lock to 2026-08-09-aware-steam

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-09-fierce-zone"
+M.registry_release = "github:mason-org/mason-registry@2026-08-09-aware-steam"
 
 return M
