b7b11006b17843e8619a61a42af5f08c0626b6a5
GitHub Action <action@github.com>
2026-08-10 00:41:03 +0000

    Update mason-registry lock to 2026-08-09-fierce-zone

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-09-sassy-goat"
+M.registry_release = "github:mason-org/mason-registry@2026-08-09-fierce-zone"
 
 return M
