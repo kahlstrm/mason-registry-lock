645d977655d1e36641012cee102a54026884a1f0
GitHub Action <action@github.com>
2026-08-11 00:41:15 +0000

    Update mason-registry lock to 2026-08-10-causal-math

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-10-fuzzy-fawn"
+M.registry_release = "github:mason-org/mason-registry@2026-08-10-causal-math"
 
 return M
