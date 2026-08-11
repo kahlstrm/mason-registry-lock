b653f3d488fd67d42b4826c03e6bffba88cda906
GitHub Action <action@github.com>
2026-08-11 00:41:15 +0000

    Update mason-registry lock to 2026-08-10-rival-jumbo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-10-smooth-space"
+M.registry_release = "github:mason-org/mason-registry@2026-08-10-rival-jumbo"
 
 return M
