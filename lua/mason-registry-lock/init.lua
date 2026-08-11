e4e2cc6d1349f0696b608bb03ae76bc460e8f79d
GitHub Action <action@github.com>
2026-08-11 00:41:15 +0000

    Update mason-registry lock to 2026-08-10-fuzzy-fawn

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-09-muted-second"
+M.registry_release = "github:mason-org/mason-registry@2026-08-10-fuzzy-fawn"
 
 return M
