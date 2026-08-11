0e7486bba5c9059df6af0eb0fd9448f50106401f
GitHub Action <action@github.com>
2026-08-11 00:41:15 +0000

    Update mason-registry lock to 2026-08-10-creamy-voice

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-10-boring-photo"
+M.registry_release = "github:mason-org/mason-registry@2026-08-10-creamy-voice"
 
 return M
