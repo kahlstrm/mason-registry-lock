766d63bfcfa05e1d4828a4d29d382a2f03742204
GitHub Action <action@github.com>
2026-08-12 00:46:57 +0000

    Update mason-registry lock to 2026-08-11-grey-faith

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-10-faulty-close"
+M.registry_release = "github:mason-org/mason-registry@2026-08-11-grey-faith"
 
 return M
