f96c3c4bdaab1bce0e1aa4e278a8b178ba27e4a7
GitHub Action <action@github.com>
2026-08-12 00:46:57 +0000

    Update mason-registry lock to 2026-08-11-groovy-hole

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-11-nimble-plough"
+M.registry_release = "github:mason-org/mason-registry@2026-08-11-groovy-hole"
 
 return M
