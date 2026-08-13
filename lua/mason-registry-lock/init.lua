67421fbb0a20a16a0253350dc23c5b7436261895
GitHub Action <action@github.com>
2026-08-13 00:49:33 +0000

    Update mason-registry lock to 2026-08-12-rubber-worm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-11-hollow-cherry"
+M.registry_release = "github:mason-org/mason-registry@2026-08-12-rubber-worm"
 
 return M
