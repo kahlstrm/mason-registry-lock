701967873450d8606c0fb80f37512e4a7fb96bac
GitHub Action <action@github.com>
2026-08-14 00:48:51 +0000

    Update mason-registry lock to 2026-08-13-aqua-course

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-13-steep-sink"
+M.registry_release = "github:mason-org/mason-registry@2026-08-13-aqua-course"
 
 return M
