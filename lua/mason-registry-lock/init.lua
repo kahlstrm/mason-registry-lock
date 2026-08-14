adb903c1687141d353535eab69a644d9d233b9dd
GitHub Action <action@github.com>
2026-08-14 00:48:51 +0000

    Update mason-registry lock to 2026-08-13-steep-sink

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-13-chubby-flight"
+M.registry_release = "github:mason-org/mason-registry@2026-08-13-steep-sink"
 
 return M
