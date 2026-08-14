5f7d35dc12086fe23d0d26a43869ca3cc5da65c6
GitHub Action <action@github.com>
2026-08-14 00:48:51 +0000

    Update mason-registry lock to 2026-08-13-quaint-video

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-13-grumpy-chive"
+M.registry_release = "github:mason-org/mason-registry@2026-08-13-quaint-video"
 
 return M
