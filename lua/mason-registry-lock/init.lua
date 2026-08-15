4cda5f7c658d6b524b1ec4e3b8810c6df5c24933
GitHub Action <action@github.com>
2026-08-15 00:25:50 +0000

    Update mason-registry lock to 2026-08-14-inborn-inch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-14-mean-ling"
+M.registry_release = "github:mason-org/mason-registry@2026-08-14-inborn-inch"
 
 return M
