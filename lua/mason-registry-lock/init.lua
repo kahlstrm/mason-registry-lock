ab90c3798537e1a224a059806ee6f47794dfa9a1
GitHub Action <action@github.com>
2026-08-15 00:25:50 +0000

    Update mason-registry lock to 2026-08-14-gritty-result

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-14-single-right"
+M.registry_release = "github:mason-org/mason-registry@2026-08-14-gritty-result"
 
 return M
