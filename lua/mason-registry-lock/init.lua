7974633f97936bf5a9148365046fa134950f8375
GitHub Action <action@github.com>
2026-08-15 00:25:50 +0000

    Update mason-registry lock to 2026-08-14-empty-miasma

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-14-formal-shore"
+M.registry_release = "github:mason-org/mason-registry@2026-08-14-empty-miasma"
 
 return M
