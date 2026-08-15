3ec8b5ec59eae53f4561707f56f8e8da3bec34e0
GitHub Action <action@github.com>
2026-08-15 00:25:50 +0000

    Update mason-registry lock to 2026-08-14-aural-carbon

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-13-nice-roll"
+M.registry_release = "github:mason-org/mason-registry@2026-08-14-aural-carbon"
 
 return M
