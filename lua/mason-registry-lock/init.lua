e6a5078a750cbcb06efafd977f04445d582a4ef4
GitHub Action <action@github.com>
2026-08-16 00:26:58 +0000

    Update mason-registry lock to 2026-08-15-faint-bison

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-15-linear-object"
+M.registry_release = "github:mason-org/mason-registry@2026-08-15-faint-bison"
 
 return M
