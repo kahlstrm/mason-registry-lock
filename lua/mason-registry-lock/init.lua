2e3b0e93ee4873f37161b22ab5e18c8163104561
GitHub Action <action@github.com>
2026-08-16 00:26:58 +0000

    Update mason-registry lock to 2026-08-15-frail-stem

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-15-liable-ide"
+M.registry_release = "github:mason-org/mason-registry@2026-08-15-frail-stem"
 
 return M
