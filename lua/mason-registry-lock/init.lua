e346ad8c42d07ffa8ef71ce6793dd3e3280174bc
GitHub Action <action@github.com>
2026-08-17 00:25:36 +0000

    Update mason-registry lock to 2026-08-16-rusty-format

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-16-empty-fact"
+M.registry_release = "github:mason-org/mason-registry@2026-08-16-rusty-format"
 
 return M
