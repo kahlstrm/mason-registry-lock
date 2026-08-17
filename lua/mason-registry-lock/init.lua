224c1fa6fc210ce79cf1e20015a11f4432d2f81d
GitHub Action <action@github.com>
2026-08-17 00:25:36 +0000

    Update mason-registry lock to 2026-08-16-empty-fact

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-16-neat-talon"
+M.registry_release = "github:mason-org/mason-registry@2026-08-16-empty-fact"
 
 return M
