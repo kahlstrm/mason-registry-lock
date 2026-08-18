b68441bcf0f98187c8e3c40d42dc630a321a884f
GitHub Action <action@github.com>
2026-08-18 00:25:15 +0000

    Update mason-registry lock to 2026-08-17-greasy-letter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-17-loud-six"
+M.registry_release = "github:mason-org/mason-registry@2026-08-17-greasy-letter"
 
 return M
