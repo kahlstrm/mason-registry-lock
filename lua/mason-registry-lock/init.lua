c5310a2fad7254670074940670c1cbad1da28b08
GitHub Action <action@github.com>
2026-08-19 00:25:36 +0000

    Update mason-registry lock to 2026-08-18-broad-priest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-18-slim-mango"
+M.registry_release = "github:mason-org/mason-registry@2026-08-18-broad-priest"
 
 return M
