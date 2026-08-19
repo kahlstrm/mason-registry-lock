e30604fd1ed22c2efe6ce0efaaac53c621c3e8b1
GitHub Action <action@github.com>
2026-08-19 00:25:36 +0000

    Update mason-registry lock to 2026-08-18-urban-puffin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-17-lazy-find"
+M.registry_release = "github:mason-org/mason-registry@2026-08-18-urban-puffin"
 
 return M
