65df586b5badb4439947dbd66fc7fab019934e96
GitHub Action <action@github.com>
2026-08-19 00:25:36 +0000

    Update mason-registry lock to 2026-08-18-slim-mango

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-18-earthy-save"
+M.registry_release = "github:mason-org/mason-registry@2026-08-18-slim-mango"
 
 return M
