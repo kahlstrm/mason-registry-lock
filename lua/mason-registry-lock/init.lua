27d42219447d7cdc7f04bed6ea71bac99a8289e6
GitHub Action <action@github.com>
2026-08-20 00:25:59 +0000

    Update mason-registry lock to 2026-08-19-dim-block

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-19-elite-power"
+M.registry_release = "github:mason-org/mason-registry@2026-08-19-dim-block"
 
 return M
