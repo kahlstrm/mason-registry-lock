6a504adfa9ae638812885d7fe1e60f2f9835eacc
GitHub Action <action@github.com>
2026-08-20 00:25:59 +0000

    Update mason-registry lock to 2026-08-19-direct-hacker

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-19-cheap-tax"
+M.registry_release = "github:mason-org/mason-registry@2026-08-19-direct-hacker"
 
 return M
