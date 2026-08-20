a65659f8d980784d0dbdac5dcaa3e588e1885555
GitHub Action <action@github.com>
2026-08-20 00:25:59 +0000

    Update mason-registry lock to 2026-08-19-bossy-event

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-19-dim-block"
+M.registry_release = "github:mason-org/mason-registry@2026-08-19-bossy-event"
 
 return M
