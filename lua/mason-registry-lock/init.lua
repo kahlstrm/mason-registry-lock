d9235b12bcf1e99b3e9e7f85eef34981f170f6b9
GitHub Action <action@github.com>
2026-08-20 00:25:59 +0000

    Update mason-registry lock to 2026-08-19-leafy-zipper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-19-direct-hacker"
+M.registry_release = "github:mason-org/mason-registry@2026-08-19-leafy-zipper"
 
 return M
