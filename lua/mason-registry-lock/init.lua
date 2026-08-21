4b14a08720e70b034a13583a9068b23cc66ae8ff
GitHub Action <action@github.com>
2026-08-21 00:28:46 +0000

    Update mason-registry lock to 2026-08-20-ample-subway

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-20-sharp-wax"
+M.registry_release = "github:mason-org/mason-registry@2026-08-20-ample-subway"
 
 return M
