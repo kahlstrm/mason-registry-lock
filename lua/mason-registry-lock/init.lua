7ee88a01e5cf6552bdf9ec22043cf6d31c2e88f4
GitHub Action <action@github.com>
2026-08-21 00:28:46 +0000

    Update mason-registry lock to 2026-08-20-noisy-level

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-20-ample-subway"
+M.registry_release = "github:mason-org/mason-registry@2026-08-20-noisy-level"
 
 return M
