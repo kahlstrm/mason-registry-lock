7b6ad3d67e621bc99b0947dc7fa0c144244b928e
GitHub Action <action@github.com>
2026-08-21 00:28:46 +0000

    Update mason-registry lock to 2026-08-20-grubby-push

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-20-noisy-level"
+M.registry_release = "github:mason-org/mason-registry@2026-08-20-grubby-push"
 
 return M
