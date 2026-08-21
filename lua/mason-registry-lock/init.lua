15579bfa159140176d3cbf1a75ae66e0bd9505ec
GitHub Action <action@github.com>
2026-08-21 00:28:46 +0000

    Update mason-registry lock to 2026-08-20-yellow-hand

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-20-rough-test"
+M.registry_release = "github:mason-org/mason-registry@2026-08-20-yellow-hand"
 
 return M
