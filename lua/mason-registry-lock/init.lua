8103e11446ae6b6ad1b6594a8e4371e3a5801db4
GitHub Action <action@github.com>
2026-08-21 00:28:46 +0000

    Update mason-registry lock to 2026-08-20-sharp-wax

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-20-zesty-turnip"
+M.registry_release = "github:mason-org/mason-registry@2026-08-20-sharp-wax"
 
 return M
