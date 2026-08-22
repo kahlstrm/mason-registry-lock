74d91ee4afb7959c75aca78bf1dbbc927e69889c
GitHub Action <action@github.com>
2026-08-22 00:25:43 +0000

    Update mason-registry lock to 2026-08-21-deeply-sink

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-21-scaly-manx"
+M.registry_release = "github:mason-org/mason-registry@2026-08-21-deeply-sink"
 
 return M
