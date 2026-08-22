95c79276d89ba6cee0235bf58af77f9dacfc64c6
GitHub Action <action@github.com>
2026-08-22 00:25:43 +0000

    Update mason-registry lock to 2026-08-21-empty-eye

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-21-somber-skin"
+M.registry_release = "github:mason-org/mason-registry@2026-08-21-empty-eye"
 
 return M
