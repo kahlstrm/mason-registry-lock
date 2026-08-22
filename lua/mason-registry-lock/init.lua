069e0ff62589a41833298e3acc68dc9574eaa43d
GitHub Action <action@github.com>
2026-08-22 00:25:43 +0000

    Update mason-registry lock to 2026-08-21-snappy-octave

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-21-grea-platy"
+M.registry_release = "github:mason-org/mason-registry@2026-08-21-snappy-octave"
 
 return M
