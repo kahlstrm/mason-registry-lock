b8b8abbb1ed78b708eed38de564c6d05f759b653
GitHub Action <action@github.com>
2026-08-22 00:25:43 +0000

    Update mason-registry lock to 2026-08-21-somber-skin

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-21-blonde-cactus"
+M.registry_release = "github:mason-org/mason-registry@2026-08-21-somber-skin"
 
 return M
