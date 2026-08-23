19b5db4ddbcb8fc9f813f6ec36c3f4a7af8d3ecb
GitHub Action <action@github.com>
2026-08-23 00:28:03 +0000

    Update mason-registry lock to 2026-08-22-placid-sled

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-22-future-growth"
+M.registry_release = "github:mason-org/mason-registry@2026-08-22-placid-sled"
 
 return M
