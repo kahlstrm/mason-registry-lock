62dc288b76b7aa8898e8c57eefa686c26d3d6419
GitHub Action <action@github.com>
2026-08-23 00:28:03 +0000

    Update mason-registry lock to 2026-08-22-future-growth

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-22-frozen-march"
+M.registry_release = "github:mason-org/mason-registry@2026-08-22-future-growth"
 
 return M
