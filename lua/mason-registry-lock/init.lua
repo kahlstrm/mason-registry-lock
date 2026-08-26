cc94b96c724ea7ec0bcd03efa8a33849e6143885
GitHub Action <action@github.com>
2026-08-26 00:28:14 +0000

    Update mason-registry lock to 2026-08-25-closed-wash

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-25-left-bowfin"
+M.registry_release = "github:mason-org/mason-registry@2026-08-25-closed-wash"
 
 return M
