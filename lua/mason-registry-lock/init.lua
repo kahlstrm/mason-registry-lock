d8e6caafe15e449da56e457a2f7a43014c060caa
GitHub Action <action@github.com>
2026-08-27 05:12:36 +0000

    Update mason-registry lock to 2026-08-26-fatal-boot

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-26-usual-bell"
+M.registry_release = "github:mason-org/mason-registry@2026-08-26-fatal-boot"
 
 return M
