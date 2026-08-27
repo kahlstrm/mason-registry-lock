de04b4b08aac29d49ac597c34d1dba6f2594b79e
GitHub Action <action@github.com>
2026-08-27 05:12:36 +0000

    Update mason-registry lock to 2026-08-27-lazy-cross

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-26-bouncy-betta"
+M.registry_release = "github:mason-org/mason-registry@2026-08-27-lazy-cross"
 
 return M
