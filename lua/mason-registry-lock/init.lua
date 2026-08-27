5724b30c30f667bcf82cfba1187ebf8ac0cb4120
GitHub Action <action@github.com>
2026-08-27 05:12:36 +0000

    Update mason-registry lock to 2026-08-26-usual-bell

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-26-prime-steer"
+M.registry_release = "github:mason-org/mason-registry@2026-08-26-usual-bell"
 
 return M
