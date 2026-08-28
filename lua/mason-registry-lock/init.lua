1eb66fe36f6d0417e4f2f457a33e7cc97509f407
GitHub Action <action@github.com>
2026-08-28 07:11:33 +0000

    Update mason-registry lock to 2026-08-27-sinful-pleco

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-27-alive-owl"
+M.registry_release = "github:mason-org/mason-registry@2026-08-27-sinful-pleco"
 
 return M
