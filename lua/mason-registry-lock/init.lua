76f20da74cd02b975adcd30ba7ddb9a0029d0c09
GitHub Action <action@github.com>
2026-08-28 07:11:33 +0000

    Update mason-registry lock to 2026-08-28-still-pile

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-27-icky-kayak"
+M.registry_release = "github:mason-org/mason-registry@2026-08-28-still-pile"
 
 return M
