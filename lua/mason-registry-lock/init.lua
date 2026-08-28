36a6add1db9fc6871326561724b1a4f48ed69be6
GitHub Action <action@github.com>
2026-08-28 07:11:33 +0000

    Update mason-registry lock to 2026-08-27-breeze-alarm

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-27-lazy-cross"
+M.registry_release = "github:mason-org/mason-registry@2026-08-27-breeze-alarm"
 
 return M
