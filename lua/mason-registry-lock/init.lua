12d80db776ca6a7f26326c89703ffeb3f0cd3aca
GitHub Action <action@github.com>
2026-08-28 07:11:33 +0000

    Update mason-registry lock to 2026-08-27-noisy-fuscia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-27-emerald-skin"
+M.registry_release = "github:mason-org/mason-registry@2026-08-27-noisy-fuscia"
 
 return M
