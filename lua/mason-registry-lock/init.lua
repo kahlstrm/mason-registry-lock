0b40f76c747080bc4071ee814347becd013fbe17
GitHub Action <action@github.com>
2026-08-28 07:11:33 +0000

    Update mason-registry lock to 2026-08-27-varied-clave

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-27-fuzzy-tooth"
+M.registry_release = "github:mason-org/mason-registry@2026-08-27-varied-clave"
 
 return M
