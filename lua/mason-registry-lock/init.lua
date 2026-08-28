d7bc4d34cfbc38c898153cb0f5e1dda81fa5944f
GitHub Action <action@github.com>
2026-08-28 07:11:33 +0000

    Update mason-registry lock to 2026-08-27-active-sheet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-27-varied-clave"
+M.registry_release = "github:mason-org/mason-registry@2026-08-27-active-sheet"
 
 return M
