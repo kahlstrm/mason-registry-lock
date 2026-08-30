0fbf3d009d95a7ddc87a03a0bb303e6cb64ad917
GitHub Action <action@github.com>
2026-08-30 01:50:57 +0000

    Update mason-registry lock to 2026-08-30-broken-cicada

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-29-doting-cut"
+M.registry_release = "github:mason-org/mason-registry@2026-08-30-broken-cicada"
 
 return M
