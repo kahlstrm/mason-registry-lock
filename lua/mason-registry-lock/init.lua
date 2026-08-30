69b5b1b956bbdae10a16bc3ce8d6a4fcc1ba4b8f
GitHub Action <action@github.com>
2026-08-30 01:50:57 +0000

    Update mason-registry lock to 2026-08-29-stupid-chest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-29-wrong-power"
+M.registry_release = "github:mason-org/mason-registry@2026-08-29-stupid-chest"
 
 return M
