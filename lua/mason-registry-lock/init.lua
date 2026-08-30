4f6b4ae326f787152a4f32d961916b3faa2949e6
GitHub Action <action@github.com>
2026-08-30 01:50:57 +0000

    Update mason-registry lock to 2026-08-30-sordid-repair

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-30-broken-cicada"
+M.registry_release = "github:mason-org/mason-registry@2026-08-30-sordid-repair"
 
 return M
