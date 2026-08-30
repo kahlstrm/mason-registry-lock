1098869ca7a7a721080f13e313d78a0bd2f06c41
GitHub Action <action@github.com>
2026-08-30 01:50:57 +0000

    Update mason-registry lock to 2026-08-29-doting-cut

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-29-stupid-chest"
+M.registry_release = "github:mason-org/mason-registry@2026-08-29-doting-cut"
 
 return M
