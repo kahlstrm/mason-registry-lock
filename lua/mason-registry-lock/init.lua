af7558f0a61774a2cbae5d5086bc76bf2039e83f
GitHub Action <action@github.com>
2026-08-30 01:50:57 +0000

    Update mason-registry lock to 2026-08-29-abject-copper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-29-dear-touch"
+M.registry_release = "github:mason-org/mason-registry@2026-08-29-abject-copper"
 
 return M
