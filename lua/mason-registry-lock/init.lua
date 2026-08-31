50697be8820655bf7696f8fbf9ec528d446a5c57
GitHub Action <action@github.com>
2026-08-31 01:45:58 +0000

    Update mason-registry lock to 2026-08-30-cheap-weight

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-30-intent-shrew"
+M.registry_release = "github:mason-org/mason-registry@2026-08-30-cheap-weight"
 
 return M
