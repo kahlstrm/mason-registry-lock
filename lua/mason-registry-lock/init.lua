3523de290b84dd657411acc6600037c79718e168
GitHub Action <action@github.com>
2026-08-31 01:45:58 +0000

    Update mason-registry lock to 2026-08-30-intent-shrew

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-30-gritty-route"
+M.registry_release = "github:mason-org/mason-registry@2026-08-30-intent-shrew"
 
 return M
