8f211ab3350f4bf08133dd4466a5e815a3d518d4
GitHub Action <action@github.com>
2026-08-31 01:45:58 +0000

    Update mason-registry lock to 2026-08-30-greedy-body

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-30-sordid-repair"
+M.registry_release = "github:mason-org/mason-registry@2026-08-30-greedy-body"
 
 return M
