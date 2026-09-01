382e8c10c646df4f79c42e178ef3df1cb7a90caf
GitHub Action <action@github.com>
2026-09-01 02:09:36 +0000

    Update mason-registry lock to 2026-08-31-slimy-lightning

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-31-innate-pond"
+M.registry_release = "github:mason-org/mason-registry@2026-08-31-slimy-lightning"
 
 return M
