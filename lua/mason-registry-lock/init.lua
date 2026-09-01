22f93aa726362f477dda244fa15b0ed97c24d8a3
GitHub Action <action@github.com>
2026-09-01 02:09:36 +0000

    Update mason-registry lock to 2026-08-31-divine-hood

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-30-tired-flag"
+M.registry_release = "github:mason-org/mason-registry@2026-08-31-divine-hood"
 
 return M
