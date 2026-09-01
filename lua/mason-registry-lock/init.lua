4cb57373d4d691e07cae7ccf7070d263196f31d2
GitHub Action <action@github.com>
2026-09-01 02:09:36 +0000

    Update mason-registry lock to 2026-08-31-innate-pond

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-08-31-divine-hood"
+M.registry_release = "github:mason-org/mason-registry@2026-08-31-innate-pond"
 
 return M
