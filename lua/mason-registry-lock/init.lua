e2f3a1c6423f02daa28a61e87aa6703661f40769
GitHub Action <action@github.com>
2026-09-03 01:36:06 +0000

    Update mason-registry lock to 2026-09-02-spare-copper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-02-adored-karate"
+M.registry_release = "github:mason-org/mason-registry@2026-09-02-spare-copper"
 
 return M
