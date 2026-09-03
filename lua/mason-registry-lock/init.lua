2250ca4159872aea21ea7cc7b0f2aadefd050ef0
GitHub Action <action@github.com>
2026-09-03 01:36:06 +0000

    Update mason-registry lock to 2026-09-03-eldest-word

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-03-local-shield"
+M.registry_release = "github:mason-org/mason-registry@2026-09-03-eldest-word"
 
 return M
