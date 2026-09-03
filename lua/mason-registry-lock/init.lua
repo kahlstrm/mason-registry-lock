4d6df99beeb0f05c8cc5363632a82d4c0c835bf6
GitHub Action <action@github.com>
2026-09-03 01:36:06 +0000

    Update mason-registry lock to 2026-09-03-groovy-angle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-03-hungry-burbot"
+M.registry_release = "github:mason-org/mason-registry@2026-09-03-groovy-angle"
 
 return M
