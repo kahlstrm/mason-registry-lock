23e3a78775afbee8a7f424fdb3683dc53706ae0d
GitHub Action <action@github.com>
2026-09-04 01:29:39 +0000

    Update mason-registry lock to 2026-09-03-level-kitten

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-03-lively-hill"
+M.registry_release = "github:mason-org/mason-registry@2026-09-03-level-kitten"
 
 return M
