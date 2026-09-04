147fd127e7bd7a8a2b3fd5acdfec41725656cea5
GitHub Action <action@github.com>
2026-09-04 01:29:39 +0000

    Update mason-registry lock to 2026-09-04-calm-yellow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-04-noisy-margin"
+M.registry_release = "github:mason-org/mason-registry@2026-09-04-calm-yellow"
 
 return M
