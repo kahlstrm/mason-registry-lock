e2e83293571aa4f353573083908377a0b5a2ac74
GitHub Action <action@github.com>
2026-09-05 01:30:57 +0000

    Update mason-registry lock to 2026-09-04-hushed-arena

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-04-mad-clock"
+M.registry_release = "github:mason-org/mason-registry@2026-09-04-hushed-arena"
 
 return M
