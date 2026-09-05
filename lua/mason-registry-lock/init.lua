b9428996d110e27ae69a258486f1662e555e3887
GitHub Action <action@github.com>
2026-09-05 01:30:57 +0000

    Update mason-registry lock to 2026-09-04-lowly-mullet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-04-slim-cry"
+M.registry_release = "github:mason-org/mason-registry@2026-09-04-lowly-mullet"
 
 return M
