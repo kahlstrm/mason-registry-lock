5d16a452569b5bc9164932ba98d62b9563842538
GitHub Action <action@github.com>
2026-09-06 01:25:22 +0000

    Update mason-registry lock to 2026-09-05-oily-groove

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-05-unused-debtor"
+M.registry_release = "github:mason-org/mason-registry@2026-09-05-oily-groove"
 
 return M
