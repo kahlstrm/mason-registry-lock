87a166565a6957516e569c51ab42d743b07e9a8f
GitHub Action <action@github.com>
2026-09-07 01:23:43 +0000

    Update mason-registry lock to 2026-09-07-abaft-pruner

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-07-torn-fence"
+M.registry_release = "github:mason-org/mason-registry@2026-09-07-abaft-pruner"
 
 return M
