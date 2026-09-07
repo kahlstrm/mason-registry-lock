0e31f122de5be3bf6f2f754999a43efe915bd829
GitHub Action <action@github.com>
2026-09-07 01:23:43 +0000

    Update mason-registry lock to 2026-09-06-mad-cycle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-06-puffy-sunday"
+M.registry_release = "github:mason-org/mason-registry@2026-09-06-mad-cycle"
 
 return M
