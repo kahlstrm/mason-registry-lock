0fea38f77968f0da7f588938060580aa1752d0de
GitHub Action <action@github.com>
2026-09-08 01:33:10 +0000

    Update mason-registry lock to 2026-09-07-awake-pint

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-07-slender-coin"
+M.registry_release = "github:mason-org/mason-registry@2026-09-07-awake-pint"
 
 return M
