26b8d1d2cbe0734056a5fb8e56abc58e62241e87
GitHub Action <action@github.com>
2026-09-08 01:33:10 +0000

    Update mason-registry lock to 2026-09-07-glossy-driver

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-07-ultra-wolf"
+M.registry_release = "github:mason-org/mason-registry@2026-09-07-glossy-driver"
 
 return M
