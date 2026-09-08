467c3cecd8bcc6d055152593773f40c49281832e
GitHub Action <action@github.com>
2026-09-08 01:33:10 +0000

    Update mason-registry lock to 2026-09-07-brisk-collar

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-07-glossy-driver"
+M.registry_release = "github:mason-org/mason-registry@2026-09-07-brisk-collar"
 
 return M
