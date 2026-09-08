a20977ba9cf775389fbe864f1b8323b99395d972
GitHub Action <action@github.com>
2026-09-08 01:33:10 +0000

    Update mason-registry lock to 2026-09-07-hungry-cipher

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-07-gigantic-bear"
+M.registry_release = "github:mason-org/mason-registry@2026-09-07-hungry-cipher"
 
 return M
