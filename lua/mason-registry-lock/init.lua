88b128ff63146ffeb1166e1f68730b7b2724af02
GitHub Action <action@github.com>
2026-09-09 01:41:12 +0000

    Update mason-registry lock to 2026-09-08-clumsy-pull

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-08-emerald-period"
+M.registry_release = "github:mason-org/mason-registry@2026-09-08-clumsy-pull"
 
 return M
