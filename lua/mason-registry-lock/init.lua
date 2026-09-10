b4fa960da6130ad096e370a638deb54f39adcafb
GitHub Action <action@github.com>
2026-09-10 01:35:52 +0000

    Update mason-registry lock to 2026-09-09-clear-celery

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-09-legal-cheque"
+M.registry_release = "github:mason-org/mason-registry@2026-09-09-clear-celery"
 
 return M
