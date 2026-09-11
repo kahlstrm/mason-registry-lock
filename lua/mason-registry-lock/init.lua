df93f811e7f0f47fdeca8fbd08beda5680248267
GitHub Action <action@github.com>
2026-09-11 01:34:52 +0000

    Update mason-registry lock to 2026-09-10-caring-pen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-10-irate-slip"
+M.registry_release = "github:mason-org/mason-registry@2026-09-10-caring-pen"
 
 return M
