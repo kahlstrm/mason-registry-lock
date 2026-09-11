aeb2a15df24491bcb15d8090213597b5dab02129
GitHub Action <action@github.com>
2026-09-11 01:34:52 +0000

    Update mason-registry lock to 2026-09-10-spare-belt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-10-rowdy-weever"
+M.registry_release = "github:mason-org/mason-registry@2026-09-10-spare-belt"
 
 return M
