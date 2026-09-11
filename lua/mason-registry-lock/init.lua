7634e8be88a97fd421c1db33c30807c4d14008b6
GitHub Action <action@github.com>
2026-09-11 01:34:52 +0000

    Update mason-registry lock to 2026-09-10-fond-daisy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-10-spry-small"
+M.registry_release = "github:mason-org/mason-registry@2026-09-10-fond-daisy"
 
 return M
