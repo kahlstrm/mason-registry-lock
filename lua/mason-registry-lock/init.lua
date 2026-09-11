fb7f46a8ab884579dfca4a0b5515c0cadf04270b
GitHub Action <action@github.com>
2026-09-11 01:34:52 +0000

    Update mason-registry lock to 2026-09-10-gentle-spark

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-10-past-liar"
+M.registry_release = "github:mason-org/mason-registry@2026-09-10-gentle-spark"
 
 return M
