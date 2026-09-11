a1df45ce34b73913d6d04ed357f2515c169f32d6
GitHub Action <action@github.com>
2026-09-11 01:34:52 +0000

    Update mason-registry lock to 2026-09-10-spry-small

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-10-polite-fender"
+M.registry_release = "github:mason-org/mason-registry@2026-09-10-spry-small"
 
 return M
