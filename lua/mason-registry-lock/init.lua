539f4bc9645b9da8154aeeb223e1c64dc6441de4
GitHub Action <action@github.com>
2026-09-12 01:41:11 +0000

    Update mason-registry lock to 2026-09-10-stale-button

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-10-gloomy-colony"
+M.registry_release = "github:mason-org/mason-registry@2026-09-10-stale-button"
 
 return M
