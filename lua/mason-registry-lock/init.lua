c4e549ed4cb4b995d2b4dfd10a746ec1facbe023
GitHub Action <action@github.com>
2026-09-12 01:41:11 +0000

    Update mason-registry lock to 2026-09-11-lawful-flute

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-11-bad-fibre"
+M.registry_release = "github:mason-org/mason-registry@2026-09-11-lawful-flute"
 
 return M
