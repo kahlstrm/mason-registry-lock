e3c54d00d6c89f671fd8d62d7940635940d0aace
GitHub Action <action@github.com>
2026-09-12 01:41:12 +0000

    Update mason-registry lock to 2026-09-12-acidic-enemy

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-12-royal-gander"
+M.registry_release = "github:mason-org/mason-registry@2026-09-12-acidic-enemy"
 
 return M
