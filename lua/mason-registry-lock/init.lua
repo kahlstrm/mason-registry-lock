935ab53c4532f4354bbfab543e95d21c649275fe
GitHub Action <action@github.com>
2026-09-12 01:41:12 +0000

    Update mason-registry lock to 2026-09-11-shady-brush

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-11-moist-nickel"
+M.registry_release = "github:mason-org/mason-registry@2026-09-11-shady-brush"
 
 return M
