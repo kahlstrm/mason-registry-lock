123ca2a3a37fb78b2c5e0f30bd5a2b9a6b235863
GitHub Action <action@github.com>
2026-09-13 01:31:33 +0000

    Update mason-registry lock to 2026-09-12-nasty-forest

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-12-decent-steam"
+M.registry_release = "github:mason-org/mason-registry@2026-09-12-nasty-forest"
 
 return M
