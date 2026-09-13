f84f0072abcc72ff97c985b4900bf31406ea7c16
GitHub Action <action@github.com>
2026-09-13 01:31:33 +0000

    Update mason-registry lock to 2026-09-12-ragged-cap

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-12-chubby-water"
+M.registry_release = "github:mason-org/mason-registry@2026-09-12-ragged-cap"
 
 return M
