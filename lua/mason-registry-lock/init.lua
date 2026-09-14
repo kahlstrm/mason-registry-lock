ac6e306ef2bdcc73307f5426d2929ece426c7e29
GitHub Action <action@github.com>
2026-09-14 01:49:33 +0000

    Update mason-registry lock to 2026-09-13-social-sampan

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-13-breeze-falce"
+M.registry_release = "github:mason-org/mason-registry@2026-09-13-social-sampan"
 
 return M
