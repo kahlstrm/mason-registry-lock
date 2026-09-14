06e449625ddf456ad7fcd6df3da6b73257a078da
GitHub Action <action@github.com>
2026-09-14 01:49:33 +0000

    Update mason-registry lock to 2026-09-13-prime-temper

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-13-showy-bugle"
+M.registry_release = "github:mason-org/mason-registry@2026-09-13-prime-temper"
 
 return M
