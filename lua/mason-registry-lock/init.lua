4af2d62079d9f36f2d5d482dd596428b1dc9b6bc
GitHub Action <action@github.com>
2026-09-14 01:49:33 +0000

    Update mason-registry lock to 2026-09-14-select-cocoa

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-13-aquatic-number"
+M.registry_release = "github:mason-org/mason-registry@2026-09-14-select-cocoa"
 
 return M
