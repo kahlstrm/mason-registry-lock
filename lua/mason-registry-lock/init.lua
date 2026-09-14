04e31de307336c81dd0dca5a866f824477330960
GitHub Action <action@github.com>
2026-09-14 01:49:33 +0000

    Update mason-registry lock to 2026-09-13-yearly-wallet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-13-silky-hockey"
+M.registry_release = "github:mason-org/mason-registry@2026-09-13-yearly-wallet"
 
 return M
