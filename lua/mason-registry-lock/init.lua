e0d09d8941f664baf620e4fad9475fae5c372d03
GitHub Action <action@github.com>
2026-09-14 01:49:33 +0000

    Update mason-registry lock to 2026-09-13-far-field

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-13-social-sampan"
+M.registry_release = "github:mason-org/mason-registry@2026-09-13-far-field"
 
 return M
