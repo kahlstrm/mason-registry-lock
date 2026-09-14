7890e2be6561ffced71fa4bbf24bb3070c8b4ce6
GitHub Action <action@github.com>
2026-09-14 01:49:33 +0000

    Update mason-registry lock to 2026-09-13-silky-hockey

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-13-prime-temper"
+M.registry_release = "github:mason-org/mason-registry@2026-09-13-silky-hockey"
 
 return M
