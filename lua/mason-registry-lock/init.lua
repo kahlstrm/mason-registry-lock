bb57c4ed8bdcf437978197b269a2fd5a29eb482c
GitHub Action <action@github.com>
2026-09-15 01:59:13 +0000

    Update mason-registry lock to 2026-09-14-impish-check

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-14-baby-twine"
+M.registry_release = "github:mason-org/mason-registry@2026-09-14-impish-check"
 
 return M
