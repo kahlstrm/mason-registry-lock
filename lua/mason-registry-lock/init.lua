881685b0f06cc013bcb7832e997a778532829892
GitHub Action <action@github.com>
2026-09-15 01:59:13 +0000

    Update mason-registry lock to 2026-09-14-worse-band

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-14-needy-baker"
+M.registry_release = "github:mason-org/mason-registry@2026-09-14-worse-band"
 
 return M
