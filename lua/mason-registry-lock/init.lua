ef38928115adf2e9bbd52943f627ba4ffc93ce9d
GitHub Action <action@github.com>
2026-09-16 01:49:59 +0000

    Update mason-registry lock to 2026-09-15-unique-parade

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-15-known-silk"
+M.registry_release = "github:mason-org/mason-registry@2026-09-15-unique-parade"
 
 return M
