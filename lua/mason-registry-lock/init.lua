afdbab641f4e83374e8c4732cf81cb5f9572571c
GitHub Action <action@github.com>
2026-09-16 01:49:59 +0000

    Update mason-registry lock to 2026-09-15-cheerful-canoe

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-15-oily-verse"
+M.registry_release = "github:mason-org/mason-registry@2026-09-15-cheerful-canoe"
 
 return M
