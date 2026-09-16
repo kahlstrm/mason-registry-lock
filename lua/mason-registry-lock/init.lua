c8c01b4c84e059d98b97f0758d6c23b6ef9db38c
GitHub Action <action@github.com>
2026-09-16 01:49:59 +0000

    Update mason-registry lock to 2026-09-15-filthy-dorab

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-15-unique-parade"
+M.registry_release = "github:mason-org/mason-registry@2026-09-15-filthy-dorab"
 
 return M
