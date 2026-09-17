931edd1537955fff3dd84d95aab3dd776410f377
GitHub Action <action@github.com>
2026-09-17 01:53:57 +0000

    Update mason-registry lock to 2026-09-16-sick-open

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-16-subtle-sucker"
+M.registry_release = "github:mason-org/mason-registry@2026-09-16-sick-open"
 
 return M
