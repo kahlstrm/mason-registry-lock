9dc1b1b4357dfe80c5dcca6a891daf2af3f91cf5
GitHub Action <action@github.com>
2026-09-17 01:53:57 +0000

    Update mason-registry lock to 2026-09-16-vain-save

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-16-copper-pound"
+M.registry_release = "github:mason-org/mason-registry@2026-09-16-vain-save"
 
 return M
