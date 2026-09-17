a059ace4601d79ff54d7429e52fc1ce6cc08f87e
GitHub Action <action@github.com>
2026-09-17 01:53:57 +0000

    Update mason-registry lock to 2026-09-16-stable-linen

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-16-random-velvet"
+M.registry_release = "github:mason-org/mason-registry@2026-09-16-stable-linen"
 
 return M
