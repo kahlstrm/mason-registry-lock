f6c0c5768541b8546776e064585d232836be96e1
GitHub Action <action@github.com>
2026-09-17 01:53:57 +0000

    Update mason-registry lock to 2026-09-16-subtle-sucker

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-16-stable-linen"
+M.registry_release = "github:mason-org/mason-registry@2026-09-16-subtle-sucker"
 
 return M
