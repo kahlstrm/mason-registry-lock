3aa17e2a010f26834b1c80d3a7f507180f02710a
GitHub Action <action@github.com>
2026-09-17 01:53:57 +0000

    Update mason-registry lock to 2026-09-16-faulty-floor

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-16-vain-save"
+M.registry_release = "github:mason-org/mason-registry@2026-09-16-faulty-floor"
 
 return M
