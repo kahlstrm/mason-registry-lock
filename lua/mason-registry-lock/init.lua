5f76797240aec11250ec9c6af4166b199dd81ae0
GitHub Action <action@github.com>
2026-09-18 01:41:12 +0000

    Update mason-registry lock to 2026-09-17-glum-tide

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-16-unripe-vision"
+M.registry_release = "github:mason-org/mason-registry@2026-09-17-glum-tide"
 
 return M
