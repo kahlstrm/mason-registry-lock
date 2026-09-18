b4988ce898683fa66328cbdc806fdfe7c7b668b4
GitHub Action <action@github.com>
2026-09-18 01:41:12 +0000

    Update mason-registry lock to 2026-09-17-mass-gray

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-17-social-small"
+M.registry_release = "github:mason-org/mason-registry@2026-09-17-mass-gray"
 
 return M
