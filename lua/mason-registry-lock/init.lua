4194beca028fe4bc7a6687517eaa57926453646f
GitHub Action <action@github.com>
2026-09-19 01:45:52 +0000

    Update mason-registry lock to 2026-09-19-corny-parent

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-19-spicy-beer"
+M.registry_release = "github:mason-org/mason-registry@2026-09-19-corny-parent"
 
 return M
