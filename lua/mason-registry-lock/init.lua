72660d470a8f16ffbfcd9f60d33d99af55c6fa4a
GitHub Action <action@github.com>
2026-09-20 01:48:09 +0000

    Update mason-registry lock to 2026-09-19-young-banjo

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-19-broad-may"
+M.registry_release = "github:mason-org/mason-registry@2026-09-19-young-banjo"
 
 return M
