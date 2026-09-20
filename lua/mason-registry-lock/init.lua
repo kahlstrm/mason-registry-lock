75f58e56a0e84c300c0fe299bf86e7ad41b634ef
GitHub Action <action@github.com>
2026-09-20 01:48:09 +0000

    Update mason-registry lock to 2026-09-19-secret-tree

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-19-wooden-attack"
+M.registry_release = "github:mason-org/mason-registry@2026-09-19-secret-tree"
 
 return M
