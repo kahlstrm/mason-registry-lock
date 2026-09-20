f17d728fe86317d5cfb9b5ab00ec34d686f42953
GitHub Action <action@github.com>
2026-09-20 01:48:09 +0000

    Update mason-registry lock to 2026-09-19-weekly-dirt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-19-secret-tree"
+M.registry_release = "github:mason-org/mason-registry@2026-09-19-weekly-dirt"
 
 return M
