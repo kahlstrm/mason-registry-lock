4d4ea358d27588d9fda66fb5a1d96fa355ebc0d4
GitHub Action <action@github.com>
2026-09-20 01:48:09 +0000

    Update mason-registry lock to 2026-09-19-wooden-attack

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-19-rigid-dace"
+M.registry_release = "github:mason-org/mason-registry@2026-09-19-wooden-attack"
 
 return M
