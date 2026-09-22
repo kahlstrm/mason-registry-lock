1db55cac9888749ff75502b7cc5c332af2300ca5
GitHub Action <action@github.com>
2026-09-22 01:58:42 +0000

    Update mason-registry lock to 2026-09-21-last-mile

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-21-bright-icicle"
+M.registry_release = "github:mason-org/mason-registry@2026-09-21-last-mile"
 
 return M
