6fa3e5b1d4f5954d4804453f7ebec3684a7b6cd8
GitHub Action <action@github.com>
2026-09-23 01:55:42 +0000

    Update mason-registry lock to 2026-09-23-major-kokopu

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-23-smooth-thread"
+M.registry_release = "github:mason-org/mason-registry@2026-09-23-major-kokopu"
 
 return M
