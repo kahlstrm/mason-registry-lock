fd7c15b4bf32312413fb7948d0800faedbcb1d5a
GitHub Action <action@github.com>
2026-09-23 01:55:42 +0000

    Update mason-registry lock to 2026-09-23-gothic-darter

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-22-retail-smile"
+M.registry_release = "github:mason-org/mason-registry@2026-09-23-gothic-darter"
 
 return M
