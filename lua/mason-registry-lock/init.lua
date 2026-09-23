5d89a2907c8d33d1df307b9d65253a0be83d10c3
GitHub Action <action@github.com>
2026-09-23 01:55:42 +0000

    Update mason-registry lock to 2026-09-23-wavy-catsup

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-23-bright-condor"
+M.registry_release = "github:mason-org/mason-registry@2026-09-23-wavy-catsup"
 
 return M
