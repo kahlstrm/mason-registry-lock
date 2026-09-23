699fe77a389d92b1b4d4e81fc75a856cd44a0974
GitHub Action <action@github.com>
2026-09-23 01:55:42 +0000

    Update mason-registry lock to 2026-09-23-smooth-thread

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-23-smarmy-letter"
+M.registry_release = "github:mason-org/mason-registry@2026-09-23-smooth-thread"
 
 return M
