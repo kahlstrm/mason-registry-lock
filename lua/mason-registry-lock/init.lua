3760dbcb478475b681c1ac9b89140b4e7f8c55bf
GitHub Action <action@github.com>
2026-09-23 01:55:42 +0000

    Update mason-registry lock to 2026-09-23-gummy-island

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-23-gothic-darter"
+M.registry_release = "github:mason-org/mason-registry@2026-09-23-gummy-island"
 
 return M
