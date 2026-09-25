fcc91ff1d5d96265ca911266e9170b545f160158
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-swanky-cause

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-wilted-attack"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-swanky-cause"
 
 return M
