b737c25eecdcbcf7c39915ac7c1ee40b3af28ec1
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-pink-perch

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-tangy-ring"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-pink-perch"
 
 return M
