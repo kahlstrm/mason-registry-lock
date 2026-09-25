65d854842371dc34327241e66c22c5174dfe782a
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-oily-mohawk

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-cute-light"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-oily-mohawk"
 
 return M
