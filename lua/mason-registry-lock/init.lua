8b3969ff38a743fdb2431d4525fd396ced2877b7
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-candid-doubt

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-magical-spade"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-candid-doubt"
 
 return M
