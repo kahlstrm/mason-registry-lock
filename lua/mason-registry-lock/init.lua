097c924426c032291d231c6ece2742effc0d6c96
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-native-muscle

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-candid-doubt"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-native-muscle"
 
 return M
