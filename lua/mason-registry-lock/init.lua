534133ea0ea5e0f4e67e83867694cda814e9af01
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-grey-cobia

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-ajar-gunnel"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-grey-cobia"
 
 return M
