00a64392ed8ab28734adea49e352c04b72b94162
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-bottle-comet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-swanky-cause"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-bottle-comet"
 
 return M
