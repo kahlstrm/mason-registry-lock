9462aafc5dc0dfa102e3faab4f226ff2fad7d0df
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-firm-quiet

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-charming-cycle"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-firm-quiet"
 
 return M
