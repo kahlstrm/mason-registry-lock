7ee71d81726d3bec746c192a79efc2173f94a68f
GitHub Action <action@github.com>
2026-09-25 02:01:45 +0000

    Update mason-registry lock to 2026-09-24-itchy-libra

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-glossy-queen"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-itchy-libra"
 
 return M
