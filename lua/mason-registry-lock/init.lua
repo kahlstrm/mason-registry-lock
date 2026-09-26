c7ef1ed5117aaf8a053e249b8d442b0a2c3a8566
GitHub Action <action@github.com>
2026-09-26 02:07:10 +0000

    Update mason-registry lock to 2026-09-25-snug-lightning

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-25-vanilla-guru"
+M.registry_release = "github:mason-org/mason-registry@2026-09-25-snug-lightning"
 
 return M
