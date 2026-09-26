ca17993da315c72a66e2cbc64d8fe3d4bb43d8b6
GitHub Action <action@github.com>
2026-09-26 02:07:10 +0000

    Update mason-registry lock to 2026-09-25-quirky-exit

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-25-snug-lightning"
+M.registry_release = "github:mason-org/mason-registry@2026-09-25-quirky-exit"
 
 return M
