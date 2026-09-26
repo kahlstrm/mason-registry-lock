ff6c5a3cc3cde969702ca542e76d4c3e7459df0e
GitHub Action <action@github.com>
2026-09-26 02:07:10 +0000

    Update mason-registry lock to 2026-09-25-stuck-market

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-25-eldest-clover"
+M.registry_release = "github:mason-org/mason-registry@2026-09-25-stuck-market"
 
 return M
