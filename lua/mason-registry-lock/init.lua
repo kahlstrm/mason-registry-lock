8c4148d87e22a7714c7c28c0ec3495a386a5f5a8
GitHub Action <action@github.com>
2026-09-26 02:07:10 +0000

    Update mason-registry lock to 2026-09-25-eldest-clover

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-25-coarse-slice"
+M.registry_release = "github:mason-org/mason-registry@2026-09-25-eldest-clover"
 
 return M
