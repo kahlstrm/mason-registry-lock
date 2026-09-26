f10a1f527aca243f85805eed9a29c69c61d51fa2
GitHub Action <action@github.com>
2026-09-26 02:07:10 +0000

    Update mason-registry lock to 2026-09-25-jade-canary

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-25-sweet-pile"
+M.registry_release = "github:mason-org/mason-registry@2026-09-25-jade-canary"
 
 return M
