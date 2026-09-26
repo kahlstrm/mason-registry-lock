7b018c099b9e46500ab5927e5a1a399f21b362f2
GitHub Action <action@github.com>
2026-09-26 02:07:10 +0000

    Update mason-registry lock to 2026-09-24-wrong-yellow

--- lua/mason-registry-lock/init.lua
+++ lua/mason-registry-lock/init.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M.registry_release = "github:mason-org/mason-registry@2026-09-24-worthy-test"
+M.registry_release = "github:mason-org/mason-registry@2026-09-24-wrong-yellow"
 
 return M
